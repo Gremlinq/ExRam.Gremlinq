c6ba1623db385bc12588ded8fcbd987413a6b1a4
Daniel Weber <dw@exram.de>
2017-03-27 13:02:28 +0200

    Implement AddVertexType and AddEdgeType.

--- ExRam.Gremlinq/Model/GremlinModel.cs
+++ ExRam.Gremlinq/Model/GremlinModel.cs
@@ -32,6 +32,19 @@ namespace ExRam.Gremlinq
             public IImmutableList<Type> VertexTypes => ImmutableList<Type>.Empty;
         }
 
+        private sealed class GremlinModelImpl : IGremlinModel
+        {
+            public GremlinModelImpl(IImmutableList<Type> vertexTypes, IImmutableList<Type> edgeTypes)
+            {
+                this.VertexTypes = vertexTypes;
+                this.EdgeTypes = edgeTypes;
+            }
+
+            public IImmutableList<Type> VertexTypes { get; }
+
+            public IImmutableList<Type> EdgeTypes { get; }
+        }
+
         public static readonly IGremlinModel Empty = new EmptyGremlinModel();
 
         public static IGremlinModel FromAssembly(Assembly assembly, Type vertexBaseType, Type edgeBaseType)
@@ -41,12 +54,30 @@ namespace ExRam.Gremlinq
 
         public static IGremlinModel AddVertexType<T>(this IGremlinModel model)
         {
-            throw new NotImplementedException();
+            var type = typeof(T);
+
+            if (model.VertexTypes.Contains(type))
+                return model;
+
+            var contraditingEdgeType = model.EdgeTypes.FirstOrDefault(edgeType => edgeType.IsAssignableFrom(type) || type.IsAssignableFrom(edgeType));
+            if (contraditingEdgeType != null)
+                throw new ArgumentException($"Proposed vertex type is inheritance hierarchy of edge type {contraditingEdgeType}.");
+
+            return new GremlinModelImpl(model.VertexTypes.Add(typeof(T)), model.EdgeTypes);
         }
 
         public static IGremlinModel AddEdgeType<T>(this IGremlinModel model)
         {
-            throw new NotImplementedException();
+            var type = typeof(T);
+
+            if (model.EdgeTypes.Contains(type))
+                return model;
+
+            var contraditingVertexType = model.VertexTypes.FirstOrDefault(vertexType => vertexType.IsAssignableFrom(type) || type.IsAssignableFrom(vertexType));
+            if (contraditingVertexType != null)
+                throw new ArgumentException($"Proposed edge type is inheritance hierarchy of vertex type {contraditingVertexType}.");
+
+            return new GremlinModelImpl(model.VertexTypes, model.EdgeTypes.Add(typeof(T)));
         }
     }
 }
