cab5f27f6ae37b1c1aa88a0f01c692a38908d655
Daniel Weber <dw@exram.de>
2017-05-09 14:36:34 +0200

    New: IGraphSchemaCreator.

---
+++ ExRam.Gremlinq/Schema creation/IGraphSchemaCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExRam.Gremlinq
+{
+    public interface IGraphSchemaCreator
+    {
+        Task CreateSchema(IGremlinModel model, CancellationToken ct);
+    }
+}
