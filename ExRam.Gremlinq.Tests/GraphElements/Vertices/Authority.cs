d9190233d51019102212cdc6b7c4fa8d52a8150f
Daniel Weber <dw@exram.de>
2017-06-13 12:08:46 +0200

    Make 'Authority' from tests abstract, check whether the model resulting from 'FromAssembly' contains abstract types.

--- ExRam.Gremlinq.Tests/GraphElements/Vertices/Authority.cs
+++ ExRam.Gremlinq.Tests/GraphElements/Vertices/Authority.cs
@@ -1,6 +1,6 @@
 namespace ExRam.Gremlinq.Tests
 {
-    public class Authority : Vertex
+    public abstract class Authority : Vertex
     {
         public string Id { get; set; }
         public string Name { get; set; }
---
+++ ExRam.Gremlinq.Tests/GraphModelTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace ExRam.Gremlinq.Tests
+{
+    public class GraphModelTest
+    {
+        [Fact]
+        public void FromAssembly_includes_abstract_types()
+        {
+            var model = GraphModel.FromAssembly(Assembly.GetExecutingAssembly(), typeof(Vertex), typeof(Edge), GraphElementNamingStrategy.Simple);
+
+            model.VertexTypes.Keys
+                .Should()
+                .Contain(typeof(Authority));
+        }
+    }
+}
