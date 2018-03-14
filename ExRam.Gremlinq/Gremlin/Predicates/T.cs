4273dfccc03e9f722033193984cec9e79daafabc
Daniel Weber <dw@exram.de>
2018-03-14 10:18:19 +0100

    Overload Equals, == and != on T.

--- ExRam.Gremlinq/Gremlin/Predicates/T.cs
+++ ExRam.Gremlinq/Gremlin/Predicates/T.cs
@@ -13,6 +13,26 @@ namespace ExRam.Gremlinq
             this._name = name;
         }
 
+        public override bool Equals(object obj)
+        {
+            return obj is T variable && this._name == variable._name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this._name.GetHashCode();
+        }
+
+        public static bool operator ==(T obj1, T obj2)
+        {
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(T obj1, T obj2)
+        {
+            return !obj1.Equals(obj2);
+        }
+
         public void Serialize(StringBuilder builder, IParameterCache parameterCache)
         {
             builder.Append(nameof(T));
