74b3797b9f72722d406fe47307d2652ffe093814
Daniel Weber <dw@exram.de>
2018-03-14 10:19:20 +0100

    Make one of two red tests green by implementing basic support for the Values-method with Id-Property.

--- ExRam.Gremlinq/Gremlin/Steps/ValuesGremlinStep.cs
+++ ExRam.Gremlinq/Gremlin/Steps/ValuesGremlinStep.cs
@@ -17,17 +17,37 @@ namespace ExRam.Gremlinq
 
         public override IEnumerable<TerminalGremlinStep> Resolve(IGraphModel model)
         {
-            yield return new TerminalGremlinStep(
-                "values",
-                this._projections
-                    .Select(projection =>
-                    {
-                        if (projection.Body is MemberExpression memberExpression)
-                            return model.GetIdentifier(memberExpression.Member.Name);
+            var keys = this._projections
+                .Select(projection =>
+                {
+                    if (projection.Body is MemberExpression memberExpression)
+                        return model.GetIdentifier(memberExpression.Member.Name);
 
-                        throw new NotSupportedException();
-                    })
-                    .ToImmutableList());
+                    throw new NotSupportedException();
+                })
+                .ToArray();
+
+            var numberOfIdSteps = keys
+                .OfType<T>()
+                .Count(x => x == T.Id);
+
+            var propertyKeys = keys
+                .OfType<string>()
+                .Cast<object>()
+                .ToArray();
+
+            if (numberOfIdSteps > 1 || (numberOfIdSteps > 0 && propertyKeys.Length > 0))
+                throw new NotSupportedException();
+
+            if (numberOfIdSteps > 0)
+                yield return new TerminalGremlinStep("id");
+            else
+            {
+                yield return new TerminalGremlinStep(
+                    "values",
+                    propertyKeys
+                        .ToImmutableList());
+            }
         }
     }
 }
