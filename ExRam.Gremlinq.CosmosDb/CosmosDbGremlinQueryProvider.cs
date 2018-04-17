0c6ae5762967109e56437bc8a1e3997b1fffa374
Daniel Weber <dw@exram.de>
2018-04-17 14:10:18 +0200

    CosmosDbGremlinQueryProvider: Don't inline parameters any more.

--- ExRam.Gremlinq.CosmosDb/CosmosDbGremlinQueryProvider.cs
+++ ExRam.Gremlinq.CosmosDb/CosmosDbGremlinQueryProvider.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Reactive;
 using ExRam.Gremlinq.Providers.WebSocket;
 using Gremlin.Net.Driver;
@@ -12,13 +10,10 @@ namespace ExRam.Gremlinq.CosmosDb
 {
     public sealed class CosmosDbGremlinQueryProvider : INativeGremlinQueryProvider<JToken>
     {
-        private readonly ILogger _logger;
         private readonly INativeGremlinQueryProvider<JToken> _baseProvider;
 
         public CosmosDbGremlinQueryProvider(IOptions<CosmosDbGraphConfiguration> configuration, ILogger logger)
         {
-            this._logger = logger;
-
             this._baseProvider = 
                 new GremlinClient(
                     new GremlinServer(
@@ -34,37 +29,7 @@ namespace ExRam.Gremlinq.CosmosDb
 
         public IAsyncEnumerable<JToken> Execute(string query, IDictionary<string, object> parameters)
         {
-            foreach (var kvp in parameters.OrderByDescending(x => x.Key.Length))
-            {
-                var value = kvp.Value;
-
-                switch (value)
-                {
-                    case Enum _:
-                        value = (int)value;
-                        break;
-                    case DateTimeOffset x:
-                        value = x.ToUniversalTime().ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffZ");
-                        break;
-                    case DateTime x:
-                        value = x.ToUniversalTime().ToString("yyyy-MM-ddTHH\\:mm\\:ss.fffffffZ");
-                        break;
-                    case TimeSpan x:
-                        value = x.TotalSeconds;
-                        break;
-                    case byte[] x:
-                        value = Convert.ToBase64String(x);
-                        break;
-                }
-
-                value = value is string
-                    ? $"'{value}'"
-                    : value.ToString().ToLower();
-
-                query = query.Replace(kvp.Key, (string)value);
-            }
-
-            return this._baseProvider.Execute(query, new Dictionary<string, object>());
+            return this._baseProvider.Execute(query, parameters);
         }
 
         public IGremlinQuery<Unit> TraversalSource => this._baseProvider.TraversalSource;
