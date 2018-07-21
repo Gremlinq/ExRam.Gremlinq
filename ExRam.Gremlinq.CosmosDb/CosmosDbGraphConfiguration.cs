2b2c4185a33ae657fb53f5e793a30c68ebb4a348
Daniel C. Weber <dw@exram.de>
2018-07-21 09:37:45 +0200

    Remove unused property from CosmosDbGraphConfiguration.

--- ExRam.Gremlinq.CosmosDb/CosmosDbGraphConfiguration.cs
+++ ExRam.Gremlinq.CosmosDb/CosmosDbGraphConfiguration.cs
@@ -8,6 +8,5 @@
         public string AuthKey { get; set; }
         public string Database { get; set; }
         public string GraphName { get; set; }
-        public string TraversalSource { get; set; } = "g";
     }
 }
