0e56de7bcedc42f0af2663c8da53b481faf7ccff
Daniel C. Weber <dw@exram.de>
2018-09-13 21:13:46 +0200

    Add a static class named "g" to mimick the Tinkerpop API even closer.

---
+++ ExRam.Gremlinq/Gremlin/g.cs
@@ -0,0 +1,52 @@
+using System;
+using LanguageExt;
+
+namespace ExRam.Gremlinq
+{
+    // ReSharper disable once InconsistentNaming
+    public static class g
+    {
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once RedundantArgumentDefaultValue
+        internal static readonly IGremlinQuery<Unit> _g = GremlinQuery.Create("g");
+
+        public static IGremlinQuery<TVertex> AddV<TVertex>(TVertex vertex)
+        {
+            return _g.AddV(vertex);
+        }
+
+        public static IGremlinQuery<TVertex> AddV<TVertex>() where TVertex : new()
+        {
+            return _g.AddV<TVertex>();
+        }
+
+        public static IGremlinQuery<Vertex> V(params object[] ids)
+        {
+            return _g.V(ids);
+        }
+
+        public static IGremlinQuery<TVertex> V<TVertex>(params object[] ids)
+        {
+            return _g.V<TVertex>(ids);
+        }
+
+        public static IGremlinQuery<Edge> E(params object[] ids)
+        {
+            return _g.E(ids);
+        }
+
+        public static IGremlinQuery<TElement> Inject<TElement>(params TElement[] elements)
+        {
+            return _g
+                .Cast<TElement>()
+                .Inject(elements);
+        }
+
+        public static IGremlinQuery<Unit> WithSubgraphStrategy(Func<IGremlinQuery<Unit>, IGremlinQuery> vertexCriterion, Func<IGremlinQuery<Unit>, IGremlinQuery> edgeCriterion)
+        {
+            return _g
+                .WithSubgraphStrategy(vertexCriterion, edgeCriterion);
+        }
+
+    }
+}
