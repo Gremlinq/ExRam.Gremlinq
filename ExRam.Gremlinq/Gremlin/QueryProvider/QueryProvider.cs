67d0ddc5bd5962dfbdc6cce2b27bba5fd18e2fc6
Daniel C. Weber <dw@exram.de>
2018-11-28 11:48:39 +0100

    Remove unnecessary json-transformation.

--- ExRam.Gremlinq/Gremlin/QueryProvider/QueryProvider.cs
+++ ExRam.Gremlinq/Gremlin/QueryProvider/QueryProvider.cs
@@ -25,11 +25,6 @@ namespace ExRam.Gremlinq
 
                 _baseRule = JsonTransformRules
                     .Empty
-                    .Lazy((token, recurse) => token is JObject jObject && jObject.Has("@type", "g:Property")
-                        ? jObject.TryGetValue("@value")
-                            .Map(value => value as JObject)
-                            .Bind(valueObject => valueObject.TryGetValue("value"))
-                        : Option<JToken>.None)
                     .Lazy((token, recurse) =>
                     {
                         if (token is JObject jObject && jObject.Has("@type", "g:Map"))
