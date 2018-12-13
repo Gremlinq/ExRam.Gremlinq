6115cdbd9cc1114d9425ab5ac2c8732eae2cdd5b
Daniel C. Weber <dw@exram.de>
2018-12-13 15:33:08 +0100

    New in Providers.WebSocket: WithRemote.

---
+++ ExRam.Gremlinq.Providers.WebSocket/GremlinQuerySourceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ExRam.Gremlinq.Providers.WebSocket;
+using Gremlin.Net.Driver;
+
+namespace ExRam.Gremlinq
+{
+    public static class GremlinQuerySourceExtensions
+    {
+        public static IGremlinQuerySource WithRemote(this IGremlinQuerySource source, string hostname, int port = 8182, bool enableSsl = false, string username = null, string password = null)
+        {
+            return source
+                .WithQueryProvider(new ClientGremlinQueryProvider(new GremlinServer(hostname, port, enableSsl, username, password)));
+        }
+    }
+}
