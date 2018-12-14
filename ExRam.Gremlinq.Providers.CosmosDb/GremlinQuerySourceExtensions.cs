b65d1793232fe16da1723a5b7a50d39e012ddb45
Daniel C. Weber <dw@exram.de>
2018-12-14 13:17:21 +0100

    New extension: WithCosmosDbRemote.

---
+++ ExRam.Gremlinq.Providers.CosmosDb/GremlinQuerySourceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ExRam.Gremlinq.Providers.CosmosDb;
+using ExRam.Gremlinq.Providers.WebSocket;
+
+namespace ExRam.Gremlinq
+{
+    public static class GremlinQuerySourceExtensions
+    {
+        public static IGremlinQuerySource WithCosmosDbRemote(this IGremlinQuerySource source, string hostname, int port = 8182, bool enableSsl = false, string database = null, string graphName = null, string authKey = null)
+        {
+            return source
+                .WithQueryProvider(new ClientGremlinQueryProvider(new CosmosDbGremlinServer(hostname, port, enableSsl, database, graphName, authKey)));
+        }
+    }
+}
