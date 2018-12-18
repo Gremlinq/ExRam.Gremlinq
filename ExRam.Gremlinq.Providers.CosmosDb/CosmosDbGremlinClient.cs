40f9a0b50bf4810fa077988a2a806359b7a6ff53
Daniel C. Weber <dw@exram.de>
2018-12-18 16:32:30 +0100

    CosmosDbGremlinClient must be publicly accessible, otherwise dependency injection won't be possible.

---
+++ ExRam.Gremlinq.Providers.CosmosDb/CosmosDbGremlinClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Structure.IO.GraphSON;
+using Newtonsoft.Json.Linq;
+
+namespace ExRam.Gremlinq.Core
+{
+    public sealed class CosmosDbGremlinClient : GremlinClient
+    {
+        private class TimeSpanSerializer : IGraphSONSerializer, IGraphSONDeserializer
+        {
+            public Dictionary<string, dynamic> Dictify(dynamic objectData, GraphSONWriter writer)
+            {
+                TimeSpan value = objectData;
+                return GraphSONUtil.ToTypedValue("Double", value.TotalMilliseconds);
+            }
+
+            public dynamic Objectify(JToken graphsonObject, GraphSONReader reader)
+            {
+                var duration = graphsonObject.ToObject<double>();
+                return TimeSpan.FromMilliseconds(duration);
+            }
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private sealed class NullGraphSSON2Reader : GraphSON2Reader
+        {
+            public override dynamic ToObject(JToken jToken)
+            {
+                return new[] { jToken };
+            }
+        }
+
+        public CosmosDbGremlinClient(GremlinServer gremlinServer) : base(
+            gremlinServer,
+            new NullGraphSSON2Reader(),
+            new GraphSON2Writer(new Dictionary<Type, IGraphSONSerializer>
+            {
+                { typeof(TimeSpan), new TimeSpanSerializer() }
+            }),
+            GraphSON2MimeType)
+        {
+
+        }
+    }
+}
--- ExRam.Gremlinq.Providers.CosmosDb/GremlinQuerySourceExtensions.cs
+++ ExRam.Gremlinq.Providers.CosmosDb/GremlinQuerySourceExtensions.cs
@@ -1,55 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using ExRam.Gremlinq.Providers.CosmosDb;
+﻿using ExRam.Gremlinq.Providers.CosmosDb;
 using ExRam.Gremlinq.Providers.WebSocket;
 using ExRam.Gremlinq.Core.Serialization;
-using Gremlin.Net.Driver;
-using Gremlin.Net.Structure.IO.GraphSON;
-using Newtonsoft.Json.Linq;
 
 namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQuerySourceExtensions
     {
-        private sealed class CosmosDbGremlinClient : GremlinClient
-        {
-            private class TimeSpanSerializer : IGraphSONSerializer, IGraphSONDeserializer
-            {
-                public Dictionary<string, dynamic> Dictify(dynamic objectData, GraphSONWriter writer)
-                {
-                    TimeSpan value = objectData;
-                    return GraphSONUtil.ToTypedValue("Double", value.TotalMilliseconds);
-                }
-
-                public dynamic Objectify(JToken graphsonObject, GraphSONReader reader)
-                {
-                    var duration = graphsonObject.ToObject<double>();
-                    return TimeSpan.FromMilliseconds(duration);
-                }
-            }
-
-            // ReSharper disable once InconsistentNaming
-            private sealed class NullGraphSSON2Reader : GraphSON2Reader
-            {
-                public override dynamic ToObject(JToken jToken)
-                {
-                    return new[] { jToken };
-                }
-            }
-
-            public CosmosDbGremlinClient(GremlinServer gremlinServer) : base(
-                gremlinServer,
-                new NullGraphSSON2Reader(),
-                new GraphSON2Writer(new Dictionary<Type, IGraphSONSerializer>
-                {
-                    { typeof(TimeSpan), new TimeSpanSerializer() }
-                }),
-                GraphSON2MimeType)
-            {
-
-            }
-        }
-
         public static IConfigurableGremlinQuerySource WithCosmosDbRemote(this IConfigurableGremlinQuerySource source, string hostname, string database, string graphName, string authKey, int port = 443, bool enableSsl = true)
         {
             return source.WithExecutor(
