50e9a24c218c1ae58c9df7cdc2f1a7b6a60f56d9
Daniel C. Weber <dw@exram.de>
2019-01-11 11:44:28 +0100

    An update of Gremlin.NET allows a workaround to be removed.

--- ExRam.Gremlinq.Providers.WebSocket/GremlinQuerySourceExtensions.cs
+++ ExRam.Gremlinq.Providers.WebSocket/GremlinQuerySourceExtensions.cs
@@ -2,7 +2,6 @@
 using ExRam.Gremlinq.Core.Serialization;
 using Gremlin.Net.Driver;
 using Gremlin.Net.Structure.IO.GraphSON;
-using Newtonsoft.Json.Linq;
 
 namespace ExRam.Gremlinq.Providers.WebSocket
 {
@@ -10,29 +9,11 @@ namespace ExRam.Gremlinq.Providers.WebSocket
     {
         private sealed class GremlinClientEx : GremlinClient
         {
-            // ReSharper disable once InconsistentNaming
-            private sealed class NullGraphSSON2Reader : GraphSON2Reader
-            {
-                public override dynamic ToObject(JToken jToken)
-                {
-                    return new[] { jToken };
-                }
-            }
-
-            // ReSharper disable once InconsistentNaming
-            private sealed class NullGraphSSON3Reader : GraphSON3Reader
-            {
-                public override dynamic ToObject(JToken jToken)
-                {
-                    return new[] { jToken };
-                }
-            }
-
             public GremlinClientEx(GremlinServer gremlinServer, GraphsonVersion version) : base(
                 gremlinServer,
                 version == GraphsonVersion.V2
-                    ? new NullGraphSSON2Reader()
-                    : (GraphSONReader)new NullGraphSSON3Reader(),
+                    ? new GraphSON2Reader()
+                    : (GraphSONReader)new GraphSON3Reader(),
                 version == GraphsonVersion.V2
                     ? new GraphSON2Writer()
                     : (GraphSONWriter)new GraphSON3Writer(),
