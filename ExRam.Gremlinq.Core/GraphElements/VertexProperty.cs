5081d372cd7de75af64b8fe124406a80242fca30
Daniel C. Weber <dw@exram.de>
2019-01-14 23:09:14 +0100

    VertexProperty: Implement IElement.

--- ExRam.Gremlinq.Core/GraphElements/VertexProperty.cs
+++ ExRam.Gremlinq.Core/GraphElements/VertexProperty.cs
@@ -6,7 +6,7 @@ using NullGuard;
 
 namespace ExRam.Gremlinq.Core.GraphElements
 {
-    public class VertexProperty<TValue, TMeta> : Property<TValue>
+    public class VertexProperty<TValue, TMeta> : Property<TValue>, IElement
     {
         public VertexProperty(TValue value)
         {
