2aa4a70ff17f9cd2a2ed1cc79e29f63d6e36ad48
Daniel C. Weber <dw@exram.de>
2019-03-04 15:26:47 +0100

    Remove unused method.

--- ExRam.Gremlinq.Core/Queries/GremlinQuery.cs
+++ ExRam.Gremlinq.Core/Queries/GremlinQuery.cs
@@ -617,8 +617,6 @@ namespace ExRam.Gremlinq.Core
 
         private GremlinQuery<TValue, Unit, Unit, Unit, Unit, Unit> ValuesForProjections<TValue>(IEnumerable<LambdaExpression> projections) => ValuesForKeys<TValue>(GetKeys(projections));
 
-        private GremlinQuery<TValue, Unit, Unit, Unit, Unit, Unit> ValuesForProjections<TValue>(IEnumerable<MemberExpression> projections) => ValuesForKeys<TValue>(GetKeys(projections));
-
         private GremlinQuery<VertexProperty<TNewPropertyValue>, Unit, Unit, TNewPropertyValue, Unit, Unit> VertexProperties<TNewPropertyValue>(LambdaExpression[] projections) => Properties<VertexProperty<TNewPropertyValue>, TNewPropertyValue, Unit>(projections);
 
         private GremlinQuery<TElement, TOutVertex, TInVertex, TPropertyValue, TMeta, TFoldedQuery> VertexProperty(LambdaExpression projection, [AllowNull] object value)
