31ff9701ffb65a3cb68e61855a2ea74fb24ce1a7
Daniel C. Weber <dw@exram.de>
2019-05-03 09:46:44 +0200

    Fix whitespace.

--- ExRam.Gremlinq.Core/Extensions/ExpressionExtensions.cs
+++ ExRam.Gremlinq.Core/Extensions/ExpressionExtensions.cs
@@ -82,97 +82,97 @@ namespace System.Linq.Expressions
                 switch (expression)
                 {
                     case UnaryExpression unaryExpression:
-                        {
-                            if (unaryExpression.NodeType == ExpressionType.Not)
-                                return unaryExpression.Operand.ToGremlinExpression(parameter).Negate();
+                    {
+                        if (unaryExpression.NodeType == ExpressionType.Not)
+                            return unaryExpression.Operand.ToGremlinExpression(parameter).Negate();
 
-                            break;
-                        }
+                        break;
+                    }
                     case MemberExpression memberExpression:
-                        {
-                            if (memberExpression.Member is PropertyInfo property && property.PropertyType == typeof(bool))
-                                return new TerminalGremlinExpression(parameter, memberExpression, new P.Eq(true));
+                    {
+                        if (memberExpression.Member is PropertyInfo property && property.PropertyType == typeof(bool))
+                            return new TerminalGremlinExpression(parameter, memberExpression, new P.Eq(true));
 
-                            break;
-                        }
+                        break;
+                    }
                     case BinaryExpression binaryExpression:
-                        {
-                            if (binaryExpression.NodeType == ExpressionType.AndAlso)
-                                return new AndGremlinExpression(parameter, binaryExpression.Left.StripConvert().ToGremlinExpression(parameter), binaryExpression.Right.StripConvert().ToGremlinExpression(parameter));
+                    {
+                        if (binaryExpression.NodeType == ExpressionType.AndAlso)
+                            return new AndGremlinExpression(parameter, binaryExpression.Left.StripConvert().ToGremlinExpression(parameter), binaryExpression.Right.StripConvert().ToGremlinExpression(parameter));
 
-                            if (binaryExpression.NodeType == ExpressionType.OrElse)
-                                return new OrGremlinExpression(parameter, binaryExpression.Left.StripConvert().ToGremlinExpression(parameter), binaryExpression.Right.StripConvert().ToGremlinExpression(parameter));
+                        if (binaryExpression.NodeType == ExpressionType.OrElse)
+                            return new OrGremlinExpression(parameter, binaryExpression.Left.StripConvert().ToGremlinExpression(parameter), binaryExpression.Right.StripConvert().ToGremlinExpression(parameter));
 
-                            return binaryExpression.Right.HasExpressionInMemberChain(parameter)
-                                ? new TerminalGremlinExpression(parameter, binaryExpression.Right.StripConvert(), binaryExpression.NodeType.Switch().ToP(binaryExpression.Left.GetValue()))
-                                : new TerminalGremlinExpression(parameter, binaryExpression.Left.StripConvert(), binaryExpression.NodeType.ToP(binaryExpression.Right.GetValue()));
-                        }
+                        return binaryExpression.Right.HasExpressionInMemberChain(parameter)
+                            ? new TerminalGremlinExpression(parameter, binaryExpression.Right.StripConvert(), binaryExpression.NodeType.Switch().ToP(binaryExpression.Left.GetValue()))
+                            : new TerminalGremlinExpression(parameter, binaryExpression.Left.StripConvert(), binaryExpression.NodeType.ToP(binaryExpression.Right.GetValue()));
+                    }
                     case MethodCallExpression methodCallExpression:
+                    {
+                        var methodInfo = methodCallExpression.Method;
+
+                        if (methodInfo.IsEnumerableAny())
                         {
-                            var methodInfo = methodCallExpression.Method;
-
-                            if (methodInfo.IsEnumerableAny())
+                            if (methodCallExpression.Arguments[0] is MethodCallExpression previousExpression && previousExpression.Method.IsEnumerableIntersect())
                             {
-                                if (methodCallExpression.Arguments[0] is MethodCallExpression previousExpression && previousExpression.Method.IsEnumerableIntersect())
-                                {
-                                    if (previousExpression.Arguments[0] is MemberExpression sourceMember)
-                                        return new TerminalGremlinExpression(parameter, sourceMember, previousExpression.Arguments[1].ToPWithin());
+                                if (previousExpression.Arguments[0] is MemberExpression sourceMember)
+                                    return new TerminalGremlinExpression(parameter, sourceMember, previousExpression.Arguments[1].ToPWithin());
 
-                                    if (previousExpression.Arguments[1] is MemberExpression argument && argument.Expression == parameter)
-                                        return new TerminalGremlinExpression(parameter, argument, previousExpression.Arguments[0].ToPWithin());
-                                }
-                                else
-                                    return new TerminalGremlinExpression(parameter, methodCallExpression.Arguments[0], new P.Neq(null));
+                                if (previousExpression.Arguments[1] is MemberExpression argument && argument.Expression == parameter)
+                                    return new TerminalGremlinExpression(parameter, argument, previousExpression.Arguments[0].ToPWithin());
                             }
-                            else if (methodInfo.IsEnumerableContains())
-                            {
-                                if (methodCallExpression.Arguments[0] is MemberExpression sourceMember && sourceMember.Expression == parameter)
-                                    return new TerminalGremlinExpression(parameter, sourceMember, new P.Eq(methodCallExpression.Arguments[1].GetValue()));
-
-                                if (methodCallExpression.Arguments[1] is MemberExpression argument && argument.Expression == parameter)
-                                    return new TerminalGremlinExpression(parameter, argument, methodCallExpression.Arguments[0].ToPWithin());
-                            }
-                            else if (methodInfo.IsStringStartsWith())
-                            {
-                                if (methodCallExpression.Arguments[0] is MemberExpression argumentExpression)
-                                {
-                                    if (methodCallExpression.Object.GetValue() is string stringValue)
-                                    {
-                                        return new TerminalGremlinExpression(
-                                            parameter,
-                                            argumentExpression,
-                                            new P.Within(Enumerable
-                                                .Range(0, stringValue.Length + 1)
-                                                .Select(i => stringValue.Substring(0, i))
-                                                .ToArray<object>()));
-                                    }
-                                }
-                                else if (methodCallExpression.Object is MemberExpression memberExpression)
-                                {
-                                    if (methodCallExpression.Arguments[0].GetValue() is string lowerBound)
-                                    {
-                                        string upperBound;
-
-                                        if (lowerBound.Length == 0)
-                                            return new TerminalGremlinExpression(parameter, memberExpression, P.True);
-
-                                        if (lowerBound[lowerBound.Length - 1] == char.MaxValue)
-                                            upperBound = lowerBound + char.MinValue;
-                                        else
-                                        {
-                                            var upperBoundChars = lowerBound.ToCharArray();
-
-                                            upperBoundChars[upperBoundChars.Length - 1]++;
-                                            upperBound = new string(upperBoundChars);
-                                        }
-
-                                        return new TerminalGremlinExpression(parameter, memberExpression, new P.Between(lowerBound, upperBound));
-                                    }
-                                }
-                            }
-
-                            break;
+                            else
+                                return new TerminalGremlinExpression(parameter, methodCallExpression.Arguments[0], new P.Neq(null));
                         }
+                        else if (methodInfo.IsEnumerableContains())
+                        {
+                            if (methodCallExpression.Arguments[0] is MemberExpression sourceMember && sourceMember.Expression == parameter)
+                                return new TerminalGremlinExpression(parameter, sourceMember, new P.Eq(methodCallExpression.Arguments[1].GetValue()));
+
+                            if (methodCallExpression.Arguments[1] is MemberExpression argument && argument.Expression == parameter)
+                                return new TerminalGremlinExpression(parameter, argument, methodCallExpression.Arguments[0].ToPWithin());
+                        }
+                        else if (methodInfo.IsStringStartsWith())
+                        {
+                            if (methodCallExpression.Arguments[0] is MemberExpression argumentExpression)
+                            {
+                                if (methodCallExpression.Object.GetValue() is string stringValue)
+                                {
+                                    return new TerminalGremlinExpression(
+                                        parameter,
+                                        argumentExpression,
+                                        new P.Within(Enumerable
+                                            .Range(0, stringValue.Length + 1)
+                                            .Select(i => stringValue.Substring(0, i))
+                                            .ToArray<object>()));
+                                }
+                            }
+                            else if (methodCallExpression.Object is MemberExpression memberExpression)
+                            {
+                                if (methodCallExpression.Arguments[0].GetValue() is string lowerBound)
+                                {
+                                    string upperBound;
+
+                                    if (lowerBound.Length == 0)
+                                        return new TerminalGremlinExpression(parameter, memberExpression, P.True);
+
+                                    if (lowerBound[lowerBound.Length - 1] == char.MaxValue)
+                                        upperBound = lowerBound + char.MinValue;
+                                    else
+                                    {
+                                        var upperBoundChars = lowerBound.ToCharArray();
+
+                                        upperBoundChars[upperBoundChars.Length - 1]++;
+                                        upperBound = new string(upperBoundChars);
+                                    }
+
+                                    return new TerminalGremlinExpression(parameter, memberExpression, new P.Between(lowerBound, upperBound));
+                                }
+                            }
+                        }
+
+                        break;
+                    }
                 }
             }
             catch (ExpressionNotSupportedException ex)
