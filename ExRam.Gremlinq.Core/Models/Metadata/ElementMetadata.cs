95764fbdb0d6161888d27e0ab8dae3ed21f8b54c
Daniel C. Weber <dw@exram.de>
2019-05-07 14:20:52 +0200

    New: ElementMetadata.

---
+++ ExRam.Gremlinq.Core/Models/Metadata/ElementMetadata.cs
@@ -0,0 +1,16 @@
+﻿using LanguageExt;
+
+namespace ExRam.Gremlinq.Core
+{
+    public struct ElementMetadata
+    {
+        public static readonly MemberMetadata Default = new MemberMetadata(default, default);
+
+        public ElementMetadata(Option<string> labelOverride)
+        {
+            LabelOverride = labelOverride;
+        }
+
+        public Option<string> LabelOverride { get; }
+    }
+}
