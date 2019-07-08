e50b31f8479db07e340d192e3611e3c47e141de8
Daniel C. Weber <dw@exram.de>
2019-07-08 13:58:47 +0200

    Support creating a continuation delegate from two queries where one is the other's prefix.

---
+++ ExRam.Gremlinq.Core.Tests/GremlinQueryExtensionsTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ExRam.Gremlinq.Core.GraphElements;
+using ExRam.Gremlinq.Tests.Entities;
+using FluentAssertions;
+using Xunit;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class GremlinQueryExtensionsTest
+    {
+        [Fact]
+        public void CreateContinuation()
+        {
+            var query1 = g
+                .UseModel(GraphModel.FromBaseTypes<IVertex, IEdge>())
+                .UseExecutionPipeline(b => b
+                    .EchoGremlinQueryAsString())
+                .V()
+                .OfType<Person>();
+
+            var query2 = query1
+                .Where(x => x.Age == 36);
+
+            var cont = ((IGremlinQuery)query1).CreateContinuationFrom(query2);
+
+            cont(GremlinQuery
+                    .Anonymous(query1.AsAdmin().Environment))
+                .Should()
+                .SerializeToGroovy("__.has(_a, _b)");
+        }
+
+        [Fact]
+        public void CreateContinuation_incomparable()
+        {
+            var query1 = g
+                .UseModel(GraphModel.FromBaseTypes<IVertex, IEdge>())
+                .UseExecutionPipeline(b => b
+                    .EchoGremlinQueryAsString())
+                .V()
+                .OfType<Person>();
+
+            var query2 = g
+                .V()
+                .OfType<Company>();
+
+            query1
+                .Invoking(q => ((IGremlinQuery)q).CreateContinuationFrom(query2))
+                .Should()
+                .Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void CreateContinuation_equal()
+        {
+            var query1 = g
+                .UseModel(GraphModel.FromBaseTypes<IVertex, IEdge>())
+                .UseExecutionPipeline(b => b
+                    .EchoGremlinQueryAsString())
+                .V()
+                .OfType<Person>();
+
+            var query2 = query1;
+
+            var cont = ((IGremlinQuery)query1).CreateContinuationFrom(query2);
+
+            cont(GremlinQuery
+                    .Anonymous(query1.AsAdmin().Environment))
+                .Should()
+                .SerializeToGroovy("__.identity()");
+        }
+    }
+}
--- ExRam.Gremlinq.Core/Extensions/GremlinQueryExtensions.cs
+++ ExRam.Gremlinq.Core/Extensions/GremlinQueryExtensions.cs
@@ -1,10 +1,80 @@
-﻿namespace ExRam.Gremlinq.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryExtensions
     {
         internal static IGremlinQuery AddStep(this IGremlinQuery query, Step step)
         {
             return query.AsAdmin().InsertStep(query.AsAdmin().Steps.Count, step);
+        }
+
+        /// <summary>
+        /// Creates a continuation delegate from a <paramref name="sourceQuery"/> and <paramref name="targetQuery"/>
+        /// when <paramref name="sourceQuery"/> is a prefix of <paramref name="targetQuery"/>.
+        /// </summary>
+        /// <typeparam name="TSourceQuery"></typeparam>
+        /// <typeparam name="TTargetQuery"></typeparam>
+        /// <param name="sourceQuery"></param>
+        /// <param name="targetQuery"></param>
+        /// <returns></returns>
+        public static Func<TSourceQuery, TTargetQuery> CreateContinuationFrom<TSourceQuery, TTargetQuery>(this TSourceQuery sourceQuery, TTargetQuery targetQuery)
+            where TSourceQuery : IGremlinQuery
+            where TTargetQuery : IGremlinQuery
+        {
+            var sourceAdmin = sourceQuery.AsAdmin();
+            var targetAdmin = targetQuery.AsAdmin();
+
+            if (!object.ReferenceEquals(sourceAdmin.Environment, targetAdmin.Environment))
+                throw new ArgumentException($"{nameof(sourceQuery)} and {nameof(targetQuery)} don't agree on environments.");
+
+            if (sourceAdmin.Steps.Count > targetAdmin.Steps.Count)
+                throw new ArgumentException($"{nameof(sourceQuery)} must not have more steps than {nameof(targetQuery)}.");
+
+            if (sourceAdmin.Steps.Count == targetAdmin.Steps.Count)
+            {
+                return _ => _
+                    .AsAdmin()
+                    .ChangeQueryType<TTargetQuery>();
+            }
+
+            using (var e1 = sourceAdmin.Steps.GetEnumerator())
+            {
+                using (var e2 = targetAdmin.Steps.GetEnumerator())
+                {
+                    var list = new List<Step>();
+
+                    while (true)
+                    {
+                        if (e2.MoveNext())
+                        {
+                            if (e1.MoveNext())
+                            {
+                                if (!object.ReferenceEquals(e1.Current, e2.Current))
+                                    throw new ArgumentException($"{nameof(sourceQuery)} is not a proper prefix of {nameof(targetQuery)}.");
+                            }
+                            else
+                            {
+                                do
+                                {
+                                    list.Add(e2.Current);
+                                } while (e2.MoveNext());
+
+                                break;
+                            }
+                        }
+                        else
+                            break;
+                    }
+
+                    return _ => _
+                        .AsAdmin()
+                        .AddSteps(list.ToArray())
+                        .AsAdmin()
+                        .ChangeQueryType<TTargetQuery>();
+                }
+            }
         }
      }
 }
