d20f971c123c028daf1a6805e166d84fa008fd22
Daniel C. Weber <dw@exram.de>
2019-11-14 10:08:28 +0100

    Handle CosmosDbKey in query serialization.

--- ExRam.Gremlinq.Providers.CosmosDb/GremlinQuerySerializerBuilderExtensions.cs
+++ ExRam.Gremlinq.Providers.CosmosDb/GremlinQuerySerializerBuilderExtensions.cs
@@ -9,6 +9,7 @@ namespace ExRam.Gremlinq.Core
         public static IGremlinQuerySerializerBuilder UseCosmosDbWorkarounds(this IGremlinQuerySerializerBuilder builder)
         {
             return builder
+                .OverrideAtomSerializer<CosmosDbKey>((key, assembler, overridden, recurse) => recurse(new[] { key.PartitionKey, key.Id }))
                 .OverrideAtomSerializer<SkipStep>((step, assembler, overridden, recurse) => recurse(new RangeStep(step.Count, -1)))
                 .OverrideAtomSerializer<NoneStep>((step, assembler, overridden, recurse) => recurse(NoneWorkaround))
                 .OverrideAtomSerializer<LimitStep>((step, assembler, overridden, recurse) =>
