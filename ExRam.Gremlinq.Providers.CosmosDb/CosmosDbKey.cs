593e260ceddcf888831f5391aa47c1fc0849c317
Daniel C. Weber <dw@exram.de>
2019-11-14 10:07:56 +0100

    Add CosmosDbKey, a class capturing the combo of PartitionKey and Id for CosmosDbKey partitioned graphs.

---
+++ ExRam.Gremlinq.Providers.CosmosDb/CosmosDbKey.cs
@@ -0,0 +1,14 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    public sealed class CosmosDbKey
+    {
+        public CosmosDbKey(string partitionKey, string id)
+        {
+            PartitionKey = partitionKey;
+            Id = id;
+        }
+
+        public string Id { get; }
+        public string PartitionKey { get; }
+    }
+}
