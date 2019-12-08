1c63c1d5b051e9b946f29dbe14085d0c892a48d0
Daniel C. Weber <dw@exram.de>
2019-12-08 12:47:16 +0100

    Add overload for By.

--- ExRam.Gremlinq.Core/Extensions/ProjectBuilderExtensions.cs
+++ ExRam.Gremlinq.Core/Extensions/ProjectBuilderExtensions.cs
@@ -9,9 +9,15 @@ namespace ExRam.Gremlinq.Core
             where TSourceQuery : IElementGremlinQuery<TElement>
         {
             if (projection.Body.StripConvert() is MemberExpression memberExpression)
-                return projectBuilder.By(memberExpression.Member.Name, _ => _.Values(projection));
+                return projectBuilder.By(memberExpression.Member.Name, projection);
 
             throw new ExpressionNotSupportedException(projection);
+        }
+
+        public static IProjectDynamicBuilder<TSourceQuery, TElement> By<TSourceQuery, TElement>(this IProjectDynamicBuilder<TSourceQuery, TElement> projectBuilder, string name, Expression<Func<TElement, object>> projection)
+            where TSourceQuery : IElementGremlinQuery<TElement>
+        {
+            return projectBuilder.By(name, _ => _.Values(projection));
         }
     }
 }
