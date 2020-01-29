e7464ba6b8b61a96d0340daf99cca1450a94cf99
Daniel C. Weber <dw@exram.de>
2020-01-29 22:03:17 +0100

    Code reuse.

--- ExRam.Gremlinq.Core/ExecutionPipelines/GremlinQueryExecutor.cs
+++ ExRam.Gremlinq.Core/ExecutionPipelines/GremlinQueryExecutor.cs
@@ -6,34 +6,25 @@ namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryExecutor
     {
-        private sealed class EchoGremlinQueryExecutor : IGremlinQueryExecutor
+        private sealed class GremlinQueryExecutorImpl : IGremlinQueryExecutor
         {
+            private readonly Func<object, IAsyncEnumerable<object>> _factory;
+
+            public GremlinQueryExecutorImpl(Func<object, IAsyncEnumerable<object>> factory)
+            {
+                _factory = factory;
+            }
+
             public IAsyncEnumerable<object> Execute(object serializedQuery)
             {
-                return AsyncEnumerableEx.Return(serializedQuery);
+                return _factory(serializedQuery);
             }
         }
 
-        private sealed class InvalidQueryExecutor : IGremlinQueryExecutor
-        {
-            public IAsyncEnumerable<object> Execute(object serializedQuery)
-            {
-                return AsyncEnumerableEx.Throw<object>(new InvalidOperationException($"'{nameof(Execute)}' must not be called on {nameof(GremlinQueryExecutor)}.{nameof(Invalid)}. If you are getting this exception while executing a query, set a proper {nameof(GremlinQueryExecutor)} on the {nameof(GremlinQuerySource)} (e.g. with 'g.UseGremlinServer(...)' for GremlinServer which can be found in the 'ExRam.Gremlinq.Providers.UseGremlinServer' package)."));
-            }
-        }
+        public static readonly IGremlinQueryExecutor Invalid = new GremlinQueryExecutorImpl(_ => AsyncEnumerableEx.Throw<object>(new InvalidOperationException($"'{nameof(IGremlinQueryExecutor.Execute)}' must not be called on {nameof(GremlinQueryExecutor)}.{nameof(Invalid)}. If you are getting this exception while executing a query, set a proper {nameof(GremlinQueryExecutor)} on the {nameof(GremlinQuerySource)} (e.g. with 'g.UseGremlinServer(...)' for GremlinServer which can be found in the 'ExRam.Gremlinq.Providers.UseGremlinServer' package).")));
 
-        private sealed class EmptyGremlinQueryExecutor : IGremlinQueryExecutor
-        {
-            public IAsyncEnumerable<object> Execute(object serializedQuery)
-            {
-                return AsyncEnumerable.Empty<object>();
-            }
-        }
+        public static readonly IGremlinQueryExecutor Echo = new GremlinQueryExecutorImpl(AsyncEnumerableEx.Return);
 
-        public static readonly IGremlinQueryExecutor Invalid = new InvalidQueryExecutor();
-
-        public static readonly IGremlinQueryExecutor Echo = new EchoGremlinQueryExecutor();
-
-        public static readonly IGremlinQueryExecutor Empty = new EmptyGremlinQueryExecutor();
+        public static readonly IGremlinQueryExecutor Empty = new GremlinQueryExecutorImpl(_ => AsyncEnumerable.Empty<object>());
     }
 }
