c82831ff08a549d172bc12227ccbb161030eda4d
Daniel C. Weber <dw@exram.de>
2020-03-16 14:39:05 +0100

    New: Providers.WebSocket.AspNet.

--- ExRam.Gremlinq.Providers.GremlinServer.AspNet/ServiceCollectionExtensions.cs
+++ ExRam.Gremlinq.Providers.GremlinServer.AspNet/ServiceCollectionExtensions.cs
@@ -1,36 +1,8 @@
-﻿using System;
-using ExRam.Gremlinq.Providers.WebSocket;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace ExRam.Gremlinq.Core.AspNet
 {
-    public static class WebSocketConfigurationBuilderExtensions
-    {
-        public static IWebSocketConfigurationBuilder Configure(this IWebSocketConfigurationBuilder builder, IConfiguration configuration)
-        {
-            var loggingSection = configuration.GetSection("QueryLoggingg");
-
-            return builder
-                .At(configuration.GetRequiredConfiguration("Uri"))
-                .ConfigureQueryLoggingOptions(o =>
-                {
-                    if (Enum.TryParse<QueryLoggingVerbosity>(loggingSection["Verbosity"], out var verbosity))
-                        o = o.SetQueryLoggingVerbosity(verbosity);
-
-                    if (Enum.TryParse<LogLevel>(loggingSection["LogLevel"], out var logLevel))
-                        o = o.SetLogLevel(logLevel);
-
-                    if (Enum.TryParse<Formatting>(loggingSection["LogLevel"], out var formatting))
-                        o = o.SetFormatting(formatting);
-
-                    return o;
-                });
-        }
-    }
-
     public static class ServiceCollectionExtensions
     {
         private sealed class UseGremlinServerGremlinQueryEnvironmentTransformation : IGremlinQueryEnvironmentTransformation
