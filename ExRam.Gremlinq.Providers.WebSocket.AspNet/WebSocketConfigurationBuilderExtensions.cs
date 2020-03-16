77c7d905a6db3fca9f5fd75d081e060cccf1ea86
Daniel C. Weber <dw@exram.de>
2020-03-16 14:47:26 +0100

    Complete WebSocket configuration.

--- ExRam.Gremlinq.Providers.WebSocket.AspNet/WebSocketConfigurationBuilderExtensions.cs
+++ ExRam.Gremlinq.Providers.WebSocket.AspNet/WebSocketConfigurationBuilderExtensions.cs
@@ -11,8 +11,10 @@ namespace ExRam.Gremlinq.Core.AspNet
         public static IWebSocketConfigurationBuilder Configure(this IWebSocketConfigurationBuilder builder, IConfiguration configuration)
         {
             var loggingSection = configuration.GetSection("QueryLoggingg");
+            var authenticationSection = configuration.GetSection("Authentication");
+            var connectionPoolSection = configuration.GetSection("ConnectionPool");
 
-            return builder
+            builder = builder
                 .At(configuration.GetRequiredConfiguration("Uri"))
                 .ConfigureQueryLoggingOptions(o =>
                 {
@@ -26,7 +28,26 @@ namespace ExRam.Gremlinq.Core.AspNet
                         o = o.SetFormatting(formatting);
 
                     return o;
+                })
+                .ConfigureConnectionPool(connectionPoolSettings =>
+                {
+                    if (int.TryParse(connectionPoolSection["MaxInProcessPerConnection"], out var maxInProcessPerConnection))
+                        connectionPoolSettings.MaxInProcessPerConnection = maxInProcessPerConnection;
+
+                    if (int.TryParse(connectionPoolSection["PoolSize"], out var poolSize))
+                        connectionPoolSettings.PoolSize = poolSize;
                 });
+
+            if (configuration["Alias"] is { } alias)
+                builder = builder.SetAlias(alias);
+
+            if (authenticationSection["Username"] is { } username && authenticationSection["Password"] is { } password)
+                builder = builder.AuthenticateBy(username, password);
+
+            if (Enum.TryParse<GraphsonVersion>(loggingSection["GraphsonVersion"], out var graphsonVersion))
+                builder = builder.SetGraphSONVersion(graphsonVersion);
+
+            return builder;
         }
     }
 }
