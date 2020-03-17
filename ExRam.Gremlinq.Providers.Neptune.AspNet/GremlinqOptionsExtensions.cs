97e092c6e22194ffc6a2af316b9f98c4009fe6a8
Daniel C. Weber <dw@exram.de>
2020-03-17 17:49:05 +0100

    Add Neptune.AspNet.

---
+++ ExRam.Gremlinq.Providers.Neptune.AspNet/GremlinqOptionsExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExRam.Gremlinq.Core.AspNet
+{
+    public static class GremlinqOptionsExtensions
+    {
+        private sealed class UseNeptuneGremlinQueryEnvironmentTransformation : IGremlinQueryEnvironmentTransformation
+        {
+            private readonly IConfiguration _configuration;
+
+            // ReSharper disable once SuggestBaseTypeForParameter
+            public UseNeptuneGremlinQueryEnvironmentTransformation(IGremlinqConfiguration configuration)
+            {
+                _configuration = configuration
+                    .GetSection("Neptune");
+            }
+
+            public IGremlinQueryEnvironment Transform(IGremlinQueryEnvironment environment)
+            {
+                return environment
+                    .UseNeptune(builder => builder.Configure(_configuration));
+            }
+        }
+
+        public static GremlinqOptions UseNeptune(this GremlinqOptions options)
+        {
+            return new GremlinqOptions(options.ServiceCollection.AddSingleton<IGremlinQueryEnvironmentTransformation, UseNeptuneGremlinQueryEnvironmentTransformation>());
+        }
+    }
+}
