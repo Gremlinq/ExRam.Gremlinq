08d9c3ed91f94bd39ba0eb623de0b3a84fdb92c8
Daniel C. Weber <dw@exram.de>
2020-03-19 16:27:30 +0100

    New: Providers.JanusGraph.

---
+++ ExRam.Gremlinq.Providers.JanusGraph/GremlinQueryEnvironmentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExRam.Gremlinq.Providers.WebSocket;
+
+namespace ExRam.Gremlinq.Core
+{
+    public static class GremlinQueryEnvironmentExtensions
+    {
+        public static IGremlinQueryEnvironment UseJanusGraph(this IGremlinQueryEnvironment environment,
+            Func<IWebSocketConfigurationBuilder, IWebSocketConfigurationBuilder> builderAction)
+        {
+            return environment
+                .UseGremlinServer(builderAction);
+        }
+    }
+}
