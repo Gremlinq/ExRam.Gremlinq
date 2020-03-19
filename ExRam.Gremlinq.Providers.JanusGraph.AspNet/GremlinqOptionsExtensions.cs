05280348415767d91545cad44f80ed92875bc6e8
Daniel C. Weber <dw@exram.de>
2020-03-19 16:36:49 +0100

    New: Providers.JanusGraph.AspNet.

---
+++ ExRam.Gremlinq.Providers.JanusGraph.AspNet/GremlinqOptionsExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExRam.Gremlinq.Core.AspNet
+{
+    public static class GremlinqOptionsExtensions
+    {
+        private sealed class UseGremlinServerGremlinQueryEnvironmentTransformation : IGremlinQueryEnvironmentTransformation
+        {
+            private readonly IConfiguration _configuration;
+
+            // ReSharper disable once SuggestBaseTypeForParameter
+            public UseGremlinServerGremlinQueryEnvironmentTransformation(IGremlinqConfiguration configuration)
+            {
+                _configuration = configuration
+                    .GetSection("JanusGraph");
+            }
+
+            public IGremlinQueryEnvironment Transform(IGremlinQueryEnvironment environment)
+            {
+                return environment
+                    .UseJanusGraph(builder => builder.Configure(_configuration));
+            }
+        }
+
+        public static GremlinqOptions UseJanusGraph(this GremlinqOptions options)
+        {
+            return new GremlinqOptions(options.ServiceCollection.AddSingleton<IGremlinQueryEnvironmentTransformation, UseGremlinServerGremlinQueryEnvironmentTransformation>());
+        }
+    }
+}
