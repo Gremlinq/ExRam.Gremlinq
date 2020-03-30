d2529c36e0dd6a5d31b1d3ac9e6b83b1af9e657a
Daniel C. Weber <dw@exram.de>
2020-03-30 15:55:34 +0200

    File structure.

--- ExRam.Gremlinq.Core/ExecutionPipelines/GraphsonDeserialization/GraphsonJsonSerializer.cs
+++ ExRam.Gremlinq.Core/ExecutionPipelines/GraphsonDeserialization/GraphsonJsonSerializer.cs
@@ -13,51 +13,6 @@ using System.Linq;
 
 namespace ExRam.Gremlinq.Core
 {
-    public interface IJTokenConverter
-    {
-        OptionUnsafe<object> TryConvert(JToken jToken, Type objectType, IJTokenConverter recurse);
-    }
-
-    internal static class JTokenConverter
-    {
-        private sealed class NullJTokenConverter : IJTokenConverter
-        {
-            public OptionUnsafe<object> TryConvert(JToken jToken, Type objectType, IJTokenConverter recurse)
-            {
-                return default;
-            }
-        }
-
-        private sealed class CombinedJTokenConverter : IJTokenConverter
-        {
-            private readonly IJTokenConverter _converter1;
-            private readonly IJTokenConverter _converter2;
-
-            public CombinedJTokenConverter(IJTokenConverter converter1, IJTokenConverter converter2)
-            {
-                _converter1 = converter1;
-                _converter2 = converter2;
-            }
-
-            public OptionUnsafe<object> TryConvert(JToken jToken, Type objectType, IJTokenConverter recurse)
-            {
-                var ret = _converter2
-                    .TryConvert(jToken, objectType, recurse);
-
-                return ret.IsSome
-                    ? ret
-                    : _converter1.TryConvert(jToken, objectType, recurse);
-            }
-        }
-
-        public static readonly IJTokenConverter Null = new NullJTokenConverter();
-
-        public static IJTokenConverter Combine(this IJTokenConverter converter1, IJTokenConverter converter2)
-        {
-            return new CombinedJTokenConverter(converter1, converter2);
-        }
-    }
-
     internal sealed class GraphsonJsonSerializer : JsonSerializer
     {
         #region Nested
---
+++ ExRam.Gremlinq.Core/ExecutionPipelines/GraphsonDeserialization/IJTokenConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using LanguageExt;
+using Newtonsoft.Json.Linq;
+
+namespace ExRam.Gremlinq.Core
+{
+    public interface IJTokenConverter
+    {
+        OptionUnsafe<object> TryConvert(JToken jToken, Type objectType, IJTokenConverter recurse);
+    }
+}
