41c6e1e819961dbd48ab402e27240a09a8fa19c9
Daniel C. Weber <dw@exram.de>
2020-04-10 00:49:27 +0200

    Graphson3GremlinQuerySerializationTest.

---
+++ ExRam.Gremlinq.Core.Tests/Serialization/Graphson3GremlinQuerySerializationTest.cs
@@ -0,0 +1,19 @@
+﻿using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Structure.IO.GraphSON;
+using Xunit.Abstractions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public sealed class Graphson3GremlinQuerySerializationTest : QuerySerializationTest
+    {
+        private static readonly GraphSON3Writer Writer = new GraphSON3Writer();
+
+        public Graphson3GremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
+            GremlinQuerySource.g.ConfigureEnvironment(_ => _
+                .UseSerializer(GremlinQuerySerializer.Default
+                    .Select(obj => Writer.WriteObject((Bytecode)obj)))),
+            testOutputHelper)
+        {
+        }
+    }
+}
