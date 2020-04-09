163ef2b930791dc0f5fddac9f5bb3a37a4ecb9f2
Daniel C. Weber <dw@exram.de>
2020-04-10 01:17:33 +0200

    Graphson2GremlinQuerySerializationTest.

---
+++ ExRam.Gremlinq.Core.Tests/Serialization/Graphson2GremlinQuerySerializationTest.cs
@@ -0,0 +1,19 @@
+﻿using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Structure.IO.GraphSON;
+using Xunit.Abstractions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public sealed class Graphson2GremlinQuerySerializationTest : QuerySerializationTest
+    {
+        private static readonly GraphSON2Writer Writer = new GraphSON2Writer();
+
+        public Graphson2GremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
+            GremlinQuerySource.g.ConfigureEnvironment(_ => _
+                .UseSerializer(GremlinQuerySerializer.Default
+                    .Select(obj => Writer.WriteObject((Bytecode)obj)))),
+            testOutputHelper)
+        {
+        }
+    }
+}
