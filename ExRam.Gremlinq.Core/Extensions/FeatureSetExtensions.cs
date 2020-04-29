986874954641f2df61d1c834b532ce226e1ebe4e
Daniel C. Weber <dw@exram.de>
2020-04-29 18:17:31 +0200

    Avoid allocation in HasFlag.

--- ExRam.Gremlinq.Core/Extensions/FeatureSetExtensions.cs
+++ ExRam.Gremlinq.Core/Extensions/FeatureSetExtensions.cs
@@ -2,16 +2,16 @@
 {
     public static class FeatureSetExtensions
     {
-        public static bool Supports(this FeatureSet featureSet, GraphFeatures graphFeatures) => featureSet.GraphFeatures.HasFlag(graphFeatures);
+        public static bool Supports(this FeatureSet featureSet, GraphFeatures graphFeatures) => (featureSet.GraphFeatures & graphFeatures) == graphFeatures;
 
-        public static bool Supports(this FeatureSet featureSet, VariableFeatures variableFeatures) => featureSet.VariableFeatures.HasFlag(variableFeatures);
+        public static bool Supports(this FeatureSet featureSet, VariableFeatures variableFeatures) => (featureSet.VariableFeatures & variableFeatures) == variableFeatures;
 
-        public static bool Supports(this FeatureSet featureSet, VertexFeatures vertexFeatures) => featureSet.VertexFeatures.HasFlag(vertexFeatures);
+        public static bool Supports(this FeatureSet featureSet, VertexFeatures vertexFeatures) => (featureSet.VertexFeatures & vertexFeatures) == vertexFeatures;
 
-        public static bool Supports(this FeatureSet featureSet, VertexPropertyFeatures vertexPropertyFeatures) => featureSet.VertexPropertyFeatures.HasFlag(vertexPropertyFeatures);
+        public static bool Supports(this FeatureSet featureSet, VertexPropertyFeatures vertexPropertyFeatures) => (featureSet.VertexPropertyFeatures & vertexPropertyFeatures) == vertexPropertyFeatures;
 
-        public static bool Supports(this FeatureSet featureSet, EdgeFeatures edgeFeatures) => featureSet.EdgeFeatures.HasFlag(edgeFeatures);
+        public static bool Supports(this FeatureSet featureSet, EdgeFeatures edgeFeatures) => (featureSet.EdgeFeatures & edgeFeatures) == edgeFeatures;
 
-        public static bool Supports(this FeatureSet featureSet, EdgePropertyFeatures edgePropertyFeatures) => featureSet.EdgePropertyFeatures.HasFlag(edgePropertyFeatures);
+        public static bool Supports(this FeatureSet featureSet, EdgePropertyFeatures edgePropertyFeatures) => (featureSet.EdgePropertyFeatures & edgePropertyFeatures) == edgePropertyFeatures;
     }
 }
