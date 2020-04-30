72e298f1c025784b75598bb34c1bf000d90d992c
Daniel C. Weber <dw@exram.de>
2020-04-30 21:25:53 +0200

    Files.

--- ExRam.Gremlinq.Core/ExecutionPipelines/GraphsonDeserialization/GraphsonJsonSerializer.cs
+++ ExRam.Gremlinq.Core/ExecutionPipelines/GraphsonDeserialization/GraphsonJsonSerializer.cs
@@ -10,29 +10,6 @@ using Newtonsoft.Json.Linq;
 
 namespace ExRam.Gremlinq.Core
 {
-    internal static class JTokenExtensions
-    {
-        public static JObject? TryUnmap(this JObject jObject)
-        {
-            if (jObject.TryGetValue("@type", out var nestedType) && "g:Map".Equals(nestedType.Value<string>(), StringComparison.OrdinalIgnoreCase))
-            {
-                if (jObject.TryGetValue("@value", out var valueToken) && valueToken is JArray mapArray)
-                {
-                    var retObject = new JObject();
-
-                    for (var i = 0; i < mapArray.Count / 2; i++)
-                    {
-                        retObject.Add(mapArray[i * 2].Value<string>(), mapArray[i * 2 + 1]);
-                    }
-
-                    return retObject;
-                }
-            }
-
-            return null;
-        }
-    }
-
     internal sealed class GraphsonJsonSerializer : JsonSerializer
     {
         #region Nested
