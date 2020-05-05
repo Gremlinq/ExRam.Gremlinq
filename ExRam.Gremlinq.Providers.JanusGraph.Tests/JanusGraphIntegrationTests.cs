4ab8bdc8be164f805514396954fc7885a141eac5
Daniel C. Weber <dw@exram.de>
2020-05-05 17:27:40 +0200

    Tests for JanusGraph.

---
+++ ExRam.Gremlinq.Providers.JanusGraph.Tests/JanusGraphIntegrationTests.cs
@@ -0,0 +1,22 @@
+﻿#if RELEASE && NETCOREAPP3_1
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Providers.WebSocket;
+using Xunit.Abstractions;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    public class GremlinServerIntegrationTests : QueryExecutionTest
+    {
+        public GremlinServerIntegrationTests(ITestOutputHelper testOutputHelper) : base(
+            g
+                .ConfigureEnvironment(env => env
+                    .UseWebSocket(builder => builder
+                        .AtLocalhost())),
+            testOutputHelper)
+        {
+        }
+    }
+}
+#endif
---
+++ ExRam.Gremlinq.Providers.JanusGraph.Tests/JanusGraphQuerySerializationTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Providers.WebSocket;
+using ExRam.Gremlinq.Tests.Entities;
+using Xunit;
+using Xunit.Abstractions;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    public sealed class GremlinServerQuerySerializationTest : QuerySerializationTest
+    {
+        public GremlinServerQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
+            g
+                .ConfigureEnvironment(env => env
+                    .UseGremlinServer(builder => builder
+                        .AtLocalhost())),
+            testOutputHelper)
+        {
+
+        }
+
+        [Fact]
+        public async Task AddV_with_enum_property_with_workaround()
+        {
+            await _g
+                .ConfigureEnvironment(env => env
+                    .ConfigureOptions(options => options
+                        .SetValue(GremlinServerGremlinqOptions.WorkaroundTinkerpop2112, true)))
+                .AddV(new Person { Id = 1, Gender = Gender.Female })
+                .Verify(this);
+        }
+    }
+}
