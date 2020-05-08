4b57dbd8db169a0e5a0a9ab0261672795ad54767
Daniel C. Weber <dw@exram.de>
2020-05-08 11:01:44 +0200

    Files.

---
+++ ExRam.Gremlinq.Core/Queries/Interfaces/IBothEdgeGremlinQueryBase.cs
@@ -0,0 +1,43 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    public interface IBothEdgeGremlinQueryBase :
+        IInOrOutEdgeGremlinQueryBase,
+        IOutEdgeGremlinQueryBase,
+        IInEdgeGremlinQueryBase
+    {
+
+    }
+    
+    public partial interface IBothEdgeGremlinQueryBase<TEdge, TOutVertex, TInVertex> :
+        IBothEdgeGremlinQueryBase,
+        IOutEdgeGremlinQueryBase<TEdge, TOutVertex>,
+        IInEdgeGremlinQueryBase<TEdge, TInVertex>
+    {
+
+    }
+
+    public interface IBothEdgeGremlinQueryRec<TSelf> :
+        IBothEdgeGremlinQueryBase,
+        IInOrOutEdgeGremlinQueryBaseRec<TSelf>,
+        IOutEdgeGremlinQueryBaseRec<TSelf>,
+        IInEdgeGremlinQueryBaseRec<TSelf>
+        where TSelf : IBothEdgeGremlinQueryRec<TSelf>
+    {
+
+    }
+
+    public interface IBothEdgeGremlinQueryRec<TEdge, TOutVertex, TInVertex, TSelf> :
+        IBothEdgeGremlinQueryRec<TSelf>,
+        IBothEdgeGremlinQueryBase<TEdge, TOutVertex, TInVertex>,
+        IEdgeGremlinQueryBaseRec<TEdge, TSelf>
+        where TSelf : IBothEdgeGremlinQueryRec<TEdge, TOutVertex, TInVertex, TSelf>
+    {
+
+    }
+
+    public interface IBothEdgeGremlinQuery<TEdge, TOutVertex, TInVertex> :
+        IBothEdgeGremlinQueryRec<TEdge, TOutVertex, TInVertex, IBothEdgeGremlinQuery<TEdge, TOutVertex, TInVertex>>
+    {
+
+    }
+}
--- ExRam.Gremlinq.Core/Queries/Interfaces/IEdgeGremlinQuery.cs
+++ ExRam.Gremlinq.Core/Queries/Interfaces/IEdgeGremlinQuery.cs
@@ -113,47 +113,4 @@ namespace ExRam.Gremlinq.Core
         IInOrOutEdgeGremlinQueryBaseRec<TEdge, TAdjacentVertex, IInOrOutEdgeGremlinQuery<TEdge, TAdjacentVertex>>
     {
     }
-
-
-
-    public interface IBothEdgeGremlinQueryBase :
-        IInOrOutEdgeGremlinQueryBase,
-        IOutEdgeGremlinQueryBase,
-        IInEdgeGremlinQueryBase
-    {
-
-    }
-
-    public partial interface IBothEdgeGremlinQueryBase<TEdge, TOutVertex, TInVertex> :
-        IBothEdgeGremlinQueryBase,
-        IOutEdgeGremlinQueryBase<TEdge, TOutVertex>,
-        IInEdgeGremlinQueryBase<TEdge, TInVertex>
-    {
-        
-    }
-
-    public interface IBothEdgeGremlinQueryRec<TSelf> :
-        IBothEdgeGremlinQueryBase,
-        IInOrOutEdgeGremlinQueryBaseRec<TSelf>,
-        IOutEdgeGremlinQueryBaseRec<TSelf>,
-        IInEdgeGremlinQueryBaseRec<TSelf>
-        where TSelf : IBothEdgeGremlinQueryRec<TSelf>
-    {
-
-    }
-
-    public interface IBothEdgeGremlinQueryRec<TEdge, TOutVertex, TInVertex, TSelf> :
-        IBothEdgeGremlinQueryRec<TSelf>,
-        IBothEdgeGremlinQueryBase<TEdge, TOutVertex, TInVertex>,
-        IEdgeGremlinQueryBaseRec<TEdge, TSelf>
-        where TSelf : IBothEdgeGremlinQueryRec<TEdge, TOutVertex, TInVertex, TSelf>
-    {
-
-    }
-
-    public interface IBothEdgeGremlinQuery<TEdge, TOutVertex, TInVertex> :
-        IBothEdgeGremlinQueryRec<TEdge, TOutVertex, TInVertex, IBothEdgeGremlinQuery<TEdge, TOutVertex, TInVertex>>
-    {
-
-    }
 }
