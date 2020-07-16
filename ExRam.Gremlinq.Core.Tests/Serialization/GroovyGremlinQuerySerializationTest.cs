3bb798ae0b019def5c39382dab349d6c8e84ba9e
Daniel Weber <dw@exram.de>
2020-07-16 23:40:07 +0200

    Files.

--- ExRam.Gremlinq.Core.Tests/Serialization/GroovyGremlinQuerySerializationTest.cs
+++ ExRam.Gremlinq.Core.Tests/Serialization/GroovyGremlinQuerySerializationTest.cs
@@ -12,25 +12,4 @@ namespace ExRam.Gremlinq.Core.Tests
         {
         }
     }
-
-    public sealed class InlinedGroovyGremlinQuerySerializationTest : QuerySerializationTest
-    {
-        public InlinedGroovyGremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
-            g.ConfigureEnvironment(_ => _
-                .UseSerializer(GremlinQuerySerializer.Default.ToGroovy(GroovyFormatting.AllowInlining))),
-            testOutputHelper)
-        {
-        }
-    }
-
-    public sealed class InlinedLaterGroovyGremlinQuerySerializationTest : QuerySerializationTest
-    {
-        public InlinedLaterGroovyGremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
-            g.ConfigureEnvironment(_ => _
-                .UseSerializer(GremlinQuerySerializer.Default.ToGroovy())
-                .ConfigureSerializer(serializer => serializer.Select(query => ((GroovyGremlinQuery)query).Inline()))),
-            testOutputHelper)
-        {
-        }
-    }
 }
---
+++ ExRam.Gremlinq.Core.Tests/Serialization/InlinedLaterGroovyGremlinQuerySerializationTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit.Abstractions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public sealed class InlinedLaterGroovyGremlinQuerySerializationTest : QuerySerializationTest
+    {
+        public InlinedLaterGroovyGremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
+            GremlinQuerySource.g.ConfigureEnvironment(_ => _
+                .UseSerializer(GremlinQuerySerializer.Default.ToGroovy())
+                .ConfigureSerializer(serializer => serializer.Select(query => ((GroovyGremlinQuery)query).Inline()))),
+            testOutputHelper)
+        {
+        }
+    }
+}
