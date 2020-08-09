ddd8ba3f6595511dbe56006139c9a2a2d9e804e4
Daniel Weber <dw@exram.de>
2020-08-09 19:13:55 +0200

    EnvironmentCache.

---
+++ ExRam.Gremlinq.Core/Environment/EnvironmentCache.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExRam.Gremlinq.Core
+{
+    internal static class EnvironmentCache
+    {
+        private sealed class EnvironmentCacheImpl : IEnvironmentCache
+        {
+            private readonly IGremlinQueryEnvironment _environment;
+
+            public EnvironmentCacheImpl(IGremlinQueryEnvironment environment)
+            {
+                _environment = environment;
+            }
+        }
+
+        private static readonly ConditionalWeakTable<IGremlinQueryEnvironment, IEnvironmentCache> Caches = new ConditionalWeakTable<IGremlinQueryEnvironment, IEnvironmentCache>();
+
+        public static IEnvironmentCache GetCache(this IGremlinQueryEnvironment environment)
+        {
+            return Caches.GetValue(
+                environment,
+                closure => new EnvironmentCacheImpl(closure));
+        }
+    }
+}
