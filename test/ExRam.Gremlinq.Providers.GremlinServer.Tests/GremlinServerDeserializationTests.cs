08674994d2478673616215d07a6c2d226cb91a38
Daniel Weber <dw@exram.de>
2021-01-11 17:13:21 +0100

    If a .verified file is not present for the GremlinServerDeserializationTest and SKIPINTEGRATIONTESTS is true, skip the test.

--- test/ExRam.Gremlinq.Providers.GremlinServer.Tests/GremlinServerDeserializationTests.cs
+++ test/ExRam.Gremlinq.Providers.GremlinServer.Tests/GremlinServerDeserializationTests.cs
@@ -18,16 +18,25 @@ namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
         public GremlinServerDeserializationTests(ITestOutputHelper testOutputHelper) : base(
             GremlinQuerySource.g
                 .ConfigureEnvironment(env => env
-                    .UseExecutor(GremlinQueryExecutor.Create((query, env) =>
+                    .UseExecutor(GremlinQueryExecutor.Create((_, _) =>
                     {
-                        var jArray = JsonConvert.DeserializeObject<JArray>(
-                            File.ReadAllText(System.IO.Path.Combine(XUnitContext.SourceDirectory, "GremlinServerIntegrationTests." + XUnitContext.MethodName + ".verified.json")));
+                        try
+                        {
+                            var jArray = JsonConvert.DeserializeObject<JArray>(
+                                File.ReadAllText(System.IO.Path.Combine(XUnitContext.SourceDirectory, "GremlinServerIntegrationTests." + XUnitContext.MethodName + ".verified.json")));
 
-                        return jArray.Count == 1
-                            ? new [] { jArray[0] }.ToAsyncEnumerable()
-                            : jArray.Count == 0
-                                ? AsyncEnumerable.Empty<object>()
-                                : throw new NotSupportedException();
+                            return jArray.Count == 1
+                                ? new[] {jArray[0]}.ToAsyncEnumerable()
+                                : jArray.Count == 0
+                                    ? AsyncEnumerable.Empty<object>()
+                                    : throw new NotSupportedException();
+                        }
+                        catch (IOException)
+                        {
+                            #if !SKIPINTEGRATIONTESTS
+                            throw;
+                            #endif
+                        }
                     }))
                     .UseDeserializer(GremlinQueryExecutionResultDeserializer.FromJToken)),
             testOutputHelper)
