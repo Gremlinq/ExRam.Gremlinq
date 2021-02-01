9f45cf99853b9d0779b7940f561ba6d9530b4b67
Daniel Weber <dw@exram.de>
2021-02-01 17:48:55 +0100

    Get rid of CosmosDbQuerySerializationTestBase.

--- test/ExRam.Gremlinq.Providers.CosmosDb.Tests/CosmosDbQuerySerializationTest.cs
+++ test/ExRam.Gremlinq.Providers.CosmosDb.Tests/CosmosDbQuerySerializationTest.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
 using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Tests.Entities;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
 {
-    public class CosmosDbQuerySerializationTest : CosmosDbQuerySerializationTestBase
+    public sealed class CosmosDbQuerySerializationTest : QuerySerializationTest
     {
         public CosmosDbQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
             GremlinQuerySource.g
@@ -15,6 +19,38 @@ namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
             testOutputHelper)
         {
 
+        }
+
+        [Fact]
+        public async Task CosmosDbKey()
+        {
+            await _g
+                .V<Person>(new CosmosDbKey("pk", "id"))
+                .Verify(this);
+        }
+
+        [Fact]
+        public async Task CosmosDbKey_with_null_partitionKey()
+        {
+            await _g
+                .V<Person>(new CosmosDbKey("id"))
+                .Verify(this);
+        }
+
+        [Fact]
+        public async Task Mixed_StringKey_CosmosDbKey()
+        {
+            await _g
+                .V<Person>(new CosmosDbKey("pk", "id"), "id2")
+                .Verify(this);
+        }
+
+        [Fact]
+        public async Task Mixed_StringKey_CosmosDbKey_with_null_partitionKey()
+        {
+            await _g
+                .V<Person>(new CosmosDbKey("id"), "id2")
+                .Verify(this);
         }
     }
 }
