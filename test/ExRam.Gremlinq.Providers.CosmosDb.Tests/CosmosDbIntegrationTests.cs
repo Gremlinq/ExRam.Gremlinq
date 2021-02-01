a53a92feca6d8a44c265397382252fb1ad039e35
Daniel Weber <dw@exram.de>
2021-02-01 22:16:23 +0100

    CosmosDbIntegrationTests review.

--- test/ExRam.Gremlinq.Providers.CosmosDb.Tests/CosmosDbIntegrationTests.cs
+++ test/ExRam.Gremlinq.Providers.CosmosDb.Tests/CosmosDbIntegrationTests.cs
@@ -30,118 +30,56 @@ namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
             //Gremlin Query Compilation Error: AddE should follow by a Vertex
         }
 
-        [Fact(Skip = "x")]
-        public override async Task AddE_from_to()
-        {
-            //Gremlin Query Compilation Error: AddE should follow by a Vertex
-        }
+        [Fact(Skip = "Gremlin Query Compilation Error: AddE should follow by a Vertex")]
+        public override async Task AddE_from_to() { }
 
-        [Fact(Skip = "x")]
-        public override async Task AddE_to_from()
-        {
-            //Gremlin Query Compilation Error: AddE should follow by a Vertex
-        }
+        [Fact(Skip = "Gremlin Query Compilation Error: AddE should follow by a Vertex")]
+        public override async Task AddE_to_from() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Aggregate_Global()
-        {
-            //Gremlin Query Compilation Error: Unable to bind to method 'aggregate', with arguments of type: (Scope, String) @ line 1, column 16.
-        }
+        [Fact(Skip = "Gremlin Query Compilation Error: Unable to bind to method 'aggregate', with arguments of type: (Scope, String) @ line 1, column 16.")]
+        public override async Task Aggregate_Global() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Choose_only_default_case()
-        {
-            //Gremlin Query Compilation Error: Unable to resolve symbol 'none' in the current context. @ line 1, column 57.
-        }
+        [Fact(Skip = "Gremlin Query Compilation Error: Unable to resolve symbol 'none' in the current context. @ line 1, column 57.")]
+        public override async Task Choose_only_default_case() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Choose_two_cases_default()
-        {
-            //Gremlin Query Compilation Error: Unable to resolve symbol 'none' in the current context. @ line 1, column 113.
-        }
+        [Fact(Skip = "Gremlin Query Compilation Error: Unable to resolve symbol 'none' in the current context. @ line 1, column 113.")]
+        public override async Task Choose_two_cases_default() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Drop()
-        {
-            //Entity with the specified id does not exist in the system. More info: https://aka.ms/cosmosdb-tsg-not-found
-        }
-        
-        [Fact(Skip = "x")]
-        public override async Task Drop_in_local()
-        {
-            //Entity with the specified id does not exist in the system. More info: https://aka.ms/cosmosdb-tsg-not-found
-        }
+        [Fact(Skip = "Entity with the specified id does not exist in the system. More info: https://aka.ms/cosmosdb-tsg-not-found")]
+        public override async Task Drop() { }
 
-        [Fact(Skip = "x")]
-        public override async Task FilterWithLambda()
-        {
-            //Gremlin query syntax error: Unsupported groovy language rule: 'closure' text: '{it.get().property('Name').value().length()==2}' @ line 1, column 35.
-        }
+        [Fact(Skip = "Gremlin query syntax error: Unsupported groovy language rule: 'closure' text: '{it.get().property('Name').value().length()==2}' @ line 1, column 35.")]
+        public override async Task FilterWithLambda() { }
 
-        [Fact(Skip = "x")]
-        public override async Task OrderBy_ThenBy_lambda()
-        {
-            //    Gremlin query syntax error: Unsupported groovy language rule: 'closure' text: '{it.property('Name').value().length()}' @ line 1, column 60.
+        [Fact(Skip = "Gremlin query syntax error: Unsupported groovy language rule: 'closure' text: '{it.property('Name').value().length()}' @ line 1, column 60.")]
+        public override async Task OrderBy_ThenBy_lambda() { }
 
-            //        Unsupported groovy language rule: 'closure' text: '{it.property('Age').value()}' @ line 1, column 103.
-        }
+        [Fact(Skip = "Gremlin query syntax error: Unsupported groovy language rule: 'closure' text: '{it.property('Name').value().length()}' @ line 1, column 39.")]
+        public override async Task OrderBy_lambda() { }
 
-        [Fact(Skip = "x")]
-        public override async Task OrderBy_lambda()
-        {
-            //Gremlin query syntax error: Unsupported groovy language rule: 'closure' text: '{it.property('Name').value().length()}' @ line 1, column 39.
-        }
+        [Fact(Skip = "Gremlin Query Execution Error: All Values: Cross Apply: The input of values() cannot be a meta or edge property.")]
+        public override async Task Properties_Meta_Values() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Properties_Meta_Values()
-        {
-            //Gremlin Query Execution Error: All Values: Cross Apply: The input of values() cannot be a meta or edge property.
-        }
+        [Fact(Skip = "Gremlin Query Execution Error: All Values: Cross Apply: The input of values() cannot be a meta or edge property.")]
+        public override async Task Properties_Values_typed() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Properties_Values_typed()
-        {
-            //Gremlin Query Execution Error: All Values: Cross Apply: The input of values() cannot be a meta or edge property.
-        }
+        [Fact(Skip = "Gremlin Query Execution Error: All Values: Cross Apply: The input of values() cannot be a meta or edge property.")]
+        public override async Task Properties_Values_untyped() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Properties_Values_untyped()
-        {
-            //Gremlin Query Execution Error: All Values: Cross Apply: The input of values() cannot be a meta or edge property.
-        }
+        [Fact(Skip = "Object reference not set to an instance of an object.")]
+        public override async Task Where_property_traversal() { }
 
-        [Fact(Skip = "x")]
-        public override async Task Where_property_traversal()
-        {
-            //Object reference not set to an instance of an object.
-        }
+        [Fact(Skip = "Gremlin Query Compilation Error: Unable to resolve symbol 'SubgraphStrategy' in the current context. @ line 1, column 21.")]
+        public override async Task WithoutStrategies1() { }
 
-        [Fact(Skip = "x")]
-        public override async Task WithoutStrategies1()
-        {
-            //Gremlin Query Compilation Error: Unable to resolve symbol 'SubgraphStrategy' in the current context. @ line 1, column 21.
-        }
+        [Fact(Skip = "Gremlin Query Compilation Error: Unable to resolve symbol 'SubgraphStrategy' in the current context. @ line 1, column 21. Unable to resolve symbol 'ElementIdStrategy' in the current context. @ line 1, column 39.")]
+        public override async Task WithoutStrategies2() { }
 
-        [Fact(Skip = "x")]
-        public override async Task WithoutStrategies2()
-        {
-            //Gremlin Query Compilation Error: Unable to resolve symbol 'SubgraphStrategy' in the current context. @ line 1, column 21.
+        [Fact(Skip = "Gremlin Query Compilation Error: Unable to resolve symbol 'SubgraphStrategy' in the current context. @ line 1, column 21. Unable to resolve symbol 'ElementIdStrategy' in the current context. @ line 1, column 39.")]
+        public override async Task WithoutStrategies3() { }
 
-            //    Unable to resolve symbol 'ElementIdStrategy' in the current context. @ line 1, column 39.
-        }
-
-        [Fact(Skip = "x")]
-        public override async Task WithoutStrategies3()
-        {
-            //Gremlin Query Compilation Error: Unable to resolve symbol 'SubgraphStrategy' in the current context. @ line 1, column 21.
-            //Unable to resolve symbol 'ElementIdStrategy' in the current context. @ line 1, column 39.
-        }
-
-        [Fact(Skip = "x")]
-        public override async Task Order_Fold_Unfold()
-        {
-            //OrderBy Id no good idea
-        }
+        [Fact(Skip = "OrderBy Id no good idea")]
+        public override async Task Order_Fold_Unfold() { }
     }
 }
 #endif
