8f9148f0afa934635de2b9885cd4ec7ca6b32b77
Daniel Weber <dw@exram.de>
2021-02-02 17:42:19 +0100

    Move QueryDeserializationTest logic to base file.

--- test/ExRam.Gremlinq.Core.Tests/QueryDeserializationTest.cs
+++ test/ExRam.Gremlinq.Core.Tests/QueryDeserializationTest.cs
@@ -1,13 +1,56 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Xunit;
 using Xunit.Abstractions;
 
 namespace ExRam.Gremlinq.Core.Tests
 {
     public abstract class QueryDeserializationTest : QueryExecutionTest
     {
-        protected QueryDeserializationTest(IGremlinQuerySource g, ITestOutputHelper testOutputHelper, [CallerFilePath] string callerFilePath = "") : base(g, testOutputHelper, callerFilePath)
+        [ThreadStatic]
+        private static Context? XUnitContext;
+
+        protected QueryDeserializationTest(ITestOutputHelper testOutputHelper, [CallerFilePath] string callerFilePath = "") : base(
+            GremlinQuerySource.g
+                .ConfigureEnvironment(env => env
+                    .UseExecutor(GremlinQueryExecutor.Create((_, _) =>
+                    {
+                        if (XUnitContext is { } context)
+                        {
+                            var prefix = context.ClassName.Substring(0, context.ClassName.Length - "DeserializationTests".Length);
+
+                            try
+                            {
+                                var jArray = JsonConvert.DeserializeObject<JArray>(
+                                    File.ReadAllText(System.IO.Path.Combine(context.SourceDirectory, prefix + "IntegrationTests." + XUnitContext.MethodName + ".verified.json")));
+
+                                return jArray.Count == 1
+                                    ? new[] { jArray[0] }.ToAsyncEnumerable()
+                                    : jArray.Count == 0
+                                        ? AsyncEnumerable.Empty<object>()
+                                        : throw new NotSupportedException();
+                            }
+                            catch (IOException)
+                            {
+#if !SKIPINTEGRATIONTESTS
+                                throw;
+#endif
+                            }
+                        }
+
+                        throw new InvalidOperationException();
+                    }))
+                    .UseDeserializer(GremlinQueryExecutionResultDeserializer.FromJToken)),
+            testOutputHelper,
+            callerFilePath)
         {
+            XUnitContext = Context;
         }
     }
 }
--- test/ExRam.Gremlinq.Providers.GremlinServer.Tests/GremlinServerDeserializationTests.cs
+++ test/ExRam.Gremlinq.Providers.GremlinServer.Tests/GremlinServerDeserializationTests.cs
@@ -1,54 +1,12 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using ExRam.Gremlinq.Core;
-using ExRam.Gremlinq.Core.Tests;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Xunit;
+﻿using ExRam.Gremlinq.Core.Tests;
 using Xunit.Abstractions;
 
 namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
 {
     public class GremlinServerDeserializationTests : QueryDeserializationTest
     {
-        [ThreadStatic]
-        private static Context? XUnitContext;
-
-        public GremlinServerDeserializationTests(ITestOutputHelper testOutputHelper) : base(
-            GremlinQuerySource.g
-                .ConfigureEnvironment(env => env
-                    .UseExecutor(GremlinQueryExecutor.Create((_, _) =>
-                    {
-                        if (XUnitContext is { } context)
-                        {
-                            var prefix = context.ClassName.Substring(0, context.ClassName.Length - "DeserializationTests".Length);
-
-                            try
-                            {
-                                var jArray = JsonConvert.DeserializeObject<JArray>(
-                                    File.ReadAllText(System.IO.Path.Combine(context.SourceDirectory, prefix + "IntegrationTests." + XUnitContext.MethodName + ".verified.json")));
-
-                                return jArray.Count == 1
-                                    ? new[] {jArray[0]}.ToAsyncEnumerable()
-                                    : jArray.Count == 0
-                                        ? AsyncEnumerable.Empty<object>()
-                                        : throw new NotSupportedException();
-                            }
-                            catch (IOException)
-                            {
-#if !SKIPINTEGRATIONTESTS
-                                throw;
-#endif
-                            }
-                        }
-
-                        throw new InvalidOperationException();
-                    }))
-                    .UseDeserializer(GremlinQueryExecutionResultDeserializer.FromJToken)),
-            testOutputHelper)
+        public GremlinServerDeserializationTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
         {
-            XUnitContext = Context;
         }
     }
 }
