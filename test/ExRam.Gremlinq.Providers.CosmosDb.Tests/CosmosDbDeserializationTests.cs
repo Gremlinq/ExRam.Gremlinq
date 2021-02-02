a43ce166e2aefbfb73f8cf9e274fd3b0ab85ef59
Daniel Weber <dw@exram.de>
2021-02-02 19:45:37 +0100

    Add CosmosDb Deserialization tests and snapshots.

---
+++ test/ExRam.Gremlinq.Providers.CosmosDb.Tests/CosmosDbDeserializationTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
+{
+    public class CosmosDbDeserializationTests : QueryDeserializationTest
+    {
+        public CosmosDbDeserializationTests(ITestOutputHelper testOutputHelper) : base(
+            GremlinQuerySource.g.ConfigureEnvironment(
+                env => env.UseCosmosDb(_ => _.At("ws://localhost", "", "").AuthenticateBy(""))),
+            testOutputHelper)
+        {
+        }
+
+        [Fact(Skip="Wrong Choose signature in Gremlinq <= 8!")]
+        public override Task Choose_Predicate2()
+        {
+            return base.Choose_Predicate2();
+        }
+    }
+}
