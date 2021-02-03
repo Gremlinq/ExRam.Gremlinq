0a8e6585e5aa3f8588038b797f1261881b057f44
Daniel Weber <dw@exram.de>
2021-02-03 18:36:31 +0100

    Cache option value.

--- src/ExRam.Gremlinq.Core/Extensions/PExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/PExtensions.cs
@@ -27,6 +27,8 @@ namespace ExRam.Gremlinq.Core
 
         public static P WorkaroundLimitations(this P p, IGremlinqOptions gremlinqOptions)
         {
+            var disabledTextPredicates = gremlinqOptions.GetValue(GremlinqOption.DisabledTextPredicates);
+
             if (p is TextP textP)
             {
                 switch (textP.OperatorName)
@@ -38,7 +40,7 @@ namespace ExRam.Gremlinq.Core
                         if (value.Length == 0)
                             return new P("true", default);
 
-                        if ((gremlinqOptions.GetValue(GremlinqOption.DisabledTextPredicates) & DisabledTextPredicates.StartingWith) == 0)
+                        if ((disabledTextPredicates & DisabledTextPredicates.StartingWith) == 0)
                             return textP;
 
                         string upperBound;
@@ -55,11 +57,11 @@ namespace ExRam.Gremlinq.Core
 
                         return P.Between(value, upperBound);
                     }
-                    case "endingWith" when (gremlinqOptions.GetValue(GremlinqOption.DisabledTextPredicates) & DisabledTextPredicates.EndingWith) != 0:
+                    case "endingWith" when (disabledTextPredicates & DisabledTextPredicates.EndingWith) != 0:
                         throw new ExpressionNotSupportedException($"Can't work around {nameof(TextP.EndingWith)} without the use of {nameof(TextP)} predicates.");
                     case "endingWith":
                         return textP;
-                    case "containing" when (gremlinqOptions.GetValue(GremlinqOption.DisabledTextPredicates) & DisabledTextPredicates.Containing) != 0:
+                    case "containing" when (disabledTextPredicates & DisabledTextPredicates.Containing) != 0:
                         throw new ExpressionNotSupportedException($"Can't work around {nameof(TextP.Containing)} without the use of {nameof(TextP)} predicates.");
                     case "containing":
                         return textP;
