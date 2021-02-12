89f6ba298db53ce0d9a28a73280a529e730d74e4
Daniel Weber <dw@exram.de>
2021-02-12 18:59:17 +0100

    StepLabelTest.

---
+++ test/ExRam.Gremlinq.Core.Tests/StepLabelTest.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class StepLabelTest
+    {
+        [Fact]
+        public void Identity_is_preserved_through_Equals_by_Cast()
+        {
+            var stepLabel = new StepLabel<object>();
+            var castStepLabel = stepLabel.Cast<string>();
+
+            stepLabel.Should().Be(castStepLabel);
+        }
+
+        [Fact]
+        public void Identity_is_preserved_through_operator_by_Cast()
+        {
+            var stepLabel = new StepLabel<object>();
+            var castStepLabel = stepLabel.Cast<string>();
+
+            ((StepLabel)stepLabel == castStepLabel).Should().BeTrue();
+        }
+    }
+}
