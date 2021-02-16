6986c062ca8d21bb3487da831dcd4e7d595b0a74
Daniel Weber <dw@exram.de>
2021-02-16 19:46:48 +0100

    TODO note.

--- src/ExRam.Gremlinq.Core/Elements/IVertex.cs
+++ src/ExRam.Gremlinq.Core/Elements/IVertex.cs
@@ -1,5 +1,6 @@
 ﻿namespace ExRam.Gremlinq.Core.GraphElements
 {
+    //TODO: Get rid of these interfaces. It's confusing to users that it need not be used. Force model creation instead.
     public interface IVertex : IElement
     {
     }
