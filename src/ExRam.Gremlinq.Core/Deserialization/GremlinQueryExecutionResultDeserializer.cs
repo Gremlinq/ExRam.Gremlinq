70198123de0eeaf86f3d24b69aefb55c1d425eeb
Daniel Weber <dw@exram.de>
2021-02-18 21:02:51 +0100

    Cleanup.

--- src/ExRam.Gremlinq.Core/Deserialization/GremlinQueryExecutionResultDeserializer.cs
+++ src/ExRam.Gremlinq.Core/Deserialization/GremlinQueryExecutionResultDeserializer.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Gremlin.Net.Structure.IO.GraphSON;
 
 namespace ExRam.Gremlinq.Core
 {
--- src/ExRam.Gremlinq.Core/Deserialization/GremlinQueryFragmentDeserializer.cs
+++ src/ExRam.Gremlinq.Core/Deserialization/GremlinQueryFragmentDeserializer.cs
@@ -8,9 +8,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Xml;
 using ExRam.Gremlinq.Core.GraphElements;
-
 using Gremlin.Net.Structure.IO.GraphSON;
-
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
