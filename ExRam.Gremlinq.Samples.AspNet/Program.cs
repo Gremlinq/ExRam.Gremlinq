9f6e6828a095ca4211314696cf9d4cf7145bd2fb
Daniel Weber <dw@exram.de>
2021-02-22 18:14:51 +0100

    Rename folder.

--- ExRam.Gremlinq.Samples.AspNet/Program.cs
+++ ExRam.Gremlinq.Samples.AspNet/Program.cs
@@ -10,11 +10,11 @@ namespace ExRam.Gremlinq.Samples.AspNet
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
