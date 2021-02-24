5ff99e3f56f81c2daedc23df2d9ca97fd82b8764
Daniel Weber <dw@exram.de>
2021-02-24 11:04:42 +0100

    Add UseGremlinServer convenience method.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.GremlinServerAspNet.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.GremlinServerAspNet.verified.cs
@@ -3,5 +3,6 @@ namespace ExRam.Gremlinq.Core.AspNet
     public static class GremlinqSetupExtensions
     {
         public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseGremlinServer(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseGremlinServer<TVertex, TEdge>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup) { }
     }
 }
