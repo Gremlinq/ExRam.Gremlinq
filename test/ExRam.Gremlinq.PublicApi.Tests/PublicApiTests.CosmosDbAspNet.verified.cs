446de27f7200acc19dd49791caf2080deef1fe82
Daniel Weber <dw@exram.de>
2021-02-24 10:55:57 +0100

    Add a convenience method for CosmosDB on AspNet.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.CosmosDbAspNet.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.CosmosDbAspNet.verified.cs
@@ -3,5 +3,7 @@ namespace ExRam.Gremlinq.Core.AspNet
     public static class GremlinqSetupExtensions
     {
         public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseCosmosDb(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseCosmosDb<TVertex, TEdge>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Linq.Expressions.Expression<System.Func<TVertex, object>> partitionKeyExpression)
+            where TVertex :  class { }
     }
 }
