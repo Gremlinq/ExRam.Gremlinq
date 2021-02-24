3da36f11496027f67bcc8c93fc6fdcb2ba46d1d9
Daniel Weber <dw@exram.de>
2021-02-24 11:08:36 +0100

    Add UseNeptune convenience method.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.NeptuneAspNet.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.NeptuneAspNet.verified.cs
@@ -3,5 +3,6 @@ namespace ExRam.Gremlinq.Core.AspNet
     public static class GremlinqSetupExtensions
     {
         public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseNeptune(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseNeptune<TVertex, TEdge>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup) { }
     }
 }
