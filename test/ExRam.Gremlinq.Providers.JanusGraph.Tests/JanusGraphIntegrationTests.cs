d60ad9a4349af10da15203acf5cb42d97981e29a
Daniel Weber <dw@exram.de>
2021-02-26 12:22:05 +0100

    Try running JanusGraph integration tests again...

--- test/ExRam.Gremlinq.Providers.JanusGraph.Tests/JanusGraphIntegrationTests.cs
+++ test/ExRam.Gremlinq.Providers.JanusGraph.Tests/JanusGraphIntegrationTests.cs
@@ -1,6 +1,8 @@
-﻿#if RELEASE && NETCOREAPP3_1
-//Id generation on the JanusGraph docker image is too nondeterministic.
-/*using ExRam.Gremlinq.Core;
+﻿#if RELEASE && NET5_0 && RUNJANUSGRAPHINTEGRATIONTESTS
+using System;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.Tests;
 using ExRam.Gremlinq.Providers.WebSocket;
 using Xunit.Abstractions;
@@ -8,16 +10,25 @@ using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Providers.JanusGraph.Tests
 {
-    public class JanusGraphIntegrationTests : QueryExecutionTest
+    public class JanusGraphIntegrationTests : QueryIntegrationTest
     {
+        private static readonly Regex RelationIdRegex = new("\"relationId\":[\\s]?\"[0-9a-z]{3}([-][0-9a-z]{3})?\"", RegexOptions.IgnoreCase);
+
         public JanusGraphIntegrationTests(ITestOutputHelper testOutputHelper) : base(
             g
                 .ConfigureEnvironment(env => env
                     .UseJanusGraph(builder => builder
-                        .AtLocalhost())),
+                        .AtLocalhost())
+                    .UseDeserializer(GremlinQueryExecutionResultDeserializer.Default)),
             testOutputHelper)
         {
         }
+
+        public override IImmutableList<Func<string, string>> Scrubbers()
+        {
+            return base.Scrubbers()
+                .Add(x => RelationIdRegex.Replace(x, "\"relationId\": \"scrubbed\""));
+        }
     }
-}*/
+}
 #endif
