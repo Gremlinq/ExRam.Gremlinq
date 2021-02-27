1227ab2b42e37aaff559dd169208ef60424771d3
Daniel Weber <dw@exram.de>
2021-02-27 11:36:16 +0100

    StrictJson.

--- test/ExRam.Gremlinq.Core.Tests/GremlinQueryFragmentDeserializerTest.cs
+++ test/ExRam.Gremlinq.Core.Tests/GremlinQueryFragmentDeserializerTest.cs
@@ -4,23 +4,29 @@ using FluentAssertions;
 using Newtonsoft.Json.Linq;
 using VerifyXunit;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ExRam.Gremlinq.Core.Tests
 {
     [UsesVerify]
-    public class GremlinQueryFragmentDeserializerTest
+    public class GremlinQueryFragmentDeserializerTest : GremlinqTestBase
     {
+        public GremlinQueryFragmentDeserializerTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+
+        }
+
         [Fact]
         public async Task Empty()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .TryDeserialize("serialized", typeof(string), GremlinQueryEnvironment.Empty));
         }
 
         [Fact]
         public async Task Base_type()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .Override<object>((serialized, type, env, overridden, recurse) => "overridden")
                 .TryDeserialize("serialized", typeof(string), GremlinQueryEnvironment.Empty));
         }
@@ -28,7 +34,7 @@ namespace ExRam.Gremlinq.Core.Tests
         [Fact]
         public async Task Irrelevant()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .Override<JObject>((serialized, type, env, overridden, recurse) => "should not be here")
                 .TryDeserialize("serialized", typeof(string), GremlinQueryEnvironment.Empty));
         }
@@ -36,7 +42,7 @@ namespace ExRam.Gremlinq.Core.Tests
         [Fact]
         public async Task Override1()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .Override<string>((serialized, type, env, overridden, recurse) => overridden("overridden", type, env, recurse))
                 .TryDeserialize("serialized", typeof(string), GremlinQueryEnvironment.Empty));
         }
@@ -44,7 +50,7 @@ namespace ExRam.Gremlinq.Core.Tests
         [Fact]
         public async Task Override2()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .Override<string>((serialized, type, env, overridden, recurse) => overridden("overridden 1", type, env, recurse))
                 .Override<string>((serialized, type, env, overridden, recurse) => overridden("overridden 2", type, env, recurse))
                 .TryDeserialize("serialized", typeof(string), GremlinQueryEnvironment.Empty));
@@ -53,7 +59,7 @@ namespace ExRam.Gremlinq.Core.Tests
         [Fact]
         public async Task Recurse()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .Override<string>((serialized, type, env, overridden, recurse) => recurse.TryDeserialize(36, type, env))
                 .TryDeserialize("serialized", typeof(int), GremlinQueryEnvironment.Empty));
         }
@@ -72,7 +78,7 @@ namespace ExRam.Gremlinq.Core.Tests
         [Fact]
         public async Task Recurse_to_previous_override()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .Override<int>((serialized, type, env, overridden, recurse) => overridden(37, type, env, recurse))
                 .Override<string>((serialized, type, env, overridden, recurse) => recurse.TryDeserialize(36, type, env))
                 .TryDeserialize("serialized", typeof(int), GremlinQueryEnvironment.Empty));
@@ -81,7 +87,7 @@ namespace ExRam.Gremlinq.Core.Tests
         [Fact]
         public async Task Recurse_to_later_override()
         {
-            await Verifier.Verify(GremlinQueryFragmentDeserializer.Identity
+            await Verify(GremlinQueryFragmentDeserializer.Identity
                 .Override<string>((serialized, type, env, overridden, recurse) => recurse.TryDeserialize(36, type, env))
                 .Override<int>((serialized, type, env, overridden, recurse) => overridden(37, type, env, recurse))
                 .TryDeserialize("serialized", typeof(int), GremlinQueryEnvironment.Empty));
