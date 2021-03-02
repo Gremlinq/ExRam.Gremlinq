1cf7462ec78c607d502e5f7d336ca85d04791e46
Daniel Weber <danielcweber@users.noreply.github.com>
2021-03-02 15:55:58 +0100

    Recurse parsing in TryUnmap and TryGetElementProperties. (#161)
    * Recurse parsing in TryUnmap.
    
    * Fix signatures.
    
    * Recurse in TryGetElementProperties.

--- src/ExRam.Gremlinq.Core/Extensions/JTokenExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/JTokenExtensions.cs
@@ -6,7 +6,7 @@ namespace ExRam.Gremlinq.Core
     internal static class JTokenExtensions
     {
         //TODO: Unmap must be able to handle non-string keys (dictionaries!)
-        public static JObject? TryUnmap(this JObject jObject)
+        public static JObject? TryUnmap(this JObject jObject, IGremlinQueryEnvironment env, IGremlinQueryFragmentDeserializer recurse)
         {
             if (jObject.TryGetValue("@type", out var nestedType) && "g:Map".Equals(nestedType.Value<string>(), StringComparison.OrdinalIgnoreCase))
             {
@@ -16,7 +16,7 @@ namespace ExRam.Gremlinq.Core
 
                     for (var i = 0; i < mapArray.Count / 2; i++)
                     {
-                        retObject.Add(mapArray[i * 2].Value<string>(), mapArray[i * 2 + 1]);
+                        retObject.Add(mapArray[i * 2].Value<string>(), (JToken)recurse.TryDeserialize(mapArray[i * 2 + 1], typeof(JToken), env)!);
                     }
 
                     return retObject;
@@ -26,10 +26,10 @@ namespace ExRam.Gremlinq.Core
             return null;
         }
 
-        public static JObject? TryGetElementProperties(this JObject jObject)
+        public static JObject? TryGetElementProperties(this JObject jObject, IGremlinQueryEnvironment env, IGremlinQueryFragmentDeserializer recurse)
         {
             return jObject.ContainsKey("id") && jObject.TryGetValue("label", out var label) && label.Type == JTokenType.String && jObject["properties"] is JObject propertiesToken
-                ? propertiesToken.TryUnmap() is { } unmapped
+                ? recurse.TryDeserialize(propertiesToken, typeof(JObject), env) is JObject unmapped
                     ? unmapped
                     : propertiesToken
                 : null;
