25118a980b63b8c1e0f9ac3ba9868d4eb8e989b6
Daniel Weber <dw@exram.de>
2021-03-11 17:14:16 +0100

    For people new to Gremlinq it proved hard to figure out how to make Coalesce work with non-matching subqueries. This is a convenience solution that'll return IValueGremlinQuery<object>, not the most specific type but it'll work.

--- test/ExRam.Gremlinq.Core.Tests/QuerySemanticsTest.cs
+++ test/ExRam.Gremlinq.Core.Tests/QuerySemanticsTest.cs
@@ -37,5 +37,18 @@ namespace ExRam.Gremlinq.Core.Tests
                 .AsAdmin()
                 .Semantics);
         }
+
+        [Fact]
+        public virtual Task Coalesce_with_2_not_matching_subQueries_has_right_semantics()
+        {
+            return Verify(g
+                .ConfigureEnvironment(_ => _)
+                .V()
+                .Coalesce(
+                    _ => _.OutE(),
+                    _ => _.In())
+                .AsAdmin()
+                .Semantics);
+        }
     }
 }
