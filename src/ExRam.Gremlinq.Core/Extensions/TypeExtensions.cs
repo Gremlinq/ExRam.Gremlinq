2ae5897e5928b1b264b348c1fb99c3849c9b6df6
Daniel Weber <dw@exram.de>
2021-03-11 16:45:34 +0100

    QuerySemanticsTest!

--- src/ExRam.Gremlinq.Core/Extensions/TypeExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/TypeExtensions.cs
@@ -19,21 +19,25 @@ namespace ExRam.Gremlinq.Core
         public static QuerySemantics? TryGetQuerySemantics(this Type type)
         {
             var semantics = default(QuerySemantics?);
-            var containsEdge = type.Name.Contains("Edge");
-            var containsValue = type.Name.Contains("Value");
-            var containsVertex = type.Name.Contains("Vertex");
-            var containsProperty = type.Name.Contains("Property");
 
-            if (containsValue)
-                semantics = QuerySemantics.Value;
-            else if (containsVertex && containsProperty)
-                semantics = QuerySemantics.VertexProperty;
-            else if (containsVertex && !containsEdge)
-                semantics = QuerySemantics.Vertex;
-            else if (containsProperty)
-                semantics = QuerySemantics.Property;
-            else if (containsEdge && !containsVertex)
-                semantics = QuerySemantics.Edge;
+            if (type.Name.Contains("Query"))
+            {
+                var containsEdge = type.Name.Contains("Edge");
+                var containsValue = type.Name.Contains("Value");
+                var containsVertex = type.Name.Contains("Vertex");
+                var containsProperty = type.Name.Contains("Property");
+
+                if (containsValue)
+                    semantics = QuerySemantics.Value;
+                else if (containsVertex && containsProperty)
+                    semantics = QuerySemantics.VertexProperty;
+                else if (containsVertex && !containsEdge)
+                    semantics = QuerySemantics.Vertex;
+                else if (containsProperty)
+                    semantics = QuerySemantics.Property;
+                else if (containsEdge && !containsVertex)
+                    semantics = QuerySemantics.Edge;
+            }
 
             return semantics;
         }
