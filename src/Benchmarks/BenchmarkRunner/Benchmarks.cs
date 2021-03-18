3b67253d3ac7bf22b503677796e5d3e7891c2efb
Daniel Weber <dw@exram.de>
2021-03-18 18:11:41 +0100

    Add a benchmarks project!

---
+++ src/Benchmarks/BenchmarkRunner/Benchmarks.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Benchmark1()
+        {
+        }
+    }
+}
---
+++ src/Benchmarks/BenchmarkRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using static BenchmarkDotNet.Running.BenchmarkRunner;
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Run<Benchmarks>();
+        }
+    }
+}
