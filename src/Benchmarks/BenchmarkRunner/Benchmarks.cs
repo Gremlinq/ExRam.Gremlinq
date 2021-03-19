481f0d92c7788c313af648bc172e3ef4007e3623
Daniel Weber <dw@exram.de>
2021-03-19 11:59:50 +0100

    First benchmark for the really large graphson-path:
    |             Method |     Mean |    Error |   StdDev |      Gen 0 |      Gen 1 |     Gen 2 | Allocated |
    |------------------- |---------:|---------:|---------:|-----------:|-----------:|----------:|----------:|
    | LargeGraphson3Path | 814.3 ms | 16.29 ms | 33.27 ms | 46000.0000 | 13000.0000 | 3000.0000 | 239.78 MB |

--- src/Benchmarks/BenchmarkRunner/Benchmarks.cs
+++ src/Benchmarks/BenchmarkRunner/Benchmarks.cs
@@ -1,13 +1,39 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.IO;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Providers.Tests;
+using ExRam.Gremlinq.Tests.Entities;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace Benchmarks
 {
     [MemoryDiagnoser]
     public class Benchmarks
     {
-        [Benchmark]
-        public void Benchmark1()
+        private readonly IGremlinQuerySource _g;
+
+        public Benchmarks()
         {
+            _g = g
+                .ConfigureEnvironment(env => env.UseModel(GraphModel.FromBaseTypes<Vertex, Edge>(lookup => lookup
+                    .IncludeAssembliesOfBaseTypes())));
+        }
+
+
+        [Benchmark]
+        public async Task LargeGraphson3Path()
+        {
+            await _g
+                .WithExecutor(GetJson("Large_Graphson3_Paths"))
+                .V<Person>()
+                .Cast<ExRam.Gremlinq.Core.GraphElements.Path[]>()
+                .ToArrayAsync();
+        }
+
+        private static string GetJson(string name)
+        {
+            return new StreamReader(File.OpenRead($"..\\..\\..\\..\\..\\..\\..\\..\\..\\..\\files\\GraphSon\\{name}.json")).ReadToEnd();
         }
     }
 }
