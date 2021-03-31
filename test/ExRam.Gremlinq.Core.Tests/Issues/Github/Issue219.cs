6de098f9504d95dba658f0760ba6216a2a07a3e8
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2021-04-01 02:24:05 +0800

    Renamed test to match github

---
+++ test/ExRam.Gremlinq.Core.Tests/Issues/Github/Issue219.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using ExRam.Gremlinq.Providers.Tests;
+
+using Xunit;
+using Xunit.Abstractions;
+
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class Issue219 : GremlinqTestBase
+    {
+        public Issue219(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+
+        }
+
+        [Fact]
+        public async Task Repro()
+        {
+            var expected = "2021-03-31T14:57:20.3482309Z";
+
+            var actual = await g
+                .ConfigureEnvironment(_ => _)
+                .WithExecutor($"[\"{expected}\"]")
+                .V<string>()
+                .SingleAsync();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
