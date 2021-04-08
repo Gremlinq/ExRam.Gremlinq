50ba2db0b2cc3f93139e453d12a22fb05027ba6c
Daniel Weber <dw@exram.de>
2021-04-08 11:46:44 +0200

    Optimize BindingKey string creation.
    | Method |      Mean |    Error |    StdDev |  Gen 0 | Gen 1 | Gen 2 | Allocated |
    |------- |----------:|---------:|----------:|-------:|------:|------:|----------:|
    |    Old | 101.32 ns | 3.566 ns | 10.514 ns | 0.0688 |     - |     - |     288 B |
    |    New |  47.82 ns | 0.991 ns |  1.060 ns | 0.0095 |     - |     - |      40 B |

--- src/Benchmarks/BenchmarkRunner/Benchmarks.cs
+++ src/Benchmarks/BenchmarkRunner/Benchmarks.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using ExRam.Gremlinq.Core;
@@ -21,7 +22,7 @@ namespace Benchmarks
         }
 
 
-        [Benchmark]
+        //[Benchmark]
         public async Task LargeGraphson3Path()
         {
             await _g
@@ -29,6 +30,46 @@ namespace Benchmarks
                 .V<Person>()
                 .Cast<ExRam.Gremlinq.Core.GraphElements.Path[]>()
                 .ToArrayAsync();
+        }
+
+        [Benchmark]
+        public void Old()
+        {
+            var key = 5326256;
+            var stringKey = string.Empty;
+
+            do
+            {
+                stringKey = (char)('a' + key % 26) + stringKey;
+                key /= 26;
+            } while (key > 0);
+
+            stringKey = "_" + stringKey;
+        }
+
+        [Benchmark]
+        public void New()
+        {
+            var key = 5326256;
+            var digits = key > 0
+                ? (int)Math.Ceiling(Math.Log(key + 1, 26)) + 1
+                : 2;
+
+            string.Create(
+                digits,
+                (key, digits),
+                (span, tuple) =>
+                {
+                    var (key, digits) = tuple;
+
+                    span[0] = '_';
+
+                    for (var i = digits - 1; i >= 1; i--)
+                    {
+                        span[i] = (char)('a' + key % 26);
+                        key /= 26;
+                    }
+                });
         }
 
         private static string GetJson(string name)
--- src/ExRam.Gremlinq.Core/Serialization/BindingKey.cs
+++ src/ExRam.Gremlinq.Core/Serialization/BindingKey.cs
@@ -16,6 +16,7 @@ namespace ExRam.Gremlinq.Core
                 _stringKey = Keys[key]._stringKey;
             else
             {
+                #if NETSTANDARD2_0
                 var stringKey = string.Empty;
 
                 do
@@ -25,6 +26,27 @@ namespace ExRam.Gremlinq.Core
                 } while (key > 0);
 
                 _stringKey = "_" + stringKey;
+                #else
+                var digits = key > 0
+                    ? (int)Math.Ceiling(Math.Log(key + 1, 26)) + 1
+                    : 2;
+
+                _stringKey = string.Create(
+                    digits,
+                    (key, digits),
+                    (span, tuple) =>
+                    {
+                        var (key, digits) = tuple;
+
+                        span[0] = '_';
+
+                        for (var i = digits - 1; i >= 1; i--)
+                        {
+                            span[i] = (char)('a' + key % 26);
+                            key /= 26;
+                        }
+                    });
+                #endif
             }
         }
 
@@ -32,7 +54,7 @@ namespace ExRam.Gremlinq.Core
 
         public static implicit operator string(BindingKey key) => key._stringKey is { } stringKey
             ? stringKey
-            : throw new ArgumentException();
+            : throw new ArgumentException(null, nameof(key));
 
         public override string ToString() => _stringKey is { } stringKey
             ? stringKey
