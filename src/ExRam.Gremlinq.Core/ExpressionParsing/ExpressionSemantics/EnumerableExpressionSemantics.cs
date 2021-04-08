75ed41f1bc86a3d0b0c66cfd85ebca148693b305
Daniel Weber <dw@exram.de>
2021-04-08 23:14:09 +0200

    Files.

---
+++ src/ExRam.Gremlinq.Core/ExpressionParsing/ExpressionSemantics/EnumerableExpressionSemantics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExRam.Gremlinq.Core
+{
+    internal sealed class EnumerableExpressionSemantics : ExpressionSemantics
+    {
+        public static readonly EnumerableExpressionSemantics Intersects = new(() => Intersects!);
+        public static readonly EnumerableExpressionSemantics Contains = new(() => IsContainedIn!);
+        public static readonly EnumerableExpressionSemantics IsContainedIn = new(() => Contains!);
+
+        private EnumerableExpressionSemantics(Func<ExpressionSemantics> flip) : base(flip)
+        {
+
+        }
+    }
+}
---
+++ src/ExRam.Gremlinq.Core/ExpressionParsing/ExpressionSemantics/ExpressionSemantics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExRam.Gremlinq.Core
+{
+    internal abstract class ExpressionSemantics
+    {
+        private readonly Func<ExpressionSemantics> _flip;
+
+        protected ExpressionSemantics(Func<ExpressionSemantics> flip)
+        {
+            _flip = flip;
+        }
+
+        public ExpressionSemantics Flip() => _flip();
+    }
+}
