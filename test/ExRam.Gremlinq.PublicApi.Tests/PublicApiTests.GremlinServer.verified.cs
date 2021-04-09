d00d1c27114c83324818deb064bf0266f41690b8
Daniel Weber <dw@exram.de>
2021-04-09 19:40:59 +0200

    Api Snapshot fixes.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.GremlinServer.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.GremlinServer.verified.cs
@@ -2,7 +2,7 @@ namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryEnvironmentExtensions
     {
-        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseGremlinServer(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder, ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder> builderAction) { }
+        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseGremlinServer(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder, ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder> builderAction) { }
     }
 }
 namespace ExRam.Gremlinq.Providers.GremlinServer
--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.Neptune.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.Neptune.verified.cs
@@ -2,11 +2,14 @@ namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryEnvironmentExtensions
     {
-        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseNeptune(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Core.INeptuneConfigurationBuilder, ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder> transformation) { }
+        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseNeptune(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Providers.Neptune.INeptuneConfigurationBuilder, ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder> transformation) { }
     }
+}
+namespace ExRam.Gremlinq.Providers.Neptune
+{
     public interface INeptuneConfigurationBuilder
     {
-        ExRam.Gremlinq.Core.INeptuneConfigurationBuilderWithUri At(System.Uri uri);
+        ExRam.Gremlinq.Providers.Neptune.INeptuneConfigurationBuilderWithUri At(System.Uri uri);
     }
     public interface INeptuneConfigurationBuilderWithUri : ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder
     {
