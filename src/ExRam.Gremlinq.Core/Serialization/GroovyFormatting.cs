cea33f2ca51afaf96369061249530eb692f57b33
Daniel Weber <dw@exram.de>
2021-04-09 15:18:18 +0200

    TODO

--- src/ExRam.Gremlinq.Core/Serialization/GroovyFormatting.cs
+++ src/ExRam.Gremlinq.Core/Serialization/GroovyFormatting.cs
@@ -3,6 +3,6 @@
     public enum GroovyFormatting
     {
         BindingsOnly,
-        AllowInlining
+        AllowInlining   //TODO vNext: Rename. "Allow" implies that this is a desirable option when it actually is pretty expensive.
     }
 }
