85e3cd88ded5826e5403f5e3d221a6794201a646
danielcweber <danielcweber@users.noreply.github.com>
2021-04-09 17:53:50 +0000

    Fix snapshots for #252.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.JanusGraph.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.JanusGraph.verified.cs
@@ -2,6 +2,17 @@ namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryEnvironmentExtensions
     {
-        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseJanusGraph(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder, ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder> builderAction) { }
+        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseJanusGraph(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Providers.JanusGraph.IJanusGraphConfigurationBuilder, ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder> transformation) { }
+    }
+}
+namespace ExRam.Gremlinq.Providers.JanusGraph
+{
+    public interface IJanusGraphConfigurationBuilder
+    {
+        ExRam.Gremlinq.Providers.JanusGraph.IJanusGraphConfigurationBuilderWithUri At(System.Uri uri);
+    }
+    public interface IJanusGraphConfigurationBuilderWithUri : ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder
+    {
+        ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder ConfigureWebSocket(System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder, ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder> transformation);
     }
 }
