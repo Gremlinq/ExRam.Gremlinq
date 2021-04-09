abdc1b83e7eedec07182b66633348301979f40d9
danielcweber <danielcweber@users.noreply.github.com>
2021-04-09 19:21:53 +0000

    Fix snapshots for #255.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.WebSocketAspNet.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.WebSocketAspNet.verified.cs
@@ -2,16 +2,10 @@ namespace ExRam.Gremlinq.Core.AspNet
 {
     public static class GremlinqSetupExtensions
     {
-        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup ConfigureWebSocketBuilder(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator, ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator> transformation) { }
         public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseWebSocket(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup) { }
-    }
-    public interface IWebSocketGremlinQueryExecutorBuilderTransformation
-    {
-        ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator Transform(ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator builder);
     }
     public static class WebSocketGremlinQueryExecutorBuilderExtensions
     {
         public static ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator Configure(this ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator builder, Microsoft.Extensions.Configuration.IConfiguration configuration) { }
-        public static ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator Transform(this ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator builder, System.Collections.Generic.IEnumerable<ExRam.Gremlinq.Core.AspNet.IWebSocketGremlinQueryExecutorBuilderTransformation> webSocketTransformations) { }
     }
 }
