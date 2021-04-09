2abcdea3b1df1957d3a19c4cec442635e6a9497d
Daniel Weber <dw@exram.de>
2021-04-09 19:17:29 +0200

    Breaking: UseNeptune takes a transformation from INeptuneConfigurationBuilder. To get the previous behaviour, first call At(Uri) on the builder, then, if needed, configure the rest by calling ConfigureWebSocket.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.Neptune.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.Neptune.verified.cs
@@ -2,6 +2,14 @@ namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryEnvironmentExtensions
     {
-        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseNeptune(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder, ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder> transformation) { }
+        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseNeptune(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<ExRam.Gremlinq.Core.INeptuneConfigurationBuilder, ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder> transformation) { }
+    }
+    public interface INeptuneConfigurationBuilder
+    {
+        ExRam.Gremlinq.Core.INeptuneConfigurationBuilderWithUri At(System.Uri uri);
+    }
+    public interface INeptuneConfigurationBuilderWithUri : ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder
+    {
+        ExRam.Gremlinq.Core.IGremlinQueryExecutorBuilder ConfigureWebSocket(System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder, ExRam.Gremlinq.Providers.WebSocket.IWebSocketGremlinQueryExecutorBuilder> transformation);
     }
 }
