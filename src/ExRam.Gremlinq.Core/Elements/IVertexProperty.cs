93d10d8c19f9fcf73c0939c401a930e69fa0d380
Daniel Weber <dw@exram.de>
2021-04-10 16:48:27 +0200

    Nobody uses IVertexProperty.Id.

--- src/ExRam.Gremlinq.Core/Elements/IVertexProperty.cs
+++ src/ExRam.Gremlinq.Core/Elements/IVertexProperty.cs
@@ -5,7 +5,5 @@ namespace ExRam.Gremlinq.Core.GraphElements
     internal interface IVertexProperty : IProperty
     {
         IEnumerable<KeyValuePair<string, object>> GetProperties(IGremlinQueryEnvironment environment);
-
-        object? Id { get; }
     }
 }
