6a336e24a6c101cf89b0eef8497b1e4b088607b0
Daniel Weber <dw@exram.de>
2021-04-13 13:59:54 +0200

    NeptuneIntegrationTests.

---
+++ test/ExRam.Gremlinq.Providers.Neptune.Tests/NeptuneIntegrationTests.cs
@@ -0,0 +1,20 @@
+﻿#if RELEASE && NET5_0 && RUNNEPTUNEINTEGRATIONTESTS
+using System;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using Xunit.Abstractions;
+
+namespace ExRam.Gremlinq.Providers.Neptune.Tests
+{
+    public class NeptuneIntegrationTests : QueryIntegrationTest
+    {
+        public NeptuneIntegrationTests(ITestOutputHelper testOutputHelper) : base(
+            GremlinQuerySource.g
+                .UseNeptune(builder => builder
+                    .At(new Uri("wss://127.0.0.1:8182"))),
+            testOutputHelper)
+        {
+        }
+    }
+}
+#endif
