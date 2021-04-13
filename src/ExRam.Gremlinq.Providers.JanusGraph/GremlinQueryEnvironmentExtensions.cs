d4d7c3741d9b376319156ac7c8efaa5fc544590c
Daniel Weber <dw@exram.de>
2021-04-13 19:52:47 +0200

    StoreByteArraysAsBase64String extension.

--- src/ExRam.Gremlinq.Providers.JanusGraph/GremlinQueryEnvironmentExtensions.cs
+++ src/ExRam.Gremlinq.Providers.JanusGraph/GremlinQueryEnvironmentExtensions.cs
@@ -45,12 +45,7 @@ namespace ExRam.Gremlinq.Core
                         .ConfigureVertexPropertyFeatures(_ => VertexPropertyFeatures.RemoveProperty | VertexPropertyFeatures.NumericIds | VertexPropertyFeatures.StringIds | VertexPropertyFeatures.Properties | VertexPropertyFeatures.BooleanValues | VertexPropertyFeatures.ByteValues | VertexPropertyFeatures.DoubleValues | VertexPropertyFeatures.FloatValues | VertexPropertyFeatures.IntegerValues | VertexPropertyFeatures.LongValues | VertexPropertyFeatures.StringValues)
                         .ConfigureEdgeFeatures(_ => EdgeFeatures.AddEdges | EdgeFeatures.RemoveEdges | EdgeFeatures.AddProperty | EdgeFeatures.RemoveProperty | EdgeFeatures.NumericIds | EdgeFeatures.StringIds | EdgeFeatures.UuidIds | EdgeFeatures.CustomIds | EdgeFeatures.AnyIds)
                         .ConfigureEdgePropertyFeatures(_ => EdgePropertyFeatures.Properties | EdgePropertyFeatures.BooleanValues | EdgePropertyFeatures.ByteValues | EdgePropertyFeatures.DoubleValues | EdgePropertyFeatures.FloatValues | EdgePropertyFeatures.IntegerValues | EdgePropertyFeatures.LongValues | EdgePropertyFeatures.StringValues))
-                    .ConfigureModel(model => model
-                        .ConfigureNativeTypes(types => types
-                            .Remove(typeof(byte[]))))
-                    .ConfigureSerializer(_ => _
-                        .ConfigureFragmentSerializer(_ => _
-                            .Override<byte[]>((bytes, env, overridden, recurse) => recurse.Serialize(Convert.ToBase64String(bytes), env)))));
+                    .StoreByteArraysAsBase64String());
         }
     }
 }
