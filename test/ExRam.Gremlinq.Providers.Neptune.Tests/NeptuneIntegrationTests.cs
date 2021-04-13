afc49b5c8dbba7528555c88044116711ae59579b
Daniel Weber <dw@exram.de>
2021-04-13 21:19:59 +0200

    Neptune: Ignore a couple of tests.

--- test/ExRam.Gremlinq.Providers.Neptune.Tests/NeptuneIntegrationTests.cs
+++ test/ExRam.Gremlinq.Providers.Neptune.Tests/NeptuneIntegrationTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Immutable;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using ExRam.Gremlinq.Core.Tests;
 using Xunit;
 using Xunit.Abstractions;
@@ -25,6 +26,76 @@ namespace ExRam.Gremlinq.Providers.Neptune.Tests
                 .Add(x => IdRegex1.Replace(x, "\"scrubbed id\""))
                 .Add(x => IdRegex2.Replace(x, "\"scrubbed id\""));
         }
+
+        [Fact(Skip= "Properties on a vertex property is not supported")]
+        public override Task AddV_with_Meta_with_properties() => base.AddV_with_Meta_with_properties();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task AddV_with_MetaModel() => base.AddV_with_MetaModel();
+
+        [Fact(Skip = "Query parsing failed at line 1, character position at 0, error message : mismatched input '[' expecting {EmptyStringLiteral, 'g'}")]
+        public override Task FilterWithLambda() => base.FilterWithLambda();
+
+        [Fact(Skip = "Query parsing failed at line 1, character position at 0, error message : mismatched input '[' expecting {EmptyStringLiteral, 'g'}")]
+        public override Task OrderBy_lambda() => base.OrderBy_lambda();
+
+        [Fact(Skip = "Query parsing failed at line 1, character position at 0, error message : mismatched input '[' expecting {EmptyStringLiteral, 'g'}")]
+        public override Task OrderBy_ThenBy_lambda() => base.OrderBy_ThenBy_lambda();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Meta_ValueMap() => base.Properties_Meta_ValueMap();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Meta_Values() => base.Properties_Meta_Values();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Meta_Values_Projected() => base.Properties_Meta_Values_Projected();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Meta_Where1() => base.Properties_Meta_Where1();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Properties_as_select() => base.Properties_Properties_as_select();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Properties_key() => base.Properties_Properties_key();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Properties_Value() => base.Properties_Properties_Value();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Properties_Where_key() => base.Properties_Properties_Where_key();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Properties1() => base.Properties_Properties1();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Properties2() => base.Properties_Properties2();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_ValueMap_typed() => base.Properties_ValueMap_typed();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_ValueMap_untyped() => base.Properties_ValueMap_untyped();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Properties_Values2() => base.Properties_Values2();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Property_single_with_dictionary_meta1() => base.Property_single_with_dictionary_meta1();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Property_single_with_meta() => base.Property_single_with_meta();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Set_Meta_Property1() => base.Set_Meta_Property1();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Set_Meta_Property2() => base.Set_Meta_Property2();
+
+        [Fact(Skip = "Properties on a vertex property is not supported")]
+        public override Task Variable_wrap() => base.Variable_wrap();
     }
+
 }
 #endif
