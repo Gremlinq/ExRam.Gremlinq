07abb490de8b9afec223cdd60b4d072aee73d9a1
Daniel Weber <dw@exram.de>
2021-04-14 10:37:09 +0200

    Replace Int32 and Int64 Ids.

--- test/ExRam.Gremlinq.Core.Tests/QueryIntegrationTest.cs
+++ test/ExRam.Gremlinq.Core.Tests/QueryIntegrationTest.cs
@@ -8,7 +8,7 @@ namespace ExRam.Gremlinq.Core.Tests
 {
     public abstract class QueryIntegrationTest : QueryExecutionTest
     {
-        private static readonly Regex IdRegex = new ("(\"id\"\\s*[:,]\\s*{\\s*\"@type\"\\s*:\\s*\"g:Int64\"\\s*,\\s*\"@value\":\\s*)([^\\s{}]+)(\\s*})", RegexOptions.IgnoreCase);
+        private static readonly Regex IdRegex = new ("(\"id\"\\s*[:,]\\s*{\\s*\"@type\"\\s*:\\s*\"g:Int(32|64)\"\\s*,\\s*\"@value\":\\s*)([^\\s{}]+)(\\s*})", RegexOptions.IgnoreCase);
 
         protected QueryIntegrationTest(IIntegrationTestFixture fixture, ITestOutputHelper testOutputHelper, [CallerFilePath] string callerFilePath = "") : base(
             fixture.GremlinQuerySource
@@ -22,7 +22,7 @@ namespace ExRam.Gremlinq.Core.Tests
         public override IImmutableList<Func<string, string>> Scrubbers()
         {
             return base.Scrubbers()
-                .Add(x => IdRegex.Replace(x, "$1\"scrubbed id\"$3"));
+                .Add(x => IdRegex.Replace(x, "$1\"scrubbed id\"$4"));
         }
     }
 }
