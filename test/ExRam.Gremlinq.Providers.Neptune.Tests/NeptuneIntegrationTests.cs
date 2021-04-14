3a4e7b179d75b1d745e632ea9a2881ba25827e10
Daniel Weber <dw@exram.de>
2021-04-14 10:37:09 +0200

    Replace Int32 and Int64 Ids.

--- test/ExRam.Gremlinq.Providers.Neptune.Tests/NeptuneIntegrationTests.cs
+++ test/ExRam.Gremlinq.Providers.Neptune.Tests/NeptuneIntegrationTests.cs
@@ -12,8 +12,7 @@ namespace ExRam.Gremlinq.Providers.Neptune.Tests
     public class NeptuneIntegrationTests : QueryIntegrationTest, IClassFixture<NeptuneFixture>
     {
         private static readonly Regex IdRegex1 = new("\"[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}([|]PartitionKey)?\"", RegexOptions.IgnoreCase);
-        private static readonly Regex IdRegex2 = new("(\"id\"\\s*[:,]\\s*{\\s*\"@type\"\\s*:\\s*\"g:Int32\"\\s*,\\s*\"@value\":\\s*)([^\\s{}]+)(\\s*})", RegexOptions.IgnoreCase);
-
+        
         public NeptuneIntegrationTests(NeptuneFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
             testOutputHelper)
@@ -23,8 +22,7 @@ namespace ExRam.Gremlinq.Providers.Neptune.Tests
         public override IImmutableList<Func<string, string>> Scrubbers()
         {
             return base.Scrubbers()
-                .Add(x => IdRegex1.Replace(x, "\"scrubbed id\""))
-                .Add(x => IdRegex2.Replace(x, "\"scrubbed id\""));
+                .Add(x => IdRegex1.Replace(x, "\"scrubbed id\""));
         }
 
         [Fact(Skip= "Properties on a vertex property is not supported")]
