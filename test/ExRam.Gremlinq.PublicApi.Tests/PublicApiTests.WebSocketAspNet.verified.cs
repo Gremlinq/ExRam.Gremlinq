a92b634b9d57a2255f3cd17b9f0ae4c86a9ed2cd
Daniel Weber <dw@exram.de>
2021-04-16 20:51:53 +0200

    Fix Api snapshots.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.WebSocketAspNet.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.WebSocketAspNet.verified.cs
@@ -2,10 +2,12 @@ namespace ExRam.Gremlinq.Core.AspNet
 {
     public static class GremlinqSetupExtensions
     {
-        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseWebSocket(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup ConfigureWebSocket(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Func<ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator, ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator> transformation) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseProvider<TConfigurator>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, string sectionName, System.Func<ExRam.Gremlinq.Core.IConfigurableGremlinQuerySource, System.Func<TConfigurator, ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation>, ExRam.Gremlinq.Core.IGremlinQuerySource> providerChoice, System.Func<TConfigurator, Microsoft.Extensions.Configuration.IConfiguration, TConfigurator> configuration)
+            where TConfigurator : ExRam.Gremlinq.Providers.WebSocket.IProviderConfigurator<TConfigurator> { }
     }
     public static class WebSocketConfiguratorExtensions
     {
-        public static ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator Configure(this ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator configurator, Microsoft.Extensions.Configuration.IConfiguration configuration) { }
+        public static ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator ConfigureFrom(this ExRam.Gremlinq.Providers.WebSocket.IWebSocketConfigurator webSocketConfigurator, Microsoft.Extensions.Configuration.IConfiguration configuration) { }
     }
 }
