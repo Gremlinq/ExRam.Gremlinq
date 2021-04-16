ed4e23eb8232a8724a28e8b0782be9b674997de6
Daniel Weber <dw@exram.de>
2021-04-16 14:48:26 +0200

    AtLocalhost extensions.

---
+++ src/ExRam.Gremlinq.Providers.JanusGraph/Extensions/JanusGraphConfiguratorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExRam.Gremlinq.Providers.JanusGraph
+{
+    public static class JanusGraphConfiguratorExtensions
+    {
+        public static IJanusGraphConfigurator AtLocalhost(this IJanusGraphConfigurator configurator)
+        {
+            return configurator.At(new Uri("ws://localhost:8182"));
+        }
+    }
+}
---
+++ src/ExRam.Gremlinq.Providers.Neptune/Extensions/NeptuneConfiguratorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExRam.Gremlinq.Providers.Neptune
+{
+    public static class NeptuneConfiguratorExtensions
+    {
+        public static INeptuneConfigurator AtLocalhost(this INeptuneConfigurator configurator)
+        {
+            return configurator.At(new Uri("ws://localhost:8182"));
+        }
+    }
+}
