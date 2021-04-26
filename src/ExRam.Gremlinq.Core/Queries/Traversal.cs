0ccc3dc37e9e286c44ad9b1f1a40da0c1677eaae
Daniel Weber <dw@exram.de>
2021-04-26 21:09:27 +0200

    Traversal: Implement IReadOnlyList directly.

--- src/ExRam.Gremlinq.Core/Queries/Traversal.cs
+++ src/ExRam.Gremlinq.Core/Queries/Traversal.cs
@@ -1,33 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 
 namespace ExRam.Gremlinq.Core
 {
-    public readonly struct Traversal
+    public readonly struct Traversal : IReadOnlyList<Step>
     {
+        private readonly IReadOnlyList<Step> _steps;
+
         public Traversal(IEnumerable<Step> steps) : this(steps.ToArray(), true)
         {
         }
 
         public Traversal(ImmutableArray<Step> steps)
         {
-            Steps = steps;
+            _steps = steps;
         }
 
         internal Traversal(IReadOnlyList<Step> steps, bool owned)
         {
-            Steps = owned
+            _steps = owned
                 ? steps
                 : steps.ToArray();
         }
 
-        public IReadOnlyList<Step> Steps { get; }
+        public IEnumerator<Step> GetEnumerator() => _steps.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public int Count { get => _steps.Count; }
+
+        public Step this[int index] => _steps[index];
 
         public static implicit operator Traversal(Step[] steps) => new(steps, false);
 
         public static implicit operator Traversal(ImmutableArray<Step> steps) => new(steps);
 
-        public static implicit operator Traversal(Step step) => new(new [] { step });
+        public static implicit operator Traversal(Step step) => new(new[] { step });
     }
 }
