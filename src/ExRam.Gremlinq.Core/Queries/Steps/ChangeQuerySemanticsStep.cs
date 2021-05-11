0afd87528c059f0dff417c652c158dd0fde47d80
Daniel Weber <dw@exram.de>
2021-05-11 23:04:47 +0200

    ChangeQuerySemanticsStep!

---
+++ src/ExRam.Gremlinq.Core/Queries/Steps/ChangeQuerySemanticsStep.cs
@@ -0,0 +1,10 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    public sealed class ChangeQuerySemanticsStep : Step
+    {
+        public ChangeQuerySemanticsStep(QuerySemantics semantics) : base(semantics)
+        {
+
+        }
+    }
+}
