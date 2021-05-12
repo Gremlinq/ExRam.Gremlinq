11ee838b1be8d48cb8caaf0e2c3372f0e964abec
Daniel Weber <dw@exram.de>
2021-05-12 19:31:09 +0200

    Only add ProjectEdgeSteps and ProjectVertexSteps to a Traversal, let serialization do the rest. Don't add the IdentityStep in ToTraversal.

---
+++ src/ExRam.Gremlinq.Core/Queries/Steps/ProjectVertexStep.cs
@@ -0,0 +1,15 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    public sealed class ProjectVertexStep : Step
+    {
+        public ProjectVertexStep(QuerySemantics? semantics = null) : base(semantics)
+        {
+
+        }
+
+        public override Step OverrideQuerySemantics(QuerySemantics semantics)
+        {
+            return new ProjectVertexStep(semantics);
+        }
+    }
+}
