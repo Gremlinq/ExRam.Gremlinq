2612c4c82d5591ad20adca3b291cc0ffce9fbc4e
Daniel Weber <dw@exram.de>
2021-05-12 19:00:47 +0200

    Files.

--- src/ExRam.Gremlinq.Core/Queries/Steps/CyclicPathStep.cs
+++ src/ExRam.Gremlinq.Core/Queries/Steps/CyclicPathStep.cs
@@ -8,13 +8,4 @@
 
         public override Step OverrideQuerySemantics(QuerySemantics semantics) => new CyclicPathStep(semantics);
     }
-
-    public sealed class SimplePathStep : Step
-    {
-        public SimplePathStep(QuerySemantics? semantics = default) : base(semantics)
-        {
-        }
-
-        public override Step OverrideQuerySemantics(QuerySemantics semantics) => new SimplePathStep(semantics);
-    }
 }
---
+++ src/ExRam.Gremlinq.Core/Queries/Steps/SimplePathStep.cs
@@ -0,0 +1,11 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    public sealed class SimplePathStep : Step
+    {
+        public SimplePathStep(QuerySemantics? semantics = default) : base(semantics)
+        {
+        }
+
+        public override Step OverrideQuerySemantics(QuerySemantics semantics) => new SimplePathStep(semantics);
+    }
+}
