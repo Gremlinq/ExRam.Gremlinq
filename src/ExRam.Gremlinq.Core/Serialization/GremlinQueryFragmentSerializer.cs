f2f5ef07c62023cd7b2b8a1f10e8c73bd61b91c1
Daniel Weber <dw@exram.de>
2021-05-14 22:30:27 +0200

    IncludeProjection includes the projection into the traversal.

--- src/ExRam.Gremlinq.Core/Serialization/GremlinQueryFragmentSerializer.cs
+++ src/ExRam.Gremlinq.Core/Serialization/GremlinQueryFragmentSerializer.cs
@@ -243,7 +243,7 @@ namespace ExRam.Gremlinq.Core
                         : step.Argument))
                 .Override<IdentityStep>((step, env, overridden, recurse) => CreateInstruction("identity"))
                 .Override<IdStep>((step, env, overridden, recurse) => CreateInstruction("id"))
-                .Override<IGremlinQueryBase>((query, env, overridden, recurse) => recurse.Serialize(query.ToTraversal(), env))
+                .Override<IGremlinQueryBase>((query, env, overridden, recurse) => recurse.Serialize(query.ToTraversal().IncludeProjection(), env))
                 .Override<InjectStep>((step, env, overridden, recurse) => CreateInstruction("inject", recurse, env, step.Elements))
                 .Override<InEStep>((step, env, overridden, recurse) => CreateInstruction("inE", recurse, env, step.Labels))
                 .Override<InStep>((step, env, overridden, recurse) => CreateInstruction("in", recurse, env, step.Labels))
