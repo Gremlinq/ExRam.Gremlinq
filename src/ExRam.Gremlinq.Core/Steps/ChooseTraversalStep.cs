48701e5b8f40a9fc2ea6f78fc0310ac02d24c70b
Daniel Weber <dw@exram.de>
2021-08-16 19:19:58 +0200

    Review.

--- src/ExRam.Gremlinq.Core/Steps/ChooseTraversalStep.cs
+++ src/ExRam.Gremlinq.Core/Steps/ChooseTraversalStep.cs
@@ -2,6 +2,7 @@
 {
     public sealed class ChooseTraversalStep : ChooseStep
     {
+        // ReSharper disable once BitwiseOperatorOnEnumWithoutFlags
         public ChooseTraversalStep(Traversal ifTraversal, Traversal thenTraversal, Traversal? elseTraversal = default) : base(thenTraversal, elseTraversal, ifTraversal.GetSideEffectSemanticsChange() | thenTraversal.GetSideEffectSemanticsChange() | elseTraversal.GetSideEffectSemanticsChange())
         {
             IfTraversal = ifTraversal;
