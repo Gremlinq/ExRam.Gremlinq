adb8e5b7451df8a89c30ee36bc003512a3e75d13
Daniel Weber <dw@exram.de>
2021-11-11 12:51:13 +0100

    Add IGremlinClientFactory so creation of Gremlin Clients can be configured better in the future.

---
+++ src/ExRam.Gremlinq.Providers.WebSocket/IGremlinClientFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net.WebSockets;
+using Gremlin.Net.Driver;
+
+namespace ExRam.Gremlinq.Providers.WebSocket
+{
+    public interface IGremlinClientFactory
+    {
+        GremlinClient Create(Gremlin.Net.Driver.GremlinServer gremlinServer, IMessageSerializer? messageSerializer = null, ConnectionPoolSettings? connectionPoolSettings = null, Action<ClientWebSocketOptions>? webSocketConfiguration = null, string? sessionId = null);
+    }
+}
