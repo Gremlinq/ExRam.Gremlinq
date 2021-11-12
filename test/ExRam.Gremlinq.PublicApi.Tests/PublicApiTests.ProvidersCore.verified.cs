1ecc5f709ed8d7a1fac09771750f106b07f0d872
Daniel Weber <dw@exram.de>
2021-11-12 14:28:47 +0100

    IProviderConfigurator: The At-Method only applies to anything network related. Besides, it doesn't add any value since it's easily configured with ConfigureWebSocket.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.ProvidersCore.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.ProvidersCore.verified.cs
@@ -1,16 +1,5 @@
 ﻿namespace ExRam.Gremlinq.Providers.Core
 {
     public interface IProviderConfigurator<out TConfigurator> : ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation
-        where out TConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TConfigurator>
-    {
-        TConfigurator At(System.Uri uri);
-    }
-}
-namespace ExRam.Gremlinq.Providers.GremlinServer
-{
-    public static class ProviderConfiguratorExtensions
-    {
-        public static TProviderConfigurator AtLocalhost<TProviderConfigurator>(this TProviderConfigurator configurator)
-            where TProviderConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TProviderConfigurator> { }
-    }
+        where out TConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TConfigurator> { }
 }
