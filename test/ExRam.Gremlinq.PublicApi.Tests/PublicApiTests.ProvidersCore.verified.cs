4fd908f489736c3f7f60511e6a322fa75b32e2fc
Daniel Weber <dw@exram.de>
2021-11-12 14:31:57 +0100

    Include Api Snapshots for Providers.Core package.

---
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.ProvidersCore.verified.cs
@@ -0,0 +1,16 @@
+﻿namespace ExRam.Gremlinq.Providers.Core
+{
+    public interface IProviderConfigurator<out TConfigurator> : ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation
+        where out TConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TConfigurator>
+    {
+        TConfigurator At(System.Uri uri);
+    }
+}
+namespace ExRam.Gremlinq.Providers.GremlinServer
+{
+    public static class ProviderConfiguratorExtensions
+    {
+        public static TProviderConfigurator AtLocalhost<TProviderConfigurator>(this TProviderConfigurator configurator)
+            where TProviderConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TProviderConfigurator> { }
+    }
+}
