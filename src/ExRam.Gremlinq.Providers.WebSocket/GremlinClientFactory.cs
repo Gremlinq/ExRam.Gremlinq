ccf3054bb912d80f668c6d2e1bbe9a2df63f529f
Daniel Weber <dw@exram.de>
2021-11-12 10:35:42 +0100

    Add GremlinClientFactory.Create.

--- src/ExRam.Gremlinq.Providers.WebSocket/GremlinClientFactory.cs
+++ src/ExRam.Gremlinq.Providers.WebSocket/GremlinClientFactory.cs
@@ -19,6 +19,20 @@ namespace ExRam.Gremlinq.Providers.WebSocket
             }
         }
 
+        private sealed class FuncGremlinClientFactory : IGremlinClientFactory
+        {
+            private readonly Func<Gremlin.Net.Driver.GremlinServer, IMessageSerializer?, ConnectionPoolSettings?, Action<ClientWebSocketOptions>?, string?, GremlinClient> _factory;
+
+            public FuncGremlinClientFactory(Func<Gremlin.Net.Driver.GremlinServer, IMessageSerializer?, ConnectionPoolSettings?, Action<ClientWebSocketOptions>?, string?, GremlinClient> factory)
+            {
+                _factory = factory;
+            }
+
+            public GremlinClient Create(Gremlin.Net.Driver.GremlinServer gremlinServer, IMessageSerializer? messageSerializer = null, ConnectionPoolSettings? connectionPoolSettings = null, Action<ClientWebSocketOptions>? webSocketConfiguration = null, string? sessionId = null) => _factory(gremlinServer, messageSerializer, connectionPoolSettings, webSocketConfiguration, sessionId);
+        }
+
         public static readonly IGremlinClientFactory Default = new DefaultGremlinClientFactory();
+
+        public static IGremlinClientFactory Create(Func<Gremlin.Net.Driver.GremlinServer, IMessageSerializer?, ConnectionPoolSettings?, Action<ClientWebSocketOptions>?, string?, GremlinClient> factory) => new FuncGremlinClientFactory(factory);
     }
 }
