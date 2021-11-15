2b913cfc5c08ea7a5f47caef82235ac58c3224ef
Daniel Weber <dw@exram.de>
2021-11-15 18:01:46 +0100

    New: IProviderConfiguratorTransformation.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.ProvidersCore.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.ProvidersCore.verified.cs
@@ -1,5 +1,10 @@
 ﻿namespace ExRam.Gremlinq.Providers.Core
 {
+    public interface IProviderConfiguratorTransformation<TConfigurator>
+        where TConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TConfigurator>
+    {
+        TConfigurator Transform(TConfigurator configurator);
+    }
     public interface IProviderConfigurator<out TConfigurator> : ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation
         where out TConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TConfigurator> { }
 }
