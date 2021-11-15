04eb7527fdf57566281a354baf3a7d561995a33e
Daniel Weber <dw@exram.de>
2021-11-15 18:59:41 +0100

    ProviderSetup: Works just like GremlinqSetup.

---
+++ src/ExRam.Gremlinq.Core.AspNet/ProviderSetup.cs
@@ -0,0 +1,16 @@
+﻿using ExRam.Gremlinq.Providers.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExRam.Gremlinq.Core.AspNet
+{
+    public readonly struct ProviderSetup<TConfigurator>
+         where TConfigurator : IProviderConfigurator<TConfigurator>
+    {
+        public ProviderSetup(IServiceCollection serviceCollection)
+        {
+            ServiceCollection = serviceCollection;
+        }
+
+        public IServiceCollection ServiceCollection { get; }
+    }
+}
