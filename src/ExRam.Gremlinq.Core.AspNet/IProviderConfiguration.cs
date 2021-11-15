ba8621be5c4895decd64afce6c64fb98046b0e44
Daniel Weber <dw@exram.de>
2021-11-15 20:00:36 +0100

    IGremlinqConfiguration and IProviderConfiguration inherit from IConfigurationSection. So we can rename these later into ...Section, plus a section provides more.

--- src/ExRam.Gremlinq.Core.AspNet/IProviderConfiguration.cs
+++ src/ExRam.Gremlinq.Core.AspNet/IProviderConfiguration.cs
@@ -2,7 +2,7 @@
 
 namespace ExRam.Gremlinq.Core.AspNet
 {
-    public interface IProviderConfiguration : IConfiguration
+    public interface IProviderConfiguration : IConfigurationSection
     {
 
     }
