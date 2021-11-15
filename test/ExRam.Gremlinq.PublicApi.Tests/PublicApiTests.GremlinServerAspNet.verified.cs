fb403d8629635c8e6ce6be17450d17cbf712b12f
Daniel Weber <dw@exram.de>
2021-11-15 18:55:11 +0100

    Extra-Configuration: Provide an IProviderConfiguration.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.GremlinServerAspNet.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.GremlinServerAspNet.verified.cs
@@ -2,7 +2,7 @@ namespace ExRam.Gremlinq.Core.AspNet
 {
     public static class GremlinqSetupExtensions
     {
-        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseGremlinServer(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Func<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator, Microsoft.Extensions.Configuration.IConfiguration, ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator>? extraConfiguration = null) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseGremlinServer(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Func<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator, ExRam.Gremlinq.Core.AspNet.IProviderConfiguration, ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator>? extraConfiguration = null) { }
         public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseGremlinServer<TVertex, TEdge>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Func<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator, Microsoft.Extensions.Configuration.IConfiguration, ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator>? extraConfiguration = null) { }
     }
 }
