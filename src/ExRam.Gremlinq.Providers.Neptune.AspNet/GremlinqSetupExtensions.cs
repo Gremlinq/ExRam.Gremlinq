c2fcb475940890a7622d782589ddfc6d77c0a690
Daniel Weber <dw@exram.de>
2021-11-15 19:53:22 +0100

    UseProvider: Simplify configuration from a section and extra configs.

--- src/ExRam.Gremlinq.Providers.Neptune.AspNet/GremlinqSetupExtensions.cs
+++ src/ExRam.Gremlinq.Providers.Neptune.AspNet/GremlinqSetupExtensions.cs
@@ -1,50 +1,37 @@
 ﻿using System;
 using ExRam.Gremlinq.Core.Models;
-using ExRam.Gremlinq.Providers.Core;
 using ExRam.Gremlinq.Providers.Neptune;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace ExRam.Gremlinq.Core.AspNet
 {
     public static class GremlinqSetupExtensions
     {
-        private sealed class NeptuneConfiguratorTransformation : IProviderConfiguratorTransformation<INeptuneConfigurator>
-        {
-            private readonly IProviderConfiguration _configuration;
-
-            public NeptuneConfiguratorTransformation(IProviderConfiguration configuration)
-            {
-                _configuration = configuration;
-            }
-
-            public INeptuneConfigurator Transform(INeptuneConfigurator configurator)
-            {
-                if (_configuration.GetSection("ElasticSearch") is { } elasticSearchSection)
-                {
-                    if (bool.TryParse(elasticSearchSection["Enabled"], out var isEnabled) && isEnabled && elasticSearchSection["EndPoint"] is { } endPoint)
-                    {
-                        var indexConfiguration = Enum.TryParse<NeptuneElasticSearchIndexConfiguration>(elasticSearchSection["IndexConfiguration"], true, out var outVar)
-                            ? outVar
-                            : NeptuneElasticSearchIndexConfiguration.Standard;
-
-                        configurator = configurator
-                            .UseElasticSearch(new Uri(endPoint), indexConfiguration);
-                    }
-                }
-
-                return configurator;
-            }
-        }
-
         public static GremlinqSetup UseNeptune(this GremlinqSetup setup, Action<ProviderSetup<INeptuneConfigurator>>? extraSetupAction = null)
         {
             return setup
                 .UseProvider(
                     "Neptune",
-                    (source, configuratorTransformation) => source.UseNeptune(configuratorTransformation),
-                    extraSetupAction)
-                .RegisterTypes(serviceCollection => serviceCollection
-                    .TryAddSingleton<IProviderConfiguratorTransformation<INeptuneConfigurator>, NeptuneConfiguratorTransformation>());
+                    (source, configuratorTransformation) => source
+                        .UseNeptune(configuratorTransformation),
+                    setup => setup
+                        .Configure((configurator, providerConfiguration) =>
+                        {
+                            if (providerConfiguration.GetSection("ElasticSearch") is { } elasticSearchSection)
+                            {
+                                if (bool.TryParse(elasticSearchSection["Enabled"], out var isEnabled) && isEnabled && elasticSearchSection["EndPoint"] is { } endPoint)
+                                {
+                                    var indexConfiguration = Enum.TryParse<NeptuneElasticSearchIndexConfiguration>(elasticSearchSection["IndexConfiguration"], true, out var outVar)
+                                        ? outVar
+                                        : NeptuneElasticSearchIndexConfiguration.Standard;
+
+                                    configurator = configurator
+                                        .UseElasticSearch(new Uri(endPoint), indexConfiguration);
+                                }
+                            }
+
+                            return configurator;
+                        }),
+                    extraSetupAction);
         }
 
         public static GremlinqSetup UseNeptune<TVertex, TEdge>(this GremlinqSetup setup, Action<ProviderSetup<INeptuneConfigurator>>? extraSetupAction = null)
--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.WebSocketAspNet.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.WebSocketAspNet.verified.cs
@@ -2,7 +2,7 @@ namespace ExRam.Gremlinq.Core.AspNet
 {
     public static class GremlinqSetupExtensions
     {
-        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseProvider<TConfigurator>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, string sectionName, System.Func<ExRam.Gremlinq.Core.IConfigurableGremlinQuerySource, System.Func<TConfigurator, ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation>, ExRam.Gremlinq.Core.IGremlinQuerySource> providerChoice, System.Action<ExRam.Gremlinq.Core.AspNet.ProviderSetup<TConfigurator>>? extraSetupAction)
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseProvider<TConfigurator>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, string sectionName, System.Func<ExRam.Gremlinq.Core.IConfigurableGremlinQuerySource, System.Func<TConfigurator, ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation>, ExRam.Gremlinq.Core.IGremlinQuerySource> providerChoice, System.Action<ExRam.Gremlinq.Core.AspNet.ProviderSetup<TConfigurator>> setupAction, System.Action<ExRam.Gremlinq.Core.AspNet.ProviderSetup<TConfigurator>>? extraSetupAction)
             where TConfigurator : ExRam.Gremlinq.Providers.Core.IProviderConfigurator<TConfigurator> { }
     }
     public static class WebSocketConfiguratorExtensions
