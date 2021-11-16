3a8aca50e38c181cdb6dbe6368efa075e3b15945
Daniel Weber <dw@exram.de>
2021-11-16 12:07:34 +0100

    Cleanup.

--- test/ExRam.Gremlinq.Core.AspNet.Tests/ProviderConfigurationSectionTests.cs
+++ test/ExRam.Gremlinq.Core.AspNet.Tests/ProviderConfigurationSectionTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
