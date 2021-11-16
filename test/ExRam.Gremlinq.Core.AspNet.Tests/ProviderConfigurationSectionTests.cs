578236d368f343751cce553445c728f5fd2eee4f
Daniel Weber <dw@exram.de>
2021-11-16 11:52:26 +0100

    Analogue test for IProviderConfigurationSection.

---
+++ test/ExRam.Gremlinq.Core.AspNet.Tests/ProviderConfigurationSectionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ExRam.Gremlinq.Core.AspNet.Tests
+{
+    public class ProviderConfigurationSectionTests
+    {
+        public ProviderConfigurationSectionTests() : base()
+        {
+
+        }
+
+        [Fact]
+        public void Indexer_can_be_null()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                    .AddInMemoryCollection()
+                    .Build())
+                .AddGremlinq(s => s
+                    .UseGremlinServer())
+                .BuildServiceProvider();
+
+            var section = serviceCollection
+                .GetRequiredService<IProviderConfigurationSection>();
+
+            section["Key"]
+                .Should()
+                .BeNull();
+        }
+    }
+}
