8d121c5c9081e42f6071b6f9b1d4c9941be07afc
Daniel Weber <dw@exram.de>
2021-11-17 11:28:57 +0100

    ProviderConfigurationSectionTests: Check provider specific config.

--- test/ExRam.Gremlinq.Core.AspNet.Tests/ProviderConfigurationSectionTests.cs
+++ test/ExRam.Gremlinq.Core.AspNet.Tests/ProviderConfigurationSectionTests.cs
@@ -1,5 +1,6 @@
-﻿using ExRam.Gremlinq.Providers.Core;
+﻿using System.Collections.Generic;
 
+using ExRam.Gremlinq.Providers.Core;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,31 +15,61 @@ namespace ExRam.Gremlinq.Core.AspNet.Tests
 
         }
 
+        private readonly IProviderConfigurationSection _section;
+
         public ProviderConfigurationSectionTests() : base()
-        {
-
-        }
-
-        [Fact]
-        public void Indexer_can_be_null()
         {
             var serviceCollection = new ServiceCollection()
                 .AddSingleton<IConfiguration>(new ConfigurationBuilder()
-                    .AddInMemoryCollection()
-                    .Build())
+                .AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        { "Gremlinq:Gremlinq_key_1", "value1" },
+                        { "Gremlinq:Gremlinq_key_2", "value2" },
+
+                        { "Gremlinq:Provider1:Provider1_key_1", "provider1_value1" },
+                        { "Gremlinq:Provider1:Provider1_key_2", "provider1_value2" },
+
+                        { "Gremlinq:Provider2:Provider2_key_1", "provider2_value1" },
+                        { "Gremlinq:Provider2:Provider2_key_2", "provider2_value2" }
+                   })
+                   .Build())
                 .AddGremlinq(s => s
                     .UseProvider<IMyProviderConfigurator>(
-                        "Provider",
+                        "Provider1",
                         (source, _) => source
                             .ConfigureEnvironment(_ => _),
                         setup => { },
                         default!))
                 .BuildServiceProvider();
 
-            var section = serviceCollection
+            _section = serviceCollection
                 .GetRequiredService<IProviderConfigurationSection>();
+        }
 
-            section["Key"]
+        [Fact]
+        public void Indexer_can_be_null()
+        {
+            _section["Key"]
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public void Provider_config()
+        {
+            _section["Provider1_key_1"]
+                .Should()
+                .Be("provider1_value1");
+
+            _section["Provider1_key_2"]
+                .Should()
+                .Be("provider1_value2");
+
+            _section["Provider2_key_1"]
+              .Should()
+              .BeNull();
+
+            _section["Provider2_key_2"]
                 .Should()
                 .BeNull();
         }
