8374451041f6139c545a19885e83d24eb4ddcef6
Daniel Weber <dw@exram.de>
2021-11-18 17:46:37 +0100

    Get the configuration of options out of UseProvider. Make it a part of every call to AddGremlinq.

--- src/ExRam.Gremlinq.Core.AspNet/Extensions/ServiceCollectionExtensions.cs
+++ src/ExRam.Gremlinq.Core.AspNet/Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.AspNet;
+using ExRam.Gremlinq.Core.Serialization;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
 using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -28,12 +31,48 @@ namespace Microsoft.Extensions.DependencyInjection
             }
         }
 
+        private sealed class ConfigureOptionsGremlinQuerySourceTransformation : IGremlinQuerySourceTransformation
+        {
+            private readonly IGremlinqConfigurationSection _gremlinqConfigurationSection;
+
+            public ConfigureOptionsGremlinQuerySourceTransformation(IGremlinqConfigurationSection gremlinqConfigurationSection)
+            {
+                _gremlinqConfigurationSection = gremlinqConfigurationSection;
+            }
+
+            public IGremlinQuerySource Transform(IGremlinQuerySource source)
+            {
+                return source
+                    .ConfigureEnvironment(environment => environment
+                        .ConfigureOptions(options =>
+                        {
+                            var loggingSection = _gremlinqConfigurationSection
+                                .GetSection("QueryLogging");
+
+                            if (Enum.TryParse<QueryLogVerbosity>(loggingSection["Verbosity"], out var verbosity))
+                                options = options.SetValue(GremlinqOption.QueryLogVerbosity, verbosity);
+
+                            if (Enum.TryParse<LogLevel>(loggingSection[$"{nameof(LogLevel)}"], out var logLevel))
+                                options = options.SetValue(GremlinqOption.QueryLogLogLevel, logLevel);
+
+                            if (Enum.TryParse<Formatting>(loggingSection[$"{nameof(Formatting)}"], out var formatting))
+                                options = options.SetValue(GremlinqOption.QueryLogFormatting, formatting);
+
+                            if (Enum.TryParse<GroovyFormatting>(loggingSection[$"{nameof(GroovyFormatting)}"], out var groovyFormatting))
+                                options = options.SetValue(GremlinqOption.QueryLogGroovyFormatting, groovyFormatting);
+
+                            return options;
+                        }));
+            }
+        }
+
         public static IServiceCollection AddGremlinq(this IServiceCollection serviceCollection, Action<GremlinqSetup> configuration)
         {
             serviceCollection
                 .AddSingleton(new GremlinqSetupInfo())
                 .AddSingleton<IGremlinqConfigurationSection, GremlinqConfigurationSection>()
                 .AddSingleton<IGremlinQuerySourceTransformation, UseLoggerGremlinQuerySourceTransformation>()
+                .AddSingleton<IGremlinQuerySourceTransformation, ConfigureOptionsGremlinQuerySourceTransformation>()
                 .AddSingleton(c =>
                 {
                     var ret = g
--- src/ExRam.Gremlinq.Providers.Core.AspNet/Extensions/GremlinqSetupExtensions.cs
+++ src/ExRam.Gremlinq.Providers.Core.AspNet/Extensions/GremlinqSetupExtensions.cs
@@ -2,12 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using ExRam.Gremlinq.Core.Serialization;
 using ExRam.Gremlinq.Providers.Core;
 using ExRam.Gremlinq.Providers.Core.AspNet;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace ExRam.Gremlinq.Core.AspNet
 {
@@ -16,47 +13,21 @@ namespace ExRam.Gremlinq.Core.AspNet
         private sealed class UseProviderGremlinQuerySourceTransformation<TProviderConfigurator> : IGremlinQuerySourceTransformation
             where TProviderConfigurator : IProviderConfigurator<TProviderConfigurator>
         {
-            private readonly IGremlinqConfigurationSection _generalSection;
-            private readonly IProviderConfigurationSection _providerSection;
             private readonly ProviderSetupInfo<TProviderConfigurator> _providerSetupInfo;
             private readonly IProviderConfiguratorTransformation<TProviderConfigurator>[] _transformations;
 
             public UseProviderGremlinQuerySourceTransformation(
-                IGremlinqConfigurationSection generalSection,
-                IProviderConfigurationSection providerSection,
                 IEnumerable<IProviderConfiguratorTransformation<TProviderConfigurator>> transformations,
                 ProviderSetupInfo<TProviderConfigurator> providerSetupInfo)
             {
-                _generalSection = generalSection;
-                _providerSection = providerSection;
                 _providerSetupInfo = providerSetupInfo;
                 _transformations = transformations.ToArray();
             }
 
             public IGremlinQuerySource Transform(IGremlinQuerySource source)
             {
-                var loggingSection = _generalSection
-                    .GetSection("QueryLogging");
-
                 return _providerSetupInfo.ProviderChoice(
-                    source
-                        .ConfigureEnvironment(environment => environment
-                            .ConfigureOptions(options =>
-                            {
-                                if (Enum.TryParse<QueryLogVerbosity>(loggingSection["Verbosity"], out var verbosity))
-                                    options = options.SetValue(GremlinqOption.QueryLogVerbosity, verbosity);
-
-                                if (Enum.TryParse<LogLevel>(loggingSection[$"{nameof(LogLevel)}"], out var logLevel))
-                                    options = options.SetValue(GremlinqOption.QueryLogLogLevel, logLevel);
-
-                                if (Enum.TryParse<Formatting>(loggingSection[$"{nameof(Formatting)}"], out var formatting))
-                                    options = options.SetValue(GremlinqOption.QueryLogFormatting, formatting);
-
-                                if (Enum.TryParse<GroovyFormatting>(loggingSection[$"{nameof(GroovyFormatting)}"], out var groovyFormatting))
-                                    options = options.SetValue(GremlinqOption.QueryLogGroovyFormatting, groovyFormatting);
-
-                                return options;
-                            })),
+                    source,
                     configurator =>
                     {
                         foreach (var transformation in _transformations)
