d39e5b465fe7e7ebcdab06a785240f162fdfa7bb
Daniel Weber <dw@exram.de>
2021-11-23 08:20:41 +0100

    Style.

--- src/ExRam.Gremlinq.Providers.WebSocket/Extensions/WebSocketConfiguratorExtensions.cs
+++ src/ExRam.Gremlinq.Providers.WebSocket/Extensions/WebSocketConfiguratorExtensions.cs
@@ -1,34 +1,21 @@
 ﻿using System;
 using Gremlin.Net.Driver;
-using _GremlinServer = Gremlin.Net.Driver.GremlinServer;
 
 namespace ExRam.Gremlinq.Providers.WebSocket
 {
     public static class WebSocketConfiguratorExtensions
     {
-        public static IWebSocketConfigurator At(this IWebSocketConfigurator builder, string uri)
-        {
-            return builder.At(new Uri(uri));
-        }
+        public static IWebSocketConfigurator At(this IWebSocketConfigurator builder, string uri) => builder.At(new Uri(uri));
 
-        public static IWebSocketConfigurator AtLocalhost(this IWebSocketConfigurator builder)
-        {
-            return builder.At(new Uri("ws://localhost:8182"));
-        }
+        public static IWebSocketConfigurator AtLocalhost(this IWebSocketConfigurator builder) => builder.At(new Uri("ws://localhost:8182"));
 
-        public static IWebSocketConfigurator ConfigureClient(this IWebSocketConfigurator configurator, Func<IGremlinClient, IGremlinClient> transformation)
-        {
-            return configurator
-                .ConfigureClientFactory(factory => GremlinClientFactory
-                    .Create((server, serializer, poolSettings, optionsTransformation, sessionId) => transformation(factory.Create(server, serializer, poolSettings, optionsTransformation, sessionId))));
-        }
+        public static IWebSocketConfigurator ConfigureClient(this IWebSocketConfigurator configurator, Func<IGremlinClient, IGremlinClient> transformation) => configurator
+            .ConfigureClientFactory(factory => GremlinClientFactory
+                .Create((server, serializer, poolSettings, optionsTransformation, sessionId) => transformation(factory.Create(server, serializer, poolSettings, optionsTransformation, sessionId))));
 
-        public static IWebSocketConfigurator ConfigureMessageSerializer(this IWebSocketConfigurator configurator, Func<IMessageSerializer, IMessageSerializer> transformation)
-        {
-            return configurator
-                .ConfigureClientFactory(factory => GremlinClientFactory
-                    .Create((server, maybeSerializer, poolSettings, optionsTransformation, sessionId) => factory.Create(server, maybeSerializer is { } serializer ? transformation(serializer) : maybeSerializer, poolSettings, optionsTransformation, sessionId)));
-        }
+        public static IWebSocketConfigurator ConfigureMessageSerializer(this IWebSocketConfigurator configurator, Func<IMessageSerializer, IMessageSerializer> transformation) => configurator
+            .ConfigureClientFactory(factory => GremlinClientFactory
+                .Create((server, maybeSerializer, poolSettings, optionsTransformation, sessionId) => factory.Create(server, maybeSerializer is { } serializer ? transformation(serializer) : maybeSerializer, poolSettings, optionsTransformation, sessionId)));
 
         public static IWebSocketConfigurator SetAlias(this IWebSocketConfigurator configurator, string alias) => configurator.ConfigureAlias(_ => alias);
 
