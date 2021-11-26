2f0e1d40cc15477fbecc0382bdbf72db7ef310e0
Daniel Weber <dw@exram.de>
2021-11-26 18:44:29 +0100

    Scrub some Guids.

---
+++ test/ExRam.Gremlinq.Core.Tests/Extensions/ScrubbersExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    internal static class ScrubbersExtensions
+    {
+        private static readonly Regex GuidRegex = new("[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}", RegexOptions.IgnoreCase);
+
+        public static IImmutableList<Func<string, string>> ScrubGuids(this IImmutableList<Func<string, string>> list)
+        {
+            return list
+                .Add(x => x.ScrubGuids());
+        }
+    }
+}
---
+++ test/ExRam.Gremlinq.Core.Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    internal static class StringExtensions
+    {
+        private static readonly Regex GuidRegex = new("[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}", RegexOptions.IgnoreCase);
+
+        public static string ScrubGuids(this string str)
+        {
+            return GuidRegex.Replace(str, "Scrubbed GUID");
+        }
+    }
+}
