5dc7886841ec864fa8a77b7a2711805822bd265e
Daniel Weber <dw@exram.de>
2022-01-26 14:13:43 +0100

    Add a method to ISerializedGremlinQuery to get the exact same query with a different Id. This is necessary in retry scenarios.

--- src/ExRam.Gremlinq.Core/SerializedQueries/BytecodeGremlinQuery.cs
+++ src/ExRam.Gremlinq.Core/SerializedQueries/BytecodeGremlinQuery.cs
@@ -1,14 +1,21 @@
-﻿using Gremlin.Net.Process.Traversal;
+﻿using System;
+using Gremlin.Net.Process.Traversal;
 
 namespace ExRam.Gremlinq.Core.Serialization
 {
     public sealed class BytecodeGremlinQuery : ISerializedGremlinQuery
     {
+        public BytecodeGremlinQuery(Bytecode bytecode) : this(Guid.NewGuid().ToString(), bytecode)
+        {
+        }
+
         public BytecodeGremlinQuery(string queryId, Bytecode bytecode)
         {
             Id = queryId;
             Bytecode = bytecode;
         }
+
+        public ISerializedGremlinQuery WithNewId() => new BytecodeGremlinQuery(Bytecode);
 
         public string Id { get; }
 
--- src/ExRam.Gremlinq.Core/SerializedQueries/GraphSONGremlinQuery.cs
+++ src/ExRam.Gremlinq.Core/SerializedQueries/GraphSONGremlinQuery.cs
@@ -1,7 +1,13 @@
-﻿namespace ExRam.Gremlinq.Core.Serialization
+﻿using System;
+
+namespace ExRam.Gremlinq.Core.Serialization
 {
     public sealed class GraphSONGremlinQuery : ISerializedGremlinQuery
     {
+        public GraphSONGremlinQuery(string graphSON) : this(Guid.NewGuid().ToString(), graphSON)
+        {
+        }
+
         public GraphSONGremlinQuery(string queryId, string graphSON)
         {
             Id = queryId;
@@ -11,5 +17,8 @@
         public string Id { get; }
 
         public string GraphSON { get; }
+
+        public ISerializedGremlinQuery WithNewId() => new GraphSONGremlinQuery(GraphSON);
+
     }
 }
