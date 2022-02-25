99695e2f05f863b36c4e15a87ce308e47a177c8b
Daniel Weber <dw@exram.de>
2022-02-25 14:47:51 +0100

    Add a new batch of tests, at first without the empty traversal protection.

---
+++ test/ExRam.Gremlinq.Core.Tests/Serialization/GroovyGremlinQueryWithEmptyProjectionValueProtectionSerializationTest.cs
@@ -0,0 +1,26 @@
+﻿using ExRam.Gremlinq.Core.Serialization;
+using Xunit;
+using Xunit.Abstractions;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public sealed class GroovyGremlinQueryWithEmptyProjectionValueProtectionSerializationTest : QuerySerializationTest, IClassFixture<GroovyGremlinQueryWithEmptyProjectionValueProtectionSerializationTest.Fixture>
+    {
+        public new sealed class Fixture : QuerySerializationTest.Fixture
+        {
+            public Fixture() : base(g
+                .ConfigureEnvironment(_ => _
+                    .UseSerializer(GremlinQuerySerializer.Default.ToGroovy())
+                    .ConfigureOptions(o => o/*.SetValue(GremlinqOption.EnableEmptyProjectionValueProtection, true)*/)))
+            {
+            }
+        }
+
+        public GroovyGremlinQueryWithEmptyProjectionValueProtectionSerializationTest(Fixture fixture, ITestOutputHelper testOutputHelper) : base(
+            fixture,
+            testOutputHelper)
+        {
+        }
+    }
+}
