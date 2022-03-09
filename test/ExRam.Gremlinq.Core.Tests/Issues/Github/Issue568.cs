ceb3045c672cafb2fad3e374c47df030473dec6a
Daniel Weber <dw@exram.de>
2022-03-09 18:22:14 +0100

    Add test for https://github.com/ExRam/ExRam.Gremlinq/issues/568.
    (cherry picked from commit 57c86fb51a88fc4d261dc627488872a51883a1e3)

---
+++ test/ExRam.Gremlinq.Core.Tests/Issues/Github/Issue568.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using ExRam.Gremlinq.Core.Models;
+using ExRam.Gremlinq.Core.Serialization;
+using Gremlin.Net.Structure;
+using Xunit;
+using Xunit.Abstractions;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class Issue568 : GremlinqTestBase
+    {
+        public abstract class VertexBaseAbstract
+        {
+            public object? Id { get; set; }
+            public string? PartitionKey { get; set; }
+        }
+
+        public class Thing : VertexBaseAbstract
+        {
+        }
+
+        public Issue568(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+
+        }
+
+        [Fact]
+        public async Task Repro()
+        {
+            await g
+                .ConfigureEnvironment(env => env
+                    .UseModel(GraphModel.FromBaseTypes<VertexBaseAbstract, Edge>())
+                    .EchoGroovyString())
+                .V<Thing>("id")
+                .Update(new Thing())
+                .Cast<string>()
+                .Verify();
+        }
+    }
+}
