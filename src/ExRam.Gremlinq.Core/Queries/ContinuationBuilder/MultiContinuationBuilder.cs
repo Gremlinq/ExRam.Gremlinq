7ac2e02aa4bc06dc3e6f2c8c2a6c8e3aaa2916f7
Daniel Weber <dw@exram.de>
2022-03-09 21:55:00 +0100

    ContinuationBuilder: Take sideEffectLabelProjections into outer query.

--- src/ExRam.Gremlinq.Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
+++ src/ExRam.Gremlinq.Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace ExRam.Gremlinq.Core
 {
@@ -9,9 +10,9 @@ namespace ExRam.Gremlinq.Core
     {
         private readonly TOuterQuery? _outer;
         private readonly TAnonymousQuery? _anonymous;
-        private readonly IImmutableList<Traversal>? _continuations;
+        private readonly IImmutableList<IGremlinQueryBase>? _continuations;
 
-        public MultiContinuationBuilder(TOuterQuery outer, TAnonymousQuery anonymous, IImmutableList<Traversal> continuations)
+        public MultiContinuationBuilder(TOuterQuery outer, TAnonymousQuery anonymous, IImmutableList<IGremlinQueryBase> continuations)
         {
             _outer = outer;
             _anonymous = anonymous;
@@ -21,15 +22,38 @@ namespace ExRam.Gremlinq.Core
         public MultiContinuationBuilder<TOuterQuery, TAnonymousQuery> With(Func<TAnonymousQuery, IGremlinQueryBase> continuation)
         {
             return _outer is { } outer && _anonymous is { } anonymous && _continuations is { } continuations
-                ? new(outer, anonymous, continuations.Add(continuation.Apply(anonymous).ToTraversal()))
+                ? new(outer, anonymous, continuations.Add(continuation.Apply(anonymous)))
                 : throw new InvalidOperationException();
         }
 
         public TNewQuery Build<TNewQuery, TState>(Func<FinalContinuationBuilder<TOuterQuery>, IImmutableList<Traversal>, TState, TNewQuery> builderTransformation, TState state)
         {
-            return _outer is { } outer && _continuations is { } continuations
-                ? builderTransformation(new FinalContinuationBuilder<TOuterQuery>(outer), continuations, state)
-                : throw new InvalidOperationException();
+            if (_outer is { } outer && _continuations is { } continuations)
+            {
+                if (continuations.Count > 0)
+                {
+                    outer = outer.CloneAs<TOuterQuery>(
+                        maybeSideEffectLabelProjectionsTransformation: projections =>
+                        {
+                            foreach (var continuation in continuations)
+                            {
+                                if (continuation is GremlinQueryBase queryBase)
+                                    projections = projections.SetItems(queryBase.SideEffectLabelProjections);
+                            }
+
+                            return projections;
+                        });
+                }
+
+                return builderTransformation(
+                    new FinalContinuationBuilder<TOuterQuery>(outer),
+                    continuations
+                        .Select(x => x.ToTraversal())
+                        .ToImmutableList(),
+                    state);
+            }
+
+            throw new InvalidOperationException();
         }
 
         public TOuterQuery OuterQuery => _outer ?? throw new InvalidOperationException();
