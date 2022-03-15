a0332b5d2b5478f927ab304d0556e24ccc2ec8dd
Daniel Weber <dw@exram.de>
2022-03-15 11:24:55 +0100

    Files.

---
+++ src/ExRam.Gremlinq.Core/Queries/Interfaces/Loop/IStartLoopBuilder.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable IDE0003
+// ReSharper disable ArrangeThisQualifier
+using System;
+
+namespace ExRam.Gremlinq.Core
+{
+    public interface IStartLoopBuilder<TQuery> where TQuery : IGremlinQueryBase
+    {
+        IRepeat<TQuery> Repeat(Func<TQuery, TQuery> loop);
+        IEmit<TQuery> Emit();
+        IUntil<TQuery> Until(Func<TQuery, IGremlinQueryBase> condition);
+    }
+}
