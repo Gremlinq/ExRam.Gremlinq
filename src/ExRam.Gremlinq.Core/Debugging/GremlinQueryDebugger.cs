7ffbe586573f929ce587fa3c71efd14b836b8102
Daniel Weber <dw@exram.de>
2022-04-28 10:03:46 +0200

    Introduce interfaces to make debug serialization of queries pluggable.

---
+++ src/ExRam.Gremlinq.Core/Debugging/GremlinQueryDebugger.cs
@@ -0,0 +1,56 @@
+﻿#pragma warning disable IDE0003
+// ReSharper disable ArrangeThisQualifier
+using Newtonsoft.Json;
+using ExRam.Gremlinq.Core.Serialization;
+
+namespace ExRam.Gremlinq.Core
+{
+    public static class GremlinQueryDebugger
+    {
+        private sealed class DefaultGremlinQueryDebugger : IGremlinQueryDebugger
+        {
+            public string? TryToString(ISerializedGremlinQuery serializedQuery)
+            {
+                var indented = true;
+                var groovyFormatting = GroovyFormatting.WithBindings;
+
+                var maybeGroovy = serializedQuery
+                    .TryToGroovy(groovyFormatting);
+
+                if (maybeGroovy is { } groovy)
+                {
+                    return JsonConvert.SerializeObject(
+                        groovy.Bindings.Count > 0
+                            ? new { groovy.Script, groovy.Bindings }
+                            : new { groovy.Script },
+                        indented
+                            ? Formatting.Indented
+                            : Formatting.None);
+                }
+
+                return default;
+            }
+        }
+
+        private sealed class OverrideGremlinQueryDebugger : IGremlinQueryDebugger
+        {
+            private readonly IGremlinQueryDebugger _baseDebugger;
+            private readonly IGremlinQueryDebugger _overridingDebugger;
+
+            public OverrideGremlinQueryDebugger(IGremlinQueryDebugger baseDebugger, IGremlinQueryDebugger overridingDebugger)
+            {
+                _baseDebugger = baseDebugger;
+                _overridingDebugger = overridingDebugger;
+            }
+
+            public string? TryToString(ISerializedGremlinQuery serializedQuery)
+            {
+                return _baseDebugger.TryToString(serializedQuery) ?? _overridingDebugger.TryToString(serializedQuery);
+            }
+        }
+
+        public static readonly IGremlinQueryDebugger Default = new DefaultGremlinQueryDebugger();
+
+        public static IGremlinQueryDebugger Override(this IGremlinQueryDebugger debugger, IGremlinQueryDebugger overridingDebugger) => new OverrideGremlinQueryDebugger(debugger, overridingDebugger);
+    }
+}
---
+++ src/ExRam.Gremlinq.Core/Debugging/IGremlinQueryDebugger.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable IDE0003
+// ReSharper disable ArrangeThisQualifier
+
+using ExRam.Gremlinq.Core.Serialization;
+
+namespace ExRam.Gremlinq.Core
+{
+    public interface IGremlinQueryDebugger
+    {
+        string? TryToString(ISerializedGremlinQuery serializedQuery);
+    }
+}
