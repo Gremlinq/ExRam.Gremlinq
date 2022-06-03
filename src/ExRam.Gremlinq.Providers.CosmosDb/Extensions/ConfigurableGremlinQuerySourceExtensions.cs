2008f03d87f5f88949fddb9f992442533b58594f
Daniel Weber <dw@exram.de>
2022-06-03 17:28:34 +0200

    Use vertex/edge/vertexProperty projections on CosmosDb.

--- src/ExRam.Gremlinq.Providers.CosmosDb/Extensions/ConfigurableGremlinQuerySourceExtensions.cs
+++ src/ExRam.Gremlinq.Providers.CosmosDb/Extensions/ConfigurableGremlinQuerySourceExtensions.cs
@@ -85,10 +85,6 @@ namespace ExRam.Gremlinq.Core
                         .ConfigureVertexPropertyFeatures(_ => VertexPropertyFeatures.StringIds | VertexPropertyFeatures.UserSuppliedIds | VertexPropertyFeatures.RemoveProperty | VertexPropertyFeatures.BooleanValues | VertexPropertyFeatures.ByteValues | VertexPropertyFeatures.DoubleValues | VertexPropertyFeatures.FloatValues | VertexPropertyFeatures.IntegerValues | VertexPropertyFeatures.LongValues | VertexPropertyFeatures.StringValues)
                         .ConfigureEdgeFeatures(_ => EdgeFeatures.AddEdges | EdgeFeatures.RemoveEdges | EdgeFeatures.StringIds | EdgeFeatures.UserSuppliedIds | EdgeFeatures.AddProperty | EdgeFeatures.RemoveProperty)
                         .ConfigureEdgePropertyFeatures(_ => EdgePropertyFeatures.Properties | EdgePropertyFeatures.BooleanValues | EdgePropertyFeatures.ByteValues | EdgePropertyFeatures.DoubleValues | EdgePropertyFeatures.FloatValues | EdgePropertyFeatures.IntegerValues | EdgePropertyFeatures.LongValues | EdgePropertyFeatures.StringValues))
-                    .ConfigureOptions(options => options
-                        .SetValue(GremlinqOption.VertexProjectionSteps, Traversal.Empty)
-                        .SetValue(GremlinqOption.EdgeProjectionSteps, Traversal.Empty)
-                        .SetValue(GremlinqOption.VertexPropertyProjectionSteps, Traversal.Empty))
                     .ConfigureSerializer(serializer => serializer
                         .ConfigureFragmentSerializer(fragmentSerializer => fragmentSerializer
                             .Override<byte[]>((bytes, env, overridden, recurse) => recurse.Serialize(Convert.ToBase64String(bytes), env))
