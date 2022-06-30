e1abd7b7e621ee53a114ec4045c38465d5d1cdb0
Daniel Weber <dw@exram.de>
2022-06-30 12:23:36 +0200

    Style.

--- src/ExRam.Gremlinq.Core/Options/GremlinqOptions.cs
+++ src/ExRam.Gremlinq.Core/Options/GremlinqOptions.cs
@@ -14,41 +14,26 @@ namespace ExRam.Gremlinq.Core
                 _dictionary = dictionary;
             }
 
-            public TValue GetValue<TValue>(GremlinqOption<TValue> option)
-            {
-                return _dictionary
-                    .Fast()
-                    .TryGetValue(option, out var value)
-                        ? (TValue)value!
-                        : option.DefaultValue;
-            }
+            public TValue GetValue<TValue>(GremlinqOption<TValue> option) => _dictionary
+                .Fast()
+                .TryGetValue(option, out var value)
+                    ? (TValue)value!
+                    : option.DefaultValue;
 
-            public bool Contains(IGremlinqOption option)
-            {
-                return _dictionary
-                    .Fast()
-                    .ContainsKey(option);
-            }
+            public bool Contains(IGremlinqOption option) => _dictionary
+                .Fast()
+                .ContainsKey(option);
 
-            public IGremlinqOptions ConfigureValue<TValue>(GremlinqOption<TValue> option, Func<TValue, TValue> configuration)
-            {
-                return new GremlinqOptionsImpl(_dictionary
-                    .SetItem(
-                        option,
-                        configuration(_dictionary.TryGetValue(option, out var value)
-                            ? (TValue)value
-                            : option.DefaultValue)!));
-            }
+            public IGremlinqOptions ConfigureValue<TValue>(GremlinqOption<TValue> option, Func<TValue, TValue> configuration) => new GremlinqOptionsImpl(_dictionary
+                .SetItem(
+                    option,
+                    configuration(_dictionary.TryGetValue(option, out var value)
+                        ? (TValue)value
+                        : option.DefaultValue)!));
 
-            public IGremlinqOptions SetValue<TValue>(GremlinqOption<TValue> option, TValue value)
-            {
-                return new GremlinqOptionsImpl(_dictionary.SetItem(option, value!));
-            }
+            public IGremlinqOptions SetValue<TValue>(GremlinqOption<TValue> option, TValue value) => new GremlinqOptionsImpl(_dictionary.SetItem(option, value!));
 
-            public IGremlinqOptions Remove(IGremlinqOption option)
-            {
-                return new GremlinqOptionsImpl(_dictionary.Remove(option));
-            }
+            public IGremlinqOptions Remove(IGremlinqOption option) => new GremlinqOptionsImpl(_dictionary.Remove(option));
         }
 
         public static readonly IGremlinqOptions Empty = new GremlinqOptionsImpl(ImmutableDictionary<IGremlinqOption, object>.Empty);
