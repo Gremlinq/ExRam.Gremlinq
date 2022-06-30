a78600251a84da9a032d0680c8ce20a6799fef9f
Daniel Weber <dw@exram.de>
2022-06-30 10:02:05 +0200

    Introduce a LabelProjections struct to bundle StepLabel-mappings.

---
+++ src/ExRam.Gremlinq.Core/Queries/LabelProjections.cs
@@ -0,0 +1,16 @@
+﻿using ExRam.Gremlinq.Core.Projections;
+
+namespace ExRam.Gremlinq.Core
+{
+    public readonly struct LabelProjections
+    {
+        public LabelProjections(Projection? stepLabelProjection, Projection? sideEffectLabelProjection)
+        {
+            StepLabelProjection = stepLabelProjection;
+            SideEffectLabelProjection = sideEffectLabelProjection;
+        }
+
+        public Projection? StepLabelProjection { get; }
+        public Projection? SideEffectLabelProjection { get; }
+    }
+}
