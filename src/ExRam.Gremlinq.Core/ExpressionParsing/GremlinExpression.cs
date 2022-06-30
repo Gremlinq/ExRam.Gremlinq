a24b1af29d55826164059535c24242911626784e
Daniel Weber <dw@exram.de>
2022-06-30 14:52:38 +0200

    Move a file.

--- src/ExRam.Gremlinq.Core/ExpressionParsing/GremlinExpression.cs
+++ src/ExRam.Gremlinq.Core/ExpressionParsing/GremlinExpression.cs
@@ -1,6 +1,4 @@
-﻿using ExRam.Gremlinq.Core.ExpressionParsing;
-
-namespace ExRam.Gremlinq.Core
+﻿namespace ExRam.Gremlinq.Core.ExpressionParsing
 {
     internal readonly struct GremlinExpression : IEquatable<GremlinExpression>
     {
