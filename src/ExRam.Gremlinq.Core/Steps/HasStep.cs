fff8b51b23410600c6a99bdef9fc56c2f01b641f
Daniel Weber <dw@exram.de>
2022-07-05 10:47:08 +0200

    Add HasStep.

---
+++ src/ExRam.Gremlinq.Core/Steps/HasStep.cs
@@ -0,0 +1,12 @@
+﻿namespace ExRam.Gremlinq.Core.Steps
+{
+    public sealed class HasStep : Step, IIsOptimizableInWhere
+    {
+        public HasStep(Key key)
+        {
+            Key = key;
+        }
+
+        public Key Key { get; }
+    }
+}
