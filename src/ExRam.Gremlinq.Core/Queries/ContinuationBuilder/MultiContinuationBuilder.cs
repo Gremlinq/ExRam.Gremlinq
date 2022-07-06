ca64e08127025be7e1e8551fc217f69166e11584
Daniel Weber <dw@exram.de>
2022-07-06 14:07:16 +0200

    DRY in MultiContinuationBuilder.

--- src/ExRam.Gremlinq.Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
+++ src/ExRam.Gremlinq.Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
@@ -20,49 +20,57 @@ namespace ExRam.Gremlinq.Core
         public MultiContinuationBuilder<TOuterQuery, TAnonymousQuery> With<TProjectedQuery, TState>(Func<TAnonymousQuery, TState, TProjectedQuery> continuation, TState state)
             where TProjectedQuery : IGremlinQueryBase
         {
-            return _outer is { } outer && _anonymous is { } anonymous && _continuations is { } continuations
-                ? new(outer, anonymous, continuations.Add(continuation.Apply(anonymous, state)))
-                : throw new InvalidOperationException();
+            return With(
+                static (outer, anonymous, continuations, state) => new MultiContinuationBuilder<TOuterQuery, TAnonymousQuery>(outer, anonymous, continuations.Add(state.continuation.Apply(anonymous, state.state))),
+                (continuation, state));
         }
 
         public TNewQuery Build<TNewQuery, TState>(Func<FinalContinuationBuilder<TOuterQuery>, Traversal[], TState, TNewQuery> builderTransformation, TState state)
         {
-            if (_outer is { } outer && _continuations is { } continuations)
-            {
-                if (outer.Flags.HasFlag(QueryFlags.IsMuted))
-                    return outer.CloneAs<TNewQuery>();
-
-                var builder = new FinalContinuationBuilder<TOuterQuery>(outer);
-                var traversalArray = continuations.Count > 0
-                    ? new Traversal[continuations.Count]
-                    : Array.Empty<Traversal>();
-
-                if (continuations.Count > 0)
+            return With(
+                static (outer, anonymous, continuations, state) =>
                 {
-                    var targetIndex = 0;
+                    var (builderTransformation, innerState) = state;
 
-                    foreach (var continuation in continuations)
+                    if (outer.Flags.HasFlag(QueryFlags.IsMuted))
+                        return outer.CloneAs<TNewQuery>();
+
+                    var builder = new FinalContinuationBuilder<TOuterQuery>(outer);
+                    var traversalArray = continuations.Count > 0
+                        ? new Traversal[continuations.Count]
+                        : Array.Empty<Traversal>();
+
+                    if (continuations.Count > 0)
                     {
-                        if (continuation is GremlinQueryBase queryBase)
+                        var targetIndex = 0;
+
+                        foreach (var continuation in continuations)
                         {
-                            builder = builder.WithNewLabelProjections(
-                                static (existingProjections, additionalProjections) => existingProjections.MergeSideEffectLabelProjections(additionalProjections),
-                                queryBase.LabelProjections);
+                            if (continuation is GremlinQueryBase queryBase)
+                            {
+                                builder = builder.WithNewLabelProjections(
+                                    static (existingProjections, additionalProjections) => existingProjections.MergeSideEffectLabelProjections(additionalProjections),
+                                    queryBase.LabelProjections);
+                            }
+
+                            traversalArray[targetIndex++] = continuation.ToTraversal();
                         }
-
-                        traversalArray[targetIndex++] = continuation.ToTraversal();
                     }
-                }
 
-                return builderTransformation(
-                    builder,
-                    traversalArray,
-                    state);
-            }
-
-            throw new InvalidOperationException();
+                    return builderTransformation(
+                        builder,
+                        traversalArray,
+                        innerState);
+                },
+                (builderTransformation, state));
         }
 
-        public TOuterQuery OuterQuery => _outer ?? throw new InvalidOperationException();
+        private TResult With<TState, TResult>(Func<TOuterQuery, TAnonymousQuery, IImmutableList<IGremlinQueryBase>, TState, TResult> continuation, TState state) => _outer is { } outer && _anonymous is { } anonymous && _continuations is { } continuations
+            ? continuation(outer, anonymous, continuations, state)
+            : throw new InvalidOperationException();
+
+        public TOuterQuery OuterQuery => With(
+            static (outer, _, _, _) => outer,
+            0);
     }
 }
