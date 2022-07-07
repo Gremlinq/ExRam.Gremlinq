13f162cd8201c0d776b356c656223cfc530e8f49
Daniel Weber <dw@exram.de>
2022-07-07 21:34:51 +0200

    Style.

--- src/ExRam.Gremlinq.Core/Extensions/GremlinQueryExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/GremlinQueryExtensions.cs
@@ -4,44 +4,42 @@ namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryExtensions
     {
-        public static ValueTask<TElement[]> ToArrayAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default)
-        {
-            return query.ToAsyncEnumerable().ToArrayAsync(ct);
-        }
+        public static ValueTask<TElement[]> ToArrayAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
+            .ToAsyncEnumerable()
+            .ToArrayAsync(ct);
 
-        public static ValueTask<TElement> FirstAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default)
-        {
-            return query.Cast<TElement>().Limit(1).ToAsyncEnumerable().FirstAsync(ct);
-        }
+        public static ValueTask<TElement> FirstAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
+            .Cast<TElement>()
+            .Limit(1)
+            .ToAsyncEnumerable()
+            .FirstAsync(ct);
 
-        public static async ValueTask<TElement?> FirstOrDefaultAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default)
-        {
-            return await query.Cast<TElement>().Limit(1).ToAsyncEnumerable().FirstOrDefaultAsync(ct);
-        }
+        public static ValueTask<TElement?> FirstOrDefaultAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
+            .Cast<TElement>()
+            .Limit(1)
+            .ToAsyncEnumerable()
+            .FirstOrDefaultAsync(ct);
 
-        public static ValueTask<TElement> SingleAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default)
-        {
-            return query.Cast<TElement>().Limit(1).ToAsyncEnumerable().SingleAsync(ct);
-        }
+        public static ValueTask<TElement> SingleAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
+            .Cast<TElement>()
+            .Limit(1)
+            .ToAsyncEnumerable()
+            .SingleAsync(ct);
 
-        public static async ValueTask<TElement?> SingleOrDefaultAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default)
-        {
-            return await query.Cast<TElement>().Limit(1).ToAsyncEnumerable().SingleOrDefaultAsync(ct);
-        }
+        public static ValueTask<TElement?> SingleOrDefaultAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
+            .Cast<TElement>()
+            .Limit(1)
+            .ToAsyncEnumerable()
+            .SingleOrDefaultAsync(ct);
 
-        internal static Traversal ToTraversal(this IGremlinQueryBase query)
-        {
-            return query.AsAdmin().Steps;
-        }
+        internal static Traversal ToTraversal(this IGremlinQueryBase query) => query
+            .AsAdmin().Steps;
 
-        internal static bool IsNone(this IGremlinQueryBase query)
-        {
-            return query.AsAdmin().Steps.PeekOrDefault() is NoneStep;
-        }
+        internal static bool IsNone(this IGremlinQueryBase query) => query
+            .AsAdmin().Steps
+            .PeekOrDefault() is NoneStep;
 
-        internal static bool IsIdentity(this IGremlinQueryBase query)
-        {
-            return query.AsAdmin().Steps.Steps.IsEmpty;
-        }
-     }
+        internal static bool IsIdentity(this IGremlinQueryBase query) => query
+            .AsAdmin().Steps.Steps.IsEmpty;
+    }
 }
