27252fc802cc79be089d02b6e8554ceffbfc11d4
Daniel Weber <dw@exram.de>
2022-07-07 21:58:04 +0200

    Files.

---
+++ src/ExRam.Gremlinq.Core/Queries/ContinuationBuilder/FinalContinuationBuilderTransformation.cs
@@ -0,0 +1,8 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    internal delegate TNewQuery FinalContinuationBuilderTransformation<TOuterQuery, TNewQuery, TState>(FinalContinuationBuilder<TOuterQuery> builder, Span<Traversal> traversals, TState state)
+         where TOuterQuery : GremlinQueryBase, IGremlinQueryBase;
+
+    internal delegate TNewQuery FinalContinuationBuilderTransformation<TOuterQuery, TNewQuery>(FinalContinuationBuilder<TOuterQuery> builder, Span<Traversal> traversals)
+        where TOuterQuery : GremlinQueryBase, IGremlinQueryBase;
+}
--- src/ExRam.Gremlinq.Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
+++ src/ExRam.Gremlinq.Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
@@ -2,12 +2,6 @@
 
 namespace ExRam.Gremlinq.Core
 {
-    internal delegate TNewQuery FinalContinuationBuilderTransformation<TOuterQuery, TNewQuery, TState>(FinalContinuationBuilder<TOuterQuery> builder, Span<Traversal> traversals, TState state)
-         where TOuterQuery : GremlinQueryBase, IGremlinQueryBase;
-
-    internal delegate TNewQuery FinalContinuationBuilderTransformation<TOuterQuery, TNewQuery>(FinalContinuationBuilder<TOuterQuery> builder, Span<Traversal> traversals)
-        where TOuterQuery : GremlinQueryBase, IGremlinQueryBase;
-
     internal readonly struct MultiContinuationBuilder<TOuterQuery, TAnonymousQuery>
         where TOuterQuery : GremlinQueryBase, IGremlinQueryBase
         where TAnonymousQuery : GremlinQueryBase, IGremlinQueryBase
