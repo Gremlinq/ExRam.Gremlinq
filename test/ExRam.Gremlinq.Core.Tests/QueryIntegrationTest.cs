db3ff1cb1d209a307c76751a0208e612a60d79e3
Daniel Weber <dw@exram.de>
2022-07-11 11:27:58 +0200

    Don't scrub numeric Ids to a string value.

--- test/ExRam.Gremlinq.Core.Tests/QueryIntegrationTest.cs
+++ test/ExRam.Gremlinq.Core.Tests/QueryIntegrationTest.cs
@@ -16,6 +16,7 @@ namespace ExRam.Gremlinq.Core.Tests
             }
         }
 
+        private static readonly Regex GuidRegex = new("(\"id\"\\s*[:,]\\s*{\\s*\"@type\"\\s*:\\s*\"g:(UUID)\"\\s*,\\s*\"@value\":\\s*)([^\\s{}]+)(\\s*})", RegexOptions.IgnoreCase);
         private static readonly Regex IdRegex = new ("(\"id\"\\s*[:,]\\s*{\\s*\"@type\"\\s*:\\s*\"g:(Int32|Int64|UUID)\"\\s*,\\s*\"@value\":\\s*)([^\\s{}]+)(\\s*})", RegexOptions.IgnoreCase);
 
         protected QueryIntegrationTest(Fixture fixture, ITestOutputHelper testOutputHelper, [CallerFilePath] string callerFilePath = "") : base(
@@ -28,7 +29,8 @@ namespace ExRam.Gremlinq.Core.Tests
         public override IImmutableList<Func<string, string>> Scrubbers()
         {
             return base.Scrubbers()
-                .Add(x => IdRegex.Replace(x, "$1\"scrubbed id\"$4"));
+                .Add(x => IdRegex.Replace(x, "$1-1$4"))
+                .Add(x => GuidRegex.Replace(x, "$1\"scrubbed uuid\"$4"));
         }
     }
 }
