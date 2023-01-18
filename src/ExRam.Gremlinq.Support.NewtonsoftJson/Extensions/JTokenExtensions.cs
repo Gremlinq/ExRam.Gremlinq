2fc5199557ac4820c298654a63678020c1d683c4
Daniel Weber <dw@exram.de>
2023-01-18 12:25:45 +0100

    Factorize Traverser expansion.

--- src/ExRam.Gremlinq.Support.NewtonsoftJson/Extensions/JTokenExtensions.cs
+++ src/ExRam.Gremlinq.Support.NewtonsoftJson/Extensions/JTokenExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Newtonsoft.Json.Linq
+﻿using ExRam.Gremlinq.Core.Deserialization;
+using ExRam.Gremlinq.Core;
+
+namespace Newtonsoft.Json.Linq
 {
     internal static class JTokenExtensions
     {
@@ -15,6 +18,36 @@
 
                     return retObject;
                 }
+
+            return null;
+        }
+
+        public static IEnumerable<object>? TryExpandTraverser(this JObject jObject, Type type, IGremlinQueryEnvironment env, IGremlinQueryFragmentDeserializer recurse)
+        {
+            //Traversers
+            if (jObject.TryGetValue("@type", out var nestedType) && "g:Traverser".Equals(nestedType.Value<string>(), StringComparison.OrdinalIgnoreCase) && jObject.TryGetValue("@value", out var valueToken) && valueToken is JObject nestedTraverserObject)
+            {
+                var bulk = 1;
+
+                if (nestedTraverserObject.TryGetValue("bulk", out var bulkToken) && recurse.TryDeserialize(bulkToken, typeof(int), env) is int bulkObject)
+                    bulk = bulkObject;
+
+                if (nestedTraverserObject.TryGetValue("value", out var traverserValue))
+                {
+                    return Core();
+
+                    IEnumerable<object> Core()
+                    {
+                        if (recurse.TryDeserialize(traverserValue, type, env) is { } item)
+                        {
+                            for (var j = 0; j < bulk; j++)
+                                yield return item;
+                        }
+
+                        yield break;
+                    }
+                }
+            }
 
             return null;
         }
