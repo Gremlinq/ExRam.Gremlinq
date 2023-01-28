a0693e79a9f8fa030e0c0faf8b874972d12ebf81
Daniel Weber <danielcweber@users.noreply.github.com>
2023-01-28 16:47:34 +0100

    Rename fields and parameters, some style work.

--- src/ExRam.Gremlinq.Core/Deserialization/IGremlinQueryFragmentDeserializer.cs
+++ src/ExRam.Gremlinq.Core/Deserialization/IGremlinQueryFragmentDeserializer.cs
@@ -4,7 +4,7 @@ namespace ExRam.Gremlinq.Core.Deserialization
 {
     public interface IGremlinQueryFragmentDeserializer
     {
-        bool TryDeserialize<TSerializedData>(TSerializedData serializedData, Type fragmentType, IGremlinQueryEnvironment environment, [NotNullWhen(true)] out object? value);
+        bool TryDeserialize<TSerializedData>(TSerializedData serializedData, Type requestedType, IGremlinQueryEnvironment environment, [NotNullWhen(true)] out object? value);
 
         IGremlinQueryFragmentDeserializer Override<TSerialized>(GremlinQueryFragmentDeserializerDelegate<TSerialized> deserializer);
     }
