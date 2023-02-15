eafed1a6041fcabb6f1bce7fe3f533409564909f
Daniel Weber <danielcweber@users.noreply.github.com>
2023-02-15 10:35:54 +0100

    Override for well known requested class types with a dedicated factory to get rid of boxing.

--- src/ExRam.Gremlinq.Core/Extensions/GremlinQueryFragmentDeserializerClassExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/GremlinQueryFragmentDeserializerClassExtensions.cs
@@ -1,4 +1,6 @@
-﻿using ExRam.Gremlinq.Core.Deserialization;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using ExRam.Gremlinq.Core.Deserialization;
 
 namespace ExRam.Gremlinq.Core
 {
@@ -19,6 +21,48 @@ namespace ExRam.Gremlinq.Core
                 : default;
         }
 
+        private sealed class FixedTypeDeserializationTransformationFactory<TStaticSerialized, TStaticRequested> : IDeserializationTransformationFactory
+           where TStaticRequested : class
+        {
+            private sealed class FixedTypeDeserializationTransformation<TSerialized> : IDeserializationTransformation<TSerialized, TStaticRequested>
+            {
+                private readonly Func<TStaticSerialized, IGremlinQueryEnvironment, IGremlinQueryFragmentDeserializer, TStaticRequested?> _func;
+
+                public FixedTypeDeserializationTransformation(Func<TStaticSerialized, IGremlinQueryEnvironment, IGremlinQueryFragmentDeserializer, TStaticRequested?> func)
+                {
+                    _func = func;
+                }
+
+                public bool Transform(TSerialized serialized, IGremlinQueryEnvironment environment, IGremlinQueryFragmentDeserializer recurse, [NotNullWhen(true)] out TStaticRequested? value)
+                {
+                    if (serialized is TStaticSerialized staticSerialized && _func(staticSerialized, environment, recurse) is { } requested)
+                    {
+                        value = requested;
+
+                        return true;
+                    }
+
+                    value = default;
+
+                    return false;
+                }
+            }
+
+            private readonly Func<TStaticSerialized, IGremlinQueryEnvironment, IGremlinQueryFragmentDeserializer, TStaticRequested?> _func;
+
+            public FixedTypeDeserializationTransformationFactory(Func<TStaticSerialized, IGremlinQueryEnvironment, IGremlinQueryFragmentDeserializer, TStaticRequested?> func)
+            {
+                _func = func;
+            }
+
+            public IDeserializationTransformation<TSerialized, TRequested>? TryCreate<TSerialized, TRequested>()
+            {
+                return ((typeof(TSerialized).IsAssignableFrom(typeof(TStaticSerialized)) || typeof(TStaticSerialized).IsAssignableFrom(typeof(TSerialized))) && (typeof(TRequested) == typeof(TStaticRequested)))
+                    ? (IDeserializationTransformation<TSerialized, TRequested>)(object)new FixedTypeDeserializationTransformation<TSerialized>(_func)
+                    : null;
+            }
+        }
+
         public static FluentForClass<TRequested> TryDeserialize<TRequested>(this IGremlinQueryFragmentDeserializer deserializer)
             where TRequested : class
         {
@@ -29,9 +73,7 @@ namespace ExRam.Gremlinq.Core
             where TRequested : class
         {
             return fragmentDeserializer
-                .Override<TSerialized>((token, type, env, recurse) => type == typeof(TRequested)
-                    ? func(token, env, recurse)
-                    : default(object?));
+                .Override(new FixedTypeDeserializationTransformationFactory<TSerialized, TRequested>(func));
         }
     }
 }
