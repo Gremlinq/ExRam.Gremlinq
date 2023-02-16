2a4bc6809dbc7ecc79e707872d6cae4eefe6e36d
Daniel Weber <danielcweber@users.noreply.github.com>
2023-02-16 15:13:58 +0100

    Unmap with a factory.

--- src/ExRam.Gremlinq.Support.NewtonsoftJson/Extensions/JTokenExtensions.cs
+++ src/ExRam.Gremlinq.Support.NewtonsoftJson/Extensions/JTokenExtensions.cs
@@ -5,23 +5,6 @@ namespace Newtonsoft.Json.Linq
 {
     internal static class JTokenExtensions
     {
-        public static JObject? TryUnmap(this JObject jObject)
-        {
-            if (jObject.TryGetValue("@type", out var nestedType) && "g:Map".Equals(nestedType.Value<string>(), StringComparison.OrdinalIgnoreCase))
-                if (jObject.TryGetValue("@value", out var valueToken) && valueToken is JArray mapArray)
-                {
-                    var retObject = new JObject();
-
-                    for (var i = 0; i < mapArray.Count / 2; i++)
-                        if (mapArray[i * 2] is JValue { Type: JTokenType.String } key)
-                            retObject.Add(key.Value<string>()!, mapArray[i * 2 + 1]);
-
-                    return retObject;
-                }
-
-            return null;
-        }
-
         public static IEnumerable<object>? TryExpandTraverser(this JObject jObject, Type type, IGremlinQueryEnvironment env, IGremlinQueryFragmentDeserializer recurse)
         {
             //Traversers
