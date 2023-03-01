b6c21b5e88298d0a2b6e509d58d573dcac88b665
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-01 16:41:05 +0100

    Factor out ConverterFactories.

---
+++ src/ExRam.Gremlinq.Core/Transformation/IdentityConverterFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ExRam.Gremlinq.Core.Deserialization;
+
+namespace ExRam.Gremlinq.Core.Transformation
+{
+    internal sealed class IdentityConverterFactory : IConverterFactory
+    {
+        private sealed class IdentityConverter<TSerialized, TRequested> : IConverter<TSerialized, TRequested>
+        {
+            public bool TryConvert(TSerialized serialized, IGremlinQueryEnvironment environment, IDeserializer recurse, [NotNullWhen(true)] out TRequested? value)
+            {
+                if (serialized is TRequested requested)
+                {
+                    value = requested;
+                    return true;
+                }
+
+                value = default;
+                return false;
+            }
+        }
+
+        public IConverter<TSerialized, TRequested> TryCreate<TSerialized, TRequested>() => new IdentityConverter<TSerialized, TRequested>();
+    }
+}
---
+++ src/ExRam.Gremlinq.Core/Transformation/SingleItemArrayFallbackConverterFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ExRam.Gremlinq.Core.Deserialization;
+
+namespace ExRam.Gremlinq.Core.Transformation
+{
+    internal sealed class SingleItemArrayFallbackConverterFactory : IConverterFactory
+    {
+        private sealed class SingleItemArrayFallbackConverter<TSerialized, TRequestedArray, TRequestedArrayItem> : IConverter<TSerialized, TRequestedArray>
+        {
+            public bool TryConvert(TSerialized serialized, IGremlinQueryEnvironment environment, IDeserializer recurse, [NotNullWhen(true)] out TRequestedArray? value)
+            {
+                if (recurse.TryDeserialize<TSerialized, TRequestedArrayItem>(serialized, environment, out var typedValue))
+                {
+                    value = (TRequestedArray)(object)new[] { typedValue };
+                    return true;
+                }
+
+                value = default;
+                return false;
+            }
+        }
+
+        public IConverter<TSerialized, TRequested>? TryCreate<TSerialized, TRequested>()
+        {
+            return typeof(TRequested).IsArray
+                ? (IConverter<TSerialized, TRequested>?)Activator.CreateInstance(typeof(SingleItemArrayFallbackConverter<,,>).MakeGenericType(typeof(TSerialized), typeof(TRequested), typeof(TRequested).GetElementType()!))
+                : default;
+        }
+    }
+}
