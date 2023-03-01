9746a8684df54278d251eb6db2bdfe8bc07767d1
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-01 18:14:16 +0100

    Allow null returns in serializer delegates to signal a deferal to an overridden serializer. Throw in GremlinQuerySerializer if no value is present.

--- src/ExRam.Gremlinq.Core/Serialization/GremlinQueryFragmentSerializerDelegate.cs
+++ src/ExRam.Gremlinq.Core/Serialization/GremlinQueryFragmentSerializerDelegate.cs
@@ -1,6 +1,6 @@
 ﻿namespace ExRam.Gremlinq.Core.Serialization
 {
-    public delegate object BaseGremlinQueryFragmentSerializerDelegate<in TFragment>(TFragment fragment, IGremlinQueryEnvironment environment, IGremlinQuerySerializer recurse);
+    public delegate object? BaseGremlinQueryFragmentSerializerDelegate<in TFragment>(TFragment fragment, IGremlinQueryEnvironment environment, IGremlinQuerySerializer recurse);
 
-    public delegate object GremlinQueryFragmentSerializerDelegate<TFragment>(TFragment fragment, IGremlinQueryEnvironment environment, BaseGremlinQueryFragmentSerializerDelegate<TFragment> overridden, IGremlinQuerySerializer recurse);
+    public delegate object? GremlinQueryFragmentSerializerDelegate<TFragment>(TFragment fragment, IGremlinQueryEnvironment environment, BaseGremlinQueryFragmentSerializerDelegate<TFragment> overridden, IGremlinQuerySerializer recurse);
 }
