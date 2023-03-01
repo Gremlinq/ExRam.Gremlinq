b0c54fd965b5a60bae8bc2425bd4ea701beb69f9
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-01 22:33:02 +0100

    Remove GremlinQueryFragmentSerializerDelegate.

--- src/ExRam.Gremlinq.Core/Serialization/GremlinQueryFragmentSerializerDelegate.cs
+++
@@ -1,6 +0,0 @@
-﻿using ExRam.Gremlinq.Core.Transformation;
-
-namespace ExRam.Gremlinq.Core.Serialization
-{
-    public delegate object? GremlinQueryFragmentSerializerDelegate<in TFragment>(TFragment fragment, IGremlinQueryEnvironment environment, ITransformer recurse);
-}
