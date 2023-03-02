306b4bc9765e20cbc35b448f48a084f3d8edc71a
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-02 14:57:26 +0100

    Move Transformer-extensions back to the main project.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.Core.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.Core.verified.cs
@@ -1089,6 +1089,32 @@ namespace ExRam.Gremlinq.Core
         Strict = 0,
         Lenient = 1,
     }
+    public static class TransformerClassExtensions
+    {
+        public static ExRam.Gremlinq.Core.Transformation.ITransformer Override<TSource, TTarget>(this ExRam.Gremlinq.Core.Transformation.ITransformer transformer, System.Func<TSource, ExRam.Gremlinq.Core.IGremlinQueryEnvironment, ExRam.Gremlinq.Core.Transformation.ITransformer, TTarget?> func)
+            where TTarget :  class { }
+        public static ExRam.Gremlinq.Core.TransformerClassExtensions.FluentForClass<TTarget> TryTransformTo<TTarget>(this ExRam.Gremlinq.Core.Transformation.ITransformer transformer)
+            where TTarget :  class { }
+        public readonly struct FluentForClass<TTarget>
+            where TTarget :  class
+        {
+            public FluentForClass(ExRam.Gremlinq.Core.Transformation.ITransformer transformer) { }
+            public TTarget? From<TSource>(TSource source, ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment) { }
+        }
+    }
+    public static class TransformerStructExtensions
+    {
+        public static ExRam.Gremlinq.Core.Transformation.ITransformer Override<TSource, TTarget>(this ExRam.Gremlinq.Core.Transformation.ITransformer transformer, System.Func<TSource, ExRam.Gremlinq.Core.IGremlinQueryEnvironment, ExRam.Gremlinq.Core.Transformation.ITransformer, TTarget?> func)
+            where TTarget :  struct { }
+        public static ExRam.Gremlinq.Core.TransformerStructExtensions.FluentForStruct<TTarget> TryTransformTo<TTarget>(this ExRam.Gremlinq.Core.Transformation.ITransformer transformer)
+            where TTarget :  struct { }
+        public readonly struct FluentForStruct<TTarget>
+            where TTarget :  struct
+        {
+            public FluentForStruct(ExRam.Gremlinq.Core.Transformation.ITransformer transformer) { }
+            public TTarget? From<TSource>(TSource source, ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment) { }
+        }
+    }
     public readonly struct Traversal : System.Collections.Generic.IEnumerable<ExRam.Gremlinq.Core.Steps.Step>, System.Collections.Generic.IReadOnlyCollection<ExRam.Gremlinq.Core.Steps.Step>, System.Collections.Generic.IReadOnlyList<ExRam.Gremlinq.Core.Steps.Step>, System.Collections.IEnumerable
     {
         public static readonly ExRam.Gremlinq.Core.Traversal Empty;
