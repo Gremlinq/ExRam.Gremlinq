980080298d6ad5913060de721953dc7c877028d7
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-09 22:55:51 +0100

    For whatever reason, VS has decided to **** up my code where it can without being asked to.

--- src/ExRam.Gremlinq.Support.NewtonsoftJson/Converters/BulkSetConverterFactory.cs
+++ src/ExRam.Gremlinq.Support.NewtonsoftJson/Converters/BulkSetConverterFactory.cs
@@ -12,22 +12,34 @@ namespace ExRam.Gremlinq.Support.NewtonsoftJson
             public bool TryConvert(JObject serialized, IGremlinQueryEnvironment environment, ITransformer recurse, [NotNullWhen(true)] out TTargetArray? value)
             {
                 if (!environment.GetCache().FastNativeTypes.ContainsKey(typeof(TTargetArray)))
+                {
                     if (serialized.TryGetValue("@type", out var typeToken) && "g:BulkSet".Equals(typeToken.Value<string>(), StringComparison.OrdinalIgnoreCase))
+                    {
                         if (serialized.TryGetValue("@value", out var valueToken) && valueToken is JArray setArray)
                         {
                             var array = new List<TTargetArrayItem>();
 
                             for (var i = 0; i < setArray.Count; i += 2)
+                            {
                                 if (recurse.TryTransform<JToken, TTargetArrayItem>(setArray[i], environment, out var element))
+                                {
                                     if (recurse.TryTransform<JToken, int>(setArray[i + 1], environment, out var bulk) && bulk != 1)
+                                    {
                                         for (var j = 0; j < bulk; j++)
+                                        {
                                             array.Add(element);
+                                        }
+                                    }
                                     else
                                         array.Add(element);
+                                }
+                            }
 
                             value = (TTargetArray)(object)array.ToArray();
                             return true;
                         }
+                    }
+                }
 
                 value = default;
                 return false;
