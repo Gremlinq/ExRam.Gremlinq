7ccf4bbf90de3404781f738d0c21d20a7bdf583d
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-09 20:34:53 +0100

    Move AddNewtonsoftJson into the providers.

--- src/ExRam.Gremlinq.Providers.GremlinServer/Extensions/ConfigurableGremlinQuerySourceExtensions.cs
+++ src/ExRam.Gremlinq.Providers.GremlinServer/Extensions/ConfigurableGremlinQuerySourceExtensions.cs
@@ -26,6 +26,7 @@ namespace ExRam.Gremlinq.Core
             return source
                 .UseWebSocket(configurator => configuratorTransformation(new GremlinServerConfigurator(configurator)))
                 .ConfigureEnvironment(environment => environment
+                    .AddNewtonsoftJson()
                     .ConfigureFeatureSet(featureSet => featureSet
                         .ConfigureGraphFeatures(graphFeatures => graphFeatures & ~(GraphFeatures.Transactions | GraphFeatures.ThreadedTransactions | GraphFeatures.ConcurrentAccess))
                         .ConfigureVertexFeatures(vertexFeatures => vertexFeatures & ~(VertexFeatures.Upsert | VertexFeatures.CustomIds))
