dff86ade442a08f84eb33631be9e2618eddfd215
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-09 16:58:41 +0100

    Rename JsonNetMessageSerializer to DefaultMessageSerializer.

---
+++ src/ExRam.Gremlinq.Providers.WebSocket/DefaultMessageSerializer.cs
@@ -0,0 +1,27 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Transformation;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Messages;
+
+namespace ExRam.Gremlinq.Providers.WebSocket
+{
+    internal sealed class DefaultMessageSerializer : IMessageSerializer
+    {
+        private readonly IGremlinQueryEnvironment _environment;
+
+        public DefaultMessageSerializer(IGremlinQueryEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public async Task<byte[]> SerializeMessageAsync(RequestMessage requestMessage) => _environment.Serializer
+            .TransformTo<byte[]>()
+            .From(requestMessage, _environment);
+
+        public async Task<ResponseMessage<List<object>>> DeserializeMessageAsync(byte[] message) => message.Length == 0
+            ? null!
+            : _environment.Deserializer
+                .TransformTo<ResponseMessage<List<object>>>()
+                .From(message, _environment);
+    }
+}
