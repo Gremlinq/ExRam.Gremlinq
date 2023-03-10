b282f9ec73660f02dab08364f96b1d6f4ce9ff53
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-10 19:48:21 +0100

    Merge ArrayExtractConverterFactory into VertexPropertyArrayExtractConverter.

--- src/ExRam.Gremlinq.Support.NewtonsoftJson/Converters/VertexPropertyExtractConverterFactory.cs
+++ src/ExRam.Gremlinq.Support.NewtonsoftJson/Converters/VertexPropertyExtractConverterFactory.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics.CodeAnalysis;
 using ExRam.Gremlinq.Core.Transformation;
 using ExRam.Gremlinq.Core;
+using Newtonsoft.Json;
+using System.Collections;
 
 namespace ExRam.Gremlinq.Support.NewtonsoftJson
 {
@@ -27,11 +29,38 @@ namespace ExRam.Gremlinq.Support.NewtonsoftJson
             }
         }
 
+        private sealed class VertexPropertyArrayExtractConverter<TTarget> : IConverter<JArray, TTarget>
+        {
+            public bool TryConvert(JArray serialized, IGremlinQueryEnvironment environment, ITransformer recurse, [NotNullWhen(true)] out TTarget? value)
+            {
+                if ((!typeof(IEnumerable).IsAssignableFrom(typeof(TTarget)) || environment.GetCache().FastNativeTypes.ContainsKey(typeof(TTarget))) && !typeof(TTarget).IsInstanceOfType(serialized))
+                {
+                    if (serialized.Count != 1)
+                    {
+                        value = serialized.Count == 0 && typeof(TTarget).IsClass
+                            ? default!  //TODO: Drop NotNullWhen(true) ?
+                            : throw new JsonReaderException($"Cannot convert array\r\n\r\n{serialized}\r\n\r\nto scalar value of type {typeof(TTarget)}.");
+
+                        return true;
+                    }
+
+                    return recurse.TryTransform(serialized[0], environment, out value);
+                }
+
+                value = default;
+                return false;
+            }
+        }
+
         public IConverter<TSource, TTarget>? TryCreate<TSource, TTarget>()
         {
-            return typeof(TSource) == typeof(JObject)
-                ? (IConverter<TSource, TTarget>)(object)new VertexPropertyExtractConverter<TTarget>()
-                : default;
+            if (typeof(TSource) == typeof(JObject))
+                return (IConverter<TSource, TTarget>)(object)new VertexPropertyExtractConverter<TTarget>();
+
+            if (typeof(TSource) == typeof(JArray))
+                return (IConverter<TSource, TTarget>)(object)new VertexPropertyArrayExtractConverter<TTarget>();
+
+            return default;
         }
     }
 }
