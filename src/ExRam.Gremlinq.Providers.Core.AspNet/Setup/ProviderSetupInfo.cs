849535b0c123bb36150277a0feeaa08800712247
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-15 14:24:53 +0100

    UseProvider: Demand more from the transformation. Use UseGremlinServer in AspNet more naturally.

--- src/ExRam.Gremlinq.Providers.Core.AspNet/Setup/ProviderSetupInfo.cs
+++ src/ExRam.Gremlinq.Providers.Core.AspNet/Setup/ProviderSetupInfo.cs
@@ -5,13 +5,13 @@ namespace ExRam.Gremlinq.Core.AspNet
     internal sealed class ProviderSetupInfo<TConfigurator>
         where TConfigurator : IProviderConfigurator<TConfigurator>
     {
-        public ProviderSetupInfo(string sectionName, Func<IConfigurableGremlinQuerySource, Func<TConfigurator, IGremlinQuerySourceTransformation>, IGremlinQuerySource> providerChoice)
+        public ProviderSetupInfo(string sectionName, Func<IConfigurableGremlinQuerySource, Func<TConfigurator, TConfigurator>, IGremlinQuerySource> providerChoice)
         {
             SectionName = sectionName;
             ProviderChoice = providerChoice;
         }
 
         public string SectionName { get; }
-        public Func<IConfigurableGremlinQuerySource, Func<TConfigurator, IGremlinQuerySourceTransformation>, IGremlinQuerySource> ProviderChoice { get; }
+        public Func<IConfigurableGremlinQuerySource, Func<TConfigurator, TConfigurator>, IGremlinQuerySource> ProviderChoice { get; }
     }
 }
