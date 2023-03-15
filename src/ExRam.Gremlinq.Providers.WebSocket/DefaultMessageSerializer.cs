2bac345dff26a8bbfa0b61f985600df433c45208
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-15 21:32:41 +0100

    DefaultMessageSerializer: Give a meaningful warning when no deserializer is configured.

--- src/ExRam.Gremlinq.Providers.WebSocket/DefaultMessageSerializer.cs
+++ src/ExRam.Gremlinq.Providers.WebSocket/DefaultMessageSerializer.cs
@@ -20,8 +20,39 @@ namespace ExRam.Gremlinq.Providers.WebSocket
 
         public async Task<ResponseMessage<List<object>>> DeserializeMessageAsync(byte[] message, CancellationToken ct) => message.Length == 0
             ? null!
-            : _environment.Deserializer
-                .TransformTo<ResponseMessage<List<object>>>()
-                .From(message, _environment);
+            : _environment.Deserializer.TryTransformTo<ResponseMessage<List<object>>>().From(message, _environment) is { } responseMessage
+                ? responseMessage
+                : throw new InvalidOperationException("""
+                       No deserializer configured!
+                       In Gremlinq v12, query result deserialization has been decoupled from the core library.
+                       To keep using Newtonsoft.Json as Json-deserialization mechanism, add a reference to
+                       ExRam.Gremlinq.Support.NewtonsoftJson (or ExRam.Gremlinq.Support.NewtonsoftJson.AspNet on ASP.NET Core)
+                       and call 'UseNewtonsoftJson()' in the provider configuration.
+
+                       Examples:
+
+                       Provider configuration
+
+                           IGremlinQuerySource g = ...
+
+                           g = g.UseCosmosDb(c => c
+                                   .UseNewtonsoftJson());
+
+                       ASP.NET Core
+
+                           IServiceCollection services = ...
+
+                           services.AddGremlinq(setup => setup
+                               .UseCosmosDb(providerSetup => providerSetup
+                                   .UseNewtonsoftJson()));
+
+                       Manual configuration
+
+                           IGremlinQuerySource g = ...
+
+                           g = g.ConfigureEnvironment(env => env
+                               .UseNewtonsoftJson());
+                       
+                       """);
     }
 }
