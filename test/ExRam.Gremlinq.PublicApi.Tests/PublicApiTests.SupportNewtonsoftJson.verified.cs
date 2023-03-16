3890a5f220d2a85bcbe4f3a128b91fbe41c8684f
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-16 20:40:36 +0100

    Bring back RegisterNativeType in Support.NewtonsoftJson.

--- test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.SupportNewtonsoftJson.verified.cs
+++ test/ExRam.Gremlinq.PublicApi.Tests/PublicApiTests.SupportNewtonsoftJson.verified.cs
@@ -7,6 +7,7 @@
     }
     public static class GremlinQueryEnvironmentExtensions
     {
+        public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment RegisterNativeType<TNative, TSerialized>(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<TNative, ExRam.Gremlinq.Core.IGremlinQueryEnvironment, ExRam.Gremlinq.Core.Transformation.ITransformer, TSerialized> serializer, System.Func<Newtonsoft.Json.Linq.JValue, ExRam.Gremlinq.Core.IGremlinQueryEnvironment, ExRam.Gremlinq.Core.Transformation.ITransformer, TNative> deserializer) { }
         public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseNewtonsoftJson(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment) { }
     }
 }
