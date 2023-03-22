9357543bc746abeb6b75bff10bcf58028426d3f6
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-22 14:46:21 +0100

    Remove Newtonsoft.Json from Core.Tests. Almost.

--- test/ExRam.Gremlinq.Core.Tests/Extensions/ScrubbersExtensions.cs
+++ test/ExRam.Gremlinq.Core.Tests/Extensions/ScrubbersExtensions.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace ExRam.Gremlinq.Core.Tests
 {
-    internal static class ScrubbersExtensions
+    public static class ScrubbersExtensions
     {
         private static readonly Regex GuidRegex = new("[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}", RegexOptions.IgnoreCase);
 
