b3bb409d17afc03cf9314948e22ef2d7fa767b33
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-22 15:15:59 +0100

    Cleanup.

--- test/ExRam.Gremlinq.Core.Tests/Extensions/ScrubbersExtensions.cs
+++ test/ExRam.Gremlinq.Core.Tests/Extensions/ScrubbersExtensions.cs
@@ -5,8 +5,6 @@ namespace ExRam.Gremlinq.Core.Tests
 {
     public static class ScrubbersExtensions
     {
-        private static readonly Regex GuidRegex = new("[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}", RegexOptions.IgnoreCase);
-
         public static IImmutableList<Func<string, string>> ScrubGuids(this IImmutableList<Func<string, string>> list)
         {
             return list
