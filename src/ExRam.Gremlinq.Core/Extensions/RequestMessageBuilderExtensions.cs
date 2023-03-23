5d2628344d6168ca332b6f9c11327341ebbfbe9e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-23 18:02:17 +0100

    Do not send the alias when it's "g".

--- src/ExRam.Gremlinq.Core/Extensions/RequestMessageBuilderExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/RequestMessageBuilderExtensions.cs
@@ -20,13 +20,15 @@ namespace Gremlin.Net.Driver.Messages
 
         public static RequestMessage.Builder AddAlias(this RequestMessage.Builder builder, IGremlinQueryEnvironment environment)
         {
-            return builder
-                .AddArgument(
-                    Tokens.ArgsAliases,
-                    new Dictionary<string, string>
-                    {
-                        { "g", environment.Options.GetValue(GremlinqOption.Alias) }
-                    });
+            return environment.Options.GetValue(GremlinqOption.Alias) is { } alias && alias != "g"
+                ? builder
+                    .AddArgument(
+                        Tokens.ArgsAliases,
+                        new Dictionary<string, string>
+                        {
+                            { "g", alias }
+                        })
+                : builder;
         }
     }
 }
