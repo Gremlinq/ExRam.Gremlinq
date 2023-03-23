54955054386dbdfb10544b5f2ce1d93fa7e18a42
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-23 14:01:15 +0100

    Add tests that verify the actual RequestMessages.

---
+++ test/ExRam.Gremlinq.Providers.CosmosDb.Tests/RequestMessageSerializationTests.cs
@@ -0,0 +1,30 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Providers.CosmosDb.Tests.Extensions;
+using Gremlin.Net.Driver.Messages;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
+{
+    public sealed class RequestMessageSerializationTests : SerializationTestsBase<RequestMessage>, IClassFixture<RequestMessageSerializationTests.Fixture>
+    {
+        public sealed class Fixture : GremlinqTestFixture
+        {
+            public Fixture() : base(g
+                .UseCosmosDb(builder => builder
+                    .At(new Uri("wss://localhost"), "database", "graph")
+                    .AuthenticateBy("authKey"))
+                .ConfigureEnvironment(env => env
+                    .AddFakePartitionKey()))
+            {
+            }
+        }
+
+        public RequestMessageSerializationTests(Fixture fixture, ITestOutputHelper testOutputHelper) : base(
+            fixture,
+            testOutputHelper)
+        {
+
+        }
+    }
+}
---
+++ test/ExRam.Gremlinq.Providers.GremlinServer.Tests/RequestMessageSerializationTests.cs
@@ -0,0 +1,27 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Providers.Core;
+using Gremlin.Net.Driver.Messages;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    public sealed class RequestMessageSerializationTests : SerializationTestsBase<RequestMessage>, IClassFixture<RequestMessageSerializationTests.Fixture>
+    {
+        public sealed class Fixture : GremlinqTestFixture
+        {
+            public Fixture() : base(g
+                .UseGremlinServer(builder => builder
+                    .AtLocalhost()))
+            {
+            }
+        }
+
+        public RequestMessageSerializationTests(Fixture fixture, ITestOutputHelper testOutputHelper) : base(
+            fixture,
+            testOutputHelper)
+        {
+
+        }
+    }
+}
