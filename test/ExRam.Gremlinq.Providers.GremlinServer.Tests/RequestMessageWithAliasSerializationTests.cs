144218f2f9cc543fdd4252d8906e77608ae5eb8a
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-24 02:22:38 +0100

    Add tests for configuration with different alias.

---
+++ test/ExRam.Gremlinq.Providers.GremlinServer.Tests/RequestMessageWithAliasSerializationTests.cs
@@ -0,0 +1,30 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Providers.Core;
+using Gremlin.Net.Driver.Messages;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    public sealed class RequestMessageWithAliasSerializationTests : SerializationTestsBase<RequestMessage>, IClassFixture<RequestMessageWithAliasSerializationTests.Fixture>
+    {
+        public sealed class Fixture : GremlinqTestFixture
+        {
+            public Fixture() : base(g
+                .UseGremlinServer(builder => builder
+                    .AtLocalhost())
+                .ConfigureEnvironment(env => env
+                    .ConfigureOptions(options => options
+                        .SetValue(GremlinqOption.Alias, "a"))))
+            {
+            }
+        }
+
+        public RequestMessageWithAliasSerializationTests(Fixture fixture, ITestOutputHelper testOutputHelper) : base(
+            fixture,
+            testOutputHelper)
+        {
+
+        }
+    }
+}
