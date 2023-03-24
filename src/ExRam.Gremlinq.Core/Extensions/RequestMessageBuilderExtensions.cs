29c4fd35a00a121e3f7be994c8cadd071351b974
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-24 03:43:24 +0100

    Remove usages of BytecodeGremlinQuery.Id.

--- src/ExRam.Gremlinq.Core/Extensions/RequestMessageBuilderExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/RequestMessageBuilderExtensions.cs
@@ -6,18 +6,6 @@ namespace Gremlin.Net.Driver.Messages
 {
     internal static class RequestMessageBuilderExtensions
     {
-        public static RequestMessage.Builder OverrideRequestId(this RequestMessage.Builder builder, BytecodeGremlinQuery query, IGremlinQueryEnvironment environment)
-        {
-            if (!Guid.TryParse(query.Id, out var requestId))
-            {
-                requestId = Guid.NewGuid();
-                environment.Logger.LogInformation($"Mapping query id {query.Id} to request id {requestId}.");
-            }
-
-            return builder
-                .OverrideRequestId(requestId);
-        }
-
         public static RequestMessage.Builder AddAlias(this RequestMessage.Builder builder, IGremlinQueryEnvironment environment)
         {
             return environment.Options.GetValue(GremlinqOption.Alias) is { } alias && alias != "g"
