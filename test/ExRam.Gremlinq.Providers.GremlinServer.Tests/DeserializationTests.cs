653993f145c2fb415da3e7eecf0613d782d98d66
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-27 23:15:18 +0200

    DeserializationTestBase: Use Verify and not the Newtonsoft-serialization from QueryExecutionTest.

--- test/ExRam.Gremlinq.Providers.GremlinServer.Tests/DeserializationTests.cs
+++ test/ExRam.Gremlinq.Providers.GremlinServer.Tests/DeserializationTests.cs
@@ -1,4 +1,5 @@
 ﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
 using ExRam.Gremlinq.Support.NewtonsoftJson.Tests;
 using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
@@ -6,13 +7,11 @@ namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
 {
     public sealed class DeserializationTests : DeserializationTestsBase, IClassFixture<DeserializationTests.Fixture>
     {
-        public new sealed class Fixture : DeserializationTestsBase.Fixture
+        public sealed class Fixture : GremlinqTestFixture
         {
-            public Fixture() : base(
-                nameof(IntegrationTests),
-                g
-                    .UseGremlinServer(_ => _
-                        .UseNewtonsoftJson()))
+            public Fixture() : base(g
+                .UseGremlinServer(_ => _
+                   .UseNewtonsoftJson()))
             {
             }
         }
