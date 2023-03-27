5b4e583b2e66fc06be500ae6240cc7a3ca3d4197
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-27 15:00:05 +0200

    Remove TransformResult.

--- src/ExRam.Gremlinq.Core/Execution/GremlinQueryExecutor.cs
+++ src/ExRam.Gremlinq.Core/Execution/GremlinQueryExecutor.cs
@@ -34,23 +34,6 @@
             }
         }
 
-        private sealed class TransformResultGremlinQueryExecutor : IGremlinQueryExecutor
-        {
-            private readonly IGremlinQueryExecutor _baseExecutor;
-            private readonly Func<IAsyncEnumerable<object>, IAsyncEnumerable<object>> _transformation;
-
-            public TransformResultGremlinQueryExecutor(IGremlinQueryExecutor baseExecutor, Func<IAsyncEnumerable<object>, IAsyncEnumerable<object>> transformation)
-            {
-                _transformation = transformation;
-                _baseExecutor = baseExecutor;
-            }
-
-            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment)
-            {
-                return _transformation(_baseExecutor.Execute(query, environment));
-            }
-        }
-
         public static readonly IGremlinQueryExecutor Invalid = Create(static (_, _) => throw new InvalidOperationException($"'{nameof(IGremlinQueryExecutor.Execute)}' must not be called on {nameof(GremlinQueryExecutor)}.{nameof(Invalid)}. If you are getting this exception while executing a query, set a proper {nameof(GremlinQueryExecutor)} on the {nameof(GremlinQuerySource)} (e.g. with 'g.UseGremlinServer(...)' for GremlinServer which can be found in the 'ExRam.Gremlinq.Providers.GremlinServer' package)."));
 
         public static readonly IGremlinQueryExecutor Identity = Create(static (query, _) => new object[] { query }.ToAsyncEnumerable());
@@ -60,7 +43,5 @@
         public static IGremlinQueryExecutor Create(Func<IGremlinQueryBase, IGremlinQueryEnvironment, IAsyncEnumerable<object>> executor) => new GremlinQueryExecutorImpl(executor);
 
         public static IGremlinQueryExecutor TransformQuery(this IGremlinQueryExecutor baseExecutor, Func<IGremlinQueryBase, IGremlinQueryBase> transformation) => new TransformQueryGremlinQueryExecutor(baseExecutor, transformation);
-
-        public static IGremlinQueryExecutor TransformResult(this IGremlinQueryExecutor baseExecutor, Func<IAsyncEnumerable<object>, IAsyncEnumerable<object>> transformation) => new TransformResultGremlinQueryExecutor(baseExecutor, transformation);
     }
 }
