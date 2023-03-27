ed9e7c75f918a86b461be11354b22900ec5e1ac1
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-27 15:14:48 +0200

    Remove usages of GremlinQueryExecutor.Create.

--- src/ExRam.Gremlinq.Core/Execution/GremlinQueryExecutor.cs
+++ src/ExRam.Gremlinq.Core/Execution/GremlinQueryExecutor.cs
@@ -34,11 +34,19 @@
             }
         }
 
-        public static readonly IGremlinQueryExecutor Invalid = Create(static (_, _) => throw new InvalidOperationException($"'{nameof(IGremlinQueryExecutor.Execute)}' must not be called on {nameof(GremlinQueryExecutor)}.{nameof(Invalid)}. If you are getting this exception while executing a query, set a proper {nameof(GremlinQueryExecutor)} on the {nameof(GremlinQuerySource)} (e.g. with 'g.UseGremlinServer(...)' for GremlinServer which can be found in the 'ExRam.Gremlinq.Providers.GremlinServer' package)."));
+        private sealed class InvalidGremlinQueryExecutor : IGremlinQueryExecutor
+        {
+            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment) => throw new InvalidOperationException($"'{nameof(IGremlinQueryExecutor.Execute)}' must not be called on {nameof(GremlinQueryExecutor)}.{nameof(Invalid)}. If you are getting this exception while executing a query, set a proper {nameof(GremlinQueryExecutor)} on the {nameof(GremlinQuerySource)} (e.g. with 'g.UseGremlinServer(...)' for GremlinServer which can be found in the 'ExRam.Gremlinq.Providers.GremlinServer' package).");
+        }
 
-        public static readonly IGremlinQueryExecutor Identity = Create(static (query, _) => new object[] { query }.ToAsyncEnumerable());
+        private sealed class EmptyGremlinQueryExecutor : IGremlinQueryExecutor
+        {
+            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment) => AsyncEnumerable.Empty<object>();
+        }
 
-        public static readonly IGremlinQueryExecutor Empty = Create(static (_, _) => AsyncEnumerable.Empty<object>());
+        public static readonly IGremlinQueryExecutor Empty = new EmptyGremlinQueryExecutor();
+
+        public static readonly IGremlinQueryExecutor Invalid = new InvalidGremlinQueryExecutor();
 
         public static IGremlinQueryExecutor Create(Func<IGremlinQueryBase, IGremlinQueryEnvironment, IAsyncEnumerable<object>> executor) => new GremlinQueryExecutorImpl(executor);
 
