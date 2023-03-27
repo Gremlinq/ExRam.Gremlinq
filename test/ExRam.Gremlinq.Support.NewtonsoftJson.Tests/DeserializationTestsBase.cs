b5fbb1035eef26b13148d9dd6a0f5ed31160ccb9
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-27 15:17:03 +0200

    Remove last usage of GremlinQueryExecutor.Create.

--- test/ExRam.Gremlinq.Support.NewtonsoftJson.Tests/DeserializationTestsBase.cs
+++ test/ExRam.Gremlinq.Support.NewtonsoftJson.Tests/DeserializationTestsBase.cs
@@ -9,29 +9,41 @@ namespace ExRam.Gremlinq.Support.NewtonsoftJson.Tests
 {
     public abstract class DeserializationTestsBase : QueryExecutionTest
     {
+        private sealed class DeserializationGremlinQueryExecutor : IGremlinQueryExecutor
+        {
+            private readonly string _sourcePrefix;
+
+            public DeserializationGremlinQueryExecutor(string sourcePrefix)
+            {
+                _sourcePrefix = sourcePrefix;
+            }
+
+            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment)
+            {
+                var context = XunitContext.Context;
+
+                try
+                {
+                    var jArray = JsonConvert.DeserializeObject<JArray>(
+                        File.ReadAllText(System.IO.Path.Combine(context.SourceDirectory, _sourcePrefix + "." + context.MethodName + ".verified.txt")));
+
+                    return jArray?
+                        .Where(obj => !(obj is JObject jObject && jObject.ContainsKey("serverException")))
+                        .Cast<object>()
+                        .ToAsyncEnumerable() ?? AsyncEnumerable.Empty<object>();
+                }
+                catch (IOException)
+                {
+                    return AsyncEnumerable.Empty<object>();
+                }
+            }
+        }
+
         public abstract class Fixture : GremlinqTestFixture
         {
             protected Fixture(string sourcePrefix, IGremlinQuerySource source) : base(source
                 .ConfigureEnvironment(env => env
-                    .UseExecutor(GremlinQueryExecutor.Create((_, _) =>
-                    {
-                        var context = XunitContext.Context;
-
-                        try
-                        {
-                            var jArray = JsonConvert.DeserializeObject<JArray>(
-                                File.ReadAllText(System.IO.Path.Combine(context.SourceDirectory, sourcePrefix + "." + context.MethodName + ".verified.txt")));
-
-                            return jArray?
-                                .Where(obj => !(obj is JObject jObject && jObject.ContainsKey("serverException")))
-                                .Cast<object>()
-                                .ToAsyncEnumerable() ?? AsyncEnumerable.Empty<object>();
-                        }
-                        catch (IOException)
-                        {
-                            return AsyncEnumerable.Empty<object>();
-                        }
-                    }))))
+                    .UseExecutor(new DeserializationGremlinQueryExecutor(sourcePrefix))))
             {
             }
         }
