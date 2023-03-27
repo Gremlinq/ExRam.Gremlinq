1b8729478bb3e7ba1973fc67c34482ff0517e178
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-27 15:19:28 +0200

    Remove GremlinQueryExecutor.Create.

--- src/ExRam.Gremlinq.Core/Execution/GremlinQueryExecutor.cs
+++ src/ExRam.Gremlinq.Core/Execution/GremlinQueryExecutor.cs
@@ -2,19 +2,14 @@
 {
     public static class GremlinQueryExecutor
     {
-        private sealed class GremlinQueryExecutorImpl : IGremlinQueryExecutor
+        private sealed class InvalidGremlinQueryExecutor : IGremlinQueryExecutor
         {
-            private readonly Func<IGremlinQueryBase, IGremlinQueryEnvironment, IAsyncEnumerable<object>> _factory;
+            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment) => throw new InvalidOperationException($"'{nameof(IGremlinQueryExecutor.Execute)}' must not be called on {nameof(GremlinQueryExecutor)}.{nameof(Invalid)}. If you are getting this exception while executing a query, set a proper {nameof(GremlinQueryExecutor)} on the {nameof(GremlinQuerySource)} (e.g. with 'g.UseGremlinServer(...)' for GremlinServer which can be found in the 'ExRam.Gremlinq.Providers.GremlinServer' package).");
+        }
 
-            public GremlinQueryExecutorImpl(Func<IGremlinQueryBase, IGremlinQueryEnvironment, IAsyncEnumerable<object>> factory)
-            {
-                _factory = factory;
-            }
-
-            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment)
-            {
-                return _factory(query, environment);
-            }
+        private sealed class EmptyGremlinQueryExecutor : IGremlinQueryExecutor
+        {
+            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment) => AsyncEnumerable.Empty<object>();
         }
 
         private sealed class TransformQueryGremlinQueryExecutor : IGremlinQueryExecutor
@@ -34,21 +29,9 @@
             }
         }
 
-        private sealed class InvalidGremlinQueryExecutor : IGremlinQueryExecutor
-        {
-            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment) => throw new InvalidOperationException($"'{nameof(IGremlinQueryExecutor.Execute)}' must not be called on {nameof(GremlinQueryExecutor)}.{nameof(Invalid)}. If you are getting this exception while executing a query, set a proper {nameof(GremlinQueryExecutor)} on the {nameof(GremlinQuerySource)} (e.g. with 'g.UseGremlinServer(...)' for GremlinServer which can be found in the 'ExRam.Gremlinq.Providers.GremlinServer' package).");
-        }
-
-        private sealed class EmptyGremlinQueryExecutor : IGremlinQueryExecutor
-        {
-            public IAsyncEnumerable<object> Execute(IGremlinQueryBase query, IGremlinQueryEnvironment environment) => AsyncEnumerable.Empty<object>();
-        }
-
         public static readonly IGremlinQueryExecutor Empty = new EmptyGremlinQueryExecutor();
 
         public static readonly IGremlinQueryExecutor Invalid = new InvalidGremlinQueryExecutor();
-
-        public static IGremlinQueryExecutor Create(Func<IGremlinQueryBase, IGremlinQueryEnvironment, IAsyncEnumerable<object>> executor) => new GremlinQueryExecutorImpl(executor);
 
         public static IGremlinQueryExecutor TransformQuery(this IGremlinQueryExecutor baseExecutor, Func<IGremlinQueryBase, IGremlinQueryBase> transformation) => new TransformQueryGremlinQueryExecutor(baseExecutor, transformation);
     }
