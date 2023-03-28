5e66a467d1c00f43ffec8f03b609d044b3d5df9d
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-28 13:41:52 +0200

    Add test for FastImmutableList.

---
+++ test/ExRam.Gremlinq.Core.Tests/FastImmutableListTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class FastImmutableListTest : VerifyBase
+    {
+        public FastImmutableListTest() : base()
+        {
+        }
+
+        [Fact]
+        public void Concurrency()
+        {
+            var list = FastImmutableList<string>.Empty
+                .Push("1")
+                .Push("2");
+
+            list
+                .Push("3")
+                .Should()
+                .Equal("1", "2", "3");
+
+            list
+                .Push("4")
+                .Should()
+                .Equal("1", "2", "4");
+        }
+    }
+}
