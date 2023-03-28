935d815e065cfb87de82cab7323350a822b6307e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-03-28 14:36:22 +0200

    Fix missing ToImmutableArray on platforms < .NET 7.0.

--- src/ExRam.Gremlinq.Core/Extensions/SpanExtensions.cs
+++ src/ExRam.Gremlinq.Core/Extensions/SpanExtensions.cs
@@ -17,7 +17,9 @@ namespace ExRam.Gremlinq.Core
             return true;
         }
 
-        public static ImmutableArray<T> ToImmutableArray<T>(this Span<T> span)
+        public static ImmutableArray<T> ToImmutableArray<T>(this Span<T> span) => ((ReadOnlySpan<T>)span).ToImmutableArray();
+
+        public static ImmutableArray<T> ToImmutableArray<T>(this ReadOnlySpan<T> span)
         {
             var builder = ImmutableArray
                 .CreateBuilder<T>(span.Length);
