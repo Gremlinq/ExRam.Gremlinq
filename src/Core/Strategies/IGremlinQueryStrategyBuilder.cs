7cfa65840ad874e174ee1ff85c2b1193601d45e2
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-12 23:12:39 +0200

    Add base interfaces and methods for traversal strategies.

---
+++ src/Core/Strategies/IGremlinQueryStrategyBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    public interface IGremlinQueryStrategyBuilder<TStrategy>
+        where TStrategy : IGremlinQueryStrategy
+    {
+        Func<IGremlinQuerySource, TStrategy> Create();
+    }
+}
