62cc048256e3400e16df2557727b5749820181ee
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-20 00:55:03 +0200

    Factor out IntegrationTestFixture.

---
+++ test/Support.NewtonsoftJson.Tests/Fixtures/IntegrationTestFixture.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Core.Transformation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ExRam.Gremlinq.Support.NewtonsoftJson.Tests
+{
+    public abstract class IntegrationTestFixture : GremlinqTestFixture
+    {
+        private static readonly Regex IdRegex = new("(\"id\"\\s*[:,]\\s*{\\s*\"@type\"\\s*:\\s*\"g:(Int32|Int64|UUID)\"\\s*,\\s*\"@value\":\\s*)([^\\s{}]+)(\\s*})", RegexOptions.IgnoreCase);
+
+        protected IntegrationTestFixture(IGremlinQuerySource source) : base(source
+            .ConfigureEnvironment(env => env
+                .UseNewtonsoftJson()
+                .ConfigureDeserializer(d => d
+                    .Add(ConverterFactory
+                        .Create<JToken, JToken>((token, env, recurse) => token)))))
+        {
+        }
+
+        public override async Task Verify<TElement>(IGremlinQueryBase<TElement> query)
+        {
+            var serialized = JsonConvert.SerializeObject(
+                await query
+                    .Cast<JToken>()
+                    .ToArrayAsync(),
+                Formatting.Indented);
+
+            var scrubbed = 
+                Scrubbers()
+                .Aggregate(serialized, (s, func) => func(s));
+
+            await GremlinqTestBase.Current.Verify(scrubbed);
+        }
+
+        protected virtual IImmutableList<Func<string, string>> Scrubbers() => ImmutableList<Func<string, string>>.Empty
+            .Add(x => IdRegex.Replace(x, "$1-1$4"))
+            .ScrubGuids();
+    }
+}
