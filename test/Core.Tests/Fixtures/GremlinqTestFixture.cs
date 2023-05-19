c9fd6f9437901c8357b75f3164b7b58bfd0be8c4
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-19 23:10:17 +0200

    CatchExecutionExceptions everywhere.

--- test/Core.Tests/Fixtures/GremlinqTestFixture.cs
+++ test/Core.Tests/Fixtures/GremlinqTestFixture.cs
@@ -1,4 +1,5 @@
-﻿using static ExRam.Gremlinq.Core.GremlinQuerySource;
+﻿using ExRam.Gremlinq.Core.Execution;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Core.Tests
 {
@@ -15,7 +16,10 @@ namespace ExRam.Gremlinq.Core.Tests
 
         protected GremlinqTestFixture(IGremlinQuerySource source)
         {
-            GremlinQuerySource = source;
+            GremlinQuerySource = source
+                .ConfigureEnvironment(env => env
+                    .ConfigureExecutor(exe => exe
+                        .CatchExecutionExceptions()));
         }
 
         public virtual async Task Verify<TElement>(IGremlinQueryBase<TElement> query) => await GremlinqTestBase.Current.Verify(query.Debug());
