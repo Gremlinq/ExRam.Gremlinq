fdc1b79b98d0295913fc0cb303ca449060679097
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-22 20:08:45 +0200

    New: GremlinQueryVerifer to separate verification from setup logic.

---
+++ test/Core.Tests/Verifiers/GremlinQueryVerifier.cs
@@ -0,0 +1,14 @@
+﻿namespace ExRam.Gremlinq.Core.Tests
+{
+    public abstract class GremlinQueryVerifier
+    {
+        public sealed class DefaultGremlinQueryVerifier : GremlinQueryVerifier
+        {
+
+        }
+
+        public static readonly GremlinQueryVerifier Default = new DefaultGremlinQueryVerifier();
+
+        public virtual async Task Verify<TElement>(IGremlinQueryBase<TElement> query) => await GremlinqTestBase.Current.Verify(query.Debug());
+    }
+}
