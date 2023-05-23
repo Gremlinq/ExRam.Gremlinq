9e58b833e897f41a190ac373ce8b0c4aeeec4c6f
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 16:38:20 +0200

    Factor out CosmosDbFixture.

---
+++ test/Providers.CosmosDb.Tests/CosmosDbFixture.cs
@@ -0,0 +1,37 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Providers.CosmosDb.Tests.Extensions;
+using ExRam.Gremlinq.Support.NewtonsoftJson.Tests;
+using Microsoft.Azure.Cosmos;
+
+namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
+{
+    public sealed class CosmosDbFixture : ExecutingTestFixture
+    {
+        private readonly Task _task;
+
+        public CosmosDbFixture() : base(Gremlinq.Core.GremlinQuerySource.g
+            .UseCosmosDb(builder => builder
+                .At(new Uri("ws://localhost:8901"), "db", "graph")
+                .AuthenticateBy(
+                    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
+                .UseNewtonsoftJson())
+            .ConfigureEnvironment(env => env
+                .AddFakePartitionKey()))
+        {
+            _task = CreateImpl();
+        }
+
+        public Task Create()
+        {
+            return _task;
+        }
+
+        private static async Task CreateImpl()
+        {
+            var cosmosClient = new CosmosClient("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+
+            var database = await cosmosClient.CreateDatabaseIfNotExistsAsync("db", ThroughputProperties.CreateAutoscaleThroughput(40000));
+            await database.Database.CreateContainerIfNotExistsAsync("graph", "/PartitionKey");
+        }
+    }
+}
