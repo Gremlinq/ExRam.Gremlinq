71624b0673916e9cf82e8273a70b58415a17c6d4
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 18:24:35 +0200

    Get rid of ExecutingTestFixture.

--- test/Providers.GremlinServer.Tests/IntegrationTests.cs
+++ test/Providers.GremlinServer.Tests/IntegrationTests.cs
@@ -1,20 +1,25 @@
 ﻿using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.Tests;
+using ExRam.Gremlinq.Core.Transformation;
 using ExRam.Gremlinq.Providers.Core;
-using ExRam.Gremlinq.Support.NewtonsoftJson.Tests;
 using ExRam.Gremlinq.Support.NewtonsoftJson.Tests.Verifier;
+using Newtonsoft.Json.Linq;
 using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
 {
     public sealed class IntegrationTests : QueryExecutionTest, IClassFixture<IntegrationTests.Fixture>
     {
-        public sealed class Fixture : ExecutingTestFixture
+        public sealed class Fixture : GremlinqTestFixture
         {
             public Fixture() : base(g
                 .UseGremlinServer(builder => builder
                     .AtLocalhost()
-                    .UseNewtonsoftJson()))
+                    .UseNewtonsoftJson())
+                .ConfigureEnvironment(env => env
+                    .ConfigureDeserializer(d => d
+                        .Add(ConverterFactory
+                            .Create<JToken, JToken>((token, env, recurse) => token)))))
             {
             }
         }
