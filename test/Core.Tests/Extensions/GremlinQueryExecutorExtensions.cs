642c773531ddce0a77906586830b3aecac56a45a
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 23:03:52 +0200

    New Project: ExRam.Gremlinq.Tests.Infrastructure.

--- test/Core.Tests/Extensions/GremlinQueryExecutorExtensions.cs
+++ test/Core.Tests/Extensions/GremlinQueryExecutorExtensions.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-
-namespace ExRam.Gremlinq.Core.Execution
+﻿namespace ExRam.Gremlinq.Core.Execution
 {
     public static class GremlinQueryExecutorExtensions
     {
@@ -20,35 +18,6 @@ namespace ExRam.Gremlinq.Core.Execution
                     .IgnoreElements();
             }
         }
-
-        private sealed class CatchingGremlinQueryExecutor : IGremlinQueryExecutor
-        {
-            private readonly IGremlinQueryExecutor _baseExecutor;
-
-            public CatchingGremlinQueryExecutor(IGremlinQueryExecutor baseExecutor)
-            {
-                _baseExecutor = baseExecutor;
-            }
-
-            public IAsyncEnumerable<T> Execute<T>(GremlinQueryExecutionContext context) => _baseExecutor
-                .Execute<T>(context)
-                .Catch<T, Exception>(ex => typeof(T).IsAssignableFrom(typeof(JObject))
-                    ? AsyncEnumerableEx
-                        .Return((T)(object)new JObject()
-                        {
-                            {
-                                "serverException",
-                                new JObject
-                                {
-                                    { "type", ex.GetType().Name },
-                                    { "message", ex.Message }
-                                }
-                            }
-                        })
-                    : AsyncEnumerable.Empty<T>());
-        }
-
-        public static IGremlinQueryExecutor CatchExecutionExceptions(this IGremlinQueryExecutor executor) => new CatchingGremlinQueryExecutor(executor);
 
         public static IGremlinQueryExecutor IgnoreResults(this IGremlinQueryExecutor executor) => new IgnoringExecutor(executor);
     }
