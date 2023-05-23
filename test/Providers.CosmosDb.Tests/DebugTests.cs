554bf44c9e9e56863ed2e5f609f227406c888b95
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 16:38:20 +0200

    Factor out CosmosDbFixture.

--- test/Providers.CosmosDb.Tests/DebugTests.cs
+++ test/Providers.CosmosDb.Tests/DebugTests.cs
@@ -1,22 +1,10 @@
-﻿using ExRam.Gremlinq.Core;
-using ExRam.Gremlinq.Core.Tests;
-using static ExRam.Gremlinq.Core.GremlinQuerySource;
+﻿using ExRam.Gremlinq.Core.Tests;
 
 namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
 {
-    public sealed class DebugTests : QueryExecutionTest, IClassFixture<DebugTests.DebugFixture>
+    public sealed class DebugTests : QueryExecutionTest, IClassFixture<CosmosDbFixture>
     {
-        public sealed class DebugFixture : GremlinqTestFixture
-        {
-            public DebugFixture() : base(g
-                .UseCosmosDb(_ => _
-                    .At("ws://localhost", "", "")
-                    .AuthenticateBy("")))
-            {
-            }
-        }
-
-        public DebugTests(DebugFixture fixture, ITestOutputHelper testOutputHelper) : base(
+        public DebugTests(CosmosDbFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
             new DebugGremlinQueryVerifier(),
             testOutputHelper)
--- test/Providers.CosmosDb.Tests/IntegrationTests.cs
+++ test/Providers.CosmosDb.Tests/IntegrationTests.cs
@@ -1,45 +1,11 @@
-﻿using ExRam.Gremlinq.Core;
-using ExRam.Gremlinq.Core.Tests;
-using ExRam.Gremlinq.Providers.CosmosDb.Tests.Extensions;
-using ExRam.Gremlinq.Support.NewtonsoftJson.Tests;
+﻿using ExRam.Gremlinq.Core.Tests;
 using ExRam.Gremlinq.Support.NewtonsoftJson.Tests.Verifier;
-using Microsoft.Azure.Cosmos;
 
 namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
 {
-    public sealed class IntegrationTests : QueryExecutionTest, IClassFixture<IntegrationTests.Fixture>
+    public sealed class IntegrationTests : QueryExecutionTest, IClassFixture<CosmosDbFixture>
     {
-        public sealed class Fixture : ExecutingTestFixture
-        {
-            private readonly Task _task;
-
-            public Fixture() : base(Gremlinq.Core.GremlinQuerySource.g
-                .UseCosmosDb(builder => builder
-                    .At(new Uri("ws://localhost:8901"), "db", "graph")
-                    .AuthenticateBy(
-                        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
-                    .UseNewtonsoftJson())
-                .ConfigureEnvironment(env => env
-                    .AddFakePartitionKey()))
-            {
-                _task = CreateImpl();
-            }
-
-            public Task Create()
-            {
-                return _task;
-            }
-
-            private static async Task CreateImpl()
-            {
-                var cosmosClient = new CosmosClient("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
-
-                var database = await cosmosClient.CreateDatabaseIfNotExistsAsync("db", ThroughputProperties.CreateAutoscaleThroughput(40000));
-                await database.Database.CreateContainerIfNotExistsAsync("graph", "/PartitionKey");
-            }
-        }
-
-        public IntegrationTests(Fixture fixture, ITestOutputHelper testOutputHelper) : base(
+        public IntegrationTests(CosmosDbFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
             new ExecutingVerifier(),
             testOutputHelper)
