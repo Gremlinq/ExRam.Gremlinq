4834486af12d67456b97a680b8d8155fd1ac4472
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 15:07:35 +0200

    Cleanup.

--- test/PublicApi.Tests/PublicApiTests.WebSocketAspNet.verified.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExRam.Gremlinq.Providers.Core.AspNet
-{
-    public static class ProviderSetupExtensions
-    {
-        public static ExRam.Gremlinq.Providers.Core.AspNet.ProviderSetup<TConfigurator> ConfigureWebSocket<TConfigurator>(this ExRam.Gremlinq.Providers.Core.AspNet.ProviderSetup<TConfigurator> setup)
-            where TConfigurator : ExRam.Gremlinq.Providers.WebSocket.IWebSocketProviderConfigurator<TConfigurator> { }
-    }
-}
