450267bfb05e79849823090c654018b47117fcbc
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 22:21:24 +0200

    Compile Regexes.

--- test/Core.Tests/Extensions/StringExtensions.cs
+++ test/Core.Tests/Extensions/StringExtensions.cs
@@ -4,7 +4,7 @@ namespace ExRam.Gremlinq.Core.Tests
 {
     internal static class StringExtensions
     {
-        private static readonly Regex GuidRegex = new("[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}", RegexOptions.IgnoreCase);
+        private static readonly Regex GuidRegex = new("[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         public static string ScrubGuids(this string str)
         {
