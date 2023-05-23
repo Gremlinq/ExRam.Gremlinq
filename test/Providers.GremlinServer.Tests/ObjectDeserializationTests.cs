70e8d68afde54d2064fdc03d1691b46576c4fe2e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 22:34:52 +0200

    Factor out ObjectDeserializingGremlinqVerifier.

--- test/Providers.GremlinServer.Tests/ObjectDeserializationTests.cs
+++ test/Providers.GremlinServer.Tests/ObjectDeserializationTests.cs
@@ -1,24 +1,13 @@
-﻿using System.Runtime.CompilerServices;
-using ExRam.Gremlinq.Core;
-using ExRam.Gremlinq.Core.Tests;
+﻿using ExRam.Gremlinq.Core.Tests;
 using ExRam.Gremlinq.Support.NewtonsoftJson.Tests;
 
 namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
 {
     public sealed class ObjectDeserializationTests : QueryExecutionTest, IClassFixture<GremlinServerFixture>
     {
-        public new sealed class Verifier : DeserializingGremlinqVerifier
-        {
-            public Verifier(ITestOutputHelper testOutputHelper, [CallerFilePath] string sourceFile = "") : base(testOutputHelper, sourceFile)
-            {
-            }
-
-            public override Task Verify<TElement>(IGremlinQueryBase<TElement> query) => base.Verify(query.Cast<object>());
-        }
-
         public ObjectDeserializationTests(GremlinServerFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
-            new Verifier(testOutputHelper),
+            new ObjectDeserializingGremlinqVerifier(testOutputHelper),
             testOutputHelper)
         {
         }
---
+++ test/Support.NewtonsoftJson.Tests/Verifier/ObjectDeserializingGremlinqVerifier.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using ExRam.Gremlinq.Core;
+
+namespace ExRam.Gremlinq.Support.NewtonsoftJson.Tests
+{
+    public sealed class ObjectDeserializingGremlinqVerifier : DeserializingGremlinqVerifier
+    {
+        public ObjectDeserializingGremlinqVerifier(ITestOutputHelper testOutputHelper, [CallerFilePath] string sourceFile = "") : base(testOutputHelper, sourceFile)
+        {
+        }
+
+        public override Task Verify<TElement>(IGremlinQueryBase<TElement> query) => base.Verify(query.Cast<object>());
+    }
+}
