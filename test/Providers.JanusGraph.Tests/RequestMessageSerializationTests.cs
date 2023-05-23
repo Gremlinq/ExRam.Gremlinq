510287e6188804529e8bfd1099a1cf904168f62c
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 21:06:35 +0200

    Factor JanusGraph fixtures.

--- test/Providers.JanusGraph.Tests/RequestMessageSerializationTests.cs
+++ test/Providers.JanusGraph.Tests/RequestMessageSerializationTests.cs
@@ -1,24 +1,13 @@
-﻿using ExRam.Gremlinq.Core;
-using ExRam.Gremlinq.Core.Tests;
+﻿using ExRam.Gremlinq.Core.Tests;
 using ExRam.Gremlinq.Core.Tests.Verifiers;
-using ExRam.Gremlinq.Providers.Core;
 using Gremlin.Net.Driver.Messages;
-using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Providers.JanusGraph.Tests
 {
-    public sealed class RequestMessageSerializationTests : QueryExecutionTest, IClassFixture<RequestMessageSerializationTests.RequestMessageFixture>
-    {
-        public sealed class RequestMessageFixture : GremlinqTestFixture
-        {
-            public RequestMessageFixture() : base(g
-                .UseJanusGraph(builder => builder
-                    .AtLocalhost()))
-            {
-            }
-        }
 
-        public RequestMessageSerializationTests(RequestMessageFixture fixture, ITestOutputHelper testOutputHelper) : base(
+    public sealed class RequestMessageSerializationTests : QueryExecutionTest, IClassFixture<SimpleJanusGraphFixture>
+    {
+        public RequestMessageSerializationTests(SimpleJanusGraphFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
             new SerializingVerifier<RequestMessage>(),
             testOutputHelper)
