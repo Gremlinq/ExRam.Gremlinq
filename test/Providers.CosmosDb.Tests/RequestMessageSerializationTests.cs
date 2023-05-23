ce35d4b935cb75b14bd12849cc20642f3b7ebb1e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-23 16:46:46 +0200

    Factor out CosmosDbFixture.

--- test/Providers.CosmosDb.Tests/RequestMessageSerializationTests.cs
+++ test/Providers.CosmosDb.Tests/RequestMessageSerializationTests.cs
@@ -1,27 +1,12 @@
-﻿using ExRam.Gremlinq.Core;
-using ExRam.Gremlinq.Core.Tests;
+﻿using ExRam.Gremlinq.Core.Tests;
 using ExRam.Gremlinq.Core.Tests.Verifiers;
-using ExRam.Gremlinq.Providers.CosmosDb.Tests.Extensions;
 using Gremlin.Net.Driver.Messages;
-using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
 {
-    public sealed class RequestMessageSerializationTests : QueryExecutionTest, IClassFixture<RequestMessageSerializationTests.RequestMessageFixture>
+    public sealed class RequestMessageSerializationTests : QueryExecutionTest, IClassFixture<CosmosDbFixture>
     {
-        public sealed class RequestMessageFixture : GremlinqTestFixture
-        {
-            public RequestMessageFixture() : base(g
-                .UseCosmosDb(builder => builder
-                    .At(new Uri("wss://localhost"), "database", "graph")
-                    .AuthenticateBy("authKey"))
-                .ConfigureEnvironment(env => env
-                    .AddFakePartitionKey()))
-            {
-            }
-        }
-
-        public RequestMessageSerializationTests(RequestMessageFixture fixture, ITestOutputHelper testOutputHelper) : base(
+        public RequestMessageSerializationTests(CosmosDbFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
             new SerializingVerifier<RequestMessage>(),
             testOutputHelper)
