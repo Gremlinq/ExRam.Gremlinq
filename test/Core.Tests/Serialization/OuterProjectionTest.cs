a8c8dc5b189587e4a6763ae5e9aa71e54e1fcd59
Daniel Weber <danielcweber@users.noreply.github.com>
2023-05-24 11:21:44 +0200

    Add snapshot tests for outer projections.

---
+++ test/Core.Tests/Serialization/OuterProjectionTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public sealed class OuterProjectionTest : QueryExecutionTest
+    {
+        private sealed class ProjectionVerifier : GremlinQueryVerifier
+        {
+            public ProjectionVerifier([CallerFilePath] string sourceFile = "") : base(sourceFile)
+            {
+            }
+
+            public override Task Verify<TElement>(IGremlinQueryBase<TElement> query) => InnerVerify(query.ToTraversal().Projection);
+        }
+
+        public OuterProjectionTest(ITestOutputHelper testOutputHelper) : base(GremlinqFixture.Empty, new ProjectionVerifier(), testOutputHelper)
+        {
+        }
+    }
+}
