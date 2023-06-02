1321b65b297694b95de0c692bb1e6d3f772ef505
Daniel Weber <dw@exram.de>
2023-06-02 18:34:49 +0200

    IGremlinQueryStrategyBuilder is unused in ExRam.Gremlinq.Core.

--- src/Core/Strategies/IGremlinQueryStrategyBuilder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExRam.Gremlinq.Core
-{
-    public interface IGremlinQueryStrategyBuilder<TStrategy>
-        where TStrategy : IGremlinQueryStrategy
-    {
-        Func<IGremlinQuerySource, TStrategy> Create();
-    }
-}
