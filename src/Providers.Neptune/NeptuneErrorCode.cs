230384ae0fdf0322ba18a84198b1e0739a603b69
Daniel Weber <danielcweber@users.noreply.github.com>
2023-06-14 14:53:56 +0200

    Properly parse Neptune execution errors into a separate exception type and an error code enum.

---
+++ src/Providers.Neptune/NeptuneErrorCode.cs
@@ -0,0 +1,26 @@
+﻿namespace ExRam.Gremlinq.Providers.Neptune
+{
+    public enum NeptuneErrorCode
+    {
+        AccessDeniedException,
+        BadRequestException,
+        CancelledByUserException,
+        ConcurrentModificationException,
+        ConstraintViolationException,
+        InternalFailureException,
+        InvalidNumericDataException,
+        InvalidParameterException,
+        MalformedQueryException,
+        MemoryLimitExceededException,
+        MethodNotAllowedException,
+        MissingParameterException,
+        QueryLimitExceededException,
+        QueryLimitException,
+        QueryTooLargeException,
+        ReadOnlyViolationException,
+        ThrottlingException,
+        TimeLimitExceededException,
+        TooManyRequestsException,
+        UnsupportedOperationException
+    }
+}
---
+++ src/Providers.Neptune/NeptuneResponseException.cs
@@ -0,0 +1,12 @@
+﻿namespace ExRam.Gremlinq.Providers.Neptune
+{
+    public sealed class NeptuneResponseException : Exception
+    {
+        public NeptuneResponseException(NeptuneErrorCode code, string detailedMessage, Exception innerException) : base(detailedMessage, innerException)
+        {
+            Code = code;
+        }
+
+        public NeptuneErrorCode Code { get; }
+    }
+}
