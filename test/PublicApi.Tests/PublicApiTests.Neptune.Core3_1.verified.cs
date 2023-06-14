69e79ae9413c9a3535e743ff4e86269489bba72a
danielcweber <danielcweber@users.noreply.github.com>
2023-06-14 13:12:19 +0000

    Fix snapshots for #1041.

--- test/PublicApi.Tests/PublicApiTests.Neptune.Core3_1.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.Neptune.Core3_1.verified.cs
@@ -17,4 +17,32 @@ namespace ExRam.Gremlinq.Providers.Neptune
         Standard = 0,
         LowercaseKeyword = 1,
     }
+    public enum NeptuneErrorCode
+    {
+        AccessDeniedException = 0,
+        BadRequestException = 1,
+        CancelledByUserException = 2,
+        ConcurrentModificationException = 3,
+        ConstraintViolationException = 4,
+        InternalFailureException = 5,
+        InvalidNumericDataException = 6,
+        InvalidParameterException = 7,
+        MalformedQueryException = 8,
+        MemoryLimitExceededException = 9,
+        MethodNotAllowedException = 10,
+        MissingParameterException = 11,
+        QueryLimitExceededException = 12,
+        QueryLimitException = 13,
+        QueryTooLargeException = 14,
+        ReadOnlyViolationException = 15,
+        ThrottlingException = 16,
+        TimeLimitExceededException = 17,
+        TooManyRequestsException = 18,
+        UnsupportedOperationException = 19,
+    }
+    public sealed class NeptuneResponseException : System.Exception
+    {
+        public NeptuneResponseException(ExRam.Gremlinq.Providers.Neptune.NeptuneErrorCode code, string detailedMessage, System.Exception innerException) { }
+        public ExRam.Gremlinq.Providers.Neptune.NeptuneErrorCode Code { get; }
+    }
 }
