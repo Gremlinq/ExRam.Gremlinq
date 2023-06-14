9b9878e9ef30f362f92ec0dc7b2e47f02aeb605e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-06-14 15:45:44 +0200

    Make NeptuneErrorCode a struct to wrap even unknown values. Implement equality, assert in test.

---
+++ test/Providers.Neptune.Tests/NeptuneErrorCodeTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace ExRam.Gremlinq.Providers.Neptune.Tests
+{
+    public sealed class NeptuneErrorCodeTests
+    {
+        [Fact]
+        public void Equality()
+        {
+            var value1 = "Value";
+            var value2 = "Value123".Substring(0, 5);
+
+            ReferenceEquals(value1, value2).Should().BeFalse();
+
+            var code1 = NeptuneErrorCode.From(value1);
+            var code2 = NeptuneErrorCode.From(value2);
+
+            code1.Equals(code2).Should().BeTrue();
+            (code1 == code2).Should().BeTrue();
+        }
+    }
+}
