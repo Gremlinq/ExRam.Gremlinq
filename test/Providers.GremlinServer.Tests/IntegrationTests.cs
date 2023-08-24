eb278153c72c79591c31537d95b3cd01c55eb5b1
Daniel Weber <danielcweber@users.noreply.github.com>
2023-08-24 11:49:52 +0200

    Fix #1121.

--- test/Providers.GremlinServer.Tests/IntegrationTests.cs
+++ test/Providers.GremlinServer.Tests/IntegrationTests.cs
@@ -1,6 +1,8 @@
-﻿using ExRam.Gremlinq.Tests.Fixtures;
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Tests.Fixtures;
 using ExRam.Gremlinq.Tests.Infrastructure;
 using ExRam.Gremlinq.Tests.TestCases;
+using FluentAssertions;
 
 namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
 {
@@ -11,6 +13,53 @@ namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
             new ExecutingVerifier(),
             testOutputHelper)
         {
+        }
+
+        [Fact]
+        public async Task SingleAsync_throws_on_too_many_values()
+        {
+            await _g
+                .Inject(1, 2)
+                .Awaiting(_ => _
+                    .SingleAsync())
+                .Should()
+                .ThrowAsync<InvalidOperationException>();
+        }
+
+        [Fact]
+        public async Task SingleAsync_throws_on_too_few_values()
+        {
+            await _g
+                .Inject(1)
+                .None()
+                .Awaiting(_ => _
+                    .SingleAsync())
+                .Should()
+                .ThrowAsync<InvalidOperationException>();
+        }
+
+        [Fact]
+        public async Task SingleOrDefaultAsync_returns_default_on_too_many_values()
+        {
+            var result = await _g
+                .Inject(1, 2)
+                .SingleOrDefaultAsync();
+
+            result
+                .Should()
+                .Be(0);
+        }
+
+        [Fact]
+        public async Task SingleOrDefaultAsync_throws_on_too_few_values()
+        {
+            var result = await _g
+                .Inject(1)
+                .None()
+                .Awaiting(_ => _
+                    .SingleOrDefaultAsync())
+                .Should()
+                .ThrowAsync<InvalidOperationException>();
         }
     }
 }
