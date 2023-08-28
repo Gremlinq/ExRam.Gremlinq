2c0a21a4b76a2b8ba8aedda956953450b95364de
Daniel Weber <danielcweber@users.noreply.github.com>
2023-08-28 09:51:52 +0200

    Rename Benchmark.

--- src/Benchmarks/BenchmarkRunner/DeserializerBenchmarks.cs
+++ src/Benchmarks/BenchmarkRunner/DeserializerBenchmarks.cs
@@ -7,13 +7,13 @@ using Newtonsoft.Json.Linq;
 namespace Benchmarks
 {
     [MemoryDiagnoser]
-    public class Benchmarks
+    public class DeserializerBenchmarks
     {
         private readonly ITransformer _oldDeserializer;
         private readonly ITransformer _newDeserializer;
         private readonly JObject _source = JObject.Parse("{ \"id\": 13, \"label\": \"Person\", \"type\": \"vertex\", \"properties\": { \"Age\": [ { \"id\": 1, \"value\": \"36\" } ], \"RegistrationDate\": [ { \"id\": 2, \"value\": 1481750076295 } ], \"Gender\": [ { \"id\": 3, \"value\": 1 } ], \"PhoneNumbers\": [ { \"id\": 4, \"value\": \"+123456\" }, { \"id\": 5, \"value\": \"+234567\" } ] } }");
 
-        public Benchmarks()
+        public DeserializerBenchmarks()
         {
             _oldDeserializer = GremlinQueryEnvironment.Default
                .UseNewtonsoftJson()
--- src/Benchmarks/BenchmarkRunner/Program.cs
+++ src/Benchmarks/BenchmarkRunner/Program.cs
@@ -6,7 +6,7 @@ namespace Benchmarks
     {
         private static void Main()
         {
-            Run<Benchmarks>();
+            Run<DeserializerBenchmarks>();
         }
     }
 }
