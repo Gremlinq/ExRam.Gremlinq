d3652271c70f3a7f307a8ca0e6c9779f052bcaf1
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-05 16:20:41 +0200

    Test for Issue219 should have the same outcome on all platforms.

--- test/Support.NewtonsoftJson.Tests/Issues/Issue219.cs
+++ test/Support.NewtonsoftJson.Tests/Issues/Issue219.cs
@@ -1,13 +1,12 @@
 ﻿using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.Transformation;
-using ExRam.Gremlinq.Tests.Infrastructure;
 using Newtonsoft.Json.Linq;
 
 namespace ExRam.Gremlinq.Support.NewtonsoftJson.Tests
 {
     public sealed class Issue219 : VerifyBase
     {
-        public Issue219() : base(GremlinQueryVerifier.DefaultSettings)
+        public Issue219() : base()
         {
 
         }
