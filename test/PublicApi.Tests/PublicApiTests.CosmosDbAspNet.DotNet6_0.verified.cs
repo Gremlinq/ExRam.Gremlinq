677cf1514a8ecdb77c24ef9a7c06013f060149ae
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-05 18:55:50 +0200

    Add back API-snapshots.

---
+++ test/PublicApi.Tests/PublicApiTests.CosmosDbAspNet.DotNet6_0.verified.cs
@@ -0,0 +1,8 @@
+﻿namespace ExRam.Gremlinq.Core.AspNet
+{
+    public static class GremlinqSetupExtensions
+    {
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseCosmosDb(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Action<ExRam.Gremlinq.Providers.Core.AspNet.ProviderSetup<ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator>>? extraSetupAction = null) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseCosmosDb<TVertex, TEdge>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Linq.Expressions.Expression<System.Func<TVertex, object>> partitionKeyExpression, System.Action<ExRam.Gremlinq.Providers.Core.AspNet.ProviderSetup<ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator>>? extraSetupAction = null) { }
+    }
+}
