c7d22a85fea9c2c2c1af402231968207dd15a014
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-06 12:20:50 +0200

    Add ScrubRegex-extension for SettingsTask.

---
+++ test/Tests.Infrastructure/Extensions/SettingsTaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace VerifyTests
+{
+    public static class SettingsTaskExtensions
+    {
+        public static SettingsTask ScrubRegex(this SettingsTask task, Regex regex, string replacement)
+        {
+            return task
+                .ScrubLinesWithReplace(str => regex.Replace(str, replacement));
+        }
+    }
+}
