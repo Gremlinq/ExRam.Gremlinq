3afc973b1155ad0ee185709af0b5fe4d5a2da787
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-06 12:51:00 +0200

    Let id-regex work on individual lines.

--- test/Tests.Infrastructure/Verifiers/ExecutingVerifier.cs
+++ test/Tests.Infrastructure/Verifiers/ExecutingVerifier.cs
@@ -11,7 +11,7 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
 {
     public class ExecutingVerifier : GremlinQueryVerifier
     {
-        private static readonly Regex IdRegex = new("(\"id\"\\s*[:,]\\s*{\\s*\"@type\"\\s*:\\s*\"g:(Int32|Int64|UUID)\"\\s*,\\s*\"@value\":\\s*)([^\\s{}]+)(\\s*})", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex IdRegex = new("\"@value\":[\\s]*[\\d]*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         public ExecutingVerifier([CallerFilePath] string sourceFile = "") : base(sourceFile)
         {
@@ -58,7 +58,7 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
 
         protected override IImmutableList<Func<string, string>> Scrubbers() => base
             .Scrubbers()
-            .Add(x => IdRegex.Replace(x, "$1-1$4"))
+            .Add(x => IdRegex.Replace(x, "\"value\": -1"))
             .ScrubGuids();
     }
 }
