75764ebbc0984dfe3f7871983174c6529742639a
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-07 14:32:54 +0200

    Remove individual fixtures for GraphSon verification but introduce a Verifier.

--- test/Core.Tests/Serialization/Graphson2GremlinQuerySerializationTest.cs
+++ test/Core.Tests/Serialization/Graphson2GremlinQuerySerializationTest.cs
@@ -1,14 +1,16 @@
 ﻿using ExRam.Gremlinq.Tests.Fixtures;
-using ExRam.Gremlinq.Tests.Infrastructure;
 using ExRam.Gremlinq.Tests.TestCases;
+using Gremlin.Net.Structure.IO.GraphSON;
 
 namespace ExRam.Gremlinq.Core.Tests
 {
-    public sealed class Graphson2GremlinQuerySerializationTest : QueryExecutionTest, IClassFixture<Graphson2StringSerializationFixture>
+    public sealed class Graphson2GremlinQuerySerializationTest : QueryExecutionTest
     {
-        public Graphson2GremlinQuerySerializationTest(Graphson2StringSerializationFixture fixture, ITestOutputHelper testOutputHelper) : base(
-            fixture,
-            new SerializingVerifier<string>(),
+        public static readonly GraphSON2Writer GraphSon2Writer = new ();
+
+        public Graphson2GremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
+            GremlinqFixture.Empty,
+            new GraphSonStringSerializingVerifier(GraphSon2Writer),
             testOutputHelper)
         {
         }
---
+++ test/Tests.Fixtures/GraphSonStringSerializingVerifier.cs
@@ -0,0 +1,30 @@
+﻿using Gremlin.Net.Structure.IO.GraphSON;
+using ExRam.Gremlinq.Core.Transformation;
+using Gremlin.Net.Process.Traversal;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using System.Runtime.CompilerServices;
+
+namespace ExRam.Gremlinq.Tests.Fixtures
+{
+    public sealed class GraphSonStringSerializingVerifier : GremlinQueryVerifier
+    {
+        private readonly GraphSONWriter _writer;
+
+        public GraphSonStringSerializingVerifier(GraphSONWriter writer, [CallerFilePath] string sourceFile = "") : base(sourceFile)
+        {
+            _writer = writer;
+        }
+
+        public override SettingsTask Verify<TElement>(IGremlinQueryBase<TElement> query)
+        {
+            var env = query.AsAdmin().Environment;
+
+            return InnerVerify(_writer
+                .WriteObject(env.Serializer
+                    .TransformTo<Bytecode>()
+                    .From(query, env))
+                .FormatJson());
+        }
+    }
+}
