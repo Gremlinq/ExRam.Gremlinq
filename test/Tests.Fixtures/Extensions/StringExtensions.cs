5930577c8cb3535cf66636950a1fa9948b2fa49e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-07 14:47:45 +0200

    Speed up GraphSonStringSerializingVerifier by avoiding a JToken roundtrip.

--- test/Tests.Fixtures/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json.Linq;
-
-namespace System
-{
-    internal static class StringExtensions
-    {
-        public static string FormatJson(this string json) => JToken.Parse(json).ToString();
-    }
-}
