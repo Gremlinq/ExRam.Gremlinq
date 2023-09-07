5672e01b68d27a90c655015315aee20a97433cc2
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-07 14:58:47 +0200

    New tests for groovy in RequestMessages.

---
+++ test/Core.Tests/Serialization/Graphson3WithGroovyGremlinQuerySerializationTest.cs
@@ -0,0 +1,18 @@
+﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.TestCases;
+using Gremlin.Net.Structure.IO.GraphSON;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public sealed class Graphson3WithGroovyGremlinQuerySerializationTest : QueryExecutionTest
+    {
+        public static readonly GraphSON3Writer GraphSon3Writer = new();
+
+        public Graphson3WithGroovyGremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
+            new GroovyGremlinQuerySerializationFixture(),
+            new GraphSonStringSerializingVerifier(GraphSon3Writer),
+            testOutputHelper)
+        {
+        }
+    }
+}
