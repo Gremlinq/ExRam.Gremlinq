9efc754360f1b1a1112f2db6947fac395cab9fce
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-07 14:32:54 +0200

    Remove individual fixtures for GraphSon verification but introduce a Verifier.

--- test/Core.Tests/Serialization/Graphson3GremlinQuerySerializationTest.cs
+++ test/Core.Tests/Serialization/Graphson3GremlinQuerySerializationTest.cs
@@ -1,14 +1,16 @@
 ﻿using ExRam.Gremlinq.Tests.Fixtures;
-using ExRam.Gremlinq.Tests.Infrastructure;
 using ExRam.Gremlinq.Tests.TestCases;
+using Gremlin.Net.Structure.IO.GraphSON;
 
 namespace ExRam.Gremlinq.Core.Tests
 {
-    public sealed class Graphson3GremlinQuerySerializationTest : QueryExecutionTest, IClassFixture<GraphSon3StringSerializationFixture>
+    public sealed class Graphson3GremlinQuerySerializationTest : QueryExecutionTest
     {
-        public Graphson3GremlinQuerySerializationTest(GraphSon3StringSerializationFixture fixture, ITestOutputHelper testOutputHelper) : base(
-            fixture,
-            new SerializingVerifier<string>(),
+        public static readonly GraphSON3Writer GraphSon3Writer = new ();
+
+        public Graphson3GremlinQuerySerializationTest(ITestOutputHelper testOutputHelper) : base(
+            GremlinqFixture.Empty,
+            new GraphSonStringSerializingVerifier(GraphSon3Writer),
             testOutputHelper)
         {
         }
