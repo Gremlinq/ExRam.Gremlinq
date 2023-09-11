ddd96e66d99d1b37e6e4704d51f44e650919ed4e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-11 12:12:32 +0200

    Have a dedicated TestContainerFixture.

--- test/Tests.Fixtures/GremlinServerFixture.cs
+++ test/Tests.Fixtures/GremlinServerFixture.cs
@@ -1,47 +1,19 @@
-﻿using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Containers;
-using ExRam.Gremlinq.Core;
+﻿using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Providers.Core;
 using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Tests.Fixtures
 {
-    public sealed class GremlinServerFixture : GremlinqFixture
+    public sealed class GremlinServerFixture : TestContainerFixture
     {
-        private readonly IContainer _gremlinServerContainer;
-
-        public GremlinServerFixture() : this(new ContainerBuilder()
-            .WithImage("tinkerpop/gremlin-server:3.7.0")
-            .WithPortBinding(8182)
-            .WithWaitStrategy(Wait
-                .ForUnixContainer()
-                .UntilPortIsAvailable(8182))
-            .Build())
+        public GremlinServerFixture() : base(
+            "tinkerpop/gremlin-server:3.7.0",
+            8182,
+            container => g
+                .UseGremlinServer(_ => _
+                    .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
+                    .UseNewtonsoftJson()))
         {
-        }
-
-        private GremlinServerFixture(IContainer container) : base(() => GetQuerySource(container))
-        {
-            _gremlinServerContainer = container;
-        }
-
-        private static async Task<IGremlinQuerySource> GetQuerySource(IContainer container) => g
-            .UseGremlinServer(_ => _
-                .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
-                .UseNewtonsoftJson());
-
-        public override async Task InitializeAsync()
-        {
-            await base.InitializeAsync();
-
-            await _gremlinServerContainer.StartAsync();
-        }
-
-        public override async Task DisposeAsync()
-        {
-            await _gremlinServerContainer.StopAsync();
-
-            await base.DisposeAsync();
         }
     }
 }
---
+++ test/Tests.Fixtures/TestContainerFixture.cs
@@ -0,0 +1,42 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using ExRam.Gremlinq.Core;
+
+namespace ExRam.Gremlinq.Tests.Fixtures
+{
+    public abstract class TestContainerFixture : GremlinqFixture
+    {
+        private readonly IContainer _container;
+
+        protected TestContainerFixture(string image, int port, Func<IContainer, IGremlinQuerySource> sourceFactory) : this(
+            new ContainerBuilder()
+                .WithImage(image)
+                .WithPortBinding(port)
+                .WithWaitStrategy(Wait
+                    .ForUnixContainer()
+                    .UntilPortIsAvailable(port))
+                .Build(),
+            sourceFactory)
+        {
+        }
+
+        private TestContainerFixture(IContainer container, Func<IContainer, IGremlinQuerySource> sourceFactory) : base(async () => sourceFactory(container))
+        {
+            _container = container;
+        }
+
+        public override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            await _container.StartAsync();
+        }
+
+        public override async Task DisposeAsync()
+        {
+            await _container.StopAsync();
+
+            await base.DisposeAsync();
+        }
+    }
+}
