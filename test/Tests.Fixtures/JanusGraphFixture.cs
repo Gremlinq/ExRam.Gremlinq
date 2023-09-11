067c70fe111e58f9e09dc654fbc26badccc482b0
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-11 12:25:37 +0200

    Use TestContainers for the JanusGraph fixture.

--- test/Tests.Fixtures/JanusGraphFixture.cs
+++ test/Tests.Fixtures/JanusGraphFixture.cs
@@ -1,18 +1,22 @@
 ﻿using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.Execution;
 using ExRam.Gremlinq.Providers.Core;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Tests.Fixtures
 {
-    public sealed class JanusGraphFixture : GremlinqFixture
+    public sealed class JanusGraphFixture : TestContainerFixture
     {
-        public JanusGraphFixture() : base(Gremlinq.Core.GremlinQuerySource.g
-            .UseJanusGraph(builder => builder
-                .At(new Uri("ws://localhost:8183"))
-                .UseNewtonsoftJson())
-            .ConfigureEnvironment(environment => environment
-                .ConfigureExecutor(_ => _
-                    .IgnoreResults())))
+        public JanusGraphFixture() : base(
+            "janusgraph/janusgraph",
+            8182,
+            container => g
+                .UseJanusGraph(builder => builder
+                    .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
+                    .UseNewtonsoftJson())
+                .ConfigureEnvironment(environment => environment
+                    .ConfigureExecutor(_ => _
+                        .IgnoreResults())))
         {
         }
     }
