b2a0a9ec7a3986d06d908ce6d10a9da74fe12644
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-11 12:32:34 +0200

    Whitespace

--- test/Tests.Fixtures/NeptuneFixture.cs
+++ test/Tests.Fixtures/NeptuneFixture.cs
@@ -7,12 +7,12 @@ namespace ExRam.Gremlinq.Tests.Fixtures
     public sealed class NeptuneFixture : TestContainerFixture
     {
         public NeptuneFixture() : base(
-           "tinkerpop/gremlin-server:3.7.0",
-           8182,
-           container => g
-               .UseNeptune(_ => _
-                   .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
-                   .UseNewtonsoftJson()))
+            "tinkerpop/gremlin-server:3.7.0",
+            8182,
+            container => g
+                .UseNeptune(_ => _
+                    .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
+                    .UseNewtonsoftJson()))
         {
         }
     }
