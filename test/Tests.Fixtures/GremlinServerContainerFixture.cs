b30d4f02517393a675971e2643eb6f77c4b13991
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-11 14:32:28 +0200

    Separate fixture for Gremlin Server integration tests.

---
+++ test/Tests.Fixtures/GremlinServerContainerFixture.cs
@@ -0,0 +1,19 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Providers.Core;
+using static ExRam.Gremlinq.Core.GremlinQuerySource;
+
+namespace ExRam.Gremlinq.Tests.Fixtures
+{
+    public sealed class GremlinServerContainerFixture : TestContainerFixture
+    {
+        public GremlinServerContainerFixture() : base(
+            "tinkerpop/gremlin-server:3.7.0",
+            8182,
+            container => g
+                .UseGremlinServer(_ => _
+                    .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
+                    .UseNewtonsoftJson()))
+        {
+        }
+    }
+}
--- test/Tests.Fixtures/GremlinServerFixture.cs
+++ test/Tests.Fixtures/GremlinServerFixture.cs
@@ -4,15 +4,12 @@ using static ExRam.Gremlinq.Core.GremlinQuerySource;
 
 namespace ExRam.Gremlinq.Tests.Fixtures
 {
-    public sealed class GremlinServerFixture : TestContainerFixture
+    public sealed class GremlinServerFixture : GremlinqFixture
     {
-        public GremlinServerFixture() : base(
-            "tinkerpop/gremlin-server:3.7.0",
-            8182,
-            container => g
-                .UseGremlinServer(_ => _
-                    .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
-                    .UseNewtonsoftJson()))
+        public GremlinServerFixture() : base(g
+            .UseGremlinServer(_ => _
+                .AtLocalhost()
+                .UseNewtonsoftJson()))
         {
         }
     }
