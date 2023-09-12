df7671dd8db101877b540dc197555f926f238915
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-12 19:41:25 +0200

    Add deserialization tests for integration tests without projection.

---
+++ test/Providers.GremlinServer.Tests/DeserializationWithoutProjectionTests.cs
@@ -0,0 +1,16 @@
+﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using ExRam.Gremlinq.Tests.TestCases;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    public sealed class DeserializationWithoutProjectionTests : QueryExecutionTest, IClassFixture<GremlinServerFixture>
+    {
+        public DeserializationWithoutProjectionTests(GremlinServerFixture fixture, ITestOutputHelper testOutputHelper) : base(
+            fixture,
+            new DeserializingGremlinqVerifier<IntegrationWithoutProjectionTests>(testOutputHelper),
+            testOutputHelper)
+        {
+        }
+    }
+}
