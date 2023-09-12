6028cc86fb68a3156750882c9074a1348be26f15
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-12 11:25:42 +0200

    Split CosmosDbFixture.

--- test/Providers.CosmosDb.Tests/IntegrationTests.cs
+++ test/Providers.CosmosDb.Tests/IntegrationTests.cs
@@ -4,9 +4,9 @@ using ExRam.Gremlinq.Tests.TestCases;
 
 namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
 {
-    public sealed class IntegrationTests : QueryExecutionTest, IClassFixture<CosmosDbFixture>
+    public sealed class IntegrationTests : QueryExecutionTest, IClassFixture<CosmosDbEmulatorFixture>
     {
-        public IntegrationTests(CosmosDbFixture fixture, ITestOutputHelper testOutputHelper) : base(
+        public IntegrationTests(CosmosDbEmulatorFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
             new ExecutingVerifier(),
             testOutputHelper)
---
+++ test/Tests.Fixtures/CosmosDbEmulatorFixture.cs
@@ -0,0 +1,39 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Providers.CosmosDb;
+using ExRam.Gremlinq.Providers.CosmosDb.Tests.Extensions;
+using Microsoft.Azure.Cosmos;
+using Polly;
+
+namespace ExRam.Gremlinq.Tests.Fixtures
+{
+    public sealed class CosmosDbEmulatorFixture : GremlinqFixture
+    {
+        private const string CosmosDbEmulatorDatabaseName = "db";
+        private static readonly string CosmosDbEmulatorCollectionName = "graph";
+        private const string CosmosDbEmulatorAuthKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        protected override async Task<IGremlinQuerySource> TransformQuerySource(IConfigurableGremlinQuerySource g)
+        {
+            using (var cosmosClient = new CosmosClient("https://localhost:8081", CosmosDbEmulatorAuthKey))
+            {
+                await Policy
+                    .Handle<CosmosException>()
+                    .WaitAndRetry(8, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)))
+                    .Execute(async () =>
+                    {
+                        var database = await cosmosClient.CreateDatabaseIfNotExistsAsync(CosmosDbEmulatorDatabaseName, ThroughputProperties.CreateAutoscaleThroughput(40000));
+
+                        await database.Database.CreateContainerIfNotExistsAsync(CosmosDbEmulatorCollectionName, "/PartitionKey");
+                    });
+
+                return g
+                    .UseCosmosDb(builder => builder
+                        .At(new Uri("ws://localhost:8901"), CosmosDbEmulatorDatabaseName, CosmosDbEmulatorCollectionName)
+                        .AuthenticateBy(CosmosDbEmulatorAuthKey)
+                        .UseNewtonsoftJson())
+                    .ConfigureEnvironment(env => env
+                        .AddFakePartitionKey());
+            }
+        }
+    }
+}
