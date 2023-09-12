27fe4fac6c46e6db14355c271e9a495c30241234
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-12 19:26:21 +0200

    Add integration tests for Gremlin Server.

---
+++ test/Tests.Fixtures/GremlinServerWithoutProjectionContainerFixture.cs
@@ -0,0 +1,18 @@
+﻿using DotNet.Testcontainers.Containers;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Providers.Core;
+
+namespace ExRam.Gremlinq.Tests.Fixtures
+{
+    public sealed class GremlinServerWithoutProjectionContainerFixture : TestContainerFixture
+    {
+        public GremlinServerWithoutProjectionContainerFixture() : base("tinkerpop/gremlin-server:3.7.0")
+        {
+        }
+
+        protected override async Task<IGremlinQuerySource> TransformQuerySource(IContainer container, IConfigurableGremlinQuerySource g) => g
+            .UseGremlinServer(_ => _
+                .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
+                .UseNewtonsoftJson());
+    }
+}
