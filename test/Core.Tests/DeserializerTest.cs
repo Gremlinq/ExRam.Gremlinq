d7c77581d0f88879debaf7d569420199f77728d6
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-20 19:44:32 +0200

    Empty deserializer will throw wenn queried.

---
+++ test/Core.Tests/DeserializerTest.cs
@@ -0,0 +1,19 @@
+﻿using ExRam.Gremlinq.Core.Deserialization;
+using ExRam.Gremlinq.Core.Transformation;
+using FluentAssertions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class DeserializerTest
+    {
+        [Fact]
+        public async Task Empty()
+        {
+            Deserializer.Default
+                .Invoking(_ => _
+                    .TryTransformTo<string>().From(42, GremlinQueryEnvironment.Empty))
+                .Should()
+                .Throw<InvalidOperationException>();
+        }
+    }
+}
