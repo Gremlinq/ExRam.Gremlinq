d8296f55d8d1cf8dafb8ebf5a2b45bb77b083ff4
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-24 23:37:06 +0200

    Id review.

--- src/Templates/console/ExRam.Gremlinq.Templates.Console/Pocos/Edge.cs
+++ src/Templates/console/ExRam.Gremlinq.Templates.Console/Pocos/Edge.cs
@@ -2,7 +2,7 @@
 {
     public class Edge
     {
-#if (ProviderIsGremlinServer)
+#if (ProviderIsGremlinServer || ProviderIsJanusGraph)
         public long Id { get; set; }
 #else
         public string Id { get; set; }
