9cec7ba513fd25cff8f6157ca0b4d16c39f3a93b
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-24 22:33:42 +0200

    Add Asp.Net template.

---
+++ src/Templates/aspnet/ExRam.Gremlinq.Templates.AspNet/Controllers/PersonsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ExRam.Gremlinq.Core;
+
+namespace ExRam.Gremlinq.Templates.AspNet
+{
+    [ApiController]
+    [Route("/Persons")]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IGremlinQuerySource _g;
+
+        public PersonsController(IGremlinQuerySource g)
+        {
+            _g = g;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() => Ok(await _g
+            .V<Person>()
+            .ToArrayAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> Add(string name, int age) => Ok(await _g
+            .AddV(new Person { Name = name, Age = age })
+            .FirstAsync());
+    }
+}
