caedbd5424d1bd5dc14a0d65dd522d7465874f5e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-24 21:06:05 +0200

    Add a dotnet new template for a sample console app!

---
+++ src/Templates/console/ExRam.Gremlinq.Templates.Console/Pocos/Cat.cs
@@ -0,0 +1,7 @@
+﻿namespace ExRam.Gremlinq.Templates.Console
+{
+    public class Cat : Pet
+    {
+
+    }
+}
---
+++ src/Templates/console/ExRam.Gremlinq.Templates.Console/Pocos/Edge.cs
@@ -0,0 +1,8 @@
+﻿namespace ExRam.Gremlinq.Templates.Console
+{
+    public class Edge
+    {
+        public object? Id { get; set; }
+        public string? Label { get; set; }
+    }
+}
