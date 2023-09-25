664ef022d4604b444a8908387f882078076c389f
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-25 23:28:10 +0200

    Style.

--- src/Core/Models/IGraphElementModel.cs
+++ src/Core/Models/IGraphElementModel.cs
@@ -5,13 +5,13 @@ namespace ExRam.Gremlinq.Core.Models
 {
     public interface IGraphElementModel
     {
-        ElementMetadata GetMetadata(Type elementType);
-
         IGraphElementModel AddAssemblies(params Assembly[] assemblies);
 
         IGraphElementModel ConfigureMetadata(Func<Type, ElementMetadata, ElementMetadata> metaDataTransformation);
 
         IGraphElementModel ConfigureMetadata(Type elementType, Func<ElementMetadata, ElementMetadata> metaDataTransformation);
+
+        ElementMetadata GetMetadata(Type elementType);
 
         IImmutableSet<Type> ElementTypes { get; }
     }
