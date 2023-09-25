87caf682b8b3c605db5583db11f8f9b321706a76
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-25 19:58:37 +0200

    Support adding assemblies to models.

--- src/Core/Models/IGraphModel.cs
+++ src/Core/Models/IGraphModel.cs
@@ -1,10 +1,14 @@
-﻿namespace ExRam.Gremlinq.Core.Models
+﻿using System.Reflection;
+
+namespace ExRam.Gremlinq.Core.Models
 {
     public interface IGraphModel
     {
         IGraphModel ConfigureVertices(Func<IGraphElementModel, IGraphElementModel> transformation);
         IGraphModel ConfigureEdges(Func<IGraphElementModel, IGraphElementModel> transformation);
         IGraphModel ConfigureProperties(Func<IGraphElementPropertyModel, IGraphElementPropertyModel> transformation);
+
+        IGraphModel AddAssemblies(params Assembly[] assemblies);
 
         IGraphElementModel VerticesModel { get; }
         IGraphElementModel EdgesModel { get; }
