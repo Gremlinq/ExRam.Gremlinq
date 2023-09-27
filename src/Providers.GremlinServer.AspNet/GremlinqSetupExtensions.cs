243fc9cff3b83f7d5b2bb9a22e190708d1057ca1
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-27 14:42:56 +0200

    UseGremlinServer takes element base type parameters.

--- src/Providers.GremlinServer.AspNet/GremlinqSetupExtensions.cs
+++ src/Providers.GremlinServer.AspNet/GremlinqSetupExtensions.cs
@@ -6,26 +6,17 @@ namespace ExRam.Gremlinq.Core.AspNet
 {
     public static class GremlinqSetupExtensions
     {
-        public static GremlinqSetup UseGremlinServer(this GremlinqSetup setup, Action<ProviderSetup<IGremlinServerConfigurator>>? extraSetupAction = null)
+        public static GremlinqSetup UseGremlinServer<TVertex, TEdge>(this GremlinqSetup setup, Action<ProviderSetup<IGremlinServerConfigurator>>? extraSetupAction = null)
         {
             return setup
                 .UseProvider(
                     "GremlinServer",
                     (source, configuratorTransformation) => source
-                        .UseGremlinServer(configuratorTransformation),
+                        .UseGremlinServer<TVertex, TEdge>(configuratorTransformation),
                     setup => setup
                         .Configure()
                         .ConfigureWebSocket(),
                     extraSetupAction);
-        }
-
-        public static GremlinqSetup UseGremlinServer<TVertex, TEdge>(this GremlinqSetup setup, Action<ProviderSetup<IGremlinServerConfigurator>>? extraSetupAction = null)
-        {
-            return setup
-                .UseGremlinServer(extraSetupAction)
-                .ConfigureEnvironment(env => env
-                    .UseModel(GraphModel
-                        .FromBaseTypes<TVertex, TEdge>()));
         }
     }
 }
--- test/PublicApi.Tests/PublicApiTests.GremlinServer.DotNet5_0.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.GremlinServer.DotNet5_0.verified.cs
@@ -2,7 +2,7 @@
 {
     public static class ConfigurableGremlinQuerySourceExtensions
     {
-        public static ExRam.Gremlinq.Core.IGremlinQuerySource UseGremlinServer(this ExRam.Gremlinq.Core.IConfigurableGremlinQuerySource source, System.Func<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator, ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation> configuratorTransformation) { }
+        public static ExRam.Gremlinq.Core.IGremlinQuerySource UseGremlinServer<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.IConfigurableGremlinQuerySource source, System.Func<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator, ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation> configuratorTransformation) { }
     }
 }
 namespace ExRam.Gremlinq.Providers.GremlinServer
