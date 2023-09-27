248c9856c80d68d6b662e16b7f22b0918528dbfa
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-27 23:01:30 +0200

    Api Snapshot fixes.

--- test/PublicApi.Tests/PublicApiTests.CosmosDbAspNet.DotNet6_0.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.CosmosDbAspNet.DotNet6_0.verified.cs
@@ -2,6 +2,6 @@
 {
     public static class GremlinqSetupExtensions
     {
-        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseCosmosDb<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Action<ExRam.Gremlinq.Providers.Core.AspNet.ProviderSetup<ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase>>>? extraSetupAction = null) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseCosmosDb<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Func<ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase>, Microsoft.Extensions.Configuration.IConfigurationSection, ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase>>? configuration = null) { }
     }
 }
--- test/PublicApi.Tests/PublicApiTests.JanusGraphAspNet.DotNet5_0.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.JanusGraphAspNet.DotNet5_0.verified.cs
@@ -2,6 +2,6 @@
 {
     public static class GremlinqSetupExtensions
     {
-        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseJanusGraph<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Action<ExRam.Gremlinq.Providers.Core.AspNet.ProviderSetup<ExRam.Gremlinq.Providers.JanusGraph.IJanusGraphConfigurator>>? extraSetupAction = null) { }
+        public static ExRam.Gremlinq.Core.AspNet.GremlinqSetup UseJanusGraph<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.AspNet.GremlinqSetup setup, System.Func<ExRam.Gremlinq.Providers.JanusGraph.IJanusGraphConfigurator, Microsoft.Extensions.Configuration.IConfigurationSection, ExRam.Gremlinq.Providers.JanusGraph.IJanusGraphConfigurator>? configuration = null) { }
     }
 }
