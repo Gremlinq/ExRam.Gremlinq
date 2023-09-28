ea7b9ac6815aedfd52b726847b097121a3da50aa
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-28 13:45:28 +0200

    Add JanusGraph.AspNet test project.

---
+++ test/Providers.JanusGraph.AspNet.Tests/ConfigurationTests.cs
@@ -0,0 +1,32 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.AspNet;
+using ExRam.Gremlinq.Tests.Entities;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExRam.Gremlinq.Providers.JanusGraph.AspNet.Tests
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void Source_can_be_created()
+        {
+            new ServiceCollection()
+                .AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string?>
+                    {
+                        { "Gremlinq:JanusGraph:Uri", "wss://your_cosmosdb-endpoint.gremlin.cosmos.azure.com:443/" },
+                    })
+                    .Build())
+                .AddGremlinq(setup => setup
+                    .UseJanusGraph<Vertex, Edge>())
+                .BuildServiceProvider()
+                .GetRequiredService<IGremlinQuerySource>()
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
