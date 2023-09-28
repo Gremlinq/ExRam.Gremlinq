510ead90f08f5dd676d920e4b38eb3a9e8bd05aa
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-28 20:53:39 +0200

    Define UseNewtonsoftJson on IGremlinQuerySource, allow configuring it on an Asp GremlinqSetup.

---
+++ src/Support.NewtonsoftJson.AspNet/GremlinqSetupExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ExRam.Gremlinq.Core.AspNet
+{
+    public static class GremlinqSetupExtensions
+    {
+        public static GremlinqSetup UseNewtonsoftJson(this GremlinqSetup setup) => setup
+            .ConfigureQuerySource(source => source
+                .UseNewtonsoftJson());
+    }
+}
---
+++ src/Support.NewtonsoftJson/Extensions/GremlinQuerySourceExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ExRam.Gremlinq.Core
+{
+    public static class GremlinQuerySourceExtensions
+    {
+        public static IGremlinQuerySource UseNewtonsoftJson(this IGremlinQuerySource source)
+        {
+            return source
+                .ConfigureEnvironment(env => env
+                    .ConfigureDeserializer(deserializer => deserializer
+                        .UseNewtonsoftJson()));
+        }
+    }
+}
