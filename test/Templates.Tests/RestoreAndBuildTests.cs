e08feda859fbe1de66a32705ac3b7de22b13dfd9
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-28 15:22:54 +0200

    Add a unit test project for the templates. It relies on the great Boxed.DotnetNewTest project!

---
+++ test/Templates.Tests/RestoreAndBuildTests.cs
@@ -0,0 +1,33 @@
+﻿using Boxed.DotnetNewTest;
+
+namespace ExRam.Gremlinq.Templates.Tests
+{
+    public class RestoreAndBuildTests
+    {
+        public RestoreAndBuildTests() => DotnetNew.InstallAsync<RestoreAndBuildTests>("ExRam.Gremlinq.sln").Wait();
+
+        [Fact]
+        public async Task Gremlinq_Console()
+        {
+            await using (var tempDirectory = TempDirectory.NewTempDirectory())
+            {
+                var project = await tempDirectory.DotnetNewAsync("gremlinq-console", "Gremlinq.Console.Template.TestProject");
+
+                await project.DotnetRestoreAsync();
+                await project.DotnetBuildAsync();
+            }
+        }
+
+        [Fact]
+        public async Task Gremlinq_AspNet()
+        {
+            await using (var tempDirectory = TempDirectory.NewTempDirectory())
+            {
+                var project = await tempDirectory.DotnetNewAsync("gremlinq-aspnet", "Gremlinq.AspNet.Template.TestProject");
+
+                await project.DotnetRestoreAsync();
+                await project.DotnetBuildAsync();
+            }
+        }
+    }
+}
