bec992c50c7a5cbebb0e95af2f15db765eb2c205
Daniel Weber <danielcweber@users.noreply.github.com>
2023-09-28 13:47:30 +0200

    Add Neptune.AspNet tests.

---
+++ test/Providers.Neptune.AspNet.Tests/ConfigurationTests.cs
@@ -0,0 +1,32 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.AspNet;
+using ExRam.Gremlinq.Tests.Entities;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExRam.Gremlinq.Providers.Neptune.AspNet.Tests
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void Source_can_be_created()
+        {
+            new ServiceCollection()
+                .AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string?>
+                    {
+                        { "Gremlinq:Neptune:Uri", "wss://your_cosmosdb-endpoint.gremlin.cosmos.azure.com:443/" },
+                    })
+                    .Build())
+                .AddGremlinq(setup => setup
+                    .UseNeptune<Vertex, Edge>())
+                .BuildServiceProvider()
+                .GetRequiredService<IGremlinQuerySource>()
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
