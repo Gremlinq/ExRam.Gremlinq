53d0035279e90ac8aa80426ff448a9c24d3b2ba3
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-01 20:13:28 +0200

    Remove the need for sectionName in ProviderSetupInfo.

--- src/Providers.Core.AspNet/Setup/ProviderSetupInfo.cs
+++ src/Providers.Core.AspNet/Setup/ProviderSetupInfo.cs
@@ -5,13 +5,11 @@ namespace ExRam.Gremlinq.Core.AspNet
     internal sealed class ProviderSetupInfo<TConfigurator>
         where TConfigurator : IProviderConfigurator<TConfigurator>
     {
-        public ProviderSetupInfo(string sectionName, Func<IConfigurableGremlinQuerySource, Func<TConfigurator, IGremlinQuerySourceTransformation>, IGremlinQuerySource> providerChoice)
+        public ProviderSetupInfo(Func<IConfigurableGremlinQuerySource, Func<TConfigurator, IGremlinQuerySourceTransformation>, IGremlinQuerySource> providerChoice)
         {
-            SectionName = sectionName;
             ProviderChoice = providerChoice;
         }
 
-        public string SectionName { get; }
         public Func<IConfigurableGremlinQuerySource, Func<TConfigurator, IGremlinQuerySourceTransformation>, IGremlinQuerySource> ProviderChoice { get; }
     }
 }
