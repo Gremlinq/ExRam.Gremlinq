555807d8cdeea692e0c3ad994944f14a1c120d07
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-02 16:57:15 +0200

    Move most extensions for GremlinServiceBuilder into Core.AspNet.

--- test/PublicApi.Tests/PublicApiTests.ProvidersCoreAspNet.DotNet6_0.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.ProvidersCoreAspNet.DotNet6_0.verified.cs
@@ -2,11 +2,7 @@
 {
     public static class GremlinqServicesBuilderExtensions
     {
-        public static ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder<TConfigurator> ConfigureBase<TConfigurator>(this ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder<TConfigurator> builder)
-            where TConfigurator : ExRam.Gremlinq.Core.IGremlinqConfigurator<TConfigurator> { }
         public static ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder<TConfigurator> ConfigureWebSocket<TConfigurator>(this ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder<TConfigurator> builder)
             where TConfigurator : ExRam.Gremlinq.Providers.Core.IWebSocketProviderConfigurator<TConfigurator> { }
-        public static ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder<TConfigurator> UseProvider<TConfigurator>(this ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder setup, System.Func<ExRam.Gremlinq.Core.IConfigurableGremlinQuerySource, System.Func<System.Func<TConfigurator, ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation>, ExRam.Gremlinq.Core.IGremlinQuerySource>> providerChoice)
-            where TConfigurator : ExRam.Gremlinq.Core.IGremlinqConfigurator<TConfigurator> { }
     }
 }
