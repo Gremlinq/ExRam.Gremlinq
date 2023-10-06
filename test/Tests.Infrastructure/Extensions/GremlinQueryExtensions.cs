ad44ae3381952673576240e2551b3bccced1258e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-06 17:06:25 +0200

    Allow enabling AutoVerify on the commandline.

--- test/Tests.Infrastructure/Extensions/GremlinQueryExtensions.cs
+++ test/Tests.Infrastructure/Extensions/GremlinQueryExtensions.cs
@@ -4,6 +4,15 @@ namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryExtensions
     {
-        public static SettingsTask Verify<TElement>(this IGremlinQueryBase<TElement> query) => GremlinqTestBase.Current.Verifier.Verify(query);
+        public static SettingsTask Verify<TElement>(this IGremlinQueryBase<TElement> query)
+        {
+            var task = GremlinqTestBase.Current.Verifier.Verify(query);
+
+#if AUTO_VERIFY
+            task = task.AutoVerify();
+#endif
+
+            return task;
+        }
     }
 }
