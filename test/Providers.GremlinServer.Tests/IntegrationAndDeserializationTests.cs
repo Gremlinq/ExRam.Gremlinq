da786323470f17e4ee43b9988dc44e2bba7e2fdd
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-11 15:00:34 +0200

    Add a new notion of integration test. The snapshots are first to be taken from the existing deserialization snapshots.

---
+++ test/Providers.GremlinServer.Tests/IntegrationAndDeserializationTests.cs
@@ -0,0 +1,17 @@
+﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using ExRam.Gremlinq.Tests.TestCases;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    [IntegrationTest]
+    public sealed class IntegrationAndDeserializationTests : QueryExecutionTest, IClassFixture<GremlinServerContainerFixture>
+    {
+        public IntegrationAndDeserializationTests(GremlinServerContainerFixture fixture, ITestOutputHelper testOutputHelper) : base(
+            fixture,
+            new DeserializingGremlinqVerifier<IntegrationTests>(testOutputHelper),
+            testOutputHelper)
+        {
+        }
+    }
+}
