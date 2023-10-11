ebc8469153d412e8d5e1ee2b20de9a043a0c179b
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-11 18:11:51 +0200

    Add actually executing verifier.

---
+++ test/Tests.Infrastructure/Verifiers/ExecutingVerifier.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Execution;
+
+namespace ExRam.Gremlinq.Tests.Infrastructure
+{
+    public class ExecutingVerifier : GremlinQueryVerifier
+    {
+        public ExecutingVerifier(Func<SettingsTask, SettingsTask>? settingsTaskModifier = null, [CallerFilePath] string sourceFile = "") : base(settingsTaskModifier, sourceFile)
+        {
+        }
+
+        public override SettingsTask Verify<TElement>(IGremlinQueryBase<TElement> query) => InnerVerify(Execute(query));
+
+        private async Task<TElement[]> Execute<TElement>(IGremlinQueryBase<TElement> query)
+        {
+            try
+            {
+                return await query
+                    .ToAsyncEnumerable()
+                    .ToArrayAsync();
+            }
+            catch (GremlinQueryExecutionException)
+            {
+                return Array.Empty<TElement>();
+            }
+        }
+
+        protected override SettingsTask InnerVerify<T>(ValueTask<T> value) => base
+            .InnerVerify(value)
+            .DontScrubDateTimes()
+            .DontIgnoreEmptyCollections()
+            .DontScrubGuids()
+            .ScrubGuids();
+    }
+}
