eeca0dc1bbd9ee5d0dac1f1178e457f11f23c0e1
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-16 19:03:16 +0200

    Add test for RetryWithExponentialBackoff.

--- test/Core.Tests/GremlinQueryExecutorTest.cs
+++ test/Core.Tests/GremlinQueryExecutorTest.cs
@@ -2,13 +2,15 @@
 
 using FluentAssertions;
 
+using NSubstitute;
+
 namespace ExRam.Gremlinq.Core.Tests
 {
-    public class GremlinQueryExecutorTest
+    public class GremlinQueryExecutorTest : VerifyBase
     {
         private readonly IVertexGremlinQuery<object> _query;
 
-        public GremlinQueryExecutorTest()
+        public GremlinQueryExecutorTest() : base()
         {
             _query = GremlinQuerySource.g.ConfigureEnvironment(_ => _).V();
         }
@@ -22,6 +24,23 @@ namespace ExRam.Gremlinq.Core.Tests
                     .ToArrayAsync())
                 .Should()
                 .ThrowAsync<InvalidOperationException>();
+        }
+
+        [Fact]
+        public async Task RetryWithExponentialBackoff()
+        {
+            var baseExecutor = Substitute.For<IGremlinQueryExecutor>();
+
+            baseExecutor
+                .Execute<object>(Arg.Any<GremlinQueryExecutionContext>())
+                .Returns(
+                    _ => AsyncEnumerableEx.Throw<object>(new GremlinQueryExecutionException(_.Arg<GremlinQueryExecutionContext>(), new DivideByZeroException())),
+                    _ => AsyncEnumerableEx.Return<object>("Result"));
+
+            await Verify(baseExecutor
+                .RetryWithExponentialBackoff((_, ex) => true)
+                .Execute<object>(GremlinQueryExecutionContext.Create(_query))
+                .ToArrayAsync());
         }
     }
 }
