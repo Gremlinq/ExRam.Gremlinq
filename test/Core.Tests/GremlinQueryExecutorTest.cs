6537d39877405b24d9d868e498895889c5055783
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-16 18:51:45 +0200

    Add GremlinQueryExecutorTest.

---
+++ test/Core.Tests/GremlinQueryExecutorTest.cs
@@ -0,0 +1,27 @@
+﻿using ExRam.Gremlinq.Core.Execution;
+
+using FluentAssertions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class GremlinQueryExecutorTest
+    {
+        private readonly IVertexGremlinQuery<object> _query;
+
+        public GremlinQueryExecutorTest()
+        {
+            _query = GremlinQuerySource.g.ConfigureEnvironment(_ => _).V();
+        }
+
+        [Fact]
+        public void Invalid()
+        {
+            GremlinQueryExecutor.Invalid
+                .Execute<object>(GremlinQueryExecutionContext.Create(_query))
+                .Awaiting(ex => ex
+                    .ToArrayAsync())
+                .Should()
+                .ThrowAsync<InvalidOperationException>();
+        }
+    }
+}
