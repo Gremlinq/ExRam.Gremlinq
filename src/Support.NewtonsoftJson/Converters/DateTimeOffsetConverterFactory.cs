89e6fe682f3e13306f8f1704232c4f8b323cf20b
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-17 14:53:08 +0200

    Optimize JValue handling by deferring to DateTimeOffset.

--- src/Support.NewtonsoftJson/Converters/DateTimeOffsetConverterFactory.cs
+++ src/Support.NewtonsoftJson/Converters/DateTimeOffsetConverterFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using ExRam.Gremlinq.Core.Transformation;
 using ExRam.Gremlinq.Core;
+using System.Globalization;
 
 namespace ExRam.Gremlinq.Support.NewtonsoftJson
 {
@@ -8,13 +9,15 @@ namespace ExRam.Gremlinq.Support.NewtonsoftJson
     {
         protected override DateTimeOffset? Convert(JValue jValue, IGremlinQueryEnvironment environment, ITransformer recurse)
         {
-            return jValue.Value switch
+            return jValue switch
             {
-                DateTime dateTime => new DateTimeOffset(dateTime),
-                DateTimeOffset dateTimeOffset => dateTimeOffset,
-                _ when jValue.Type == JTokenType.Integer => DateTimeOffset.FromUnixTimeMilliseconds(jValue.Value<long>()),
+                { Value: DateTimeOffset dateTimeOffset } => dateTimeOffset,
+                { Value: DateTime dateTime } => new DateTimeOffset(dateTime),
+                { Value: string dateTimeString } when DateTime.TryParse(dateTimeString, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var parseResult) => parseResult,
+                { Type: JTokenType.Integer } => DateTimeOffset.FromUnixTimeMilliseconds(jValue.Value<long>()),
                 _ => default(DateTimeOffset?)
             };
         }
     }
 }
+
