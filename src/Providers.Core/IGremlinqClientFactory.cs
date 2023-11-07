c6e1db6dc313e7a1c913f762817ed5b1834b9a22
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-07 15:05:47 +0100

    Change IGremlinClientFactory to IGremlinqClientFactory, factory out a IGremlinqClient interface, use it in various places.

---
+++ src/Providers.Core/IGremlinqClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.WebSockets;
+using ExRam.Gremlinq.Core;
+using Gremlin.Net.Driver;
+
+namespace ExRam.Gremlinq.Providers.Core
+{
+    public interface IGremlinqClientFactory
+    {
+        IGremlinqClientFactory ConfigureServer(Func<GremlinServer, GremlinServer> transformation);
+
+        IGremlinqClientFactory ConfigureWebSocketOptions(Action<ClientWebSocketOptions> configuration);
+
+        IGremlinqClientFactory ConfigureConnectionPool(Action<ConnectionPoolSettings> configuration);
+
+        IGremlinqClient Create(IGremlinQueryEnvironment environment);
+    }
+}
