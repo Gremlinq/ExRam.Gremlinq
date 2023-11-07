3c2c63e94d808618a05a546d357ee327ebc37725
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-07 20:44:11 +0100

    Factor out receive-loop.

---
+++ src/Providers.Core/Extensions/MemoryPoolExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Buffers;
+
+namespace ExRam.Gremlinq.Providers.Core
+{
+    internal static class MemoryPoolExtensions
+    {
+        public static IMemoryOwner<T> Double<T>(this MemoryPool<T> pool, IMemoryOwner<T> memory)
+        {
+            using (memory)
+            {
+                var newMemory = pool.Rent(memory.Memory.Length * 2);
+
+                memory.Memory.CopyTo(newMemory.Memory);
+
+                return newMemory;
+            }
+        }
+    }
+}
