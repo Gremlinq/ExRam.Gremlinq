19760e99b91327e14f73da79154d512aa38caa34
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-08 12:12:36 +0100

    Factor byte[]->JToken deserialization out into own ConverterFactory.

--- src/Support.NewtonsoftJson/Extensions/TransformerExtensions.cs
+++ src/Support.NewtonsoftJson/Extensions/TransformerExtensions.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.Transformation;
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace ExRam.Gremlinq.Support.NewtonsoftJson
 {
@@ -78,27 +73,7 @@ namespace ExRam.Gremlinq.Support.NewtonsoftJson
                 .Add(new ByteArrayDeferralConverterFactory())
                 .Add(new NewtonsoftJsonSerializerConverterFactory())
                 .Add(new VertexPropertyPropertiesConverterFactory())
-                .Add(ConverterFactory
-                    .Create<ReadOnlyMemory<byte>, JToken>((memory, _, _, _) =>
-                    {
-                        var stream = default(Stream?);
-
-                        if (MemoryMarshal.TryGetArray(memory, out var segment) && segment is { Array: { } array })
-                            stream = new MemoryStream(array, segment.Offset, segment.Count);
-                        else
-                            stream = new MemoryStream(memory.ToArray());
-
-                        using (stream)
-                        {
-                            using (var streamReader = new StreamReader(stream))
-                            {
-                                using (var jsonTextReader = new JsonTextReader(streamReader) { DateParseHandling = DateParseHandling.None })
-                                {
-                                    return JToken.ReadFrom(jsonTextReader);
-                                }
-                            }
-                        }
-                    }))
+                .Add(new ByteArrayToJTokenDeferralConverterFactory())
                 .Add(new ResponseMessageConverterFactory())
                 .Add(new DictionaryConverterFactory())
                 .Add(new DynamicObjectConverterFactory())
