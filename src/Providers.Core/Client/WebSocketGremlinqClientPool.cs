5d46330d1cbb2063e4e19ae5a875ead1a182e598
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-08 20:06:03 +0100

    Add stub for a pool.

---
+++ src/Providers.Core/Client/WebSocketGremlinqClientPool.cs
@@ -0,0 +1,19 @@
+﻿using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Messages;
+
+namespace ExRam.Gremlinq.Providers.Core
+{
+    internal sealed class WebSocketGremlinqClientPool : IGremlinClient
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultSet<T>> SubmitAsync<T>(RequestMessage requestMessage, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
