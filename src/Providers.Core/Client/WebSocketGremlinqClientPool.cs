11981cd15be10cfa90dc72276a3dec390f470b9e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-08 20:27:40 +0100

    First implementation of WebSocketGremlinqClientPool which does round-robin.

--- src/Providers.Core/Client/WebSocketGremlinqClientPool.cs
+++ src/Providers.Core/Client/WebSocketGremlinqClientPool.cs
@@ -1,18 +1,53 @@
-﻿using Gremlin.Net.Driver;
+﻿using ExRam.Gremlinq.Core;
+using System.Net.WebSockets;
+
+using Gremlin.Net.Driver;
 using Gremlin.Net.Driver.Messages;
+using System.Runtime.CompilerServices;
 
 namespace ExRam.Gremlinq.Providers.Core
 {
-    internal sealed class WebSocketGremlinqClientPool : IGremlinClient
+    internal sealed class WebSocketGremlinqClientPool : IGremlinqClient
     {
-        public void Dispose()
+        private int _connectionIndex = 0;
+        private readonly IGremlinqClient[] _clients;
+
+        public WebSocketGremlinqClientPool(GremlinServer server, ConnectionPoolSettings poolSettings, Action<ClientWebSocketOptions> optionsTransformation, IGremlinQueryEnvironment environment)
         {
-            throw new NotImplementedException();
+            _clients = Enumerable
+                .Range(0, poolSettings.PoolSize)
+                .Select(_ => new WebSocketGremlinqClient(server, optionsTransformation, environment))
+                .ToArray();
         }
 
-        public Task<ResultSet<T>> SubmitAsync<T>(RequestMessage requestMessage, CancellationToken cancellationToken = default)
+        public IAsyncEnumerable<ResponseMessage<T>> SubmitAsync<T>(RequestMessage message)
         {
-            throw new NotImplementedException();
+            return Core(message, this);
+
+            static async IAsyncEnumerable<ResponseMessage<T>> Core(RequestMessage message, WebSocketGremlinqClientPool @this, [EnumeratorCancellation] CancellationToken ct = default)
+            {
+                var slot = Math.Abs((Interlocked.Increment(ref @this._connectionIndex) - 1) % @this._clients.Length);
+
+                try
+                {
+                    await foreach(var item in @this._clients[slot].SubmitAsync<T>(message).WithCancellation(ct))
+                    {
+                        yield return item;
+                    }
+                }
+                finally
+                {
+                    Interlocked.Decrement(ref @this._connectionIndex);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach(var client in _clients)
+            {
+                client.Dispose();
+            }
         }
     }
 }
