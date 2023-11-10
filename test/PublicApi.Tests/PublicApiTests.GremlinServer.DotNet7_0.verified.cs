1e1ae5c6c8f66f2485b6a3ed453cccf467813e63
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-10 21:18:36 +0100

    Snapshots.

--- test/PublicApi.Tests/PublicApiTests.GremlinServer.DotNet7_0.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.GremlinServer.DotNet7_0.verified.cs
@@ -4,5 +4,5 @@
     {
         public static ExRam.Gremlinq.Core.IGremlinQuerySource UseGremlinServer<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.IGremlinQuerySource source, System.Func<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator, ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation> configuratorTransformation) { }
     }
-    public interface IGremlinServerConfigurator : ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation, ExRam.Gremlinq.Core.IGremlinqConfigurator<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator>, ExRam.Gremlinq.Providers.Core.IWebSocketProviderConfigurator<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator> { }
+    public interface IGremlinServerConfigurator : ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation, ExRam.Gremlinq.Core.IGremlinqConfigurator<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator>, ExRam.Gremlinq.Providers.Core.IProviderConfigurator<ExRam.Gremlinq.Providers.GremlinServer.IGremlinServerConfigurator, ExRam.Gremlinq.Providers.Core.IPoolGremlinqClientFactory<ExRam.Gremlinq.Providers.Core.IWebSocketGremlinqClientFactory>> { }
 }
