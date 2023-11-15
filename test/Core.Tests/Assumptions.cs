c1191142bedf0ee175097eaeb74e4cd76c5474a5
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-15 12:11:27 +0100

    Add an assumption about SemaphoreSlim.

---
+++ test/Core.Tests/Assumptions.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class Assumptions
+    {
+        [Fact]
+        public async Task SemaphoreSlim_WaitAsync_fails_upon_cancellation()
+        {
+            var semaphore = new SemaphoreSlim(0);
+            var cts = new CancellationTokenSource();
+
+            var waitTask = semaphore.WaitAsync(cts.Token);
+            await Task.Delay(100);
+
+            waitTask.IsCompleted
+                .Should()
+                .BeFalse();
+
+            cts.Cancel();
+
+            await waitTask
+                .Awaiting(_ => _)
+                .Should()
+                .ThrowAsync<OperationCanceledException>();
+        }
+    }
+}
