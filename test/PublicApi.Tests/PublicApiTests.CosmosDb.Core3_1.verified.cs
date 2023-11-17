a581ef3eddfeb218ab0fcfbe7882ee6d236ed19e
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-17 14:38:52 +0100

    Add equality operators on CosmosDbKey.

--- test/PublicApi.Tests/PublicApiTests.CosmosDb.Core3_1.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.CosmosDb.Core3_1.verified.cs
@@ -10,12 +10,17 @@
         public static ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase> At<TVertexBase>(this ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase> configurator, System.Uri uri, string databaseName, string graphName) { }
         public static ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase> AtLocalhost<TVertexBase>(this ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase> configurator, string databaseName, string graphName) { }
     }
-    public readonly struct CosmosDbKey
+    public readonly struct CosmosDbKey : System.IEquatable<ExRam.Gremlinq.Providers.CosmosDb.CosmosDbKey>
     {
         public CosmosDbKey(string id) { }
         public CosmosDbKey(string partitionKey, string id) { }
         public string Id { get; }
         public string? PartitionKey { get; }
+        public bool Equals(ExRam.Gremlinq.Providers.CosmosDb.CosmosDbKey other) { }
+        public override bool Equals(object? obj) { }
+        public override int GetHashCode() { }
+        public static bool operator !=(ExRam.Gremlinq.Providers.CosmosDb.CosmosDbKey left, ExRam.Gremlinq.Providers.CosmosDb.CosmosDbKey right) { }
+        public static bool operator ==(ExRam.Gremlinq.Providers.CosmosDb.CosmosDbKey left, ExRam.Gremlinq.Providers.CosmosDb.CosmosDbKey right) { }
     }
     public interface ICosmosDbConfigurator<TVertexBase> : ExRam.Gremlinq.Core.IGremlinQuerySourceTransformation, ExRam.Gremlinq.Core.IGremlinqConfigurator<ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase>>, ExRam.Gremlinq.Providers.Core.IProviderConfigurator<ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase>, ExRam.Gremlinq.Providers.Core.IPoolGremlinqClientFactory<ExRam.Gremlinq.Providers.Core.IWebSocketGremlinqClientFactory>>
     {
