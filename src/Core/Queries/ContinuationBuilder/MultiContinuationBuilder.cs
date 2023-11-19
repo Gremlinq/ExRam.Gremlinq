e6a70ec87d73d503dd9a6d3f4ab3a975a09683f5
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-19 22:05:11 +0100

    More style.

--- src/Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
+++ src/Core/Queries/ContinuationBuilder/MultiContinuationBuilder.cs
@@ -20,17 +20,12 @@ namespace ExRam.Gremlinq.Core
         }
 
         public MultiContinuationBuilder<TOuterQuery, TAnonymousQuery> With<TProjectedQuery, TState>(Func<TAnonymousQuery, TState, TProjectedQuery> continuation, TState state)
-            where TProjectedQuery : IGremlinQueryBase
-        {
-            return With(
+            where TProjectedQuery : IGremlinQueryBase => With(
                 static (outer, anonymous, continuations, flags, state) => new MultiContinuationBuilder<TOuterQuery, TAnonymousQuery>(outer, anonymous, continuations.Push(state.continuation.Apply(anonymous, state.state)), flags),
                 (continuation, state));
-        }
 
         public TNewQuery Build<TNewQuery, TState>(FinalContinuationBuilderTransformation<TOuterQuery, TNewQuery, TState> builderTransformation, TState state)
-            where TNewQuery : IGremlinQueryBase
-        {
-            return With(
+            where TNewQuery : IGremlinQueryBase => With(
                 static (outer, _, continuations, flags, state) =>
                 {
                     var (builderTransformation, innerState) = state;
@@ -66,7 +61,6 @@ namespace ExRam.Gremlinq.Core
                     }
                 },
                 (builderTransformation, state));
-        }
 
         private TResult With<TState, TResult>(Func<TOuterQuery, TAnonymousQuery, FastImmutableList<IGremlinQueryBase>, ContinuationFlags, TState, TResult> continuation, TState state) => _outer is { } outer && _anonymous is { } anonymous && _continuations is var continuations
             ? continuation(outer, anonymous, continuations, _flags, state)
