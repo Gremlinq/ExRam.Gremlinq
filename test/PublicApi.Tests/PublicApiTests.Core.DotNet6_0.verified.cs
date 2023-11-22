f9d2ded2a9ad4cfce8aa82b6e3552a1a1d115765
Daniel Weber <danielcweber@users.noreply.github.com>
2023-11-22 19:01:46 +0100

    Change IVertexPropertyGremlinQuery.ValueMap signature to return maps.

--- test/PublicApi.Tests/PublicApiTests.Core.DotNet6_0.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.Core.DotNet6_0.verified.cs
@@ -979,9 +979,9 @@
     {
         ExRam.Gremlinq.Core.IElementGremlinQuery<object> Lower();
         ExRam.Gremlinq.Core.IPropertyGremlinQuery<ExRam.Gremlinq.Core.GraphElements.Property<object>> Properties(params string[] keys);
-        ExRam.Gremlinq.Core.IGremlinQuery<System.Collections.Generic.IDictionary<string, object>> ValueMap(params string[] keys);
-        ExRam.Gremlinq.Core.IGremlinQuery<System.Collections.Generic.IDictionary<string, TTarget>> ValueMap<TTarget>();
-        ExRam.Gremlinq.Core.IGremlinQuery<System.Collections.Generic.IDictionary<string, TTarget>> ValueMap<TTarget>(params string[] keys);
+        ExRam.Gremlinq.Core.IMapGremlinQuery<System.Collections.Generic.IDictionary<string, object>> ValueMap(params string[] keys);
+        new ExRam.Gremlinq.Core.IMapGremlinQuery<System.Collections.Generic.IDictionary<string, TValue>> ValueMap<TValue>();
+        ExRam.Gremlinq.Core.IMapGremlinQuery<System.Collections.Generic.IDictionary<string, TValue>> ValueMap<TValue>(params string[] keys);
         ExRam.Gremlinq.Core.IGremlinQuery<object> Values(params string[] keys);
         new ExRam.Gremlinq.Core.IGremlinQuery<TTarget> Values<TTarget>();
         ExRam.Gremlinq.Core.IGremlinQuery<TTarget> Values<TTarget>(params string[] keys);
