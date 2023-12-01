1d2c80afa16d88141384debba113be5dff9379bf
Daniel Weber <danielcweber@users.noreply.github.com>
2023-12-01 10:37:45 +0100

    Fix usings in the templates, used version in console template, tested version.

--- templates/aspnet/ExRam.Gremlinq.Templates.AspNet/Program.cs
+++ templates/aspnet/ExRam.Gremlinq.Templates.AspNet/Program.cs
@@ -1,11 +1,22 @@
 ﻿using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.AspNet;
 using ExRam.Gremlinq.Core.Models;
-using ExRam.Gremlinq.Providers.Core.AspNet;
 using ExRam.Gremlinq.Templates.AspNet;
+#if (provider == "GremlinServer")
+using ExRam.Gremlinq.Providers.GremlinServer.AspNet;
+#elif (provider == "Neptune")
+using ExRam.Gremlinq.Providers.Neptune.AspNet;
+#elif (provider == "CosmosDb")
+using ExRam.Gremlinq.Providers.CosmosDb.AspNet;
+#elif (provider == "JanusGraph")
+using ExRam.Gremlinq.Providers.JanusGraph.AspNet;
+#endif
+using ExRam.Gremlinq.Support.NewtonsoftJson.AspNet;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
