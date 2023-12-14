3ec554aceb97fd58267a3303de9ab43ee6edc0ae
Daniel Weber <danielcweber@users.noreply.github.com>
2023-12-14 16:06:53 +0100

    Add integration test to assert exception behaviour.

---
+++ test/Providers.CosmosDb.Tests/NoPasswordIntegrationTests.cs
@@ -0,0 +1,48 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Tests.Entities;
+using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+using ExRam.Gremlinq.Support.NewtonsoftJson;
+
+using static ExRam.Gremlinq.Providers.CosmosDb.Tests.NoPasswordIntegrationTests;
+using FluentAssertions;
+using ExRam.Gremlinq.Core.Execution;
+
+namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
+{
+    [IntegrationTest]
+    public sealed class NoPasswordIntegrationTests : GremlinqTestBase, IClassFixture<NoPasswordFixture>
+    {
+        public sealed class NoPasswordFixture : GremlinqFixture
+        {
+            private const string CosmosDbEmulatorDatabaseName = "db";
+            private const string CosmosDbEmulatorCollectionName = "graph";
+
+            protected override async Task<IGremlinQuerySource> TransformQuerySource(IGremlinQuerySource g) => g
+                .UseCosmosDb<Vertex, Edge>(conf => conf
+                    .At(new Uri("ws://localhost:8901"), CosmosDbEmulatorDatabaseName, CosmosDbEmulatorCollectionName)
+                    .WithPartitionKey(x => x.Label!)
+                    .UseNewtonsoftJson());
+        }
+
+        private readonly IGremlinQuerySource _g;
+
+        public NoPasswordIntegrationTests(NoPasswordFixture fixture) : base(new JTokenExecutingVerifier())
+        {
+            _g = fixture.GremlinQuerySource.Result;
+        }
+
+        [Fact]
+        public async Task No_password_bubbles_up()
+        {
+            await _g
+                .Inject(42)
+                .ToArrayAsync()
+                .Awaiting(_ => _)
+                .Should()
+                .ThrowAsync<GremlinQueryExecutionException>()
+                .WithInnerException<GremlinQueryExecutionException, NotSupportedException>();
+        }
+    }
+}
