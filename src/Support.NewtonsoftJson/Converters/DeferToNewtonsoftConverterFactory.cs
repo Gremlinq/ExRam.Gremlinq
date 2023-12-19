eedb5df298f1da52cf1996bc0542ebf75ecd55ba
Daniel Weber <danielcweber@users.noreply.github.com>
2023-12-19 19:36:48 +0100

    Cache last deserialized JToken.

--- src/Support.NewtonsoftJson/Converters/DeferToNewtonsoftConverterFactory.cs
+++ src/Support.NewtonsoftJson/Converters/DeferToNewtonsoftConverterFactory.cs
@@ -15,43 +15,55 @@ namespace ExRam.Gremlinq.Support.NewtonsoftJson
     {
         private static readonly JsonSerializer JsonSerializer = JsonSerializer.CreateDefault();
 
-        private sealed class DeferToNewtonsoftConverter<TBinaryMessage, TTarget> : IConverter<TBinaryMessage, TTarget>
+        private static class DeferToNewtonsoftConverter<TBinaryMessage>
             where TBinaryMessage : IMemoryOwner<byte>
         {
-            private readonly IGremlinQueryEnvironment _environment;
+            private static readonly ThreadLocal<(TBinaryMessage, JToken)?> LastSerialization = new();
 
-            public DeferToNewtonsoftConverter(IGremlinQueryEnvironment environment)
+            public sealed class DeferToNewtonsoftConverterImpl<TTarget> : IConverter<TBinaryMessage, TTarget>
             {
-                _environment = environment;
-            }
+                private readonly IGremlinQueryEnvironment _environment;
 
-            public bool TryConvert(TBinaryMessage source, ITransformer defer, ITransformer recurse, [NotNullWhen(true)] out TTarget? value)
-            {
-                var stream = MemoryMarshal.TryGetArray<byte>(source.Memory, out var segment) && segment is { Array: { } array }
-                    ? new MemoryStream(array, segment.Offset, segment.Count)
-                    : new MemoryStream(source.Memory.ToArray());
-
-                using (stream)
+                public DeferToNewtonsoftConverterImpl(IGremlinQueryEnvironment environment)
                 {
-                    using (var streamReader = new StreamReader(stream))
-                    {
-                        using (var jsonTextReader = new JsonTextReader(streamReader) { DateParseHandling = DateParseHandling.None })
-                        {
-                            if (JsonSerializer.Deserialize<JToken>(jsonTextReader) is { } jToken)
-                                return recurse.TryTransform(jToken, _environment, out value);
-                        }
-                    }
+                    _environment = environment;
                 }
 
-                value = default;
-                return false;
+                public bool TryConvert(TBinaryMessage source, ITransformer defer, ITransformer recurse, [NotNullWhen(true)] out TTarget? value)
+                {
+                    if (LastSerialization.Value is { Item1: { } lastMessage, Item2: { } lastToken } && EqualityComparer<TBinaryMessage>.Default.Equals(lastMessage, source))
+                        return recurse.TryTransform(lastToken, _environment, out value);
+
+                    var stream = MemoryMarshal.TryGetArray<byte>(source.Memory, out var segment) && segment is { Array: { } array }
+                        ? new MemoryStream(array, segment.Offset, segment.Count)
+                        : new MemoryStream(source.Memory.ToArray());
+
+                    using (stream)
+                    {
+                        using (var streamReader = new StreamReader(stream))
+                        {
+                            using (var jsonTextReader = new JsonTextReader(streamReader) { DateParseHandling = DateParseHandling.None })
+                            {
+                                if (JsonSerializer.Deserialize<JToken>(jsonTextReader) is { } jToken)
+                                {
+                                    LastSerialization.Value = (source, jToken);
+
+                                    return recurse.TryTransform(jToken, _environment, out value);
+                                }
+                            }
+                        }
+                    }
+
+                    value = default;
+                    return false;
+                }
             }
         }
 
         public IConverter<TSource, TTarget>? TryCreate<TSource, TTarget>(IGremlinQueryEnvironment environment) => typeof(TSource) == typeof(GraphSon2BinaryMessage)
-            ? (IConverter<TSource, TTarget>)(object)new DeferToNewtonsoftConverter<GraphSon2BinaryMessage, TTarget>(environment)
+            ? (IConverter<TSource, TTarget>)(object)new DeferToNewtonsoftConverter<GraphSon2BinaryMessage>.DeferToNewtonsoftConverterImpl<TTarget>(environment)
             : typeof(TSource) == typeof(GraphSon3BinaryMessage)
-                ? (IConverter<TSource, TTarget>)(object)new DeferToNewtonsoftConverter<GraphSon3BinaryMessage, TTarget>(environment)
+                ? (IConverter<TSource, TTarget>)(object)new DeferToNewtonsoftConverter<GraphSon3BinaryMessage>.DeferToNewtonsoftConverterImpl<TTarget>(environment)
                 : default;
     }
 }
