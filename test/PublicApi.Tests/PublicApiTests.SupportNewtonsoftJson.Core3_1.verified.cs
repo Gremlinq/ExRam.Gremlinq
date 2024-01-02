01f9b9e4121382d5f7799f11f0aea032cfb8a354
Daniel Weber <danielcweber@users.noreply.github.com>
2024-01-02 18:56:22 +0100

    Remove a convenience UseNewtonsoftJson because the convenience is actually pretty limited but confusion is slightly bigger.

--- test/PublicApi.Tests/PublicApiTests.SupportNewtonsoftJson.Core3_1.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.SupportNewtonsoftJson.Core3_1.verified.cs
@@ -10,8 +10,4 @@
         public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment RegisterNativeType<TNative, TSerialized>(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment, System.Func<TNative, ExRam.Gremlinq.Core.IGremlinQueryEnvironment, ExRam.Gremlinq.Core.Transformation.ITransformer, ExRam.Gremlinq.Core.Transformation.ITransformer, TSerialized> serializer, System.Func<Newtonsoft.Json.Linq.JValue, ExRam.Gremlinq.Core.IGremlinQueryEnvironment, ExRam.Gremlinq.Core.Transformation.ITransformer, ExRam.Gremlinq.Core.Transformation.ITransformer, TNative> deserializer) { }
         public static ExRam.Gremlinq.Core.IGremlinQueryEnvironment UseNewtonsoftJson(this ExRam.Gremlinq.Core.IGremlinQueryEnvironment environment) { }
     }
-    public static class GremlinQuerySourceExtensions
-    {
-        public static ExRam.Gremlinq.Core.IGremlinQuerySource UseNewtonsoftJson(this ExRam.Gremlinq.Core.IGremlinQuerySource source) { }
-    }
 }
