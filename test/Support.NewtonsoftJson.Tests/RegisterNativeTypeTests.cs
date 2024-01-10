dcc485784cdaa9089cb1369ef51f4b82ea32a996
Andrii Chebukin <XperiAndri@Outlook.com>
2024-01-11 01:07:10 +0400

    Implemented a test for fantomly typed value object

--- test/Support.NewtonsoftJson.Tests/RegisterNativeTypeTests.cs
+++ test/Support.NewtonsoftJson.Tests/RegisterNativeTypeTests.cs
@@ -16,18 +16,40 @@ namespace ExRam.Gremlinq.Support.NewtonsoftJson.Tests
             public string WrappedId { get; }
         }
 
+        private readonly struct FancyFantomlyTypedId<TFantomInterface>
+        {
+            public FancyFantomlyTypedId(string wrappedId)
+            {
+                WrappedId = wrappedId;
+            }
+
+            public string WrappedId { get; }
+        }
+
+        interface FantomInterface { }
+
         public RegisterNativeTypeTests() : base()
         {
         }
 
         [Fact]
-        public Task Serialization() => Verify(g
+        public Task CanSerializeSimpleValueObject() => Verify(g
             .ConfigureEnvironment(env => env
                 .UseNewtonsoftJson()
                 .RegisterNativeType(
                     (fancyId, env, _, recurse) => fancyId.WrappedId,
                     (jValue, env, _, recurse) => new FancyId(jValue.Value<string>()!)))
             .Inject(new FancyId("fancyId"))
+            .Debug());
+
+        [Fact]
+        public Task CanSerializeFantomlyTypedValueObject() => Verify(g
+            .ConfigureEnvironment(env => env
+                .UseNewtonsoftJson()
+                .RegisterNativeType(
+                    (fancyId, env, _, recurse) => fancyId.WrappedId,
+                    (jValue, env, _, recurse) => new FancyFantomlyTypedId<FantomInterface>(jValue.Value<string>()!)))
+            .Inject(new FancyFantomlyTypedId<FantomInterface>("fancyId"))
             .Debug());
 
         //TODO: DeserializationTests
