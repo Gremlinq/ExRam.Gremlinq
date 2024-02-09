3eba222c5f210d4d16b438a3d203c5f559c78fc1
Daniel Weber <danielcweber@users.noreply.github.com>
2024-02-09 16:06:59 +0100

    Simplify WellKnownMethods.

--- src/Core/Extensions/WellKnownMethods.cs
+++ src/Core/Extensions/WellKnownMethods.cs
@@ -6,21 +6,18 @@ namespace ExRam.Gremlinq.Core
     internal static class WellKnownMethods
     {
         // ReSharper disable ReturnValueOfPureMethodIsNotUsed
-        public static readonly MethodInfo ObjectToString = Get<object>(static _ => _.ToString());
-        public static readonly MethodInfo EnumerableAny = Get(static () => Enumerable.Any<object>(default!)).GetGenericMethodDefinition();
-        public static readonly MethodInfo EnumerableIntersect = Get(static () => Enumerable.Intersect<object>(default!, default!)).GetGenericMethodDefinition();
-#pragma warning disable 8625
-        public static readonly MethodInfo EnumerableContainsElement = Get(static () => Enumerable.Contains<object>(default!, default)).GetGenericMethodDefinition();
-#pragma warning restore 8625
+        public static readonly MethodInfo ObjectToString = Get(_ => _.ToString());
+        public static readonly MethodInfo EnumerableAny = Get(_ => Enumerable.Any<object>(default!));
+        public static readonly MethodInfo EnumerableIntersect = Get(_ => Enumerable.Intersect<object>(default!, default!));
+        public static readonly MethodInfo EnumerableContainsElement = Get(_ => Enumerable.Contains<object>(default!, default!));
 
-        private static MethodInfo Get(Expression<Action> expression)
+        private static MethodInfo Get(Expression<Action<object>> expression)
         {
-            return ((MethodCallExpression)expression.Body).Method;
-        }
+            var method = ((MethodCallExpression)expression.Body).Method;
 
-        private static MethodInfo Get<TSource>(Expression<Action<TSource>> expression)
-        {
-            return ((MethodCallExpression)expression.Body).Method;
+            return method.IsGenericMethod
+                ? method.GetGenericMethodDefinition()
+                : method;
         }
     }
 }
