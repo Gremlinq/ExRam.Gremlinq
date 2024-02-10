288df3fab98845b8f93c53cb324b7da620054471
Daniel Weber <danielcweber@users.noreply.github.com>
2024-02-10 01:01:30 +0100

    Get rid of usages of ExpressionFragment.TryGetValue.

--- src/Core/ExpressionParsing/ExpressionFragment.cs
+++ src/Core/ExpressionParsing/ExpressionFragment.cs
@@ -7,17 +7,15 @@ namespace ExRam.Gremlinq.Core.ExpressionParsing
     {
         private readonly object? _value;
 
-        public static readonly ExpressionFragment True = new(true, default);
-        public static readonly ExpressionFragment False = new(false, default);
-        public static readonly ExpressionFragment Null = new(default, default);
+        public static readonly ExpressionFragment True = new(true, Expression.Constant(true));
+        public static readonly ExpressionFragment False = new(false, Expression.Constant(false));
+        public static readonly ExpressionFragment Null = new(default, Expression.Constant(null, typeof(object)));
 
         private ExpressionFragment(object? value, Expression? expression = default)
         {
             _value = value;
             Expression = expression;
         }
-
-        public object? TryGetValue() => _value;
 
         public bool Equals(ExpressionFragment other) => Equals(_value, other._value) && Equals(Expression, other.Expression);
 
@@ -37,21 +35,7 @@ namespace ExRam.Gremlinq.Core.ExpressionParsing
 
         public static ExpressionFragment Create(Expression expression, IGremlinQueryEnvironment environment)
         {
-            expression = expression.StripConvert();
-
-            if (expression.RefersToParameter(out _))
-                return new(default, expression.StripConvert());
-
-            if (expression.TryParseStepLabelExpression(out var stepLabel, out var stepLabelExpression))
-                return new(default!, expression.StripConvert());
-
-            return new(
-                expression.GetValue() switch
-                {
-                    IEnumerable enumerable when enumerable is not ICollection && !environment.SupportsType(enumerable.GetType()) => enumerable.Cast<object>().ToArray(),
-                    { } val => val,
-                    _ => null
-                });
+            return new(default, expression.StripConvert());
         }
     }
 }
