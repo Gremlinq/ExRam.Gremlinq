d77ff3e476c73a87aacacc831c2e7af6ba4a602c
Daniel Weber <danielcweber@users.noreply.github.com>
2024-02-10 17:31:49 +0100

    Strip almost everything from ExpressionFragment and make it a static class with only the factory method.

--- src/Core/ExpressionParsing/ExpressionFragment.cs
+++ src/Core/ExpressionParsing/ExpressionFragment.cs
@@ -5,29 +5,16 @@ namespace ExRam.Gremlinq.Core.ExpressionParsing
 {
     internal static class Expressions
     {
-        public static readonly ExpressionFragment True = ExpressionFragment.Create(Expression.Constant(true));
-        public static readonly ExpressionFragment False = ExpressionFragment.Create(Expression.Constant(false));
-        public static readonly ExpressionFragment Null = ExpressionFragment.Create(Expression.Constant(null, typeof(object)));
+        public static readonly Expression True = ExpressionFragment.Create(Expression.Constant(true));
+        public static readonly Expression False = ExpressionFragment.Create(Expression.Constant(false));
+        public static readonly Expression Null = ExpressionFragment.Create(Expression.Constant(null, typeof(object)));
     }
 
-    internal readonly struct ExpressionFragment
+    internal static class ExpressionFragment
     {
-        private ExpressionFragment(Expression expression)
+        public static Expression Create(Expression expression)
         {
-            Expression = expression;
-        }
-
-        public bool Equals(ExpressionFragment other) => Equals(Expression, other.Expression);
-
-        public override bool Equals(object? obj) => obj is ExpressionFragment other && Equals(other);
-
-        public override int GetHashCode() => Expression.GetHashCode();
-
-        public Expression Expression { get; }
-
-        public static ExpressionFragment Create(Expression expression)
-        {
-            return new(expression.StripConvert());
+            return expression.StripConvert();
         }
     }
 }
