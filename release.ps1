00178a9f1bbd52a6877a6a984776780b831d751d
Daniel Weber <danielcweber@users.noreply.github.com>
2024-03-11 12:55:38 +0100

    Switch back to initial branch and delete "PrepareRelease" branch.

--- release.ps1
+++ release.ps1
@@ -1,5 +1,9 @@
-git checkout -b PrepareRelease
+$current = $(git branch --show-current)
 $nbgv = nbgv prepare-release --format json | ConvertFrom-Json
 git rebase $nbgv.NewBranch.Name $nbgv.CurrentBranch.Name -Xtheirs
 git tag $nbgv.NewBranch.Name $nbgv.NewBranch.Name
 git branch -d $nbgv.NewBranch.Name
+git checkout -b PrepareRelease
+git push
+git checkout $current
+git branch -d PrepareRelease
