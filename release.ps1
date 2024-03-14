5806350caf519f85b7f3e2b6868cebce74021600
Daniel Weber <danielcweber@users.noreply.github.com>
2024-03-14 08:43:10 +0100

    Rearrange release.ps1 to force signed commits.

--- release.ps1
+++ release.ps1
@@ -1,9 +1,11 @@
 $current = $(git branch --show-current)
+git checkout -b PrepareRelease
 $nbgv = nbgv prepare-release --format json | ConvertFrom-Json
+git rebase $current $nbgv.NewBranch.Name
 git rebase $nbgv.NewBranch.Name $nbgv.CurrentBranch.Name -Xtheirs
 git tag $nbgv.NewBranch.Name $nbgv.NewBranch.Name
 git branch -d $nbgv.NewBranch.Name
-git checkout -b PrepareRelease
 git push
 git checkout $current
+git merge PrepareRelease
 git branch -d PrepareRelease
