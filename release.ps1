0e9974417a05cde20f28f43054b65c8864cc7ebb
Daniel Weber <danielcweber@users.noreply.github.com>
2024-03-18 22:00:46 +0100

    Force rebase to force signed commits.

--- release.ps1
+++ release.ps1
@@ -1,7 +1,7 @@
 $current = $(git branch --show-current)
 git checkout -b PrepareRelease
 $nbgv = nbgv prepare-release --format json | ConvertFrom-Json
-git rebase $current $nbgv.NewBranch.Name
+git rebase -f $current $nbgv.NewBranch.Name
 git rebase $nbgv.NewBranch.Name $nbgv.CurrentBranch.Name -Xtheirs
 git tag $nbgv.NewBranch.Name $nbgv.NewBranch.Name
 git branch -d $nbgv.NewBranch.Name
