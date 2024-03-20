f4c8cc2a99e1366bcd524337f2d113b7a40778bb
Daniel Weber <danielcweber@users.noreply.github.com>
2024-03-20 20:21:08 +0100

    Introduce PasswordSecuredGremlinServerContainerFixture and tests that, for snapshot baseline, still use the usual image.

---
+++ test/Providers.GremlinServer.Tests/PasswordSecuredIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using ExRam.Gremlinq.Tests.TestCases;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    [IntegrationTest]
+    public sealed class PasswordSecuredIntegrationTests : QueryExecutionTest, IClassFixture<GremlinServerContainerFixture>
+    {
+        public PasswordSecuredIntegrationTests(GremlinServerContainerFixture fixture, ITestOutputHelper testOutputHelper) : base(
+            fixture,
+            new JTokenExecutingVerifier(),
+            testOutputHelper)
+        {
+        }
+    }
+}
---
+++ test/Tests.Fixtures/PasswordSecuredGremlinServerContainerFixture.cs
@@ -0,0 +1,22 @@
+﻿using DotNet.Testcontainers.Containers;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Providers.Core;
+using ExRam.Gremlinq.Tests.Entities;
+using ExRam.Gremlinq.Support.NewtonsoftJson;
+using ExRam.Gremlinq.Providers.GremlinServer;
+
+namespace ExRam.Gremlinq.Tests.Fixtures
+{
+    public sealed class PasswordSecuredGremlinServerContainerFixture : DockerfileTestContainerFixture
+    {
+        public PasswordSecuredGremlinServerContainerFixture() : base("PasswordSecureGremlinServerDockerfile")
+        {
+        }
+
+
+        protected override async Task<IGremlinQuerySource> TransformQuerySource(IContainer container, IGremlinQuerySource g) => g
+            .UseGremlinServer<Vertex, Edge>(_ => _
+                .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
+                .UseNewtonsoftJson());
+    }
+}
