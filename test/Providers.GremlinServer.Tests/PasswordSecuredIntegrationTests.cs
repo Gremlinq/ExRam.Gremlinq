49a8891eeefe4674ec2388f8024a78823ba3a2fd
Daniel Weber <danielcweber@users.noreply.github.com>
2024-03-20 20:54:41 +0100

    Use PasswordSecuredGremlinServerContainerFixture.

--- test/Providers.GremlinServer.Tests/PasswordSecuredIntegrationTests.cs
+++ test/Providers.GremlinServer.Tests/PasswordSecuredIntegrationTests.cs
@@ -5,9 +5,9 @@ using ExRam.Gremlinq.Tests.TestCases;
 namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
 {
     [IntegrationTest]
-    public sealed class PasswordSecuredIntegrationTests : QueryExecutionTest, IClassFixture<GremlinServerContainerFixture>
+    public sealed class PasswordSecuredIntegrationTests : QueryExecutionTest, IClassFixture<PasswordSecuredGremlinServerContainerFixture>
     {
-        public PasswordSecuredIntegrationTests(GremlinServerContainerFixture fixture, ITestOutputHelper testOutputHelper) : base(
+        public PasswordSecuredIntegrationTests(PasswordSecuredGremlinServerContainerFixture fixture, ITestOutputHelper testOutputHelper) : base(
             fixture,
             new JTokenExecutingVerifier(),
             testOutputHelper)
