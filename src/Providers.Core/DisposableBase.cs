b7fc404a91e5fcfbb00cb6a567bb7d1fb1b99489
Daniel Weber <danielcweber@users.noreply.github.com>
2024-04-19 12:24:07 +0200

    Introduce a base class DisposableBase to handle multiple calls to Dispose

---
+++ src/Providers.Core/DisposableBase.cs
@@ -0,0 +1,18 @@
+﻿namespace ExRam.Gremlinq.Providers.Core
+{
+    internal abstract class DisposableBase : IDisposable
+    {
+        private int _disposed;
+
+        void IDisposable.Dispose()
+        {
+            if (Interlocked.CompareExchange(ref _disposed, 1, 0) == 0)
+            {
+                Dispose();
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        protected abstract void Dispose();
+    }
+}
