b9e79b13625776df63acef3cedf4a505c5632d03
Daniel Weber <danielcweber@users.noreply.github.com>
2024-05-22 09:14:25 +0200

    Set upstream.

--- release.ps1
+++ release.ps1
@@ -9,7 +9,7 @@ git rebase -f $current $nbgv.NewBranch.Name
 git rebase $nbgv.NewBranch.Name $nbgv.CurrentBranch.Name -Xtheirs
 git tag $nbgv.NewBranch.Name $nbgv.NewBranch.Name
 git branch -d $nbgv.NewBranch.Name
-git push
+git push --set-upstream origin PrepareRelease
 git checkout $current
 git merge PrepareRelease
 git branch -d PrepareRelease
