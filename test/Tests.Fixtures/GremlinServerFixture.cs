69d16434f2ff6d2fbc1444c7d5ab828af494bcce
Daniel Weber <danielcweber@users.noreply.github.com>
2024-06-03 16:17:11 +0200

    Ignore CosmosDb specific property "PartitionKey" in GremlinServerFixture.

--- test/Tests.Fixtures/GremlinServerFixture.cs
+++ test/Tests.Fixtures/GremlinServerFixture.cs
@@ -1,4 +1,5 @@
 ﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Models;
 using ExRam.Gremlinq.Providers.Core;
 using ExRam.Gremlinq.Tests.Entities;
 using ExRam.Gremlinq.Support.NewtonsoftJson;
@@ -11,6 +12,11 @@ namespace ExRam.Gremlinq.Tests.Fixtures
         protected override async Task<IGremlinQuerySource> TransformQuerySource(IGremlinQuerySource g) => g
             .UseGremlinServer<Vertex, Edge>(_ => _
                 .AtLocalhost()
-                .UseNewtonsoftJson());
+                .UseNewtonsoftJson())
+            .ConfigureEnvironment(env => env
+                .ConfigureModel(model => model
+                    .ConfigureElements(v => v
+                        .ConfigureElement<Element>(conf => conf
+                            .IgnoreAlways(p => p.PartitionKey)))));
     }
 }
