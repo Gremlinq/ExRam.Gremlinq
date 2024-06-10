1baddb23186821db0124b06a32771488f95534ef
Daniel Weber <danielcweber@users.noreply.github.com>
2024-06-10 16:34:14 +0200

    Use SerializingVerifier<RequestMessage> in Graphson3WithGroovyGremlinQuerySerializationTest.

--- test/Core.Tests/Serialization/Graphson3WithGroovyGremlinQuerySerializationTest.cs
+++ test/Core.Tests/Serialization/Graphson3WithGroovyGremlinQuerySerializationTest.cs
@@ -1,16 +1,16 @@
 ﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
 using ExRam.Gremlinq.Tests.TestCases;
-using Gremlin.Net.Structure.IO.GraphSON;
+
+using Gremlin.Net.Driver.Messages;
 
 namespace ExRam.Gremlinq.Core.Tests
 {
     public sealed class Graphson3WithGroovyGremlinQuerySerializationTest : QueryExecutionTest, IClassFixture<GroovyGremlinQuerySerializationFixture>
     {
-        private static readonly GraphSON3Writer GraphSon3Writer = new();
-
         public Graphson3WithGroovyGremlinQuerySerializationTest(GroovyGremlinQuerySerializationFixture fixture) : base(
             fixture,
-            new GraphSonStringSerializingVerifier(GraphSon3Writer))
+            new SerializingVerifier<RequestMessage>())
         {
         }
     }
