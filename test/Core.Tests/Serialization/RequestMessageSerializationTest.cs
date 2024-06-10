0d96f08ba68124ecdcf8806c32a0776028daf8a9
Daniel Weber <danielcweber@users.noreply.github.com>
2024-06-10 16:22:07 +0200

    Add RequestMessageSerializationTest that does not care for ToDict.

---
+++ test/Core.Tests/Serialization/RequestMessageSerializationTest.cs
@@ -0,0 +1,16 @@
+﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using ExRam.Gremlinq.Tests.TestCases;
+
+using Gremlin.Net.Driver.Messages;
+
+namespace ExRam.Gremlinq.Core.Tests;
+
+public sealed class RequestMessageSerializationTest : QueryExecutionTest, IClassFixture<GremlinqFixture>
+{
+    public RequestMessageSerializationTest(GremlinqFixture fixture) : base(
+        fixture,
+        new SerializingVerifier<RequestMessage>())
+    {
+    }
+}
