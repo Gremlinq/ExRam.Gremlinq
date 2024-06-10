c928a241c185d17ae68d4af9c6367b01deaa7533
Daniel Weber <danielcweber@users.noreply.github.com>
2024-06-10 15:24:06 +0200

    Copy tests for Graphson2 and Graphson3 serialization.

---
+++ test/Core.Tests/Serialization/Graphson3BinaryMessageSerializationTest.cs
@@ -0,0 +1,17 @@
+﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.TestCases;
+using Gremlin.Net.Structure.IO.GraphSON;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public sealed class Graphson3BinaryMessageSerializationTest : QueryExecutionTest, IClassFixture<GremlinqFixture>
+    {
+        private static readonly GraphSON3Writer GraphSon3Writer = new ();
+
+        public Graphson3BinaryMessageSerializationTest(GremlinqFixture fixture) : base(
+            fixture,
+            new GraphSonStringSerializingVerifier(GraphSon3Writer))
+        {
+        }
+    }
+}
