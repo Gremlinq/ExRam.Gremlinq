5b88b3049eb76c3fe5f0c8be30d9db06d0d90e15
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-07 23:50:23 +0200

    InnerVerify does not need to be virtual.

--- test/Tests.Infrastructure/Verifiers/GremlinQueryVerifier.cs
+++ test/Tests.Infrastructure/Verifiers/GremlinQueryVerifier.cs
@@ -16,9 +16,9 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
 
         protected virtual SettingsTask ModifySettingsTask(SettingsTask task) => task;
 
-        protected virtual Task InnerVerify<T>(T value) => InnerVerify(new ValueTask<T>(value));
+        protected Task InnerVerify<T>(T value) => InnerVerify(new ValueTask<T>(value));
 
-        protected virtual Task InnerVerify<T>(ValueTask<T> value) => ModifySettingsTask(Verifier
+        protected Task InnerVerify<T>(ValueTask<T> value) => ModifySettingsTask(Verifier
             .Verify(value, sourceFile: _sourceFile));
     }
 }
