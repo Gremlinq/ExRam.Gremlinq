0ad3d2d4806b8bc4a700bc3bc63f9b428752b79e
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-08 11:19:38 +0200

    Style.

--- test/Tests.Infrastructure/Extensions/SettingsTaskExtensions.cs
+++ test/Tests.Infrastructure/Extensions/SettingsTaskExtensions.cs
@@ -6,16 +6,8 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
     {
         private static readonly Regex GuidRegex = new("[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
-        public static SettingsTask ScrubRegex(this SettingsTask task, Regex regex, string replacement)
-        {
-            return task
-                .ScrubLinesWithReplace(str => regex.Replace(str, replacement));
-        }
+        public static SettingsTask ScrubRegex(this SettingsTask task, Regex regex, string replacement) => task.ScrubLinesWithReplace(str => regex.Replace(str, replacement));
 
-        public static SettingsTask ScrubGuids(this SettingsTask task)
-        {
-            return task
-                .ScrubRegex(GuidRegex, "12345678-9012-3456-7890-123456789012");
-        }
+        public static SettingsTask ScrubGuids(this SettingsTask task) => task.ScrubRegex(GuidRegex, "12345678-9012-3456-7890-123456789012");
     }
 }
