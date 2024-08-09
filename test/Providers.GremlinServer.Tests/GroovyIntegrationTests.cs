be6aa1c9ac2099dd8fdfb1786c055982e749f834
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-09 13:38:54 +0200

    Duplicate GremlinServer integration tests.

---
+++ test/Providers.GremlinServer.Tests/GroovyIntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    [IntegrationTest("Linux", true)]
+    [IntegrationTest("Windows")]
+    public class GroovyIntegrationTests : QueryExecutionTest, IClassFixture<GremlinServerContainerFixture>
+    {
+        public GroovyIntegrationTests(GremlinServerContainerFixture fixture) : base(
+            fixture,
+            new JTokenExecutingVerifier())
+        {
+        }
+    }
+}
