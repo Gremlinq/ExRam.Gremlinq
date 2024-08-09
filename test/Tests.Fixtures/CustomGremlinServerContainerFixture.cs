9ba05e5aaaad7f89ccfb8d2d33b3a3d41c584484
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-09 15:23:35 +0200

    GroovyIntegrationTests: Execute groovy instead of bytecode. Some snapshots change in results order but seem to be otherwise equivalent.

--- test/Tests.Fixtures/CustomGremlinServerContainerFixture.cs
+++ test/Tests.Fixtures/CustomGremlinServerContainerFixture.cs
@@ -4,6 +4,10 @@ using ExRam.Gremlinq.Providers.Core;
 using ExRam.Gremlinq.Tests.Entities;
 using ExRam.Gremlinq.Support.NewtonsoftJson;
 using ExRam.Gremlinq.Providers.GremlinServer;
+using ExRam.Gremlinq.Core.Transformation;
+using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Driver.Messages;
+using ExRam.Gremlinq.Core.Serialization;
 
 namespace ExRam.Gremlinq.Tests.Fixtures
 {
@@ -17,6 +21,12 @@ namespace ExRam.Gremlinq.Tests.Fixtures
             .UseGremlinServer<Vertex, Edge>(_ => _
                 .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
                 .UseNewtonsoftJson())
+            .ConfigureEnvironment(env => env
+                .ConfigureSerializer(ser => ser
+                    .Add(ConverterFactory
+                        .Create<Bytecode, RequestMessage>((bytecode, env, _, recurse) => recurse.TryTransform(bytecode, env, out GroovyGremlinScript groovyQuery) && recurse.TryTransform(groovyQuery, env, out RequestMessage? message)
+                            ? message
+                            : null))))
             .IgnoreCosmosDbSpecificProperties();
     }
 }
