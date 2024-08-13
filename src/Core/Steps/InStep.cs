9b8e9f0ff219e039cf551c6a7bf1bd44f118b849
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-13 11:13:22 +0200

    InStep.NoLabels: Use parameterless constructor.

--- src/Core/Steps/InStep.cs
+++ src/Core/Steps/InStep.cs
@@ -4,7 +4,7 @@ namespace ExRam.Gremlinq.Core.Steps
 {
     public sealed class InStep : DerivedLabelNamesStep
     {
-        public static readonly InStep NoLabels = new(ImmutableArray<string>.Empty);
+        public static readonly InStep NoLabels = new();
 
         public InStep() : this(ImmutableArray<string>.Empty)
         {
