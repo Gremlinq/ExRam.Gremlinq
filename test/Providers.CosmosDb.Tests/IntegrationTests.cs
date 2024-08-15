17bddfdb5514d13078721683d34c3f675da11e45
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-15 16:31:01 +0200

    CosmosDb-integrationtests: Scrub status attributes.

--- test/Providers.CosmosDb.Tests/IntegrationTests.cs
+++ test/Providers.CosmosDb.Tests/IntegrationTests.cs
@@ -1,4 +1,6 @@
-﻿using ExRam.Gremlinq.Tests.Fixtures;
+﻿using System.Runtime.CompilerServices;
+
+using ExRam.Gremlinq.Tests.Fixtures;
 using ExRam.Gremlinq.Tests.Infrastructure;
 
 namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
@@ -6,10 +8,30 @@ namespace ExRam.Gremlinq.Providers.CosmosDb.Tests
     [IntegrationTest("Windows", true)]
     public class IntegrationTests : QueryExecutionTest, IClassFixture<CosmosDbEmulatorFixture>
     {
+        public class CosmosDbEmulatorExecutingVerifier : ExecutingVerifier
+        {
+            public CosmosDbEmulatorExecutingVerifier([CallerFilePath] string sourceFile = "") : base(sourceFile)
+            {
+            }
+
+            protected override SettingsTask ModifySettingsTask(SettingsTask task) => base
+                .ModifySettingsTask(task)
+                .ScrubMember("x-ms-server-time-ms")
+                .ScrubMember("x-ms-total-server-time-ms")
+                .ScrubMember("x-ms-request-charge")
+                .ScrubMember("x-ms-total-request-charge");
+        }
+
         public IntegrationTests(CosmosDbEmulatorFixture fixture) : base(
             fixture,
-            new ExecutingVerifier())
+            new CosmosDbEmulatorExecutingVerifier())
         {
+        }
+
+        [Fact(Skip = "id as key cannot be scrubbed.")]
+        public override Task Group_with_key_identity()
+        {
+            return base.Group_with_key_identity();
         }
     }
 }
