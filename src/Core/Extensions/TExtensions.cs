b16ba03fb490dc9a7425f7574a07ac2115fe77bd
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-29 12:21:37 +0200

    Cleanup.

--- src/Core/Extensions/TExtensions.cs
+++ src/Core/Extensions/TExtensions.cs
@@ -2,13 +2,11 @@
 
 using Gremlin.Net.Process.Traversal;
 
-using _Step = ExRam.Gremlinq.Core.Steps.Step;
-
 namespace ExRam.Gremlinq.Core
 {
     internal static class TExtensions
     {
-        public static _Step? TryToStep(this T t)
+        public static Step? TryToStep(this T t)
         {
             return t.EnumValue switch
             {
