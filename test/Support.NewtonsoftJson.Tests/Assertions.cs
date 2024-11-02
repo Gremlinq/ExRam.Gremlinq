62117f6bfa56c63d1b853a555cfa39f9b8603bc9
Daniel Weber <danielcweber@users.noreply.github.com>
2024-11-02 16:28:08 +0100

    Assert that the behaviour change of the previous commit is in accordance with Newtonsoft.Json deserialization behaviour.

---
+++ test/Support.NewtonsoftJson.Tests/Assertions.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+using Newtonsoft.Json;
+
+namespace ExRam.Gremlinq.Support.NewtonsoftJson.Tests
+{
+    public class Assertions
+    {
+        public class VertexProperty
+        {
+            public string? Value { get; set; }
+        }
+
+        public class Vertex
+        {
+            public string? Id { get; set; }
+
+            public VertexProperty? Property { get; set; }
+        }
+
+        [Fact]
+        public void Empty_json_objects_are_deserialized_to_non_null_properties()
+        {
+            JsonConvert
+                .DeserializeObject<Vertex>("{ \"id\": \"Hallo\" }")?
+                .Property
+                .Should()
+                .BeNull();
+
+            JsonConvert
+                .DeserializeObject<Vertex>("{ \"id\": \"Hallo\" }")?
+                .Id
+                .Should()
+                .NotBeNull();
+
+            JsonConvert
+                .DeserializeObject<Vertex>("{ \"id\": \"Hallo\", \"property\": { } }")?
+                .Property
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
