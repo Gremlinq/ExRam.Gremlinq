194c31c78a98f16e74e7f2807244d85db98db3b2
Daniel Weber <danielcweber@users.noreply.github.com>
2024-11-10 00:28:57 +0100

    Supress warning about opt-in-features. They are obviously in error (see https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca2252).

--- test/Tests.Infrastructure/ISourceFileNameProvider.cs
+++ test/Tests.Infrastructure/ISourceFileNameProvider.cs
@@ -1,4 +1,5 @@
-﻿namespace ExRam.Gremlinq.Tests.Infrastructure
+﻿#pragma warning disable CA2252
+namespace ExRam.Gremlinq.Tests.Infrastructure
 {
     public interface ISourceFileNameProvider<T>
     {
