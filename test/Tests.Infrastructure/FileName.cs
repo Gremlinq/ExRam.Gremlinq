ce3135abe77deba6209ea8dc1903e2e5588e3c7d
Daniel Weber <danielcweber@users.noreply.github.com>
2024-11-10 14:42:37 +0100

    FileName.Of.

--- test/Tests.Infrastructure/FileName.cs
+++ test/Tests.Infrastructure/FileName.cs
@@ -4,6 +4,8 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
 {
     public static class FileName
     {
+        public static string Of<T>() where T : ISourceFileNameProvider<T> => T.GetSourceFileName();
+
         public static string OfThis([CallerFilePath] string? sourceFile = "") => sourceFile ?? throw new InvalidOperationException();
     }
 }
