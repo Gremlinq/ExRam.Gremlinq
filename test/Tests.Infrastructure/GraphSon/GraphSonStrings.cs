54c0631aed9af5d34f408c1cb4ed972710fad903
Daniel Weber <danielcweber@users.noreply.github.com>
2024-11-11 13:22:09 +0100

    Cleanup.

--- test/Tests.Infrastructure/GraphSon/GraphSonStrings.cs
+++ test/Tests.Infrastructure/GraphSon/GraphSonStrings.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace ExRam.Gremlinq.Tests.Infrastructure
+﻿namespace ExRam.Gremlinq.Tests.Infrastructure
 {
     public static class GraphSonStrings
     {
