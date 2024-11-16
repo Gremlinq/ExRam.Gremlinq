af43adcd65e785122306eb89890d274d85f1a5a1
Daniel Weber <danielcweber@users.noreply.github.com>
2024-11-16 14:40:01 +0100

    Code layout.

--- test/Tests.Infrastructure/GraphSon/GraphsonSupportTestBase.cs
+++ test/Tests.Infrastructure/GraphSon/GraphsonSupportTestBase.cs
@@ -52,28 +52,76 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
         protected abstract TNativeToken CreateNativeToken(string str);
 
         [Fact]
-        public Task ImmutableArray() => Verify<ImmutableArray<int>>("[ [ 1, 3, 5 ] ]");
+        public Task Array() => Verify<Language[]>(ArrayOfLanguages);
 
         [Fact]
-        public Task ImmutableDictionary_string_keys_int_values() => Verify<ImmutableDictionary<string, int>>(String_Keys_Int_Values_Map);
+        public Task Configured_property_name() => Verify<Person>(
+            "[ { \"id\": 13, \"label\": \"Person\", \"type\": \"vertex\", \"properties\": { \"replacement\": [ { \"id\": 1, \"value\": \"nameValue\" } ] } } ]",
+            env => env
+                .ConfigureModel(model => model
+                    .ConfigureVertices(_ => _
+                        .ConfigureElement<Person>(conf => conf
+                            .ConfigureName(x => x.Name, "replacement")))));
 
         [Fact]
-        public Task ImmutableDictionary_string_keys_typed_int_values() => Verify<ImmutableDictionary<string, int>>(String_Keys_Typed_Int_Values_Map);
+        public Task DateTime_from_double() => Verify<DateTime>("[ 123456789.2 ]");
+
+        [Fact]
+        public Task DateTime_from_number() => Verify<DateTime>("[ 123456789 ]");
+
+        [Fact]
+        public Task DateTime_from_string() => Verify<DateTime>("[ \"2018-12-17T08:00:00Z\" ]");
+
+        [Fact]
+        public Task DateTime_is_UTC() => Verify<Company>(Single_Company);
+
+        [Fact]
+        public Task DateTimeOffset_from_number() => Verify<DateTimeOffset>("[ 123456789 ]");
+
+        [Fact]
+        public Task DateTimeOffset_from_string() => Verify<DateTimeOffset>("[ \"2018-12-17T08:00:00Z\" ]");
+
+        [Fact]
+        public Task DynamicData() => Verify<dynamic>("[ { \"values\": [ ], \"count\": { \"@type\": \"g:Int32\", \"@value\": 36 } } ]");
+
+        [Fact]
+        public Task Edge() => Verify<WorksFor>(UntypedEdge);
+
+        [Fact]
+        public Task Empty_to_ints() => Verify<(int[] ints, string[] strings)>("[{ \"Item1\": [], \"Item2\": [] }]");
+
+        [Fact]
+        public Task Empty1() => Verify<object>("[]");
+
+        [Fact]
+        public Task Empty2() => Verify<Person>("[]");
+
+        [Fact]
+        public Task Graphson2Path() => Verify<Path>(Graphson2_Paths);
+
+        [Fact]
+        public Task GraphSon3_Tuple() => Verify<(Person, Language)>(Graphson3_Tuple_of_Person_Language);
+
+        [Fact]
+        public Task Graphson3Path() => Verify<Path>(Graphson3_Paths);
+
+        [Fact]
+        public Task GraphSon3ReferenceVertex() => Verify<object>(Graphson3ReferenceVertex);
+
+        [Fact]
+        public Task Guid() => Verify<Guid>("[ \"FCE0765A-454F-4D00-83DA-D76790156E29\" ]");
 
         [Fact]
         public Task IDictionary_string_keys_typed_int_values() => Verify<IDictionary<string, int>>(String_Keys_Typed_Int_Values_Map);
 
         [Fact]
-        public Task ImmutableList_ints() => Verify<ImmutableList<int>>(Int_List);
+        public Task IList_typed_ints() => Verify<IList<int>>(Typed_Int_List);
 
         [Fact]
-        public Task ImmutableList_typed_ints() => Verify<ImmutableList<int>>(Typed_Int_List);
+        public Task ImmutableArray() => Verify<ImmutableArray<int>>("[ [ 1, 3, 5 ] ]");
 
         [Fact]
         public Task ImmutableArray_typed_ints() => Verify<ImmutableArray<int>>(Typed_Int_List);
-
-        [Fact]
-        public Task IList_typed_ints() => Verify<IList<int>>(Typed_Int_List);
 
         [Fact]
         public Task ImmutableDictionary_map_of_string_keys_typed_int_values() => Verify<ImmutableDictionary<string, int>>("""
@@ -90,40 +138,22 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
             """);
 
         [Fact]
-        public Task GraphSon3ReferenceVertex() => Verify<object>(Graphson3ReferenceVertex);
+        public Task ImmutableDictionary_string_keys_int_values() => Verify<ImmutableDictionary<string, int>>(String_Keys_Int_Values_Map);
 
         [Fact]
-        public Task Edge() => Verify<WorksFor>(UntypedEdge);
+        public Task ImmutableDictionary_string_keys_typed_int_values() => Verify<ImmutableDictionary<string, int>>(String_Keys_Typed_Int_Values_Map);
 
         [Fact]
-        public Task DynamicData() => Verify<dynamic>("[ { \"values\": [ ], \"count\": { \"@type\": \"g:Int32\", \"@value\": 36 } } ]");
+        public Task ImmutableList_ints() => Verify<ImmutableList<int>>(Int_List);
 
         [Fact]
-        public Task Empty1() => Verify<object>("[]");
-
-        [Fact]
-        public Task Empty2() => Verify<Person>("[]");
-
-        [Fact]
-        public Task String_Ids() => Verify<object>("[ \"id1\", \"id2\" ]");
-
-        [Fact]
-        public Task String_Ids2() => Verify<object>("[ \"1\", \"2\" ]");
+        public Task ImmutableList_typed_ints() => Verify<ImmutableList<int>>(Typed_Int_List);
 
         [Fact]
         public Task Int_Ids() => Verify<object>("[ 1, 2 ]");
 
         [Fact]
-        public Task Empty_to_ints() => Verify<(int[] ints, string[] strings)>("[{ \"Item1\": [], \"Item2\": [] }]");
-
-        [Fact]
-        public Task Mixed_Ids() => Verify<object>("[ 1, \"id2\" ]");
-
-        [Fact]
-        public Task DateTime_is_UTC() => Verify<Company>(Single_Company);
-
-        [Fact]
-        public Task Language_unknown_type() => Verify<object>(Single_Language);
+        public Task Language_by_vertex_inheritance() => Verify<object>(Single_Language);
 
         [Fact]
         public Task Language_strongly_typed() => Verify<Language>(Single_Language);
@@ -132,103 +162,25 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
         public Task Language_to_generic_vertex() => Verify<Vertex>(Single_Language);
 
         [Fact]
+        public Task Language_unknown_type() => Verify<object>(Single_Language);
+
+        [Fact]
         public Task Languages_to_object() => Verify<object>(ArrayOfLanguages);
-
-        [Fact]
-        public Task Person_strongly_typed() => Verify<Person>(Single_Person);
-
-        [Fact]
-        public Task Person_with_null() => Verify<Person>(Single_Person_with_null);
-
-        [Fact]
-        public Task Person_StringId() => Verify<Person>(Single_Person_String_Id);
-
-        [Fact]
-        public Task Person_lowercase_strongly_typed() => Verify<Person>(Single_Person_lowercase_properties);
-
-        [Fact]
-        public Task Person_without_PhoneNumbers_strongly_typed() => Verify<Person>(Single_Person_without_PhoneNumbers);
-
-        [Fact]
-        public Task TimeFrame_strongly_typed() => Verify<TimeFrame>(Single_TimeFrame);
-
-        [Fact]
-        public Task Language_by_vertex_inheritance() => Verify<object>(Single_Language);
-
-        [Fact]
-        public Task Tuple() => Verify<(Person, Language)>(Tuple_of_Person_Language);
-
-        [Fact]
-        public Task GraphSon3_Tuple() => Verify<(Person, Language)>(Graphson3_Tuple_of_Person_Language);
-
-        [Fact]
-        public Task Tuple_vertex_vertex() => Verify<(Vertex, Vertex)>(Tuple_of_Person_Language);
-
-        [Fact]
-        public Task NamedTuple() => Verify<PersonLanguageTuple>(Named_tuple_of_Person_Language);
-
-        [Fact]
-        public Task Graphson2Path() => Verify<Path>(Graphson2_Paths);
-
-        [Fact]
-        public Task Graphson3Path() => Verify<Path>(Graphson3_Paths);
-
-        [Fact]
-        public Task Array() => Verify<Language[]>(ArrayOfLanguages);
-
-        [Fact]
-        public Task Nested_Array() => Verify<Language[][]>(Nested_array_of_Languages);
-
-        [Fact]
-        public Task Scalar() => Verify<int>("[ 36 ]");
-
-        [Fact]
-        public Task Property_from_Scalar() => Verify<Property<int>>("[ 36 ]");
-
-        [Fact]
-        public Task Scalar_as_object() => Verify<object>("[ 36 ]");
-
-        [Fact]
-        public Task DateTime_from_string() => Verify<DateTime>("[ \"2018-12-17T08:00:00Z\" ]");
-
-        [Fact]
-        public Task DateTimeOffset_from_string() => Verify<DateTimeOffset>("[ \"2018-12-17T08:00:00Z\" ]");
-
-        [Fact]
-        public Task DateTime_from_number() => Verify<DateTime>("[ 123456789 ]");
-
-        [Fact]
-        public Task DateTime_from_double() => Verify<DateTime>("[ 123456789.2 ]");
-
-        [Fact]
-        public Task TimeSpan_from_integer() => Verify<TimeSpan>("[ 123456789 ]");
-
-        [Fact]
-        public Task TimeSpan_from_double() => Verify<TimeSpan>("[ 123456789.2 ]");
-
-        [Fact]
-        public Task DateTimeOffset_from_number() => Verify<DateTimeOffset>("[ 123456789 ]");
 
         [Fact]
         public Task Meta_Properties() => Verify<Country>(Country_with_meta_properties);
 
         [Fact]
-        public Task VertexProperties() => Verify<VertexProperty<object>>(Vertex_Properties);
-
-        [Fact]
-        public Task VertexProperty_as_object() => Verify<object>("[ { \"value\": 1540202009475, \"id\": 1, \"label\": \"Property1\", \"properties\": { \"metaKey\": \"MetaValue\" } } ]");
-
-        [Fact]
-        public Task Property_as_object() => Verify<object>("[ { \"value\": 1540202009475, \"key\": \"Property1\" } ]");
-
-        [Fact]
-        public Task VertexProperties_with_model() => Verify<VertexProperty<object, MetaPoco>>(Vertex_Properties);
-
-        [Fact]
         public Task MetaProperties() => Verify<Property<object>>(Properties);
 
         [Fact]
-        public Task Guid() => Verify<Guid>("[ \"FCE0765A-454F-4D00-83DA-D76790156E29\" ]");
+        public Task Mixed_Ids() => Verify<object>("[ 1, \"id2\" ]");
+
+        [Fact]
+        public Task NamedTuple() => Verify<PersonLanguageTuple>(Named_tuple_of_Person_Language);
+
+        [Fact]
+        public Task Nested_Array() => Verify<Language[][]>(Nested_array_of_Languages);
 
         [Fact]
         public Task Nullable() => Verify<int?>("[ 42 ]");
@@ -237,18 +189,66 @@ namespace ExRam.Gremlinq.Tests.Infrastructure
         public Task Nullable_null() => Verify<int?>("[ 42, null ]");
 
         [Fact]
-        public Task VertexPropertyWithoutProperties() => Verify<VertexProperty<object, object>>("[ { \"id\": 166, \"value\": \"bob\", \"label\": \"Name\" } ]");
+        public Task Person_lowercase_strongly_typed() => Verify<Person>(Single_Person_lowercase_properties);
+
+        [Fact]
+        public Task Person_StringId() => Verify<Person>(Single_Person_String_Id);
+
+        [Fact]
+        public Task Person_strongly_typed() => Verify<Person>(Single_Person);
+
+        [Fact]
+        public Task Person_with_null() => Verify<Person>(Single_Person_with_null);
+
+        [Fact]
+        public Task Person_without_PhoneNumbers_strongly_typed() => Verify<Person>(Single_Person_without_PhoneNumbers);
+
+        [Fact]
+        public Task Property_as_object() => Verify<object>("[ { \"value\": 1540202009475, \"key\": \"Property1\" } ]");
+
+        [Fact]
+        public Task Property_from_Scalar() => Verify<Property<int>>("[ 36 ]");
+
+        [Fact]
+        public Task Scalar() => Verify<int>("[ 36 ]");
+
+        [Fact]
+        public Task Scalar_as_object() => Verify<object>("[ 36 ]");
+
+        [Fact]
+        public Task String_Ids() => Verify<object>("[ \"id1\", \"id2\" ]");
+
+        [Fact]
+        public Task String_Ids2() => Verify<object>("[ \"1\", \"2\" ]");
+
+        [Fact]
+        public Task TimeFrame_strongly_typed() => Verify<TimeFrame>(Single_TimeFrame);
+
+        [Fact]
+        public Task TimeSpan_from_double() => Verify<TimeSpan>("[ 123456789.2 ]");
+
+        [Fact]
+        public Task TimeSpan_from_integer() => Verify<TimeSpan>("[ 123456789 ]");
+
+        [Fact]
+        public Task Tuple() => Verify<(Person, Language)>(Tuple_of_Person_Language);
+
+        [Fact]
+        public Task Tuple_vertex_vertex() => Verify<(Vertex, Vertex)>(Tuple_of_Person_Language);
+
+        [Fact]
+        public Task VertexProperties() => Verify<VertexProperty<object>>(Vertex_Properties);
+
+        [Fact]
+        public Task VertexProperties_with_model() => Verify<VertexProperty<object, MetaPoco>>(Vertex_Properties);
+
+        [Fact]
+        public Task VertexProperty_as_object() => Verify<object>("[ { \"value\": 1540202009475, \"id\": 1, \"label\": \"Property1\", \"properties\": { \"metaKey\": \"MetaValue\" } } ]");
 
         [Fact]
         public Task VertexPropertyWithDateTimeOffset() => Verify<VertexProperty<string, PropertyValidity>>("[ { \"id\": 166, \"value\": \"bob\", \"label\": \"Name\", \"properties\": { \"ValidFrom\": 1548112365431 } } ]");
 
         [Fact]
-        public Task Configured_property_name() => Verify<Person>(
-            "[ { \"id\": 13, \"label\": \"Person\", \"type\": \"vertex\", \"properties\": { \"replacement\": [ { \"id\": 1, \"value\": \"nameValue\" } ] } } ]",
-            env => env
-                .ConfigureModel(model => model
-                    .ConfigureVertices(_ => _
-                        .ConfigureElement<Person>(conf => conf
-                            .ConfigureName(x => x.Name, "replacement")))));
+        public Task VertexPropertyWithoutProperties() => Verify<VertexProperty<object, object>>("[ { \"id\": 166, \"value\": \"bob\", \"label\": \"Name\" } ]");
     }
 }
