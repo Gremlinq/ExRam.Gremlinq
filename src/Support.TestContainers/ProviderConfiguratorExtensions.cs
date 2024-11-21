7e0742efb673de6a95c594d04697c11cca1480b7
Daniel Weber <danielcweber@users.noreply.github.com>
2024-11-21 20:08:49 +0100

    Initial working version of a TestContainers setup upon the first request on an IGremlinqClient.

---
+++ src/Support.TestContainers/ProviderConfiguratorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using ExRam.Gremlinq.Providers.Core;
+
+namespace ExRam.Gremlinq.Support.TestContainers
+{
+
+    public static class ProviderConfiguratorExtensions
+    {
+        public static IPoolGremlinqClientFactory<IWebSocketGremlinqClientFactory> UseTestContainers(this IPoolGremlinqClientFactory<IWebSocketGremlinqClientFactory> factory, Func<TestContainersConfigurator, IPoolGremlinqClientFactory<IWebSocketGremlinqClientFactory>> continuation) => continuation(new TestContainersConfigurator(factory));
+    }
+}
---
+++ test/Support.TestContainers.Tests/GremlinServerIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using DotNet.Testcontainers.Builders;
+
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Support.NewtonsoftJson;
+using ExRam.Gremlinq.Providers.GremlinServer;
+using ExRam.Gremlinq.Tests.Entities;
+
+namespace ExRam.Gremlinq.Support.TestContainers.Tests
+{
+    public class GremlinServerIntegrationTests : VerifyBase
+    {
+        private readonly IGremlinQuerySource _g;
+
+        public GremlinServerIntegrationTests() : base()
+        {
+            _g = GremlinQuerySource.g
+                .UseGremlinServer<Vertex, Edge>(configurator => configurator
+                    .ConfigureClientFactory(factory => factory
+                        .UseTestContainers(c => c
+                            .ConfigureContainer(builder => builder
+                                .WithImage("tinkerpop/gremlin-server")
+                                .WithPortBinding(8182, true)
+                                .WithWaitStrategy(Wait
+                                    .ForUnixContainer()
+                                    .UntilPortIsAvailable(8182)))))
+                    .UseNewtonsoftJson());
+        }
+
+        [Fact]
+        public Task Inject_sum() => Verify(_g
+            .Inject(1, 2, 3)
+            .Sum()
+            .FirstAsync());
+    }
+}
