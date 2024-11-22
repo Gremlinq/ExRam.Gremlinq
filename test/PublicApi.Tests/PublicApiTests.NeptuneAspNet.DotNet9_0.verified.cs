33bc9d31be711e7b3a65d16dad5c62756b305c37
Daniel Weber <danielcweber@users.noreply.github.com>
2024-11-22 21:13:30 +0100

    Add API Snapshots for .NET 9.

---
+++ test/PublicApi.Tests/PublicApiTests.NeptuneAspNet.DotNet9_0.verified.cs
@@ -0,0 +1,7 @@
+﻿namespace ExRam.Gremlinq.Providers.Neptune.AspNet
+{
+    public static class GremlinqServicesBuilderExtensions
+    {
+        public static ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder<ExRam.Gremlinq.Providers.Neptune.INeptuneConfigurator> UseNeptune<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder setup) { }
+    }
+}
