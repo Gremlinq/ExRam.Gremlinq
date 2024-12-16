952e3b65d233f49feb52533ced420bdc07693d14
Daniel Weber <danielcweber@users.noreply.github.com>
2024-12-16 15:53:19 +0100

    CustomGremlinServerContainerFixture: The groovy-stuff is subject to GroovyIntegrationTests.

--- test/Providers.GremlinServer.Tests/GroovyIntegrationTests.cs
+++ test/Providers.GremlinServer.Tests/GroovyIntegrationTests.cs
@@ -1,13 +1,37 @@
-﻿using ExRam.Gremlinq.Tests.Fixtures;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Serialization;
+using ExRam.Gremlinq.Core.Transformation;
+using ExRam.Gremlinq.Tests.Fixtures;
 using ExRam.Gremlinq.Tests.Infrastructure;
+
+using Gremlin.Net.Driver.Messages;
+using Gremlin.Net.Process.Traversal;
 
 namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
 {
     [IntegrationTest("Linux", true)]
     [IntegrationTest("Windows")]
-    public class GroovyIntegrationTests : QueryExecutionTest, IClassFixture<CustomGremlinServerContainerFixture>
+    public class GroovyIntegrationTests : QueryExecutionTest, IClassFixture<GroovyIntegrationTests.Fixture>
     {
-        public GroovyIntegrationTests(CustomGremlinServerContainerFixture fixture) : base(
+        public class Fixture : CustomGremlinServerContainerFixture
+        {
+            protected override IGremlinQuerySource TransformQuerySource(IContainer container, IGremlinQuerySource g)
+            {
+                return base
+                    .TransformQuerySource(container, g)
+                    .ConfigureEnvironment(env => env
+                        .ConfigureSerializer(ser => ser
+                            .Add(ConverterFactory
+                                .Create<Bytecode, RequestMessage>((bytecode, env, _, recurse) => recurse.TryTransform(bytecode, env, out GroovyGremlinScript groovyQuery) && recurse.TryTransform(groovyQuery, env, out RequestMessage? message)
+                                    ? message
+                                    : null))));
+            }
+        }
+
+        public GroovyIntegrationTests(Fixture fixture) : base(
             fixture,
             new ExecutingVerifier())
         {
