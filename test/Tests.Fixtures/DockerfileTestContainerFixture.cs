d6cdda51ed2debb6b25f10947dd360f75ad9ca61
Daniel Weber <danielcweber@users.noreply.github.com>
2024-12-19 18:05:36 +0100

    Use SourceFileName.OfThis.

--- test/Tests.Fixtures/DockerfileTestContainerFixture.cs
+++ test/Tests.Fixtures/DockerfileTestContainerFixture.cs
@@ -1,29 +1,23 @@
-﻿using System.Runtime.CompilerServices;
-
-using DotNet.Testcontainers.Builders;
+﻿using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Images;
+
+using ExRam.Gremlinq.Tests.Infrastructure;
 
 namespace ExRam.Gremlinq.Tests.Fixtures
 {
     public abstract class DockerfileTestContainerFixture : TestContainerFixtureBase
     {
         private readonly string _dockerfile;
-        private readonly string _callerFilePath;
 
-        protected DockerfileTestContainerFixture(string dockerfile, int port = 8182) : this(dockerfile, port, 0)
-        {
-        }
-
-        private DockerfileTestContainerFixture(string dockerfile, int port = 8182, int _ = 0, [CallerFilePath] string callerFilePath = "") : base(port)
+        protected DockerfileTestContainerFixture(string dockerfile, int port = 8182) : base(port)
         {
             _dockerfile = dockerfile;
-            _callerFilePath = callerFilePath;
         }
 
         protected override async Task<IImage> GetImage()
         {
             var futureImage = new ImageFromDockerfileBuilder()
-                .WithDockerfileDirectory(Path.Combine(Path.GetDirectoryName(_callerFilePath)!, "Dockerfiles"))
+                .WithDockerfileDirectory(Path.Combine(Path.GetDirectoryName(SourceFileName.OfThis())!, "Dockerfiles"))
                 .WithDockerfile(_dockerfile)
                 .Build();
 
