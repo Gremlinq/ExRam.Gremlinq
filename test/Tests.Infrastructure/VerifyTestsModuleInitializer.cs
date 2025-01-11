db3db656a6e5a1361b8f9fbfca1bbb96dc87e747
Daniel Weber <danielcweber@users.noreply.github.com>
2025-01-11 18:24:03 +0100

    Apply some code style to the Argon-converter.

--- test/Tests.Infrastructure/VerifyTestsModuleInitializer.cs
+++ test/Tests.Infrastructure/VerifyTestsModuleInitializer.cs
@@ -4,37 +4,22 @@ using Argon;
 
 using ExRam.Gremlinq.Core;
 
-class VerifyTestsModuleInitializer
+internal static class VerifyTestsModuleInitializer
 {
     private sealed class TreeConverter : JsonConverter
     {
-        public override bool CanConvert(Type type)
-        {
-            return typeof(ITree).IsAssignableFrom(type);
-        }
+        public override bool CanConvert(Type type) => typeof(ITree).IsAssignableFrom(type);
 
-        public override object? ReadJson(JsonReader reader, Type type, object? existingValue, JsonSerializer serializer)
-        {
-            throw new NotImplementedException();
-        }
+        public override object? ReadJson(JsonReader reader, Type type, object? existingValue, JsonSerializer serializer) => throw new NotImplementedException();
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            WriteJsonEx(writer, (dynamic)value, serializer);
-        }
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => WriteJsonEx(writer, (dynamic)value, serializer);
 
-        public void WriteJsonEx<TKey, TValue>(JsonWriter writer, Tree<TKey, TValue> value, JsonSerializer serializer)
+        private static void WriteJsonEx<TKey, TValue>(JsonWriter writer, Tree<TKey, TValue> value, JsonSerializer serializer)
             where TKey : notnull
-            where TValue : ITree
-        {
-            serializer.Serialize(writer, value.Select(kvp => new { kvp.Key, kvp.Value }).ToArray());
-        }
+            where TValue : ITree => serializer.Serialize(writer, value.Select(kvp => new { kvp.Key, kvp.Value }).ToArray());
 
-        public void WriteJsonEx<TKey>(JsonWriter writer, Tree<TKey> value, JsonSerializer serializer)
-            where TKey : notnull
-        {
-            serializer.Serialize(writer, value.Select(kvp => new { kvp.Key, kvp.Value }).ToArray());
-        }
+        private static void WriteJsonEx<TKey>(JsonWriter writer, Tree<TKey> value, JsonSerializer serializer)
+            where TKey : notnull => serializer.Serialize(writer, value.Select(kvp => new { kvp.Key, kvp.Value }).ToArray());
     }
 
     [ModuleInitializer]
