4dc6cdf22c1fd165e203f7f14e9938afb621d847
Daniel Weber <danielcweber@users.noreply.github.com>
2025-01-12 23:02:51 +0100

    Small fix in types.

--- src/Core/Queries/GremlinQuery.TreeBuilder.cs
+++ src/Core/Queries/GremlinQuery.TreeBuilder.cs
@@ -9,15 +9,15 @@ namespace ExRam.Gremlinq.Core
     {
         private sealed partial class TreeBuilder<TNode1, TNode2, TNode3, TNode4, TNode5, TNode6, TNode7, TNode8, TNode9, TNode10, TNode11, TNode12, TNode13, TNode14, TNode15, TNode16, TNode17, TNode18, TNode19, TNode20, TNode21, TNode22, TNode23, TNode24, TNode25, TNode26, TNode27, TNode28, TNode29>
         {
+            private readonly ImmutableList<TreeStep.ByStep> _bySteps;
             private readonly GremlinQuery<T1, T2, T3, T4> _sourceQuery;
-            private readonly IImmutableList<TreeStep.ByStep> _bySteps;
 
             public TreeBuilder(GremlinQuery<T1, T2, T3, T4> sourceQuery) : this(sourceQuery, ImmutableList<TreeStep.ByStep>.Empty)
             {
 
             }
 
-            public TreeBuilder(GremlinQuery<T1, T2, T3, T4> sourceQuery, IImmutableList<TreeStep.ByStep> bySteps)
+            public TreeBuilder(GremlinQuery<T1, T2, T3, T4> sourceQuery, ImmutableList<TreeStep.ByStep> bySteps)
             {
                 _bySteps = bySteps;
                 _sourceQuery = sourceQuery;
