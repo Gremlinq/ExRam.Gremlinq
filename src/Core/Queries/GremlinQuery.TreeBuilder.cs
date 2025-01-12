022683dfdea0c50dfda5f6ccb8a9a0414a592a36
Daniel Weber <danielcweber@users.noreply.github.com>
2025-01-12 16:53:31 +0100

    Bugfix: TreeStep was missing in operator with builder.

--- src/Core/Queries/GremlinQuery.TreeBuilder.cs
+++ src/Core/Queries/GremlinQuery.TreeBuilder.cs
@@ -24,6 +24,7 @@ namespace ExRam.Gremlinq.Core
                         static (builder, state) =>
                         {
                             return builder
+                                .AddStep(TreeStep.Instance)
                                 .As<IGremlinQuery<TTree>>()
                                 .Build();
                         },
