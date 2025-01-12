7f97ada0142e76bcb541db248de9e0284e35994b
Daniel Weber <danielcweber@users.noreply.github.com>
2025-01-12 19:30:31 +0100

    Add TreeStep ByTraversals.

--- src/Core/Steps/TreeStep.cs
+++ src/Core/Steps/TreeStep.cs
@@ -2,6 +2,23 @@
 {
     public sealed class TreeStep : Step
     {
+        public abstract class ByStep : Step
+        {
+            protected ByStep(SideEffectSemanticsChange sideEffectSemanticsChange = SideEffectSemanticsChange.None) : base(sideEffectSemanticsChange)
+            {
+            }
+        }
+
+        public sealed class ByKeyStep : ByStep
+        {
+            public ByKeyStep(Key key)
+            {
+                Key = key;
+            }
+
+            public Key Key { get; }
+        }
+
         public static readonly TreeStep Instance = new();
     }
 }
