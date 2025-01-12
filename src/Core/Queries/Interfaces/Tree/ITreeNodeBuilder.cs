e47d6c50ab988adb966e6e1a5c7e8bbab385c818
Daniel Weber <danielcweber@users.noreply.github.com>
2025-01-12 19:04:13 +0100

    New ITreeNodeBuilder.

---
+++ src/Core/Queries/Interfaces/Tree/ITreeNodeBuilder.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace ExRam.Gremlinq.Core
+{
+    public interface ITreeNodeBuilder<TNode>
+    {
+        ITreeNodeBuilder<TNode> By<TKey>(Expression<Func<TNode, TKey>> expression);
+    }
+}
