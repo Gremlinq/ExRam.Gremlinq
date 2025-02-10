2aff025c63f7b633acd3e63fbf1510651b81ba05
Daniel Weber <danielcweber@users.noreply.github.com>
2025-02-10 15:43:31 +0100

    Add StringGremlinQuery to implement IStringGremlinQuery.

---
+++ src/Core/Queries/StringGremlinQuery.cs
@@ -0,0 +1,19 @@
+﻿#pragma warning disable IDE0003
+// ReSharper disable ArrangeThisQualifier
+using System.Collections.Immutable;
+
+namespace ExRam.Gremlinq.Core
+{
+    internal sealed class StringGremlinQuery : GremlinQueryBase<string, object, object, IGremlinQueryBase>,
+        IStringGremlinQuery
+    {
+        public StringGremlinQuery(
+            IGremlinQueryEnvironment environment,
+            Traversal steps,
+            IImmutableDictionary<StepLabel, LabelProjections> labelProjections,
+            IImmutableDictionary<object, object?> metadata) : base(environment, steps, labelProjections, metadata)
+        {
+
+        }
+    }
+}
