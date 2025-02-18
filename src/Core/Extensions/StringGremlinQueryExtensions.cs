51b21eb153b6824828adb3e6f82493de5c9cb7c8
Daniel Weber <danielcweber@users.noreply.github.com>
2025-02-18 18:06:36 +0100

    Avoid extra asString in extensions when the element type is known to be string (or at least that's what's likely given the signature).

--- src/Core/Extensions/StringGremlinQueryExtensions.cs
+++ src/Core/Extensions/StringGremlinQueryExtensions.cs
@@ -3,23 +3,28 @@
     public static class StringGremlinQueryExtensions
     {
         public static IStringGremlinQuery<string> Concat(this IGremlinQueryBase<string> query, params string[] strings) => query
-            .AsString()
+            .AsAdmin()
+            .ChangeQueryType<IStringGremlinQuery<string>>()
             .Concat(strings);
 
         public static IStringGremlinQuery<string> Concat(this IGremlinQueryBase<string> query, params Func<IStringGremlinQuery<string>, IGremlinQueryBase<string>>[] stringTraversals) => query
-            .AsString()
+            .AsAdmin()
+            .ChangeQueryType<IStringGremlinQuery<string>>()
             .Concat(stringTraversals);
 
         public static IStringGremlinQuery<string> Substring(this IGremlinQueryBase<string> query, int startIndex) => query
-            .AsString()
+            .AsAdmin()
+            .ChangeQueryType<IStringGremlinQuery<string>>()
             .Substring(startIndex);
 
         public static IStringGremlinQuery<string> Substring(this IGremlinQueryBase<string> query, int startIndex, int length) => query
-            .AsString()
+            .AsAdmin()
+            .ChangeQueryType<IStringGremlinQuery<string>>()
             .Substring(startIndex, length);
 
         public static IStringGremlinQuery<string> Substring(this IGremlinQueryBase<string> query, Range range) => query
-            .AsString()
+            .AsAdmin()
+            .ChangeQueryType<IStringGremlinQuery<string>>()
             .Substring(range);
     }
 }
