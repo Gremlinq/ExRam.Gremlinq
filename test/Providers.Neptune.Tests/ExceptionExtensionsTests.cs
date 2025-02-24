293bb855034e332367a256d1ed8e214209f9f9c2
Daniel Weber <danielcweber@users.noreply.github.com>
2025-02-24 18:37:12 +0100

    Add test.

---
+++ test/Providers.Neptune.Tests/ExceptionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Execution;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+using Gremlin.Net.Driver.Exceptions;
+using Gremlin.Net.Driver.Messages;
+
+namespace ExRam.Gremlinq.Providers.Neptune.Tests
+{
+    public class ExceptionExtensionsTests
+    {
+        [Fact]
+        public Task ServerError_InternalFailureException() => Verify(new GremlinQueryExecutionException(
+            GremlinQueryExecutionContext.Create(GremlinQuerySource.g.Inject(42)),
+            new ResponseException(
+                ResponseStatusCode.ServerError,
+                ImmutableDictionary<string, object>.Empty,
+                """
+                ServerError: {"detailedMessage":"The variable a for math() step must resolve to a Number - it is instead of type null with value null","code":"InternalFailureException","requestId":"1bfc68f6-b58e-4b55-b411-cd5552d27f0b","message":"The variable a for math() step must resolve to a Number - it is instead of type null with value null"}
+                """))
+            .TryGetNeptuneGremlinQueryExecutionException())
+            .ScrubGuids();
+    }
+}
