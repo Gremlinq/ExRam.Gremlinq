227b8fc430f289467f43116f1f79d490efa23e22
Daniel Weber <danielcweber@users.noreply.github.com>
2025-03-28 08:08:51 +0100

    Polyfill OverloadResolutionPriorityAttribute for platforms < .NET 9.

---
+++ src/Core/OverloadResolutionPriorityAttribute.cs
@@ -0,0 +1,15 @@
+﻿#if !NET9_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class OverloadResolutionPriorityAttribute : Attribute
+    {
+        public OverloadResolutionPriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+
+        public int Priority { get; }
+    }
+}
+#endif
