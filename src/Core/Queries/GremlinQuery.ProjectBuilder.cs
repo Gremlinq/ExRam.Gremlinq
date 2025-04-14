bd3c1768b58e233727e0c3513e81d982f4be0324
Daniel Weber <danielcweber@users.noreply.github.com>
2025-04-14 16:17:01 +0200

    Code style.

--- src/Core/Queries/GremlinQuery.ProjectBuilder.cs
+++ src/Core/Queries/GremlinQuery.ProjectBuilder.cs
@@ -62,48 +62,33 @@ namespace ExRam.Gremlinq.Core
             }
 
             IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1> IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1>.By(Func<GremlinQuery<T1, T2, T3, T4>, IGremlinQueryBase> projection)
-            {
-                return ByLambda<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(projection);
-            }
+                => ByLambda<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(projection);
 
             IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1> IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1>.By(string name, Func<GremlinQuery<T1, T2, T3, T4>, IGremlinQueryBase> projection)
-            {
-                return ByLambda<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(projection, name);
-            }
+                => ByLambda<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(projection, name);
 
             IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1> IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1>.By(string name, Expression<Func<T1, object>> projection)
-            {
-                return ByExpression<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(projection, name);
-            }
+                => ByExpression<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(projection, name);
 
-            IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1> IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1>.By(Expression<Func<T1, object>> projection)
-            {
-                return projection.IsIdentityExpression()
-                    ? ByLambda<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(static __ => __.Identity())
-                    : projection.Body.Strip() is MemberExpression memberExpression
-                        ? ByExpression<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(memberExpression, memberExpression.Member.Name)
-                        : throw new ExpressionNotSupportedException(projection);
-            }
+            IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1> IProjectDynamicBuilder<GremlinQuery<T1, T2, T3, T4>, T1>.By(Expression<Func<T1, object>> projection) => projection.IsIdentityExpression()
+                ? ByLambda<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(static __ => __.Identity())
+                : projection.Body.Strip() is MemberExpression memberExpression
+                    ? ByExpression<object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object>(memberExpression, memberExpression.Member.Name)
+                    : throw new ExpressionNotSupportedException(projection);
 
             IMapGremlinQuery<TItem1> IProjectMapResult<TItem1>.Build() => Build<IMapGremlinQuery<TItem1>>();
 
             IGremlinQuery<dynamic> IProjectDynamicResult.Build() => Build<IGremlinQuery<dynamic>>();
 
-            IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1> IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1>.By<TSourceProperty, TTargetProperty>(Expression<Func<TItem1, TTargetProperty>> targetExpression, Func<GremlinQuery<T1, T2, T3, T4>, IGremlinQueryBase<TSourceProperty>> projection)
-            {
-                return By(
-                    targetExpression,
-                    static (@this, memberName, projection) => @this.ByLambda<TItem1, TItem2, TItem3, TItem4, TItem5, TItem6, TItem7, TItem8, TItem9, TItem10, TItem11, TItem12, TItem13, TItem14, TItem15, TItem16>(projection, memberName),
-                    projection);
-            }
+            IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1> IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1>.By<TSourceProperty, TTargetProperty>(Expression<Func<TItem1, TTargetProperty>> targetExpression, Func<GremlinQuery<T1, T2, T3, T4>, IGremlinQueryBase<TSourceProperty>> projection) => By(
+                targetExpression,
+                static (@this, memberName, projection) => @this.ByLambda<TItem1, TItem2, TItem3, TItem4, TItem5, TItem6, TItem7, TItem8, TItem9, TItem10, TItem11, TItem12, TItem13, TItem14, TItem15, TItem16>(projection, memberName),
+                projection);
 
-            IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1> IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1>.By<TSourceProperty, TTargetProperty>(Expression<Func<TItem1, TTargetProperty>> targetExpression, Expression<Func<T1, TSourceProperty>> projection)
-            {
-                return By(
-                    targetExpression,
-                    static (@this, memberName, projection) => @this.ByExpression<TItem1, TItem2, TItem3, TItem4, TItem5, TItem6, TItem7, TItem8, TItem9, TItem10, TItem11, TItem12, TItem13, TItem14, TItem15, TItem16>(projection, memberName),
-                    projection);
-            }
+            IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1> IProjectMapBuilder<GremlinQuery<T1, T2, T3, T4>, T1, TItem1>.By<TSourceProperty, TTargetProperty>(Expression<Func<TItem1, TTargetProperty>> targetExpression, Expression<Func<T1, TSourceProperty>> projection) => By(
+                targetExpression,
+                static (@this, memberName, projection) => @this.ByExpression<TItem1, TItem2, TItem3, TItem4, TItem5, TItem6, TItem7, TItem8, TItem9, TItem10, TItem11, TItem12, TItem13, TItem14, TItem15, TItem16>(projection, memberName),
+                projection);
 
             private ProjectBuilder<TNewItem1, TNewItem2, TNewItem3, TNewItem4, TNewItem5, TNewItem6, TNewItem7, TNewItem8, TNewItem9, TNewItem10, TNewItem11, TNewItem12, TNewItem13, TNewItem14, TNewItem15, TNewItem16> ByLambda<TNewItem1, TNewItem2, TNewItem3, TNewItem4, TNewItem5, TNewItem6, TNewItem7, TNewItem8, TNewItem9, TNewItem10, TNewItem11, TNewItem12, TNewItem13, TNewItem14, TNewItem15, TNewItem16>(Func<GremlinQuery<T1, T2, T3, T4>, IGremlinQueryBase> projection, string? name = default)
             {
