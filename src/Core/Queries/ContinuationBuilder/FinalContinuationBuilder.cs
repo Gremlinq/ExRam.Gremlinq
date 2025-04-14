4e65f7e545bd487f0a47dc82732e87b39496ec8b
Daniel Weber <danielcweber@users.noreply.github.com>
2025-04-14 19:53:22 +0200

    Add AddSteps with Spans to FinalContinuationBuilder.

--- src/Core/Queries/ContinuationBuilder/FinalContinuationBuilder.cs
+++ src/Core/Queries/ContinuationBuilder/FinalContinuationBuilder.cs
@@ -26,6 +26,8 @@ namespace ExRam.Gremlinq.Core
 
         public FinalContinuationBuilder<TOuterQuery> AddStep(Step step) => new (_outer, _steps.Push(step), _labelProjections);
 
+        public FinalContinuationBuilder<TOuterQuery> AddSteps(ReadOnlySpan<Step> steps) => new(_outer, _steps.Push(steps), _labelProjections);
+
         public FinalContinuationBuilder<TOuterQuery> WithSteps<TState>(Func<Traversal, TState, Traversal> traversalTransformation, TState state) => new (_outer, traversalTransformation(_steps, state), _labelProjections);
 
         public FinalContinuationBuilder<TOuterQuery> WithNewProjection<TState>(Func<Projection, TState, Projection> projectionTransformation, TState state) => new(_outer, _steps.WithProjection(projectionTransformation(_steps.Projection, state)), _labelProjections);
