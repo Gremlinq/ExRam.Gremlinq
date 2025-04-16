72a64c0d18c8750e396af6a5291c310186e71e88
Daniel Weber <danielcweber@users.noreply.github.com>
2025-04-16 11:34:25 +0200

    Swap type parameters.

--- src/Core/Queries/ContinuationBuilder/FinalContinuationBuilderTransformation.cs
+++ src/Core/Queries/ContinuationBuilder/FinalContinuationBuilderTransformation.cs
@@ -3,7 +3,7 @@
     internal delegate TResult FinalContinuationBuilderTransformation<TOuterQuery, TResult>(FinalContinuationBuilder<TOuterQuery> builder, Memory<Traversal> traversals)
         where TOuterQuery : GremlinQueryBase, IGremlinQueryBase;
 
-    internal delegate TResult FinalContinuationBuilderTransformation<TOuterQuery, TState, TResult>(FinalContinuationBuilder<TOuterQuery> builder, ReadOnlySpan<TState> state)
+    internal delegate TResult FinalContinuationBuilderTransformation<TOuterQuery, TResult, TState>(FinalContinuationBuilder<TOuterQuery> builder, ReadOnlySpan<TState> state)
         where TOuterQuery : GremlinQueryBase, IGremlinQueryBase
         where TResult : IStartGremlinQuery;
 }
