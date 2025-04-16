cdcb20d4bb138a20370088b51837a67d56fc00d4
Daniel Weber <danielcweber@users.noreply.github.com>
2025-04-16 12:02:50 +0200

    Work around missing AsSpan on Lists.

---
+++ src/Core/Extensions/ListExtensions.cs
@@ -0,0 +1,21 @@
+﻿#if !NET7_0_OR_GREATER
+using System.Runtime.CompilerServices;
+
+namespace ExRam.Gremlinq.Core
+{
+    internal static class ListExtensions
+    {
+        //TODO: To be worked around.
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Span<T> AsSpan<T>(this List<T> list)
+        {
+            return
+#if NET5_0_OR_GREATER
+                System.Runtime.InteropServices.CollectionsMarshal.AsSpan(list);
+#else
+                list.ToArray();
+#endif
+        }
+    }
+}
+#endif
