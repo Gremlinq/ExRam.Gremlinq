8012a66c926cd9440724c39597aa28f3a184cd33
Daniel Weber <danielcweber@users.noreply.github.com>
2025-04-17 08:16:23 +0200

    Factor GremlinQueryFactory.

--- src/Core/Queries/GremlinQuery.cs
+++ src/Core/Queries/GremlinQuery.cs
@@ -1,10 +1,8 @@
 ﻿#pragma warning disable IDE0003
 // ReSharper disable ArrangeThisQualifier
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 
 using CommunityToolkit.HighPerformance;
@@ -25,18 +23,6 @@ namespace ExRam.Gremlinq.Core
 {
     internal abstract class GremlinQueryBase
     {
-        private delegate IGremlinQueryBase QueryContinuation(
-            IGremlinQueryEnvironment environment,
-            Traversal newTraversal,
-            IImmutableDictionary<StepLabel, LabelProjections> newLabelProjections,
-            IImmutableDictionary<object, object?> newMetadata);
-
-        private static readonly ConcurrentDictionary<Type, QueryContinuation> QueryContinuations = new();
-        private static readonly Type[] QueryGenericTypeDefinitionArguments = typeof(GremlinQuery<,,,>).GetGenericArguments();
-        private static readonly QueryContinuation ObjectQueryContinuation = CreateQueryContinuation<object, object, object, IGremlinQueryBase>();
-        private static readonly Type[] ImplementedInterfaces = typeof(GremlinQuery<,,,>).GetInterfaces().Append(typeof(GremlinQuery<,,,>)).ToArray();
-        private static readonly MethodInfo TryCreateQueryContinuationMethod = typeof(GremlinQueryBase).GetMethod(nameof(CreateQueryContinuation), BindingFlags.NonPublic | BindingFlags.Static)!;
-
         protected GremlinQueryBase(
             IGremlinQueryEnvironment environment,
             Traversal steps,
@@ -50,61 +36,6 @@ namespace ExRam.Gremlinq.Core
         }
 
         public override string ToString() => $"GremlinQuery(Steps.Count: {Steps.Count})";
-
-        internal static TTargetQuery CloneAs<TTargetQuery>(IGremlinQueryEnvironment environment, Traversal newTraversal, IImmutableDictionary<StepLabel, LabelProjections> newLabelProjections, IImmutableDictionary<object, object?> newMetadata)
-        {
-            var queryFactory = typeof(TTargetQuery).IsGenericType
-                ? QueryContinuations.GetOrAdd(
-                    typeof(TTargetQuery),
-                    static requestedType =>
-                    {
-                        var requestedTypeDefinition = requestedType.GetGenericTypeDefinition();
-                        var queryTypeArguments = new Type?[QueryGenericTypeDefinitionArguments.Length];
-
-                        for (var i = 0; i < ImplementedInterfaces.Length; i++)
-                        {
-                            if (ImplementedInterfaces[i] is { IsGenericType: true } queryImplementedInterface && queryImplementedInterface.GetGenericTypeDefinition() == requestedTypeDefinition)
-                            {
-                                var matchingImplementedInterfaceTypeArguments = queryImplementedInterface.GetGenericArguments();
-
-                                for (var j = 0; j < QueryGenericTypeDefinitionArguments.Length; j++)
-                                {
-                                    for (var k = 0; k < matchingImplementedInterfaceTypeArguments.Length; k++)
-                                    {
-                                        if (matchingImplementedInterfaceTypeArguments[k] == QueryGenericTypeDefinitionArguments[j])
-                                        {
-                                            queryTypeArguments[j] = requestedType.GetGenericArguments()[k];
-
-                                            break;
-                                        }
-                                    }
-
-                                    queryTypeArguments[j] ??= j == 1 && queryTypeArguments[0]!.IsArray
-                                        ? queryTypeArguments[0]!.GetElementType()!
-                                        : QueryGenericTypeDefinitionArguments[j].GetGenericParameterConstraints().SingleOrDefault() ?? typeof(object);
-                                }
-
-
-                                return (QueryContinuation?)TryCreateQueryContinuationMethod
-                                    .MakeGenericMethod(queryTypeArguments!)
-                                    .Invoke(null, null)!;
-                            }
-                        }
-
-                        throw new NotSupportedException();
-                    })
-                : ObjectQueryContinuation;
-
-            return queryFactory(environment, newTraversal, newLabelProjections, newMetadata) is TTargetQuery newTargetQuery
-                ? newTargetQuery
-                : throw new NotSupportedException($"Cannot create a query of type {typeof(TTargetQuery)}.");
-        }
-
-        private static QueryContinuation CreateQueryContinuation<T1, T2, T3, T4>() where T4 : IGremlinQueryBase => (environment, newTraversal, newLabelProjections, newMetadata) => new GremlinQuery<T1, T2, T3, T4>(
-            environment,
-            newTraversal,
-            newLabelProjections,
-            newMetadata);
 
         protected internal Traversal Steps { get; }
         protected internal IGremlinQueryEnvironment Environment { get; }
