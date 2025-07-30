443b0018d303a19f7961f2841415182e93a95ad5
Daniel Weber <danielcweber@users.noreply.github.com>
2025-07-30 16:22:43 +0200

    Workaround TestContainers bug.

--- test/Tests.Fixtures/TestContainerFixture.cs
+++ test/Tests.Fixtures/TestContainerFixture.cs
@@ -80,6 +80,10 @@ namespace ExRam.Gremlinq.Tests.Fixtures
                 {
 
                 }
+                catch (NullReferenceException)
+                {
+                    //TODO: Remove upon https://github.com/testcontainers/testcontainers-dotnet/issues/1496 fix.
+                }
             }
 
             await base.DisposeAsync();
