a90c05630a0ddaccf33630dcfc82ab4e9222fb7e
Daniel Weber <danielcweber@users.noreply.github.com>
2025-07-31 19:35:17 +0200

    StringComparisonTranslationStrictness.Strict is the default value.

--- test/Providers.Neptune.Tests/ElasticSearchSerializationTests.cs
+++ test/Providers.Neptune.Tests/ElasticSearchSerializationTests.cs
@@ -17,9 +17,6 @@ namespace ExRam.Gremlinq.Providers.Neptune.Tests
         public async Task Where_property_contains_empty_string_with_TextP_support_strict()
         {
             await _g
-                .ConfigureEnvironment(env => env
-                    .ConfigureOptions(options => options
-                        .SetValue(GremlinqOption.StringComparisonTranslationStrictness, StringComparisonTranslationStrictness.Strict)))
                 .V<Country>()
                 .Where(c => c.CountryCallingCode!.Contains(""))
                 .Verify();
@@ -29,9 +26,6 @@ namespace ExRam.Gremlinq.Providers.Neptune.Tests
         public async Task Where_property_contains_empty_string_with_TextP_support_case_insensitive_strict()
         {
             await _g
-                .ConfigureEnvironment(env => env
-                    .ConfigureOptions(options => options
-                        .SetValue(GremlinqOption.StringComparisonTranslationStrictness, StringComparisonTranslationStrictness.Strict)))
                 .V<Country>()
                 .Where(c => c.CountryCallingCode!.Contains("", StringComparison.OrdinalIgnoreCase))
                 .Verify();
