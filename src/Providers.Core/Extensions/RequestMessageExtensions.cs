d6f87ca5ac555e550e833e91d210581f8aedad13
Daniel Weber <danielcweber@users.noreply.github.com>
2025-08-13 11:50:13 +0200

    RequestMessageExtensions.TryGetGroovyScript: Return a CheapGroovyGremlinScript. The snapshots now reflect that bindings aren't checked on duplicates anymore.

--- src/Providers.Core/Extensions/RequestMessageExtensions.cs
+++ src/Providers.Core/Extensions/RequestMessageExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Immutable;
-
-using ExRam.Gremlinq.Core;
+﻿using ExRam.Gremlinq.Core;
 using ExRam.Gremlinq.Core.Serialization;
 
 using Gremlin.Net.Driver;
@@ -11,21 +9,21 @@ namespace ExRam.Gremlinq.Providers.Core
 {
     internal static class RequestMessageExtensions
     {
-        public static GroovyGremlinScript? TryGetGroovyScript(this RequestMessage requestMessage, IGremlinQueryEnvironment environment, bool includeBindings)
+        public static CheapGroovyGremlinScript? TryGetGroovyScript(this RequestMessage requestMessage, IGremlinQueryEnvironment environment, bool includeBindings)
         {
             if (requestMessage.Operation == Tokens.OpsBytecode)
             {
                 if (requestMessage.Arguments.TryGetValue(Tokens.ArgsGremlin, out var bytecodeObject) && bytecodeObject is Bytecode bytecode)
-                    return bytecode.ToGroovyScript(environment, includeBindings);
+                    return GroovyWriter.ToCheapGroovyScript(bytecode, environment, includeBindings);
             }
             else if (requestMessage.Operation == Tokens.OpsEval)
             {
                 if (requestMessage.Arguments.TryGetValue(Tokens.ArgsGremlin, out var scriptObject) && scriptObject is string script)
                 {
-                    return GroovyGremlinScript.From(
+                    return CheapGroovyGremlinScript.From(
                         script,
                         includeBindings && requestMessage.Arguments.TryGetValue(Tokens.ArgsBindings, out var bindingsObject) && bindingsObject is IReadOnlyDictionary<string, object?> bindings
-                            ? bindings.ToImmutableDictionary()
+                            ? bindings
                             : null);
                 }
             }
