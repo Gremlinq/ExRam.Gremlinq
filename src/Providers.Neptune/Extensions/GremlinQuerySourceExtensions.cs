16d3d81b2e60bbcb41f52179ae61b2d189c74d1e
Daniel Weber <danielcweber@users.noreply.github.com>
2025-09-03 17:44:38 +0200

    Assign UseDFE-StepLabel a sensible value.

--- src/Providers.Neptune/Extensions/GremlinQuerySourceExtensions.cs
+++ src/Providers.Neptune/Extensions/GremlinQuerySourceExtensions.cs
@@ -4,7 +4,7 @@ namespace ExRam.Gremlinq.Providers.Neptune
 {
     public static class GremlinQuerySourceExtensions
     {
-        private static readonly StepLabel<bool> UseDFEStepLabel = new ();
+        private static readonly StepLabel<bool> UseDFEStepLabel = "Neptune#useDFE";
 
         public static IGremlinQuerySource UseDFE(this IGremlinQuerySource source, bool enabled = true) => source
             .WithSideEffect(UseDFEStepLabel, enabled);
