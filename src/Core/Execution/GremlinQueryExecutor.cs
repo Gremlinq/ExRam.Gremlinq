c8ff756a8b664c92e209d736a262a2b12d502978
Daniel Weber <danielcweber@users.noreply.github.com>
2025-09-26 00:06:01 +0200

    Cleanup.

--- src/Core/Execution/GremlinQueryExecutor.cs
+++ src/Core/Execution/GremlinQueryExecutor.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-using Microsoft.Extensions.Logging;
 
 namespace ExRam.Gremlinq.Core.Execution
 {
--- src/Providers.Core/Extensions/GremlinqClientExtensions.cs
+++ src/Providers.Core/Extensions/GremlinqClientExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Text;
 
 using ExRam.Gremlinq.Core;
