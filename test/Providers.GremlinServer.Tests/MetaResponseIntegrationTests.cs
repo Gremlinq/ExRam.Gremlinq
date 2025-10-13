da0297560fc7df3f11ba1e6afb475aacb110e341
Daniel Weber <danielcweber@users.noreply.github.com>
2025-10-13 21:00:22 +0200

    Add integration tests for meta responses (which don't materialize yet).

---
+++ test/Providers.GremlinServer.Tests/MetaResponseIntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using ExRam.Gremlinq.Providers.Core;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    [IntegrationTest("Linux", true)]
+    [IntegrationTest("Windows")]
+    public class MetaResponseIntegrationTests : QueryExecutionTest, IClassFixture<GremlinServerContainerFixture>
+    {
+        private sealed class MetaResponseExecutingVerifier : ExecutingVerifier
+        {
+            public override Task Verify<TElement>(IGremlinQueryBase<TElement> query) => base
+                .Verify(query.Cast<MetaResponse<TElement>>());
+        }
+
+        public MetaResponseIntegrationTests(GremlinServerContainerFixture fixture) : base(
+            fixture,
+            new MetaResponseExecutingVerifier())
+        {
+        }
+    }
+}
