e84abbc45a4f3aa83f70f1a0ece37a59a7809dfe
Daniel Weber <danielcweber@users.noreply.github.com>
2025-10-13 19:54:28 +0200

    Add MetaResponse.

---
+++ src/Providers.Core/Factory/MetaResponse.cs
@@ -0,0 +1,22 @@
+﻿using Gremlin.Net.Driver.Messages;
+
+namespace ExRam.Gremlinq.Providers.Core
+{
+    public readonly struct MetaResponse<T>
+    {
+        private readonly T _value;
+        private readonly ResponseStatus? _responseStatus;
+
+        internal MetaResponse(T value, ResponseStatus responseStatus)
+        {
+            _value = value;
+            _responseStatus = responseStatus;
+        }
+
+        public T Value => _responseStatus is not null
+            ? _value
+            : throw new InvalidOperationException();
+
+        public ResponseStatus ResponseStatus => _responseStatus ?? throw new InvalidOperationException();
+    }
+}
