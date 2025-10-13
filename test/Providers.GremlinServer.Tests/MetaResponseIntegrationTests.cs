3924f8949699d07d775f5a37fd2f466f70107c02
Daniel Weber <danielcweber@users.noreply.github.com>
2025-10-13 21:15:02 +0200

    Fix test verifier.

--- test/Providers.GremlinServer.Tests/MetaResponseIntegrationTests.cs
+++ test/Providers.GremlinServer.Tests/MetaResponseIntegrationTests.cs
@@ -11,6 +11,11 @@ namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
     {
         private sealed class MetaResponseExecutingVerifier : ExecutingVerifier
         {
+            public MetaResponseExecutingVerifier() : base()
+            {
+
+            }
+
             public override Task Verify<TElement>(IGremlinQueryBase<TElement> query) => base
                 .Verify(query.Cast<MetaResponse<TElement>>());
         }
