489c803064a60abc7bf7f0c22230a7cc9d7a6080
Daniel Weber <danielcweber@users.noreply.github.com>
2025-10-22 17:57:16 +0200

    Save an allocation of the query in GremlinQueryExtensions.First/Single/Last/OrDefault etc.

--- src/Core/Extensions/GremlinQueryExtensions.cs
+++ src/Core/Extensions/GremlinQueryExtensions.cs
@@ -1,44 +1,56 @@
-﻿namespace ExRam.Gremlinq.Core
+﻿using ExRam.Gremlinq.Core.Steps;
+
+using Gremlin.Net.Process.Traversal;
+
+namespace ExRam.Gremlinq.Core
 {
     public static class GremlinQueryExtensions
     {
+        private static readonly LimitStep LimitGlobal2 = new(2, Scope.Global);
+
         public static ValueTask<TElement[]> ToArrayAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
             .ToAsyncEnumerable()
             .ToArrayAsync(ct);
 
         public static ValueTask<TElement> FirstAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
-            .Cast<TElement>()
-            .Limit(1)
+            .AsAdmin()
+            .ConfigureSteps<IGremlinQuery<TElement>>(static traversal => traversal
+                .Push(LimitStep.LimitGlobal1))
             .ToAsyncEnumerable()
             .FirstAsync(ct);
 
         public static ValueTask<TElement?> FirstOrDefaultAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
-            .Cast<TElement>()
-            .Limit(1)
+            .AsAdmin()
+            .ConfigureSteps<IGremlinQuery<TElement>>(static traversal => traversal
+                .Push(LimitStep.LimitGlobal1))
             .ToAsyncEnumerable()
             .FirstOrDefaultAsync(ct);
 
         public static ValueTask<TElement> SingleAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
-            .Cast<TElement>()
-            .Limit(2)
+            .AsAdmin()
+            .ConfigureSteps<IGremlinQuery<TElement>>(static traversal => traversal
+                .Push(LimitGlobal2))
             .ToAsyncEnumerable()
             .SingleAsync(ct);
 
         public static ValueTask<TElement?> SingleOrDefaultAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
-            .Cast<TElement>()
-            .Limit(2)
+            .AsAdmin()
+            .ConfigureSteps<IGremlinQuery<TElement>>(static traversal => traversal
+                .Push(LimitGlobal2))
             .ToAsyncEnumerable()
             .SingleOrDefaultAsync(ct);
 
         public static ValueTask<TElement> LastAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
-            .Cast<TElement>()
-            .Tail(1)
+            .AsAdmin()
+            .ConfigureSteps<IGremlinQuery<TElement>>(static traversal => traversal
+                .Push(TailStep.TailGlobal1))
             .ToAsyncEnumerable()
             .FirstAsync(ct);
 
         public static ValueTask<TElement?> LastOrDefaultAsync<TElement>(this IGremlinQueryBase<TElement> query, CancellationToken ct = default) => query
-            .Cast<TElement>()
-            .Tail(1)
+            .AsAdmin()
+            .ConfigureSteps<IGremlinQuery<TElement>>(static traversal => traversal
+                .Push(TailStep.TailGlobal1))
             .ToAsyncEnumerable()
             .FirstOrDefaultAsync(ct);
 
