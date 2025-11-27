ed2880dad63469ec296e42959e7b49a468fa7265
Daniel Weber <danielcweber@users.noreply.github.com>
2025-11-27 11:32:20 +0100

    Use a nice extension block.

--- src/Core/Queries/ContinuationBuilder/FinalContinuationBuilderExtensions.cs
+++ src/Core/Queries/ContinuationBuilder/FinalContinuationBuilderExtensions.cs
@@ -8,131 +8,134 @@ namespace ExRam.Gremlinq.Core
 {
     internal static class FinalContinuationBuilderExtensions
     {
-        public static FinalContinuationBuilder WithNewProjection(this FinalContinuationBuilder builder, Projection newProjection) => builder
-            .WithNewProjection(static (_, newProjection) => newProjection, newProjection);
-
-        public static FinalContinuationBuilder WithNewProjection(this FinalContinuationBuilder builder, Func<Projection, Projection> projectionTransformation) => builder
-            .WithNewProjection(
-                static (projection, projectionTransformation) => projectionTransformation(projection),
-                projectionTransformation);
-
-        public static FinalContinuationBuilder Where(this FinalContinuationBuilder builder, Traversal traversal) => builder
-            .AddSteps(traversal.Count > 0 && traversal.Steps.All(static x => x is IFilterStep)
-                ? traversal.Steps
-                : [new FilterStep.ByTraversalStep(traversal)]);
-
-        public static FinalContinuationBuilder None(this FinalContinuationBuilder builder) => builder
-            .WithSteps(static traversal => traversal.IsIdentity()
-                ? NoneStep.Instance
-                : traversal.Push(NoneStep.Instance));
-
-        public static FinalContinuationBuilder OfType<TElement, TNewElement>(this FinalContinuationBuilder builder, IGraphElementModel model, bool force)
+        extension(FinalContinuationBuilder builder)
         {
-            if (typeof(TNewElement) != typeof(object) && (force || !typeof(TNewElement).IsAssignableFrom(typeof(TElement))))
-            {
-                var labels = model.TryGetFilterLabels(typeof(TNewElement), builder.OuterQuery.Environment.Options.GetValue(GremlinqOption.FilterLabelsVerbosity)) ?? ImmutableArray.Create(typeof(TNewElement).Name);
+            public FinalContinuationBuilder WithNewProjection(Projection newProjection) => builder
+                .WithNewProjection(static (_, newProjection) => newProjection, newProjection);
 
-                if (labels.Length > 0)
-                    builder = builder.AddStep(new HasLabelStep(labels));
+            public FinalContinuationBuilder WithNewProjection(Func<Projection, Projection> projectionTransformation) => builder
+                .WithNewProjection(
+                    static (projection, projectionTransformation) => projectionTransformation(projection),
+                    projectionTransformation);
+
+            public FinalContinuationBuilder Where(Traversal traversal) => builder
+                .AddSteps(traversal.Count > 0 && traversal.Steps.All(static x => x is IFilterStep)
+                    ? traversal.Steps
+                    : [new FilterStep.ByTraversalStep(traversal)]);
+
+            public FinalContinuationBuilder None() => builder
+                .WithSteps(static traversal => traversal.IsIdentity()
+                    ? NoneStep.Instance
+                    : traversal.Push(NoneStep.Instance));
+
+            public FinalContinuationBuilder OfType<TElement, TNewElement>(IGraphElementModel model, bool force)
+            {
+                if (typeof(TNewElement) != typeof(object) && (force || !typeof(TNewElement).IsAssignableFrom(typeof(TElement))))
+                {
+                    var labels = model.TryGetFilterLabels(typeof(TNewElement), builder.OuterQuery.Environment.Options.GetValue(GremlinqOption.FilterLabelsVerbosity)) ?? ImmutableArray.Create(typeof(TNewElement).Name);
+
+                    if (labels.Length > 0)
+                        builder = builder.AddStep(new HasLabelStep(labels));
+                }
+
+                return builder;
             }
 
-            return builder;
-        }
-
-        public static FinalContinuationBuilder And(this FinalContinuationBuilder builder, Span<Traversal> traversals)
-        {
-            if (traversals.Length == 0)
-                throw new ArgumentException("Expected at least 1 sub-query.");
-
-            var count = 0;
-            var containsNoneStep = false;
-            var containsWriteStep = false;
-
-            for (var i = 0; i < traversals.Length; i++)
+            public FinalContinuationBuilder And(Span<Traversal> traversals)
             {
-                var traversal = traversals[i];
+                if (traversals.Length == 0)
+                    throw new ArgumentException("Expected at least 1 sub-query.");
 
-                if (traversal.IsNone())
-                    containsNoneStep = true;
+                var count = 0;
+                var containsNoneStep = false;
+                var containsWriteStep = false;
 
-                if (traversal.SideEffectSemantics == SideEffectSemantics.Write)
-                    containsWriteStep = true;
-                else if (traversal.IsIdentity())
-                    continue;
+                for (var i = 0; i < traversals.Length; i++)
+                {
+                    var traversal = traversals[i];
 
-                traversals[count++] = traversal;
-            }
+                    if (traversal.IsNone())
+                        containsNoneStep = true;
 
-            if (containsNoneStep && !containsWriteStep)
-                builder = builder.None();
-            else
-            {
-                var fusedTraversals = traversals[..count]
-                    .Fuse(static (p1, p2) => p1.And(p2));
+                    if (traversal.SideEffectSemantics == SideEffectSemantics.Write)
+                        containsWriteStep = true;
+                    else if (traversal.IsIdentity())
+                        continue;
 
-                if (fusedTraversals is [var single])
-                    builder = builder.Where(single);
+                    traversals[count++] = traversal;
+                }
+
+                if (containsNoneStep && !containsWriteStep)
+                    builder = builder.None();
                 else
                 {
-                    if (fusedTraversals.All(static traversal => traversal.Steps.All(static x => x is IFilterStep)))
-                    {
-                        for (var i = 0; i < fusedTraversals.Length; i++)
-                        {
-                            builder = builder
-                                .AddSteps(fusedTraversals[i].Steps);
-                        }
-                    }
+                    var fusedTraversals = traversals[..count]
+                        .Fuse(static (p1, p2) => p1.And(p2));
+
+                    if (fusedTraversals is [var single])
+                        builder = builder.Where(single);
                     else
                     {
-                        builder = builder
-                            .AddStep(new AndStep(LogicalStep<AndStep>.FlattenLogicalTraversals(fusedTraversals)));
+                        if (fusedTraversals.All(static traversal => traversal.Steps.All(static x => x is IFilterStep)))
+                        {
+                            for (var i = 0; i < fusedTraversals.Length; i++)
+                            {
+                                builder = builder
+                                    .AddSteps(fusedTraversals[i].Steps);
+                            }
+                        }
+                        else
+                        {
+                            builder = builder
+                                .AddStep(new AndStep(LogicalStep<AndStep>.FlattenLogicalTraversals(fusedTraversals)));
+                        }
                     }
                 }
+
+                return builder;
             }
 
-            return builder;
-        }
-
-        public static FinalContinuationBuilder Or(this FinalContinuationBuilder builder, Span<Traversal> traversals)
-        {
-            if (traversals.Length == 0)
-                throw new ArgumentException("Expected at least 1 sub-query.");
-
-            var count = 0;
-            var containsWriteStep = false;
-            var containsIdentityStep = false;
-
-            for (var i = 0; i < traversals.Length; i++)
+            public FinalContinuationBuilder Or(Span<Traversal> traversals)
             {
-                var traversal = traversals[i];
+                if (traversals.Length == 0)
+                    throw new ArgumentException("Expected at least 1 sub-query.");
 
-                if (traversal.IsIdentity())
-                    containsIdentityStep = true;
-                else if (traversal.SideEffectSemantics == SideEffectSemantics.Write)
-                    containsWriteStep = true;
-                else if (traversal.IsNone())
-                    continue;
+                var count = 0;
+                var containsWriteStep = false;
+                var containsIdentityStep = false;
 
-                traversals[count++] = traversal;
-            }
-
-            if (!containsIdentityStep || containsWriteStep)
-            {
-                var fusedTraversals = traversals[..count]
-                    .Fuse(static (p1, p2) => p1.Or(p2));
-
-                builder = fusedTraversals switch
+                for (var i = 0; i < traversals.Length; i++)
                 {
-                    [] => builder
-                        .None(),
-                    [var singleTraversal] => builder
-                        .Where(singleTraversal),
-                    _ => builder
-                        .AddStep(new OrStep(LogicalStep<OrStep>.FlattenLogicalTraversals(fusedTraversals)))
-                };
-            }
+                    var traversal = traversals[i];
 
-            return builder;
+                    if (traversal.IsIdentity())
+                        containsIdentityStep = true;
+                    else if (traversal.SideEffectSemantics == SideEffectSemantics.Write)
+                        containsWriteStep = true;
+                    else if (traversal.IsNone())
+                        continue;
+
+                    traversals[count++] = traversal;
+                }
+
+                if (!containsIdentityStep || containsWriteStep)
+                {
+                    var fusedTraversals = traversals[..count]
+                        .Fuse(static (p1, p2) => p1.Or(p2));
+
+                    builder = fusedTraversals switch
+                    {
+                        [] => builder
+                            .None(),
+                        [var singleTraversal] => builder
+                            .Where(singleTraversal),
+                        _ => builder
+                            .AddStep(new OrStep(LogicalStep<OrStep>.FlattenLogicalTraversals(fusedTraversals)))
+                    };
+                }
+
+                return builder;
+            }
         }
     }
 }
