5ed1390551cf22842520127a09f632572aa9130f
Daniel Weber <danielcweber@users.noreply.github.com>
2025-12-09 15:17:58 +0100

    Add a test that creates AirRoutes on GremlinServer.

---
+++ test/Providers.GremlinServer.Tests/AirRoutesTests.cs
@@ -0,0 +1,47 @@
+﻿using DotNet.Testcontainers.Containers;
+
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Providers.Core;
+using ExRam.Gremlinq.Testing.AirRoutes;
+using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using ExRam.Gremlinq.Support.NewtonsoftJson;
+
+namespace ExRam.Gremlinq.Providers.GremlinServer.Tests
+{
+    [IntegrationTest("Linux", true)]
+    [IntegrationTest("Windows")]
+    public class AirRoutesTests : IClassFixture<AirRoutesTests.Fixture>
+    {
+        public class Fixture : ImageTestContainerFixture
+        {
+            public Fixture() : base("tinkerpop/gremlin-server:3.7.5")
+            {
+            }
+
+            protected override IGremlinQuerySource TransformQuerySource(IContainer container, IGremlinQuerySource g) => g
+                .UseGremlinServer<Vertex, Edge>(_ => _
+                    .At(new UriBuilder("ws", container.Hostname, container.GetMappedPublicPort(8182)).Uri)
+                    .UseNewtonsoftJson());
+        }
+
+        private readonly IGremlinQuerySource _source;
+
+        public AirRoutesTests(Fixture fixture)
+        {
+            _source = fixture
+                .GetQuerySource();
+        }
+
+        [Fact]
+        public async Task CreateSmall()
+        {
+            await _source
+                .CreateAirRoutesSmall(TestContext.Current.CancellationToken);
+
+            var airports = await _source.V();
+
+            await Verify( _source.V().Count().FirstAsync(TestContext.Current.CancellationToken));
+        }
+    }
+}
