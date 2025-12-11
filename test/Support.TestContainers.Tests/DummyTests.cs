aeb1d5e1d8a3565db6fce4aa9d9b532415e3d5fd
Daniel Weber <danielcweber@users.noreply.github.com>
2025-12-11 20:50:25 +0100

    Supress "Zero tests ran".

---
+++ test/Support.TestContainers.Tests/DummyTests.cs
@@ -0,0 +1,11 @@
+﻿namespace ExRam.Gremlinq.Support.TestContainers.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void Supress_Zero_tests_ran_on_Windows_and_NET10()
+        {
+
+        }
+    }
+}
