0fd205588767b056c3525edad16ba5aed2cad09b
Daniel Weber <danielcweber@users.noreply.github.com>
2025-12-16 11:38:05 +0100

    Ids can be strings when using GremlinServer with TestContainers.

--- templates/console/ExRam.Gremlinq.Templates.Console/Elements/Edge.cs
+++ templates/console/ExRam.Gremlinq.Templates.Console/Elements/Edge.cs
@@ -2,7 +2,7 @@
 {
     public class Edge
     {
-#if (provider == "GremlinServer" || provider == "JanusGraph")
+#if ((provider == "GremlinServer" && !useTestContainers) || provider == "JanusGraph")
         public long? Id { get; set; }
 #else
         public string? Id { get; set; }
