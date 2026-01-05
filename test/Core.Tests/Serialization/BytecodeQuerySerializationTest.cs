774bae828d9acbccce0c6d28d3c47ab1488ac479
Daniel Weber <danielcweber@users.noreply.github.com>
2026-01-05 18:19:50 +0100

    Only check serialization of Drop. Remove stray snapshots.

--- test/Core.Tests/Serialization/BytecodeQuerySerializationTest.cs
+++ test/Core.Tests/Serialization/BytecodeQuerySerializationTest.cs
@@ -1,4 +1,6 @@
-﻿using ExRam.Gremlinq.Tests.Infrastructure;
+﻿using ExRam.Gremlinq.Tests.Entities;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
 using Gremlin.Net.Process.Traversal;
 
 namespace ExRam.Gremlinq.Core.Tests
@@ -8,5 +10,19 @@ namespace ExRam.Gremlinq.Core.Tests
         public BytecodeQuerySerializationTest(GremlinqFixture fixture) : base(fixture, new SerializingVerifier<Bytecode>())
         {
         }
+
+        [Fact]
+        public virtual Task Drop() => _g
+            .V<Person>()
+            .Drop()
+            .Verify();
+
+        [Fact]
+        public virtual Task Drop_in_local() => _g
+            .Inject(1)
+            .Local(__ => __
+                .V()
+                .Drop())
+            .Verify();
     }
 }
