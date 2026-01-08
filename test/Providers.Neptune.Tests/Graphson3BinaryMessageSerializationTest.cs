c3689aaad6781a8ae1f5c40b19deff1e68dcd6a4
Daniel Weber <danielcweber@users.noreply.github.com>
2026-01-08 13:51:08 +0100

    Add GraphSon3 request message snapshots for Neptune.

---
+++ test/Providers.Neptune.Tests/Graphson3BinaryMessageSerializationTest.cs
@@ -0,0 +1,15 @@
+﻿using ExRam.Gremlinq.Providers.Core;
+using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+namespace ExRam.Gremlinq.Providers.Neptune.Tests
+{
+    public class Graphson3BinaryMessageSerializationTest : QueryExecutionTest, IClassFixture<NeptuneFixture>
+    {
+        public Graphson3BinaryMessageSerializationTest(NeptuneFixture fixture) : base(
+            fixture,
+            new BinaryMessageSerializingVerifier<GraphSon3BinaryMessage>())
+        {
+        }
+    }
+}
