6fa082181e90cd1a35fc1108e8ecf2d6fe031edb
Daniel Weber <danielcweber@users.noreply.github.com>
2026-01-08 15:05:53 +0100

    Add assertion for TimeSpans on real Neptune.

---
+++ test/Providers.Neptune.Tests/RealNeptuneAssertions.cs
@@ -0,0 +1,22 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Tests.Fixtures;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+namespace ExRam.Gremlinq.Providers.Neptune.Tests
+{
+    public class RealNeptuneAssertions : GremlinqTestBase, IClassFixture<NeptuneFixture>
+    {
+        private readonly IGremlinQuerySource _source;
+
+        public RealNeptuneAssertions(NeptuneFixture fixture) : base(new ExecutingVerifier())
+        {
+            _source = fixture
+                .GetQuerySource();
+        }
+
+        [Fact(Explicit = true)]
+        public Task Native_TimeSpan() => _source
+            .Inject(TimeSpan.FromMinutes(10))
+            .Verify();
+    }
+}
