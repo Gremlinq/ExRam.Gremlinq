24822386fa6c047daadd5104435abd8dcd79186e
Daniel Weber <danielcweber@users.noreply.github.com>
2026-01-08 14:58:04 +0100

    Add actual NeptuneFixture that gets its configuraton from the environment.

---
+++ test/Tests.Fixtures/NeptuneFixture.cs
@@ -0,0 +1,24 @@
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Tests.Entities;
+using ExRam.Gremlinq.Tests.Infrastructure;
+using ExRam.Gremlinq.Providers.Core;
+using ExRam.Gremlinq.Support.NewtonsoftJson;
+using ExRam.Gremlinq.Providers.Neptune;
+
+namespace ExRam.Gremlinq.Tests.Fixtures
+{
+    public sealed class NeptuneFixture : GremlinqFixture
+    {
+        protected override IGremlinQuerySource TransformQuerySource(IGremlinQuerySource g) => base
+            .TransformQuerySource(g)
+            .UseNeptune<Vertex, Edge>(configurator => configurator
+                .At(new Uri(Environment.GetEnvironmentVariable("Gremlinq:Neptune:Uri")!))
+                .UseIAMAuthentication(iam => iam
+                    .UseSigV4()
+                    .WithUri(new Uri(Environment.GetEnvironmentVariable("Gremlinq:Neptune:Uri")!))
+                    .WithRegion("eu-central-1")
+                    .WithAccessKeyId(Environment.GetEnvironmentVariable("Gremlinq:Neptune:IAM:AccessKeyId")!)
+                    .WithSecretAccessKey(Environment.GetEnvironmentVariable("Gremlinq:Neptune:IAM:SecretAccessKey")!))
+                .UseNewtonsoftJson());
+    }
+}
