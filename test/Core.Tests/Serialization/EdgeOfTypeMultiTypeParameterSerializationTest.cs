1a9da8f9cd23f9dba601fa6b49a884fbfde0007a
Daniel Weber <danielcweber@users.noreply.github.com>
2026-02-26 20:53:06 +0100

    Add tests for the new overloads.

---
+++ test/Core.Tests/Serialization/EdgeOfTypeMultiTypeParameterSerializationTest.cs
@@ -0,0 +1,137 @@
+﻿using ExRam.Gremlinq.Core.Models;
+using ExRam.Gremlinq.Tests.Entities;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+using Gremlin.Net.Process.Traversal;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class EdgeOfTypeMultiTypeParameterSerializationTest : GremlinqTestBase, IClassFixture<GremlinqFixture>
+    {
+        public class NestedEdge;
+
+        public class Edge1 : NestedEdge;
+        public class Edge2 : NestedEdge;
+        public class Edge3 : NestedEdge;
+        public class Edge4 : NestedEdge;
+        public class Edge5 : NestedEdge;
+        public class Edge6 : NestedEdge;
+        public class Edge7 : NestedEdge;
+        public class Edge8 : NestedEdge;
+        public class Edge9 : NestedEdge;
+        public class Edge10 : NestedEdge;
+        public class Edge11 : NestedEdge;
+        public class Edge12 : NestedEdge;
+        public class Edge13 : NestedEdge;
+        public class Edge14 : NestedEdge;
+        public class Edge15 : NestedEdge;
+        public class Edge16 : NestedEdge;
+
+        private readonly IGremlinQuerySource _g;
+
+        public EdgeOfTypeMultiTypeParameterSerializationTest(GremlinqFixture fixture) : base(new SerializingVerifier<Bytecode>())
+        {
+            _g = fixture
+                .GetQuerySource()
+                .ConfigureEnvironment(env => env
+                    .UseModel(GraphModel
+                        .FromBaseTypes<Vertex, NestedEdge>()));
+        }
+
+        [Fact]
+        public virtual Task OfType_with_two_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_three_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_four_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_five_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_six_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_seven_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_eight_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_nine_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_ten_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9, Edge10>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_eleven_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9, Edge10, Edge11>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_twelve_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9, Edge10, Edge11, Edge12>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_thirteen_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9, Edge10, Edge11, Edge12, Edge13>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_fourteen_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9, Edge10, Edge11, Edge12, Edge13, Edge14>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_fifteen_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9, Edge10, Edge11, Edge12, Edge13, Edge14, Edge15>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_sixteen_types() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge4, Edge5, Edge6, Edge7, Edge8, Edge9, Edge10, Edge11, Edge12, Edge13, Edge14, Edge15, Edge16>()
+            .Verify();
+
+        [Fact]
+        public virtual Task Labels_are_distinct() => _g
+            .E<NestedEdge>()
+            .OfType<Edge1, Edge2, Edge3, Edge1, Edge2, Edge3, Edge4, Edge5, Edge5, Edge6>()
+            .Verify();
+    }
+}
---
+++ test/Core.Tests/Serialization/VertexOfTypeMultiTypeParameterSerializationTest.cs
@@ -0,0 +1,137 @@
+﻿using ExRam.Gremlinq.Core.Models;
+using ExRam.Gremlinq.Tests.Entities;
+using ExRam.Gremlinq.Tests.Infrastructure;
+
+using Gremlin.Net.Process.Traversal;
+
+namespace ExRam.Gremlinq.Core.Tests
+{
+    public class VertexOfTypeMultiTypeParameterSerializationTest : GremlinqTestBase, IClassFixture<GremlinqFixture>
+    {
+        public class NestedVertex;
+
+        public class Vertex1 : NestedVertex;
+        public class Vertex2 : NestedVertex;
+        public class Vertex3 : NestedVertex;
+        public class Vertex4 : NestedVertex;
+        public class Vertex5 : NestedVertex;
+        public class Vertex6 : NestedVertex;
+        public class Vertex7 : NestedVertex;
+        public class Vertex8 : NestedVertex;
+        public class Vertex9 : NestedVertex;
+        public class Vertex10 : NestedVertex;
+        public class Vertex11 : NestedVertex;
+        public class Vertex12 : NestedVertex;
+        public class Vertex13 : NestedVertex;
+        public class Vertex14 : NestedVertex;
+        public class Vertex15 : NestedVertex;
+        public class Vertex16 : NestedVertex;
+
+        private readonly IGremlinQuerySource _g;
+
+        public VertexOfTypeMultiTypeParameterSerializationTest(GremlinqFixture fixture) : base(new SerializingVerifier<Bytecode>())
+        {
+            _g = fixture
+                .GetQuerySource()
+                .ConfigureEnvironment(env => env
+                    .UseModel(GraphModel
+                        .FromBaseTypes<NestedVertex, Edge>()));
+        }
+
+        [Fact]
+        public virtual Task OfType_with_two_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_three_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_four_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_five_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_six_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_seven_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_eight_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_nine_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_ten_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9, Vertex10>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_eleven_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9, Vertex10, Vertex11>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_twelve_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9, Vertex10, Vertex11, Vertex12>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_thirteen_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9, Vertex10, Vertex11, Vertex12, Vertex13>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_fourteen_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9, Vertex10, Vertex11, Vertex12, Vertex13, Vertex14>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_fifteen_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9, Vertex10, Vertex11, Vertex12, Vertex13, Vertex14, Vertex15>()
+            .Verify();
+
+        [Fact]
+        public virtual Task OfType_with_sixteen_types() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex6, Vertex7, Vertex8, Vertex9, Vertex10, Vertex11, Vertex12, Vertex13, Vertex14, Vertex15, Vertex16>()
+            .Verify();
+
+        [Fact]
+        public virtual Task Labels_are_distinct() => _g
+            .V<NestedVertex>()
+            .OfType<Vertex1, Vertex2, Vertex3, Vertex1, Vertex2, Vertex3, Vertex4, Vertex5, Vertex5, Vertex6>()
+            .Verify();
+    }
+}
