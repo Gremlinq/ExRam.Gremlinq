e449a20a896f1c3800bfe8dfb9a29b19c4869b57
Daniel Weber <danielcweber@users.noreply.github.com>
2026-02-26 17:31:20 +0100

    The force parameter to OfType is redundant

--- src/Core/Queries/ContinuationBuilder/FinalContinuationBuilderExtensions.cs
+++ src/Core/Queries/ContinuationBuilder/FinalContinuationBuilderExtensions.cs
@@ -32,9 +32,9 @@ namespace ExRam.Gremlinq.Core
                     ? NoneStep.Instance
                     : traversal.Push(NoneStep.Instance));
 
-            public FinalContinuationBuilder OfType<TElement>(Type[] edgeTypes, IGraphElementModel model, bool force)
+            public FinalContinuationBuilder OfType<TElement>(Type[] edgeTypes, IGraphElementModel model)
             {
-                if (edgeTypes.All(static edgeType => edgeType != typeof(object)) && (force || edgeTypes.All(static edgeType => !edgeType.IsAssignableFrom(typeof(TElement)))))
+                if (edgeTypes.All(static edgeType => edgeType != typeof(object)) && edgeTypes.All(static edgeType => !edgeType.IsAssignableFrom(typeof(TElement))))
                 {
                     var labels = model
                         .GetFilterLabels(edgeTypes, builder.OuterQuery.Environment.Options.GetValue(GremlinqOption.FilterLabelsVerbosity));
--- src/Core/Queries/GremlinQuery.cs
+++ src/Core/Queries/GremlinQuery.cs
@@ -334,7 +334,7 @@ namespace ExRam.Gremlinq.Core
             .Continue()
             .Build(static builder => builder
                 .AddStep(BothVStep.Instance)
-                .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel, false)
+                .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel)
                 .WithNewProjection(Projection.Vertex)
                 .BuildAuto<TNewElement>());
 
@@ -607,7 +607,7 @@ namespace ExRam.Gremlinq.Core
             .Build(
                 static (builder, ids) => builder
                     .AddStep(new EStep(ids.ToImmutableArray()))
-                    .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.EdgesModel, false)
+                    .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.EdgesModel)
                     .WithNewProjection(Projection.Edge)
                     .BuildAuto<TNewElement>(),
                 ids);
@@ -795,7 +795,7 @@ namespace ExRam.Gremlinq.Core
                 .Build(
                     static (builder, step) => builder
                         .AddStep(step)
-                        .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel, false)
+                        .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel)
                         .WithNewProjection(Projection.Vertex)
                         .BuildAs<TNewQuery>(),
                     step);
@@ -927,7 +927,7 @@ namespace ExRam.Gremlinq.Core
             .Continue()
             .Build(
                 static (builder, model) => builder
-                    .OfType<T1>(TypeArrayCache<TNewElement>.Types, model, false)
+                    .OfType<T1>(TypeArrayCache<TNewElement>.Types, model)
                     .BuildAs<TTargetQuery>(),
                 model);
 
@@ -980,7 +980,7 @@ namespace ExRam.Gremlinq.Core
             .Continue()
             .Build(static builder => builder
                 .AddStep(OtherVStep.Instance)
-                .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel, false)
+                .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel)
                 .WithNewProjection(Projection.Vertex)
                 .BuildAuto<TNewElement>());
 
@@ -1265,7 +1265,7 @@ namespace ExRam.Gremlinq.Core
             .Build(
                 static (builder, ids) => builder
                     .AddStep(new VStep(ids.ToImmutableArray()))
-                    .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel, false)
+                    .OfType<object>(TypeArrayCache<TNewElement>.Types, builder.OuterQuery.Environment.Model.VerticesModel)
                     .WithNewProjection(Projection.Vertex)
                     .BuildAuto<TNewElement>(),
                 ids);
