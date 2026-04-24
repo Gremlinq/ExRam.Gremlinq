9d88d2a1e16f4e9716008314e71e0a78bf21c923
Daniel Weber <danielcweber@users.noreply.github.com>
2026-04-24 17:51:15 +0200

    Block-scoped Namespace.

--- test/Tests.Entities/Element.cs
+++ test/Tests.Entities/Element.cs
@@ -1,16 +1,17 @@
-namespace ExRam.Gremlinq.Tests.Entities;
-
-public abstract class Element
+namespace ExRam.Gremlinq.Tests.Entities
 {
-    public object? Id { get; set; }
+    public abstract class Element
+    {
+        public object? Id { get; set; }
 
-    public string? Label { get; set; }
+        public string? Label { get; set; }
 
-    public string? PartitionKey { get; set; }
+        public string? PartitionKey { get; set; }
 
 #if Gremlinq_Extensions
-    public string? _Etag { get; set; }
+        public string? _Etag { get; set; }
 
-    public string? _Self { get; set; }
+        public string? _Self { get; set; }
 #endif
+    }
 }
