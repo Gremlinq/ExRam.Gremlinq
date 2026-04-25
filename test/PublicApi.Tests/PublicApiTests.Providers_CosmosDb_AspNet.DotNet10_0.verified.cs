e2ba32c5413314e4dcac2550d2f03f485523890e
Daniel Weber <danielcweber@users.noreply.github.com>
2026-04-25 15:44:45 +0200

    Obsolete CosmosDb "Use..." methods.

--- test/PublicApi.Tests/PublicApiTests.Providers_CosmosDb_AspNet.DotNet10_0.verified.cs
+++ test/PublicApi.Tests/PublicApiTests.Providers_CosmosDb_AspNet.DotNet10_0.verified.cs
@@ -2,6 +2,14 @@ namespace ExRam.Gremlinq.Providers.CosmosDb.AspNet
 {
     public static class GremlinqServicesBuilderExtensions
     {
+        [System.Obsolete(@"This method and the CosmosDb provider packages will be removed in ExRam.Gremlinq v14.
+
+To keep using the CosmosDb provider packages beyond ExRam.Gremlinq v13, subscribe to any of the Gremlinq.Extensions offers (https://docs.gremlinq.net/extensions/).
+
+Existing customers of any Gremlinq.Extensions product already have access to all ExRam.Gremlinq.Providers.CosmosDb.* packages on the Gremlinq.Extensions NuGet feed.
+Simply update to the latest 13.x version, and this message will disappear.
+
+For details on the v14 transition and available options, see https://docs.gremlinq.net/cosmosdb-provider-packages/", false)]
         public static ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder<ExRam.Gremlinq.Providers.CosmosDb.ICosmosDbConfigurator<TVertexBase>> UseCosmosDb<TVertexBase, TEdgeBase>(this ExRam.Gremlinq.Core.AspNet.IGremlinqServicesBuilder setup) { }
     }
 }
