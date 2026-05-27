835b801cd6f681edf8744db9fdf3d5a3e745f41d
Daniel Weber <danielcweber@users.noreply.github.com>
2026-05-27 12:01:15 +0200

    Try to limit concurrent tests being run to not trigger excessive paging on CI.

--- test/AssemblyInfo.cs
+++ test/AssemblyInfo.cs
@@ -1,4 +1,8 @@
 using ExRam.Gremlinq.Tests.Infrastructure;
 
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
+[assembly: CollectionBehavior(
+    CollectionBehavior.CollectionPerAssembly,
+    DisableTestParallelization = true,
+    MaxParallelThreads = 4)]
+
 [assembly: TestFramework(typeof(GremlinqTestFramework))]
