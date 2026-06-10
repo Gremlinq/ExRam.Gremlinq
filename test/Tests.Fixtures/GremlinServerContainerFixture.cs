73d74af866284852e92157957da364f6003fe442
Daniel Weber <danielcweber@users.noreply.github.com>
2026-06-10 16:54:38 +0200

    Remove dead code, apparently for showcase reasons but what's the point.

--- test/Tests.Fixtures/GremlinServerContainerFixture.cs
+++ test/Tests.Fixtures/GremlinServerContainerFixture.cs
@@ -1,5 +1,4 @@
 using ExRam.Gremlinq.Core;
-using ExRam.Gremlinq.Providers.Core;
 using ExRam.Gremlinq.Providers.GremlinServer;
 using ExRam.Gremlinq.Support.NewtonsoftJson;
 using ExRam.Gremlinq.Support.TestContainers;
@@ -13,11 +12,6 @@ namespace ExRam.Gremlinq.Tests.Fixtures
         protected override IGremlinQuerySource TransformQuerySource(IGremlinQuerySource g) => g
             .UseGremlinServer<Vertex, Edge>(_ => _
                 .UseGremlinServerContainer("3.7.6")
-                .ConfigureClientFactory(factory => factory
-                    .ConfigureClient(client => client
-                        .TransformRequest(async (requestMessage, _) => requestMessage  //Just for demo/coverage purposes.
-                        /*.Rebuild()
-                        /*.Create()*/)))
                 .UseNewtonsoftJson())
             .IgnoreCosmosDbSpecificProperties();
     }
