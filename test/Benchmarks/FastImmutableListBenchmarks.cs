8f94828dc6c08c11c4989dbc5322c19578db9cbc
Daniel Weber <danielcweber@users.noreply.github.com>
2026-07-14 10:05:59 +0200

    Add Benchmarks for FastImmutableList

---
+++ test/Benchmarks/FastImmutableListBenchmarks.cs
@@ -0,0 +1,37 @@
+using BenchmarkDotNet.Attributes;
+using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.Steps;
+
+namespace ExRam.Gremlinq.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class FastImmutableListBenchmarks
+    {
+        private FastImmutableList<Step> _steps;
+        
+        private static readonly Step[] Steps4 = [CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local];
+        private static readonly Step[] Steps8 = [CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local];
+        private static readonly Step[] Steps16 = [CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local];
+        private static readonly Step[] Steps32 = [CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local];
+
+        [GlobalSetup]
+        public void Setup() => _steps = FastImmutableList<Step>.Empty
+            .Push(CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local, CountStep.Local);
+
+        [Benchmark]
+        public object Push_4() => _steps
+            .Push(Steps4);
+
+        [Benchmark]
+        public object Push_8() => _steps
+            .Push(Steps8);
+        
+        [Benchmark]
+        public object Push_16() => _steps
+            .Push(Steps16);
+
+        [Benchmark]
+        public object Push_32() => _steps
+            .Push(Steps32);
+    }
+}
